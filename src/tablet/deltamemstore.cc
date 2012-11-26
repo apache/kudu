4f9b97ba8b008f3e54f892bedb7a3ed8fc6a4ae4
Todd Lipcon <todd@cloudera.com>
2012-11-26 11:14:30 -0800

    Move RowDelta code to new rowdelta.{cc,h}

--- src/tablet/deltamemstore.cc
+++ src/tablet/deltamemstore.cc
@@ -4,8 +4,6 @@
 
 #include "tablet/deltafile.h"
 #include "tablet/deltamemstore.h"
-#include "util/bitmap.h"
-#include "util/coding-inl.h"
 #include "util/status.h"
 
 namespace kudu { namespace tablet {
@@ -69,127 +67,6 @@ Status DeltaMemStore::FlushToFile(DeltaFileWriter *dfw) const {
     dfw->AppendDelta(entry.first, entry.second);
   }
   return Status::OK();
-}
-
-////////////////////////////////////////////////////////////
-// RowDelta implementation
-////////////////////////////////////////////////////////////
-
-
-RowDelta::RowDelta(const Schema &schema,
-                   uint8_t *data) :
-  data_(data) {
-  DCHECK(data != NULL);
-}
-
-RowDelta RowDelta::CopyToArena(const Schema &schema, Arena *arena) const {
-  void *copied_data = arena->AddBytes(data_, SizeForSchema(schema));
-  CHECK(copied_data) << "failed to allocate";
-
-  RowDelta ret(schema, reinterpret_cast<uint8_t *>(copied_data));
-
-  // Iterate over the valid columns, copying any STRING data into
-  // the target arena.
-  for (TrueBitIterator it(ret.bitmap(), schema.num_columns());
-       !it.done();
-       ++it) {
-    int i = *it;
-    if (schema.column(i).type_info().type() == STRING) {
-      Slice *s = reinterpret_cast<Slice *>(ret.col_ptr(schema, i));
-      CHECK(arena->RelocateSlice(*s, s))
-        << "Unable to relocate slice " << s->ToString()
-        << " (col " << i << " in schema " << schema.ToString() << ")";
-    }
-  }
-  return ret;
-}
-
-void RowDelta::UpdateColumn(const Schema &schema,
-                            size_t col_idx,
-                            const void *new_val) {
-  DCHECK_LT(col_idx, schema.num_columns());
-  memcpy(col_ptr(schema, col_idx), new_val,
-         schema.column(col_idx).type_info().size());
-  BitmapSet(bitmap(), col_idx);
-}
-
-bool RowDelta::IsUpdated(size_t col_idx) const {
-  return BitmapTest(bitmap(), col_idx);
-}
-
-bool RowDelta::ApplyColumnUpdate(const Schema &schema,
-                                 size_t col_idx,
-                                 void *dst) const {
-  DCHECK_LT(col_idx, schema.num_columns());
-
-  if (IsUpdated(col_idx)) {
-    size_t bm_size = BitmapSize(schema.num_columns());
-    size_t off = schema.column_offset(col_idx);
-    memcpy(dst, &data_[bm_size + off],
-           schema.column(col_idx).type_info().size());
-    return true;
-  }
-  return false;
-}
-
-void RowDelta::MergeUpdatesFrom(const Schema &schema,
-                                const RowDelta &from,
-                                Arena *arena) {
-  size_t bm_size = BitmapSize(schema.num_columns());
-
-  // Copy the data from the other row, where the other row
-  // has its bitfield set.
-  
-  // Iterate over the valid columns, copying any STRING data into
-  // the target arena.
-  for (TrueBitIterator it(from.bitmap(), schema.num_columns());
-       !it.done();
-       ++it) {
-    size_t i = *it;
-    if (schema.column(i).type_info().type() == STRING) {
-      // If it's a Slice column, need to relocate the referred-to data
-      // as well as the slice itself.
-      // TODO: potential optimization here: if the new value is smaller than
-      // the old value, we could potentially just overwrite.
-      const Slice *src = reinterpret_cast<const Slice *>(from.col_ptr(schema, i));
-      Slice *dst = reinterpret_cast<Slice *>(col_ptr(schema, i));
-      CHECK(arena->RelocateSlice(*src, dst))
-        << "Unable to relocate slice " << src->ToString()
-        << " (col " << i << " in schema " << schema.ToString() << ")";
-    } else {
-      size_t off = schema.column_offset(i) + bm_size;
-      size_t size = schema.column(i).type_info().size();
-      memcpy(&data_[off], &from.data_[off], size);
-    }
-  }
-
-  // Merge the set of updated fields
-  BitmapMergeOr(bitmap(), from.bitmap(), schema.num_columns());
-}
-
-void RowDelta::SerializeToBuffer(
-  const Schema &schema, faststring *dst) const {
-
-  // First append the bitmap.
-  dst->append(bitmap(), BitmapSize(schema.num_columns()));
-
-  // Iterate over the valid columns, copying their data into the buffer.
-  for (TrueBitIterator it(bitmap(), schema.num_columns());
-       !it.done();
-       ++it) {
-    size_t i = *it;
-    const TypeInfo &ti = schema.column(i).type_info();
-    if (ti.type() == STRING) {
-      const Slice *src = reinterpret_cast<const Slice *>(col_ptr(schema, i));
-
-      // If it's a Slice column, copy the length followed by the data.
-      InlinePutVarint32(dst, src->size());
-      dst->append(src->data(), src->size());
-    } else {
-      // Otherwise, just copy the data itself.
-      dst->append(col_ptr(schema, i), ti.size());
-    }
-  }
 }
 
 
---
+++ src/tablet/rowdelta.h
@@ -0,0 +1,130 @@
+// Copyright (c) 2012, Cloudera, inc.
+#ifndef KUDU_TABLET_ROWDELTA_H
+#define KUDU_TABLET_ROWDELTA_H
+
+#include <boost/scoped_array.hpp>
+
+#include "common/schema.h"
+#include "util/faststring.h"
+#include "util/bitmap.h"
+
+namespace kudu {
+
+class Arena;
+
+namespace tablet {
+
+struct RowDelta {
+public:
+  RowDelta(const Schema &schema,
+           uint8_t *data);
+
+  // Return the number of bytes of storage needed for a row delta.
+  static size_t SizeForSchema(const Schema &schema) {
+    size_t bm_size = BitmapSize(schema.num_columns());
+    return bm_size + schema.byte_size();
+  }
+
+  // Copy the delta data itself to a new arena, and return a new object
+  // which references storage in the destination arena.
+  //
+  // This copies both the updated row itself as well as any updated
+  // STRING data.
+  RowDelta CopyToArena(const Schema &schema, Arena *arena) const;
+
+  // Clear any updated columns
+  void Clear(const Schema &schema) {
+    memset(bitmap(), 0, BitmapSize(schema.num_columns()));
+  }
+
+
+  // Marks the given column as updated with the given new value.
+  //
+  // NOTE: this does _not_ copy the indirected data into the DMS's
+  // arena. That is the responsibility of a higher layer (eg
+  // DeltaMemStore::Update)
+  void UpdateColumn(const Schema &schema, size_t col_idx,
+                    const void *new_val);
+
+  // Return true if the given column has been updated in this row delta.
+  bool IsUpdated(size_t col_idx) const;
+
+  // If this row delta contains an update for the given column, copy
+  // the new value to 'dst'.
+  //
+  // NOTE: This does not copy any referenced data.
+  // TODO: it should probably copy referenced data to an arena.
+  bool ApplyColumnUpdate(const Schema &schema,
+                         size_t col_idx,
+                         void *dst) const;
+
+  // Merge updates from another delta. The two RowDelta objects
+  // must correspond to the same schema.
+  // If 'from' has references to external data (eg slices), then that
+  // data is copied into the provided destination arena.
+  void MergeUpdatesFrom(const Schema &schema,
+                        const RowDelta &from,
+                        Arena *arena);
+
+  // Serialize the delta into a compact form in the destination buffer.
+  // The result is entirely self-contained, suitable for storing
+  // on disk (ie no pointers).
+  //
+  // The format here is simply the same bitmap stored in-memory in this
+  // class (1-bit set for any updated column), followed by the data for
+  // only the updated columns.
+  //
+  // In the case of Slice columns, the data is represented as
+  // <vint32 length> <data>
+  // Other columns are copied exactly from their in-memory form.
+  void SerializeToBuffer(const Schema &schema,
+                         faststring *dst) const;
+
+private:
+  const uint8_t *bitmap() const { return data_; }
+  uint8_t *bitmap() { return data_; }
+
+  uint8_t *col_ptr(const Schema &schema, size_t idx) {
+    size_t bm_size = BitmapSize(schema.num_columns());
+    size_t off = schema.column_offset(idx);
+    return &data_[bm_size + off];
+  }
+
+  const uint8_t *col_ptr(const Schema &schema, size_t idx) const {
+    return const_cast<RowDelta *>(this)->col_ptr(schema, idx);
+  }
+
+  // This conceptually would have a reference back to an associated
+  // DeltaMemStore or Schema, but in order to save space it is instead passed
+  // to all of the call sites.
+
+  // The underlying data. Format
+  // [bitset of updated columns]  (width: 1 bit per column in the schema)
+  // a row (with all columns present)
+  uint8_t *data_;
+};
+
+
+// A RowDelta which manages its own dynamically-allocated
+// storage, freeing it when it goes out of scope.
+class ScopedRowDelta {
+public:
+  ScopedRowDelta(const Schema &schema) :
+    data_(new uint8_t[RowDelta::SizeForSchema(schema)]),
+    delta_(schema, data_.get()) {
+    delta_.Clear(schema);
+  }
+
+  RowDelta &get() { return delta_; }
+  const RowDelta &get() const { return delta_; }
+
+private:
+  scoped_array<uint8_t> data_;
+  RowDelta delta_;
+};
+
+
+} // namespace tablet
+} // namespace kudu
+
+#endif
