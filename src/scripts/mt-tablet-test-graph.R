9a0b5cb16027662a1bb3e903310479cc7b3862dd
Todd Lipcon <todd@cloudera.com>
2013-02-08 22:50:24 -0800

    clean up graphing to show SI units on axis labels

--- src/scripts/mt-tablet-test-graph.R
+++ src/scripts/mt-tablet-test-graph.R
@@ -1,27 +1,51 @@
 library(ggplot2)
 library(reshape)
 
+si_num <- function (x) {
+
+  if (!is.na(x)) {
+    if (x >= 1e9) { 
+      rem <- format(x/1e6, digits=3)
+      rem <- append(rem, "B");
+    }
+
+    if (x >= 1e6) { 
+      rem <- format(x/1e6, digits=3)
+      rem <- append(rem, "M");
+    }
+
+    else if (x > 1e3) { 
+      rem <- format(x/1e3, digits=3)
+      rem <- append(rem, "K");
+    }
+    else {
+      return(x);
+    }
+
+    return(paste(rem, sep="", collapse=""));
+  }
+  else return(NA);
+}
+
+si_vec <- function(x) {
+  sapply(x, FUN=si_num);
+}
+
 d <- read.table(file="/tmp/graph.tsv", header=T)
 
 d$insert_rate = c(0, diff(d$inserted)/diff(d$time))
-d <- subset(d, select = -c(inserted))
 
-# Put scan rate in millions/sec
-d$scan_rate <- d$scan_rate / 1000000
-
-# Put insert rate in K/sec
-d$insert_rate <- d$insert_rate / 1000
-
-# Put memstore usage in MB
-d$memstore_mb <- d$memstore_kb / 1000
+# Put memstore usage in bytes
+d$memstore_bytes <- d$memstore * 1024
 d <- subset(d, select = -c(memstore_kb))
 
 d <- rename(d, c(
-  insert_rate="Insert rate (K rows/s)",
-  memstore_mb="Memstore Memory Usage (MB)",
-  scan_rate="Scan int col (M rows/sec)"))
+  insert_rate="Insert rate (rows/sec)",
+  memstore="Memstore Memory Usage",
+  scan_rate="Scan int col (rows/sec)"))
 
 
 d.melted = melt(d, id="time")
 print(qplot(time, value, data=d.melted, geom="line", group = variable)
+                  + scale_y_continuous(labels=si_vec)
                   + facet_grid(variable~., scale = "free_y"))
