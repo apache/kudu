apache/kudu

commit 9d54acc3ce250c37d5cbea4386b91fcb37277941

cleanup: use ScopedRowBlock in more spots

src/common/rowblock.h
@@ -29,8 +29,13 @@ public:
   uint8_t *row_ptr(size_t idx) {
     return data_ + schema_.byte_size() * idx;
   }
+
   const uint8_t *row_ptr(size_t idx) const {
     return data_ + schema_.byte_size() * idx;
+  }
+
+  const Slice row_slice(size_t idx) const {
+    return Slice(row_ptr(0), schema_.byte_size());
   }
 
   const Schema &schema() const { return schema_; }

src/tablet/mt-tablet-test.cc
@@ -78,13 +78,11 @@ public:
     shared_ptr<TimeSeries> updates = ts_collector_.GetTimeSeries("updated");
 
     // TODO: move the update code into the SETUP class
-    uint8_t buf[schema_.byte_size()];
-    Slice row_slice(reinterpret_cast<const char *>(buf),
-                    schema_.byte_size());
-    ScopedRowDelta update(schema_);
 
-    Arena tmp_arena (1024, 1024);
-    RowBlock block(schema_, &buf[0], 1, &tmp_arena);
+    Arena tmp_arena(1024, 1024);
+    ScopedRowBlock block(schema_, 1, &tmp_arena);
+    Slice row_slice(block.row_slice(0));
+    ScopedRowDelta update(schema_);
 
     uint64_t updates_since_last_report = 0;
     while (running_insert_count_.count() > 0) {
@@ -133,8 +131,7 @@ public:
   // This is meant to test that outstanding iterators don't end up
   // trying to reference already-freed memstore memory.
   void SlowReaderThread(int tid) {
-    uint8_t buf[schema_.byte_size()];
-    RowBlock block(schema_, &buf[0], 1, &arena_);
+    ScopedRowBlock block(schema_, 1, &arena_);
 
     int max_iters = FLAGS_num_insert_threads * FLAGS_inserts_per_thread / 10;
 

src/tablet/tablet-test-base.h
@@ -189,8 +189,7 @@ public:
     int batch_size = std::max(
       (size_t)1, std::min((size_t)(expected_count / 10),
                           4*1024*1024 / schema_.byte_size()));
-    scoped_array<uint8_t> buf(new uint8_t[schema_.byte_size() * batch_size]);
-    RowBlock block(schema_, &buf[0], batch_size, &arena_);
+    ScopedRowBlock block(schema_, batch_size, &arena_);
 
     if (expected_count > INT_MAX) {
       LOG(INFO) << "Not checking rows for duplicates -- duplicates expected since "
@@ -210,11 +209,10 @@ public:
 
       CHECK_GT(n, 0);
       LOG(INFO) << "Fetched batch of " << n << "\n"
-                << "First row: " << schema_.DebugRow(&buf[0]);
+                << "First row: " << schema_.DebugRow(block.row_ptr(0));
 
       for (int i = 0; i < n; i++) {
-        Slice s(reinterpret_cast<const char *>(&buf[i * schema_.byte_size()]),
-                schema_.byte_size());
+        Slice s(block.row_slice(i));
         int row = *schema_.ExtractColumnFromRow<UINT32>(s, 1);
         if (row >= first_row && row < first_row + expected_count) {
           size_t idx = row - first_row;

src/tablet/tablet-test.cc
@@ -163,8 +163,7 @@ TYPED_TEST(TestTablet, TestRowIteratorComplex) {
   ASSERT_STATUS_OK(this->tablet_->NewRowIterator(this->schema_, &iter));
   ASSERT_STATUS_OK(iter->Init());
 
-  scoped_array<uint8_t> buf(new uint8_t[this->schema_.byte_size() * 100]);
-  RowBlock block(this->schema_, &buf[0], 100, &this->arena_);
+  ScopedRowBlock block(this->schema_, 100, &this->arena_);
 
   // Copy schema into local scope, since gcc is getting confused by
   // too many templates.
@@ -176,10 +175,7 @@ TYPED_TEST(TestTablet, TestRowIteratorComplex) {
     ASSERT_STATUS_OK(iter->CopyNextRows(&n, &block));
     LOG(INFO) << "Fetched batch of " << n;
     for (size_t i = 0; i < n; i++) {
-      const char *row_ptr = reinterpret_cast<const char *>(
-        &buf[schema.byte_size() * i]);
-      Slice row_slice(row_ptr, schema.byte_size());
-      uint32_t val_read = *schema.ExtractColumnFromRow<UINT32>(row_slice, 1);
+      uint32_t val_read = *schema.ExtractColumnFromRow<UINT32>(block.row_slice(i), 1);
       bool removed = inserted.erase(val_read);
       ASSERT_TRUE(removed) << "Got value " << val_read << " but either "
                            << "the value was invalid or was already "

commit 5322534d73f3b72f498b9c0f4f24ff05ab1973be

fix RowBlock::row_slice() to return correct results
Was previously always returning the first row.
Fixes test failure introduced by 9d54acc3ce250c37d5cbea4386b91fcb37277941

src/common/rowblock.h
@@ -35,7 +35,7 @@ public:
   }
 
   const Slice row_slice(size_t idx) const {
-    return Slice(row_ptr(0), schema_.byte_size());
+    return Slice(row_ptr(idx), schema_.byte_size());
   }
 
   const Schema &schema() const { return schema_; }