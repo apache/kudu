e45d96eb7a7c7cd4c1067684d0d09f883e2f32c0
Colin Patrick Mccabe <cmccabe@cloudera.com>
2013-03-10 21:04:26 -0700

    status: add the ability to associate a posix error code
    Signed-off-by: Colin Patrick McCabe <cmccabe@cloudera.com>

---
+++ src/util/status-test.cc
@@ -0,0 +1,28 @@
+// Copyright (c) 2013, Cloudera, inc.
+//
+// Some portions Copyright (c) 2011 The LevelDB Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file. See the AUTHORS file for names of contributors.
+
+#include <glog/logging.h>
+#include <gtest/gtest.h>
+
+#include <errno.h>
+#include <vector>
+#include "util/status.h"
+
+namespace kudu {
+
+TEST(StatusTest, TestPosixCode) {
+  Status ok = Status::OK();
+  ASSERT_EQ(0, ok.posix_code());
+  Status file_error = Status::IOError("file error", Slice(), ENOTDIR);
+  ASSERT_EQ(ENOTDIR, file_error.posix_code());
+}
+
+TEST(StatusTest, TestToString) {
+  Status file_error = Status::IOError("file error", Slice(), ENOTDIR);
+  ASSERT_EQ(string("IO error: file error (error 20)"), file_error.ToString());
+}
+
+}  // namespace kudu
--- src/util/status.cc
+++ src/util/status.cc
@@ -13,24 +13,26 @@ namespace kudu {
 const char* Status::CopyState(const char* state) {
   uint32_t size;
   strings::memcpy_inlined((char *)&size, state, sizeof(size));
-  char* result = new char[size + 5];
-  strings::memcpy_inlined(result, state, size + 5);
+  char* result = new char[size + 7];
+  strings::memcpy_inlined(result, state, size + 7);
   return result;
 }
 
-Status::Status(Code code, const Slice& msg, const Slice& msg2) {
+Status::Status(Code code, const Slice& msg, const Slice& msg2,
+               int16_t posix_code) {
   assert(code != kOk);
   const uint32_t len1 = msg.size();
   const uint32_t len2 = msg2.size();
   const uint32_t size = len1 + (len2 ? (2 + len2) : 0);
-  char* result = new char[size + 5];
+  char* result = new char[size + 7];
   memcpy(result, &size, sizeof(size));
   result[4] = static_cast<char>(code);
-  memcpy(result + 5, msg.data(), len1);
+  memcpy(result + 5, &posix_code, sizeof(posix_code));
+  memcpy(result + 7, msg.data(), len1);
   if (len2) {
-    result[5 + len1] = ':';
-    result[6 + len1] = ' ';
-    memcpy(result + 7 + len1, msg2.data(), len2);
+    result[7 + len1] = ':';
+    result[8 + len1] = ' ';
+    memcpy(result + 9 + len1, msg2.data(), len2);
   }
   state_ = result;
 }
@@ -69,9 +71,24 @@ std::string Status::ToString() const {
     std::string result(type);
     uint32_t length;
     memcpy(&length, state_, sizeof(length));
-    result.append(state_ + 5, length);
+    int16_t posix = posix_code();
+    result.append(state_ + 7, length);
+    if (posix != -1) {
+      char buf[64];
+      snprintf(buf, sizeof(buf), " (error %d)", posix);
+      result.append(buf);
+    }
     return result;
   }
+}
+
+int16_t Status::posix_code() const {
+  if (state_ == NULL) {
+    return 0;
+  }
+  int16_t posix_code;
+  memcpy(&posix_code, state_ + 5, sizeof(posix_code));
+  return posix_code;
 }
 
 }  // namespace kudu
