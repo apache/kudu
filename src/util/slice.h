6e39c4403257abe6602191242689731f06a24808
Todd Lipcon <todd@cloudera.com>
2013-03-12 22:56:49 -0700

    slice: allow ToDebugString to take a length beyond which to abbreviate

--- src/util/slice.h
+++ src/util/slice.h
@@ -90,23 +90,37 @@ class Slice {
     return std::string(reinterpret_cast<const char *>(data_), size_);
   }
 
-  std::string ToDebugString() const {
+  std::string ToDebugString(size_t max_len=0) const {
+    size_t bytes_to_print = size_;
+    bool abbreviated = false;
+    if (max_len != 0 && bytes_to_print > max_len) {
+      bytes_to_print = max_len;
+      abbreviated = true;
+    }
+
     int size = 0;
-    for (int i = 0; i < size_; i++) {
+    for (int i = 0; i < bytes_to_print; i++) {
       if (!isgraph(data_[i])) {
         size += 4;
       } else {
         size++;
       }
     }
+    if (abbreviated) {
+      size += 20; // extra padding
+    }
+
     std::string ret;
     ret.reserve(size);
-    for (int i = 0; i < size_; i++) {
+    for (int i = 0; i < bytes_to_print; i++) {
       if (!isgraph(data_[i])) {
         StringAppendF(&ret, "\\x%02x", data_[i] & 0xff);
       } else {
         ret.push_back(data_[i]);
       }
+    }
+    if (abbreviated) {
+      StringAppendF(&ret, "...<%zd bytes total>", size_);
     }
     return ret;
   }
