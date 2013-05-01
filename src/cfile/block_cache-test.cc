441f94eabefdab13c5020f8a9085527e65def695
Todd Lipcon <todd@cloudera.com>
2013-05-01 12:32:08 -0700

    block_cache-test: fix an ASAN failure due to mismatched alloc/free
    This is a new error type caught by trunk builds of clang++ -- the test code was
    using malloc (via strdup) to allocate something, and delete[] (in the cache) to
    free it.
    
      ==18173==ERROR: AddressSanitizer: alloc-dealloc-mismatch (malloc vs operator delete []) on 0x60200000e050
          #0 0x45b2db in operator delete[](void*) _asan_rtl_
          #1 0x47b0c5 in kudu::cfile::BlockCache::ValueDeleter(kudu::Slice const&, void*) /home/todd/git/kudu/src/cfile/block_cache.cc:72
          #2 0x482dd9 in kudu::(anonymous namespace)::LRUCache::Unref(kudu::(anonymous namespace)::LRUHandle*) /home/todd/git/kudu/src/util/cache.cc:197
          #3 0x488a0b in ~LRUCache /home/todd/git/kudu/src/util/cache.cc:187
          #4 0x487ede in ~ShardedLRUCache /home/todd/git/kudu/src/util/cache.cc:299
          #5 0x48016b in ~ShardedLRUCache /home/todd/git/kudu/src/util/cache.cc:299
          #6 0x47444a in base::DefaultDeleter<kudu::Cache>::operator()(kudu::Cache*) const /home/todd/git/kudu/src/gutil/gscoped_ptr.h:145
          #7 0x474249 in ~gscoped_ptr_impl /home/todd/git/kudu/src/gutil/gscoped_ptr.h:228
          #8 0x4740cc in ~gscoped_ptr_impl /home/todd/git/kudu/src/gutil/gscoped_ptr.h:224
          #9 0x473fbc in ~gscoped_ptr /home/todd/git/kudu/src/gutil/gscoped_ptr.h:318
          #10 0x473dac in ~gscoped_ptr /home/todd/git/kudu/src/gutil/gscoped_ptr.h:318
          #11 0x473b37 in ~BlockCache /home/todd/git/kudu/src/cfile/block_cache.h:17
          #12 0x47080c in ~BlockCache /home/todd/git/kudu/src/cfile/block_cache.h:17
          #13 0x46ebb3 in kudu::cfile::TestBlockCache_TestBasics_Test::TestBody() /home/todd/git/kudu/src/cfile/block_cache-test.cc:41
          #14 0x4df61c in void testing::internal::HandleSehExceptionsInMethodIfSupported<testing::Test, void>(testing::Test*, void (testing::Test::*)(), char const*) ??:0
          #15 0x4da57d in void testing::internal::HandleExceptionsInMethodIfSupported<testing::Test, void>(testing::Test*, void (testing::Test::*)(), char const*) ??:0
          #16 0x4c6dec in testing::Test::Run() ??:0
          #17 0x4c7561 in testing::TestInfo::Run() ??:0
          #18 0x4c7b33 in testing::TestCase::Run() ??:0
          #19 0x4cca5f in testing::internal::UnitTestImpl::RunAllTests() ??:0
          #20 0x4e0b10 in bool testing::internal::HandleSehExceptionsInMethodIfSupported<testing::internal::UnitTestImpl, bool>(testing::internal::UnitTestImpl*, bool (testing::internal::UnitTestImpl::*)(), char const*) ??:0
          #21 0x4db48b in bool testing::internal::HandleExceptionsInMethodIfSupported<testing::internal::UnitTestImpl, bool>(testing::internal::UnitTestImpl*, bool (testing::internal::UnitTestImpl::*)(), char const*) ??:0
          #22 0x4cb7b1 in testing::UnitTest::Run() ??:0
          #23 0x4e9699 in main /home/todd/git/kudu/src/util/test_main.cc:15
          #24 0x7f6012dbc76c in __libc_start_main /build/buildd/eglibc-2.15/csu/libc-start.c:226
      0x60200000e050 is located 0 bytes inside of 12-byte region [0x60200000e050,0x60200000e05c)
      allocated by thread T0 here:
          #0 0x45ac4b in __interceptor_malloc _asan_rtl_
          #1 0x7f6012e23d81 in __GI___strdup /build/buildd/eglibc-2.15/string/strdup.c:43
      ==18173==HINT: if you don't care about these warnings you may set ASAN_OPTIONS=alloc_dealloc_mismatch=0
      ==18173==ABORTING

--- src/cfile/block_cache-test.cc
+++ src/cfile/block_cache-test.cc
@@ -2,6 +2,7 @@
 
 #include <gtest/gtest.h>
 #include <glog/logging.h>
+#include <string.h>
 
 #include "cfile/block_cache.h"
 #include "util/slice.h"
@@ -9,8 +10,12 @@
 namespace kudu {
 namespace cfile {
 
+static const char *DATA_TO_CACHE = "hello world";
+
 TEST(TestBlockCache, TestBasics) {
-  char *DATUM_1 = strdup("hello world");
+  size_t data_size = strlen(DATA_TO_CACHE) + 1;
+  uint8_t *DATUM_1 = new uint8_t[data_size];
+  memcpy(DATUM_1, DATA_TO_CACHE, data_size);
 
   BlockCache cache(1024);
 
@@ -25,14 +30,13 @@ TEST(TestBlockCache, TestBasics) {
 
   // Insert and re-lookup
   BlockCacheHandle inserted_handle;
-  cache.Insert(id, 1, Slice(DATUM_1), &inserted_handle);
+  cache.Insert(id, 1, Slice(DATUM_1, data_size), &inserted_handle);
   ASSERT_TRUE(inserted_handle.valid());
 
   BlockCacheHandle retrieved_handle;
   ASSERT_TRUE(cache.Lookup(id, 1, &retrieved_handle));
   ASSERT_TRUE(retrieved_handle.valid());
-  ASSERT_EQ(reinterpret_cast<const char *>(retrieved_handle.data().data()),
-            DATUM_1);
+  ASSERT_EQ(retrieved_handle.data().data(), DATUM_1);
 
   // Ensure that a lookup for a different offset doesn't
   // return this data.
