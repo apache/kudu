96305636e563a6150b6d5ed868b28ba0d5dca5a1
Todd Lipcon <todd@cloudera.com>
2013-05-14 00:46:52 -0700

    thirdparty: use wget -c to continue truncated downloads

--- thirdparty/download-thirdparty.sh
+++ thirdparty/download-thirdparty.sh
@@ -11,56 +11,56 @@ source vars.sh
 
 if [ ! -d gtest-${GTEST_VERSION} ]; then
   echo "Fetching gtest"
-  wget http://googletest.googlecode.com/files/gtest-${GTEST_VERSION}.zip
+  wget -c http://googletest.googlecode.com/files/gtest-${GTEST_VERSION}.zip
   unzip gtest-${GTEST_VERSION}.zip
   rm gtest-${GTEST_VERSION}.zip
 fi
 
 if [ ! -d glog-${GLOG_VERSION} ]; then
   echo "Fetching glog"
-  wget http://google-glog.googlecode.com/files/glog-${GLOG_VERSION}.tar.gz
+  wget -c http://google-glog.googlecode.com/files/glog-${GLOG_VERSION}.tar.gz
   tar xzf glog-${GLOG_VERSION}.tar.gz
   rm glog-${GLOG_VERSION}.tar.gz
 fi
 
 if [ ! -d gflags-${GFLAGS_VERSION} ]; then
   echo "Fetching gflags"
-  wget http://gflags.googlecode.com/files/gflags-${GFLAGS_VERSION}.zip
+  wget -c http://gflags.googlecode.com/files/gflags-${GFLAGS_VERSION}.zip
   unzip gflags-${GFLAGS_VERSION}.zip
   rm gflags-${GFLAGS_VERSION}.zip
 fi
 
 if [ ! -d gperftools-${GPERFTOOLS_VERSION} ]; then
   echo "Fetching gperftools"
-  wget http://gperftools.googlecode.com/files/gperftools-${GPERFTOOLS_VERSION}.tar.gz
+  wget -c http://gperftools.googlecode.com/files/gperftools-${GPERFTOOLS_VERSION}.tar.gz
   tar xzf gperftools-${GPERFTOOLS_VERSION}.tar.gz
   rm gperftools-${GPERFTOOLS_VERSION}.tar.gz
 fi
 
 if [ ! -d protobuf-${PROTOBUF_VERSION} ]; then
   echo "Fetching protobuf"
-  wget http://protobuf.googlecode.com/files/protobuf-${PROTOBUF_VERSION}.tar.gz
+  wget -c http://protobuf.googlecode.com/files/protobuf-${PROTOBUF_VERSION}.tar.gz
   tar xzf protobuf-${PROTOBUF_VERSION}.tar.gz
   rm protobuf-${PROTOBUF_VERSION}.tar.gz
 fi
 
 if [ ! -d cmake-${CMAKE_VERSION} ]; then
   echo "Fetching cmake"
-  wget http://www.cmake.org/files/v2.8/cmake-${CMAKE_VERSION}.tar.gz
+  wget -c http://www.cmake.org/files/v2.8/cmake-${CMAKE_VERSION}.tar.gz
   tar xzf cmake-${CMAKE_VERSION}.tar.gz
   rm cmake-${CMAKE_VERSION}.tar.gz
 fi
 
 if [ ! -d snappy-${SNAPPY_VERSION} ]; then
   echo "Fetching snappy"
-  wget http://snappy.googlecode.com/files/snappy-${SNAPPY_VERSION}.tar.gz
+  wget -c http://snappy.googlecode.com/files/snappy-${SNAPPY_VERSION}.tar.gz
   tar xzf snappy-${SNAPPY_VERSION}.tar.gz
   rm snappy-${SNAPPY_VERSION}.tar.gz
 fi
 
 if [ ! -d zlib-${ZLIB_VERSION} ]; then
   echo "Fetching zlib"
-  wget http://zlib.net/zlib-${ZLIB_VERSION}.tar.gz
+  wget -c http://zlib.net/zlib-${ZLIB_VERSION}.tar.gz
   tar xzf zlib-${ZLIB_VERSION}.tar.gz
   rm zlib-${ZLIB_VERSION}.tar.gz
 fi
