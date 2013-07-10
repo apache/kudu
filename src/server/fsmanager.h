e7a0010db171f581b17cf3bbc78eab11152ea7f1
Matteo Bertozzi <matteo.bertozzi@cloudera.com>
2013-07-10 14:58:18 +0100

    FsManager - kudu root.dir layout manager
    Change-Id: I1ab84042434d5791fd3b919d87bde7065d1b90e0
    Reviewed-on: http://gerrit.ent.cloudera.com:8080/41
    Tested-by: jenkins <kitchen-build@cloudera.com>
    Reviewed-by: Todd Lipcon <todd@cloudera.com>

---
+++ src/server/fsmanager.h
@@ -0,0 +1,346 @@
+// Copyright (c) 2013, Cloudera, inc.
+
+#ifndef KUDU_COMMON_FS_MANAGER_H
+#define KUDU_COMMON_FS_MANAGER_H
+
+#include <boost/noncopyable.hpp>
+#include <boost/lexical_cast.hpp>
+#include <google/protobuf/message_lite.h>
+#include <tr1/memory>
+#include <string>
+#include <vector>
+
+#include "gutil/gscoped_ptr.h"
+#include "gutil/strtoint.h"
+#include "server/oid_generator.h"
+#include "util/env.h"
+
+namespace kudu {
+
+using std::ostream;
+using std::string;
+using std::vector;
+using std::tr1::shared_ptr;
+using google::protobuf::MessageLite;
+
+class BlockId {
+ public:
+  BlockId() {}
+  BlockId(const string& id) { SetId(id); }
+
+  void SetId(const string& id) {
+    CHECK_GE(id.size(), 8);
+    id_ = id;
+  }
+
+  bool IsNull() const { return id_.empty(); }
+  const string& ToString() const { return id_; }
+
+  // Used for on-disk partition
+  string hash0() const { return id_.substr(0, 2); }
+  string hash1() const { return id_.substr(2, 2); }
+  string hash2() const { return id_.substr(4, 2); }
+  string hash3() const { return id_.substr(6, 2); }
+
+  size_t hash() const {
+    return (strto32(hash0().c_str(), NULL, 16) << 24) +
+           (strto32(hash1().c_str(), NULL, 16) << 16) +
+           (strto32(hash2().c_str(), NULL, 16) << 8) +
+           strto32(hash3().c_str(), NULL, 16);
+  }
+
+  bool operator==(const BlockId& other) const {
+    return id_ == other.id_;
+  }
+
+ private:
+  string id_;
+};
+
+struct BlockIdHash {
+  size_t operator()(const BlockId& block_id) const {
+    return block_id.hash();
+  }
+};
+
+enum FsMetadataType {
+  // metadata file that contains the rowset blocks
+  kFsMetaTabletData,
+};
+
+// Returns the on-disk name of the specified metadata type
+const char *FsMetadataTypeToString(FsMetadataType type);
+
+// FsManager provides helpers to read data and metadata files,
+// and it's responsible for abstracting the file-system layout.
+//
+// The user should not be aware of where files are placed,
+// but instead should interact with the storage in terms of "open the block xyz"
+// or "write a new schema metadata file for table kwz".
+//
+// The FsManager should never be accessed directly, but instead the "Metadata"
+// wrappers like "TableMetadata" or "TabletMetadata" should be used.
+// Those wrappers are also responsible for writing every transaction like
+// "add this new data file" to the related WALs.
+// The TabletMetadata is also responsible for keeping track of the tablet files;
+// each new file added/removed is added to the WAL and then flushed
+// to the "tablet/data-files" meta file.
+//
+// The current layout is:
+//    <kudu.root.dir>/data/
+//    <kudu.root.dir>/data/<prefix-0>/<prefix-2>/<prefix-4>/<name>
+//    <kudu.root.dir>/tables/
+//    <kudu.root.dir>/tables/<table-id>/meta/{meta-name}.{id}
+//    <kudu.root.dir>/tables/<table-id>/tablets/{tablet-id}/{meta-name}.{id}
+//    <kudu.root.dir>/wals/<server-id>/{ts-split}/{prefix}.{timestamp}
+class FsManager {
+ public:
+  FsManager(Env *env, const string& root_path)
+    : env_(env), root_path_(root_path)
+  {}
+
+  Status CreateInitialFileSystemLayout();
+  void DumpFileSystemTree(ostream& out);
+
+  // ==========================================================================
+  //  Data read/write interfaces
+  // ==========================================================================
+
+  Status CreateNewBlock(shared_ptr<WritableFile> *writer, BlockId *block_id);
+  Status OpenBlock(const BlockId& block_id, shared_ptr<RandomAccessFile> *reader);
+
+  Status DeleteBlock(const BlockId& block) {
+    return env_->DeleteFile(GetBlockPath(block));
+  }
+
+  bool BlockExists(const BlockId& block) const {
+    return env_->FileExists(GetBlockPath(block));
+  }
+
+  // ==========================================================================
+  //  Metadata read/write interfaces
+  // ==========================================================================
+
+  Status WriteTableMetadataFile(const string& table, FsMetadataType meta, const MessageLite& msg) {
+    RETURN_NOT_OK(CreateTableDir(table));
+    return WriteMetadataFile(GetTableMetadataDir(table), FsMetadataTypeToString(meta), msg);
+  }
+
+  Status ReadTableMetadataFile(const string& table, FsMetadataType meta, MessageLite *msg) {
+    return ReadMetadataFile(GetTableMetadataDir(table), FsMetadataTypeToString(meta), msg);
+  }
+
+  Status DeleteTableMetadataFile(const string& table, FsMetadataType meta) {
+    return DeleteMetadataFile(GetTableMetadataDir(table), FsMetadataTypeToString(meta));
+  }
+
+  Status WriteTabletMetadataFile(const string& table, const string& tablet,
+                                 FsMetadataType meta, const MessageLite& msg)
+  {
+    RETURN_NOT_OK(CreateTabletDir(table, tablet));
+    return WriteMetadataFile(GetTabletMetadataDir(table, tablet), FsMetadataTypeToString(meta), msg);
+  }
+
+  Status ReadTabletMetadataFile(const string& table, const string& tablet,
+                                FsMetadataType meta, MessageLite *msg)
+  {
+    return ReadMetadataFile(GetTabletMetadataDir(table, tablet), FsMetadataTypeToString(meta), msg);
+  }
+
+  Status DeleteTabletMetadataFile(const string& table, const string& tablet, FsMetadataType meta) {
+    return DeleteMetadataFile(GetTabletMetadataDir(table, tablet), FsMetadataTypeToString(meta));
+  }
+
+  // ==========================================================================
+  //  Wal read/write interfaces
+  // ==========================================================================
+
+  Status NewWalFile(const string& server, const string& prefix, uint64_t timestamp,
+                    shared_ptr<WritableFile> *writer);
+  Status OpenWalFile(const string& server, const string& prefix, uint64_t timestamp,
+                     shared_ptr<RandomAccessFile> *reader);
+
+  // ==========================================================================
+  //  on-disk path
+  // ==========================================================================
+  const string& GetRootDir() const {
+    return root_path_;
+  }
+
+  string GetDataRootDir() const {
+    return env_->JoinPathSegments(GetRootDir(), kDataDirName);
+  }
+
+  string GetBlockPath(const BlockId& block_id) const {
+    CHECK(!block_id.IsNull());
+    string path = GetDataRootDir();
+    path = env_->JoinPathSegments(path, block_id.hash0());
+    path = env_->JoinPathSegments(path, block_id.hash1());
+    path = env_->JoinPathSegments(path, block_id.hash2());
+    path = env_->JoinPathSegments(path, block_id.ToString());
+    return path;
+  }
+
+
+  string GetMetadataFileName(const string& meta, size_t seq_id) const {
+    return meta + '.' + boost::lexical_cast<string>(seq_id);
+  }
+
+  string GetMetadataFilePath(const string& path, const string& meta, size_t seq_id) const {
+    return env_->JoinPathSegments(path, GetMetadataFileName(meta, seq_id));
+  }
+
+
+  string GetTablesRootDir() const {
+    return env_->JoinPathSegments(GetRootDir(), kTablesDirName);
+  }
+
+  string GetTableRootDir(const string& oid) const {
+    return env_->JoinPathSegments(GetTablesRootDir(), oid);
+  }
+
+
+  string GetTabletsRootDir(const string& oid) const {
+    return env_->JoinPathSegments(GetTableRootDir(oid), kTableTabletsDirName);
+  }
+
+  string GetTabletMetadataDir(const string& table, const string& oid) const {
+    return env_->JoinPathSegments(GetTabletsRootDir(table), oid);
+  }
+
+  string GetTableMetadataDir(const string& oid) const {
+    return env_->JoinPathSegments(GetTableRootDir(oid), kTableMetaDirName);
+  }
+
+
+  string GetWalsRootDir() const {
+    return env_->JoinPathSegments(root_path_, kWalsDirName);
+  }
+
+  string GetWalFilePath(const string& server, const string& prefix, uint64_t timestamp) const {
+    string path = env_->JoinPathSegments(GetWalsRootDir(), server);
+    path = env_->JoinPathSegments(path, boost::lexical_cast<string>(timestamp / kWalPartitionMillis));
+    return env_->JoinPathSegments(path, prefix + "." + boost::lexical_cast<string>(timestamp));
+  }
+
+  // ==========================================================================
+  //  Name generator
+  // ==========================================================================
+
+  string GenerateName() {
+    return oid_generator_.Next();
+  }
+
+  Env *env() { return env_; }
+
+  // ==========================================================================
+  //  file-system helpers
+  // ==========================================================================
+  bool Exists(const string& path) const {
+    return env_->FileExists(path);
+  }
+
+  Status ListDir(const string& path, vector<string> *objects) const {
+    return env_->GetChildren(path, objects);
+  }
+
+  Status CreateDirIfMissing(const string& path) {
+    if (!env_->FileExists(path)) {
+      return env_->CreateDir(path);
+    }
+    return Status::OK();
+  }
+
+ private:
+  // ==========================================================================
+  //  on-disk dir creations
+  // ==========================================================================
+
+  // TODO: This should be removed, and part of the file creation
+  //       to ensure that the file can be created.
+  Status CreateBlockDir(const BlockId& block_id) {
+    CHECK(!block_id.IsNull());
+
+    string path = GetDataRootDir();
+    RETURN_NOT_OK(CreateDirIfMissing(path));
+
+    path = env_->JoinPathSegments(path, block_id.hash0());
+    RETURN_NOT_OK(CreateDirIfMissing(path));
+
+    path = env_->JoinPathSegments(path, block_id.hash1());
+    RETURN_NOT_OK(CreateDirIfMissing(path));
+
+    path = env_->JoinPathSegments(path, block_id.hash2());
+    return CreateDirIfMissing(path);
+  }
+
+  // TODO: This should be removed, and part of the file creation
+  //       to ensure that the file can be created.
+  Status CreateTableDir(const string& oid) {
+    string path = GetTablesRootDir();
+    RETURN_NOT_OK(CreateDirIfMissing(path));
+
+    path = env_->JoinPathSegments(path, oid);
+    RETURN_NOT_OK(CreateDirIfMissing(path));
+
+    RETURN_NOT_OK(CreateDirIfMissing(env_->JoinPathSegments(path, kTableMetaDirName)));
+    RETURN_NOT_OK(CreateDirIfMissing(env_->JoinPathSegments(path, kTableTabletsDirName)));
+    return Status::OK();
+  }
+
+  Status CreateTabletDir(const string& table, const string& oid) {
+    string path = GetDataRootDir();
+    RETURN_NOT_OK(CreateTableDir(table));
+
+    path = GetTabletsRootDir(table);
+    RETURN_NOT_OK(CreateDirIfMissing(path));
+
+    path = env_->JoinPathSegments(path, oid);
+    return CreateDirIfMissing(path);
+  }
+
+  Status CreateWalsDir(const string& server, const string& prefix, uint64_t timestamp) {
+    string path = GetWalsRootDir();
+    RETURN_NOT_OK(CreateDirIfMissing(path));
+
+    path = env_->JoinPathSegments(GetWalsRootDir(), server);
+    RETURN_NOT_OK(CreateDirIfMissing(path));
+
+    path = env_->JoinPathSegments(path, boost::lexical_cast<string>(timestamp / kWalPartitionMillis));
+    return CreateDirIfMissing(path);
+  }
+
+  // ==========================================================================
+  //  Generic Metadata read/write interaface
+  // ==========================================================================
+  uint64_t GetLastMetadataSeqId(const vector<string>& meta_files, const string& name,
+                                vector<uint64_t> *available_ids);
+  Status WriteMetadataFile(const string& path, const string& meta, const MessageLite& msg);
+  Status ReadMetadataFile(const string& path, const string& meta, MessageLite *msg);
+  Status DeleteMetadataFile(const string& path, const string& meta);
+
+  // ==========================================================================
+  //  file-system helpers
+  // ==========================================================================
+  void DumpFileSystemTree(ostream& out, const string& prefix,
+                          const string& path, const vector<string>& objects);
+
+  static const char *kDataDirName;
+  static const char *kWalsDirName;
+  static const char *kTablesDirName;
+  static const char *kTableMetaDirName;
+  static const char *kTableTabletsDirName;
+  static const char *kCorruptedSuffix;
+  static const uint64_t kWalPartitionMillis = 3600000;   // 1hour
+
+  Env *env_;
+  string root_path_;
+
+  ObjectIdGenerator oid_generator_;
+
+  DISALLOW_COPY_AND_ASSIGN(FsManager);
+};
+
+} // namespace kudu
+
+#endif
---
+++ src/server/oid_generator.h
@@ -0,0 +1,32 @@
+// Copyright (c) 2012, Cloudera, inc.
+
+#ifndef KUDU_OID_GENERATOR_H
+#define KUDU_OID_GENERATOR_H
+
+#include <boost/uuid/uuid_generators.hpp>
+#include "gutil/macros.h"
+#include "util/locks.h"
+
+namespace kudu {
+
+// Generates a unique 32byte id, based on uuid v4.
+// This class is thread safe
+class ObjectIdGenerator {
+ public:
+  ObjectIdGenerator() {}
+  ~ObjectIdGenerator() {}
+
+  std::string Next();
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(ObjectIdGenerator);
+
+  typedef simple_spinlock LockType;
+
+  LockType oid_lock_;
+  boost::uuids::random_generator oid_generator_;
+};
+
+} // namespace kudu
+
+#endif
