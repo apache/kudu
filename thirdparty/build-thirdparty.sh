1c51efd48dbe0f73e4c8b2afa96686f7dab861e4
Mike Percy <mpercy@cloudera.com>
2013-08-02 14:51:47 -0700

    Allow for building selected modules only.
    This is useful for devs when modifying configure/make commands,
    upgrading library versions, adding new dependencies, etc.
    
    Change-Id: I42493f808ac98baa8f78dc939228a34289c5f74a
    Reviewed-on: http://gerrit.ent.cloudera.com:8080/161
    Reviewed-by: Michael Percy <mpercy@cloudera.com>
    Reviewed-by: Todd Lipcon <todd@cloudera.com>
    Tested-by: jenkins <kitchen-build@cloudera.com>

--- thirdparty/build-thirdparty.sh
+++ thirdparty/build-thirdparty.sh
@@ -3,12 +3,35 @@
 
 set -x
 set -e
-
 TP_DIR=$(readlink -f $(dirname $BASH_SOURCE))
 
 source $TP_DIR/vars.sh
 
-##############################
+################################################################################
+
+if [ "$#" = "0" ]; then
+  F_ALL=1
+else
+  # Allow passing specific libs to build on the command line
+  for arg in "$*"; do
+    case $arg in
+      "cmake")      F_CMAKE=1 ;;
+      "cyrus-sasl") F_CYRUS_SASL=1 ;;
+      "gflags")     F_GFLAGS=1 ;;
+      "glog")       F_GLOG=1 ;;
+      "gperftools") F_GPERFTOOLS=1 ;;
+      "gtest")      F_GTEST=1 ;;
+      "libev")      F_LIBEV=1 ;;
+      "lz4")        F_LZ4=1 ;;
+      "protobuf")   F_PROTOBUF=1 ;;
+      "snappy")     F_SNAPPY=1 ;;
+      "zlib")       F_ZLIB=1 ;;
+      *)            echo "Unknown module: $arg"; exit 1 ;;
+    esac
+  done
+fi
+
+################################################################################
 
 # On some systems, autotools installs libraries to lib64 rather than lib.  Fix
 # this by setting up lib64 as a symlink to lib.  We have to do this step first
@@ -20,68 +43,90 @@ ln -sf lib "$PREFIX/lib64"
 export PATH=$PREFIX/bin:$PATH
 
 # build cmake
-cd $CMAKE_DIR
-./bootstrap --prefix=$PREFIX --parallel=8
-make -j
-make install
+if [ -n "$F_ALL" -o -n "$F_CMAKE" ]; then
+  cd $CMAKE_DIR
+  ./bootstrap --prefix=$PREFIX --parallel=8
+  make -j
+  make install
+fi
 
 # build gflags
-cd $GFLAGS_DIR
-./configure --with-pic --prefix=$PREFIX
-make -j4 install
+if [ -n "$F_ALL" -o -n "$F_GFLAGS" ]; then
+  cd $GFLAGS_DIR
+  ./configure --with-pic --prefix=$PREFIX
+  make -j4 install
+fi
 
 # build glog
-cd $GLOG_DIR
-./configure --with-pic --prefix=$PREFIX --with-gflags=$PREFIX
-make -j4 install
+if [ -n "$F_ALL" -o -n "$F_GLOG" ]; then
+  cd $GLOG_DIR
+  ./configure --with-pic --prefix=$PREFIX --with-gflags=$PREFIX
+  make -j4 install
+fi
 
 # build gperftools
-cd $GPERFTOOLS_DIR
-./configure --enable-frame-pointers --with-pic --prefix=$PREFIX
-make -j4 install
+if [ -n "$F_ALL" -o -n "$F_GPERFTOOLS" ]; then
+  cd $GPERFTOOLS_DIR
+  ./configure --enable-frame-pointers --with-pic --prefix=$PREFIX
+  make -j4 install
+fi
 
 # build gtest
-cd $GTEST_DIR
-cmake .
-make -j4
+if [ -n "$F_ALL" -o -n "$F_GTEST" ]; then
+  cd $GTEST_DIR
+  cmake .
+  make -j4
+fi
 
 # build protobuf
-cd $PROTOBUF_DIR
-./configure --with-pic --disable-shared --prefix=$PREFIX
-make -j4 install
+if [ -n "$F_ALL" -o -n "$F_PROTOBUF" ]; then
+  cd $PROTOBUF_DIR
+  ./configure --with-pic --disable-shared --prefix=$PREFIX
+  make -j4 install
+fi
 
 # build snappy
-cd $SNAPPY_DIR
-./configure --with-pic --prefix=$PREFIX
-make -j4 install
+if [ -n "$F_ALL" -o -n "$F_SNAPPY" ]; then
+  cd $SNAPPY_DIR
+  ./configure --with-pic --prefix=$PREFIX
+  make -j4 install
+fi
 
 # build zlib
-cd $ZLIB_DIR
-./configure --prefix=$PREFIX
-make -j4 install
+if [ -n "$F_ALL" -o -n "$F_ZLIB" ]; then
+  cd $ZLIB_DIR
+  ./configure --prefix=$PREFIX
+  make -j4 install
+fi
 
 # build lz4
-cd $LZ4_DIR
-$PREFIX/bin/cmake -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX $LZ4_DIR
-make -j4 install
+if [ -n "$F_ALL" -o -n "$F_LZ4" ]; then
+  cd $LZ4_DIR
+  $PREFIX/bin/cmake -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX $LZ4_DIR
+  make -j4 install
+fi
 
 ## build libev
-cd $LIBEV_DIR
-./configure --with-pic --disable-shared --prefix=$PREFIX
-make -j4 install
+if [ -n "$F_ALL" -o -n "$F_LIBEV" ]; then
+  cd $LIBEV_DIR
+  ./configure --with-pic --disable-shared --prefix=$PREFIX
+  make -j4 install
+fi
 
 ## build cyrus-sasl
-cd $CYRUS_SASL_DIR
-[ -r Makefile ] && make distclean # (Jenkins was complaining about CFLAGS changes)
-# Disable everything except those protocols needed -- currently just Kerberos.
-# Sasl does not have a --with-pic configuration.
-CFLAGS="-fPIC -DPIC" CXXFLAGS="-fPIC -DPIC" ./configure \
-  --disable-digest --disable-sql --disable-cram --disable-ldap --disable-otp \
-  --enable-static --enable-staticdlopen --with-dblib=none --without-des\
-  --prefix=$PREFIX
-make clean
-make # no -j4 ... concurrent build probs on RHEL?
-make install
+if [ -n "$F_ALL" -o -n "$F_CYRUS_SASL" ]; then
+  cd $CYRUS_SASL_DIR
+  [ -r Makefile ] && make distclean # (Jenkins was complaining about CFLAGS changes)
+  # Disable everything except those protocols needed -- currently just Kerberos.
+  # Sasl does not have a --with-pic configuration.
+  CFLAGS="-fPIC -DPIC" CXXFLAGS="-fPIC -DPIC" ./configure \
+    --disable-digest --disable-sql --disable-cram --disable-ldap --disable-otp \
+    --enable-static --enable-staticdlopen --with-dblib=none --without-des \
+    --prefix=$PREFIX
+  make clean
+  make # no -j4 ... concurrent build probs on RHEL?
+  make install
+fi
 
 echo "---------------------"
 echo "Thirdparty dependencies built and installed into $PREFIX successfully"
