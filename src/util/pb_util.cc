c4bb69cc59e71cf19b7c6e7f9d8cc04b3b99d0fc
Todd Lipcon <todd@cloudera.com>
2013-08-02 17:38:40 -0700

    pb_util: fix WritableFileOutputStream behavior after flush
    This fixes this failure seen in an mt-tablet-test run:
      F0802 16:40:58.869029 12100 pb_util.cc:207] Check failed: count <= buffer_offset_ (8107 vs. 0)
      *** Check failure stack trace: ***
      *** Aborted at 1375486858 (unix time) try "date -d @1375486858" if you are using GNU date ***
      PC: @       0x335c632885 (unknown)
      *** SIGABRT (@0xfa000002f2b) received by PID 12075 (TID 0x7ffd37add700) from PID 12075; stack trace: ***
          @       0x335ca0f4a0 (unknown)
          @       0x335c632885 (unknown)
          @       0x335c634065 (unknown)
          @     0x7ffd45d39a69 google::logging_fail()
          @     0x7ffd45d3999d google::LogMessage::Fail()
          @     0x7ffd45d3e017 google::LogMessage::SendToLog()
          @     0x7ffd45d3be79 google::LogMessage::Flush()
          @     0x7ffd45d3c17d google::LogMessageFatal::~LogMessageFatal()
          @           0x5d5eb4 kudu::pb_util::WritableFileOutputStream::BackUp()
          @           0x6111ae google::protobuf::MessageLite::SerializeToZeroCopyStream()
          @           0x5d5077 kudu::pb_util::SerializeToWritableFile()
          @           0x57499f kudu::FsManager::WriteMetadataBlock()
    
    The issue was that when the buffer was full, causing a flush, it would reset buffer_offset_ to 0
    rather than to the full buffer size, causing the next BackUp() to fail.
    
    This patch moves the implementations to a new header and adds a unit test.
    
    Change-Id: If39cee46e81dffde74f0994c3a00334948d2ceac
    Reviewed-on: http://gerrit.ent.cloudera.com:8080/133
    Tested-by: jenkins <kitchen-build@cloudera.com>
    Reviewed-by: Matteo Bertozzi <matteo.bertozzi@cloudera.com>

--- src/util/pb_util.cc
+++ src/util/pb_util.cc
@@ -12,6 +12,7 @@
 #include <google/protobuf/message_lite.h>
 
 #include "util/pb_util.h"
+#include "util/pb_util-internal.h"
 #include "util/status.h"
 #include "util/env.h"
 
@@ -20,6 +21,7 @@ namespace pb_util {
 
 namespace {
 
+using google::protobuf::MessageLite;
 using std::string;
 
 // When serializing, we first compute the byte size, then serialize the message.
@@ -87,170 +89,6 @@ bool SerializeToString(const MessageLite &msg, faststring *output) {
   return AppendToString(msg, output);
 }
 
-// Input Stream used by ParseFromSequentialFile()
-class SequentialFileFileInputStream : public google::protobuf::io::ZeroCopyInputStream {
- public:
-  SequentialFileFileInputStream(SequentialFile *rfile, size_t buffer_size = kDefaultBufferSize)
-    : buffer_used_(0), buffer_offset_(0),
-      buffer_size_(buffer_size), buffer_(new uint8[buffer_size_]),
-      total_read_(0), rfile_(rfile)
-  {
-    CHECK_GT(buffer_size, 0);
-  }
-
-  ~SequentialFileFileInputStream() {
-  }
-
-  bool Next(const void **data, int *size);
-  bool Skip(int count);
-
-  void BackUp(int count) {
-    CHECK_GE(count, 0);
-    CHECK_LE(count, buffer_offset_);
-    buffer_offset_ -= count;
-    total_read_ -= count;
-  }
-
-  long ByteCount() const {
-    return total_read_;
-  }
-
- private:
-  static const size_t kDefaultBufferSize = 8192;
-
-  Status status_;
-
-  size_t buffer_used_;
-  size_t buffer_offset_;
-  const size_t buffer_size_;
-  gscoped_ptr<uint8_t[]> buffer_;
-
-  size_t total_read_;
-  SequentialFile *rfile_;
-};
-
-bool SequentialFileFileInputStream::Next(const void **data, int *size) {
-  if (PREDICT_FALSE(!status_.ok())) {
-    LOG(WARNING) << "Already failed on a previous read: " << status_.ToString();
-    return false;
-  }
-
-  size_t available = (buffer_used_ - buffer_offset_);
-  if (available > 0) {
-    *data = buffer_.get() + buffer_offset_;
-    *size = available;
-    buffer_offset_ += available;
-    total_read_ += available;
-    return true;
-  }
-
-  Slice result;
-  status_ = rfile_->Read(buffer_size_, &result, buffer_.get());
-  if (!status_.ok()) {
-    LOG(WARNING) << "Read at " << buffer_offset_ << " failed: " << status_.ToString();
-    return false;
-  }
-
-  if (result.data() != buffer_.get()) {
-    memcpy(buffer_.get(), result.data(), result.size());
-  }
-
-  buffer_used_ = result.size();
-  buffer_offset_ = buffer_used_;
-  total_read_ += buffer_used_;
-  *data = buffer_.get();
-  *size = buffer_used_;
-  return buffer_used_ > 0;
-}
-
-bool SequentialFileFileInputStream::Skip(int count) {
-  CHECK_GT(count, 0);
-  int avail = (buffer_used_ - buffer_offset_);
-  if (avail > count) {
-    buffer_offset_ += count;
-    total_read_ += count;
-  } else {
-    buffer_used_ = 0;
-    buffer_offset_ = 0;
-    status_ = rfile_->Skip(count - avail);
-    total_read_ += count - avail;
-  }
-  return status_.ok();
-}
-
-// Output Stream used by SerializeToWritableFile()
-class WritableFileOutputStream : public google::protobuf::io::ZeroCopyOutputStream {
- public:
-  WritableFileOutputStream(WritableFile *wfile, size_t buffer_size = kDefaultBufferSize)
-    : buffer_offset_(0), buffer_size_(buffer_size), buffer_(new uint8[buffer_size_]),
-      written_(0), wfile_(wfile)
-  {
-    CHECK_GT(buffer_size, 0);
-  }
-
-  ~WritableFileOutputStream() {
-  }
-
-  bool Flush() {
-    if (buffer_offset_ > 0) {
-      Slice data(buffer_.get(), buffer_offset_);
-      status_ = wfile_->Append(data);
-      buffer_offset_ = 0;
-    }
-    return status_.ok();
-  }
-
-  bool Next(void **data, int *size);
-
-  void BackUp(int count) {
-    CHECK_GE(count, 0);
-    CHECK_LE(count, buffer_offset_);
-    buffer_offset_ -= count;
-    written_ -= count;
-  }
-
-  long ByteCount() const {
-    return written_;
-  }
-
- private:
-  static const size_t kDefaultBufferSize = 8192;
-
-  Status status_;
-
-  size_t buffer_offset_;
-  const size_t buffer_size_;
-  gscoped_ptr<uint8_t[]> buffer_;
-
-  size_t written_;
-  WritableFile *wfile_;
-};
-
-bool WritableFileOutputStream::Next(void **data, int *size) {
-  if (PREDICT_FALSE(!status_.ok())) {
-    LOG(WARNING) << "Already failed on a previous write: " << status_.ToString();
-    return false;
-  }
-
-  size_t available = (buffer_size_ - buffer_offset_);
-  if (available > 0) {
-    *data = buffer_.get() + buffer_offset_;
-    *size = available;
-    buffer_offset_ += available;
-    written_ += available;
-    return true;
-  }
-
-  if (!Flush()) {
-    return false;
-  }
-
-  buffer_offset_ = 0;
-  written_ += buffer_size_;
-  *data = buffer_.get();
-  *size = buffer_size_;
-  return true;
-}
 
 bool ParseFromSequentialFile(MessageLite *msg, SequentialFile *rfile) {
   SequentialFileFileInputStream istream(rfile);
