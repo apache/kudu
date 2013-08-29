56cac04e1b17cf5a0a434b57b3e0262bb4d9ab80
Todd Lipcon <todd@cloudera.com>
2013-08-28 21:35:29 -0700

    Wrap tests and tee their output to a test log directory
    This should make it easier to quickly see the failing tests
    when a Jenkins job fails.
    
    Change-Id: I6e9ce598d77f12a178a81c8cff9f15647eeedf2a
    Reviewed-on: http://gerrit.ent.cloudera.com:8080/347
    Tested-by: jenkins
    Reviewed-by: Michael Percy <mpercy@cloudera.com>

---
+++ build-support/run-test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Copyright (c) 2013, Cloudera, inc.
+#
+# Script which wraps running a test and redirects its output to a
+# test log directory.
+
+ME=$(dirname $BASH_SOURCE)
+ROOT=$(readlink -f $ME/..)
+
+TEST_OUT=$ROOT/build/test-logs/
+mkdir -p $TEST_OUT
+TEST_NAME=$(basename $1)
+
+set -e
+
+"$@" 2>&1 | tee $TEST_OUT/$TEST_NAME.txt
