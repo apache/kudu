a70c1c04af86fd0bd9a7d18a4f888c3a0ebff026
Todd Lipcon <todd@cloudera.com>
2013-09-17 19:32:22 -0700

    Add incremental mode for lint script.
    Developers can now use "make ilint" for faster lint checks on their
    local commits.
    
    Change-Id: I25812f3d61565ef887ed18fb3c7379605d0528fe
    Reviewed-on: http://gerrit.ent.cloudera.com:8080/480
    Tested-by: jenkins
    Reviewed-by: Michael Percy <mpercy@cloudera.com>

---
+++ build-support/get-upstream-commit.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Copyright (c) 2013, Cloudera, inc.
+#
+# Script which tries to determine the most recent git hash in the current
+# branch which was checked in by gerrit. This commit hash is printed to
+# stdout.
+#
+# It does so by looking for the verification signoff from Jenkins.  This is
+# more foolproof than trying to guess at the "origin/" branch name, since the
+# developer might be working on some local topic branch.
+set -e
+
+git log --grep='Tested-by: jenkins' -n1 --pretty=format:%H
--- build-support/lint.sh
+++ build-support/lint.sh
@@ -6,11 +6,37 @@ ROOT=$(readlink -f $ME/..)
 TMP=$(mktemp)
 trap "rm $TMP" EXIT
 
+ONLY_CHANGED=false
+
+for flag in "$@" ; do
+  case $flag in
+    --changed-only | -c)
+      ONLY_CHANGED=true
+      ;;
+    *)
+      echo unknown flag: $flag
+      exit 1
+      ;;
+  esac
+done
+
+if $ONLY_CHANGED ; then
+  FILES=$(git diff --name-only $($ME/get-upstream-commit.sh)  \
+    | egrep  '\.(cc|h)$' | grep -v gutil)
+  if [ -z "$FILES" ]; then
+    echo No source files changed
+    exit 0
+  fi
+else
+  FILES=$(find $ROOT/src -name *.cc -or -name *.h | grep -v .pb. | grep -v gutil)
+fi
+
+cd $ROOT
+
 $ROOT/thirdparty/installed/bin/cpplint.py \
   --verbose=4 \
   --filter=-whitespace/comments,-whitespace/line_length,-readability/todo,-build/header_guard,-build/include_order \
-  $(find $ROOT/src -name *.cc -or -name *.h | grep -v .pb. | grep -v gutil) \
-  2>&1 | grep -v 'Done processing' | tee $TMP
+  $FILES 2>&1 | grep -v 'Done processing' | tee $TMP
 
 NUM_ERRORS=$(grep "Total errors found" $TMP | awk '{print $4}')
 
