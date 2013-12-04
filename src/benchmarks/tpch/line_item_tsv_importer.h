873de061b2de18170282bb59e9849c2b9f2f5adf
Todd Lipcon <todd@cloudera.com>
2013-12-03 19:43:54 -0800

    tpch: speed up importer
    This improves the speed of the TPCH lineitem parser by using the
    gutil string split function instead of the boost tokenizer. This is faster
    since it doesn't cause allocation/deallocation of std::strings for each
    column, but instead just uses StringPieces which refer to substrings of
    the original line.
    
    Before:
    I1203 19:35:02.777410 17027 tpch1.cc:241] Times for loading: real 100.977s      user 98.408s    sys 2.074s
    I1203 19:35:04.021838 17027 tpch1.cc:193] Result:
    I1203 19:35:04.021878 17027 tpch1.cc:204] A, F, 37734107, -2.11154e+07, 25.522, -14.28, 0.04, 1478493
    I1203 19:35:04.021930 17027 tpch1.cc:204] R, F, 37719753, 3.32123e+06, 25.5058, 2.24, 0.05, 1478870
    I1203 19:35:04.021942 17027 tpch1.cc:204] N, O, 74476040, -1.03714e+07, 25.5022, -3.55, 0.04, 2920374
    I1203 19:35:04.021955 17027 tpch1.cc:204] N, F, 991417, -1.57339e+07, 25.5165, -404.94, 0.05, 38854
    I1203 19:35:04.021981 17027 tpch1.cc:248] Times for querying for iteration # 0: real 1.245s     user 1.222s     sys 0.019s
    
    After:
    I1203 19:42:53.335116 26006 tpch1.cc:241] Times for loading: real 57.502s       user 55.253s    sys 2.029s
    I1203 19:42:54.538929 26006 tpch1.cc:193] Result:
    I1203 19:42:54.538961 26006 tpch1.cc:204] A, F, 37734107, -2.11154e+07, 25.522, -14.28, 0.04, 1478493
    I1203 19:42:54.539012 26006 tpch1.cc:204] R, F, 37719753, 3.32123e+06, 25.5058, 2.24, 0.05, 1478870
    I1203 19:42:54.539026 26006 tpch1.cc:204] N, O, 74476040, -1.03714e+07, 25.5022, -3.55, 0.04, 2920374
    I1203 19:42:54.539037 26006 tpch1.cc:204] N, F, 991417, -1.57339e+07, 25.5165, -404.94, 0.05, 38854
    I1203 19:42:54.539067 26006 tpch1.cc:248] Times for querying for iteration # 0: real 1.204s     user 1.182s     sys 0.018s
    
    Change-Id: I74a0e5f01d04dca322289ddf66ba29cf77e46f56
    Reviewed-on: http://gerrit.ent.cloudera.com:8080/1022
    Tested-by: jenkins
    Reviewed-by: Jean-Daniel Cryans <jdcryans@cloudera.com>

--- src/benchmarks/tpch/line_item_tsv_importer.h
+++ src/benchmarks/tpch/line_item_tsv_importer.h
@@ -7,15 +7,15 @@
 #include <vector>
 #include <string>
 
-#include <boost/tokenizer.hpp>
-
 #include "common/schema.h"
 #include "common/row.h"
+#include "gutil/strings/split.h"
+#include "gutil/strings/stringpiece.h"
+
 
 namespace kudu {
 
-static const boost::char_separator<char> kPipeSeparator("|");
-typedef boost::tokenizer<boost::char_separator<char> > Tokenizer;
+static const char* const kPipeSeparator = "|";
 
 // Utility class used to parse the lineitem tsv file
 class LineItemTsvImporter {
@@ -27,15 +27,14 @@ class LineItemTsvImporter {
   // Fills the row builder with a single line item from the file.
   // It returns 0 if it's done or the order number if it got a line
   int GetNextLine(RowBuilder &rb) {
-    if (!getline(in_,line_)) {
+    if (!getline(in_, line_)) {
       return 0;
     }
     columns_.clear();
     rb.Reset();
-    Tokenizer tokens(line_, kPipeSeparator);
 
     // grab all the columns_ individually
-    columns_.assign(tokens.begin(), tokens.end());
+    columns_ = strings::Split(line_, kPipeSeparator);
 
     int order_number = ConvertToIntAndPopulate(columns_[0], &rb);
     ConvertToIntAndPopulate(columns_[3], &rb);
@@ -46,23 +45,29 @@ class LineItemTsvImporter {
     ConvertDoubleToIntAndPopulate(columns_[6], &rb);
     ConvertDoubleToIntAndPopulate(columns_[7], &rb);
     for (int i = 8; i < 16; i++)  {
-      rb.AddString(Slice(columns_[i]));
+      rb.AddString(Slice(columns_[i].data(), columns_[i].size()));
     }
     return order_number;
   }
 
-  int ConvertToIntAndPopulate(const string &chars, RowBuilder *rb) {
+  int ConvertToIntAndPopulate(const StringPiece &chars, RowBuilder *rb) {
+    // TODO: extra copy here, since we don't have a way to parse StringPiece
+    // into ints.
+    chars.CopyToString(&tmp_);
     int number;
-    bool ok_parse = SimpleAtoi(chars.c_str(), &number);
+    bool ok_parse = SimpleAtoi(tmp_.c_str(), &number);
     CHECK(ok_parse);
     rb->AddUint32(number);
     return number;
   }
 
-  void ConvertDoubleToIntAndPopulate(const string &chars, RowBuilder *rb) {
+  void ConvertDoubleToIntAndPopulate(const StringPiece &chars, RowBuilder *rb) {
+    // TODO: extra copy here, since we don't have a way to parse StringPiece
+    // into ints.
+    chars.CopyToString(&tmp_);
     char *error = NULL;
     errno = 0;
-    const char *cstr = chars.c_str();
+    const char *cstr = tmp_.c_str();
     double number = strtod(cstr, &error);
     CHECK(errno == 0 &&  // overflow/underflow happened
         error != cstr);
@@ -72,8 +77,8 @@ class LineItemTsvImporter {
 
  private:
   std::ifstream in_;
-  vector<string> columns_;
-  string line_;
+  vector<StringPiece> columns_;
+  string line_, tmp_;
 };
 } // namespace kudu
 #endif
