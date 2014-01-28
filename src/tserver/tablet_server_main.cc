cc637a1e75f2932bc98df47fb91459389e152423
Todd Lipcon <todd@cloudera.com>
2014-01-28 13:52:29 -0800

    Rip out tserver hack for demo tablets
    This removes the hack which allowed the tablet server to create
    tablets for demos on startup. All of our clients now support
    looking up tablets dynamically, and we can create the demo tables
    from the command line, so this support is no longer needed.
    
    Change-Id: Ibbf3f2b1c959bb6af3a8fa9aad9fce76a8ab7576
    Reviewed-on: http://gerrit.ent.cloudera.com:8080/1384
    Reviewed-by: Matteo Bertozzi <matteo.bertozzi@cloudera.com>
    Reviewed-by: Jean-Daniel Cryans <jdcryans@cloudera.com>
    Tested-by: jenkins

--- src/tserver/tablet_server_main.cc
+++ src/tserver/tablet_server_main.cc
@@ -6,8 +6,6 @@
 #include <iostream>
 
 #include "common/schema.h"
-#include "gutil/strings/split.h"
-#include "server/fsmanager.h"
 #include "server/metadata.h"
 #include "server/rpc_server.h"
 #include "tablet/tablet.h"
@@ -17,19 +15,14 @@
 #include "tablet/tablet_peer.h"
 #include "tserver/tablet_server.h"
 #include "tserver/ts_tablet_manager.h"
-#include "benchmarks/ycsb-schema.h"
-#include "util/env.h"
 #include "util/logging.h"
-
 
 using kudu::metadata::QuorumPB;
 using kudu::metadata::QuorumPeerPB;
-using kudu::metadata::TabletMetadata;
 using kudu::tablet::Tablet;
 using kudu::tablet::TabletPeer;
 using kudu::tserver::TabletServer;
 
-static const char* const kYCSBTabletId = "ycsb";
 static const char* const kQuorumFlagFormat =
   "Malformed \"tablet_quorum_for_demo\" flag. "
   "Format: name:host0:port0,name:host1:port1,name:host2:port2\n"
@@ -37,8 +30,6 @@ static const char* const kQuorumFlagFormat =
   "Number of hosts may be 2 or 3.";
 
 DEFINE_int32(flush_threshold_mb, 64, "Minimum memrowset size to flush");
-DEFINE_string(tablet_server_tablet_id, kYCSBTabletId,
-              "Which tablet to use: ycsb (default)");
 
 DEFINE_string(tablet_quorum_for_demo, "",
               "The locations of other tablet servers in the quorum, for demo purposes.\n"
@@ -49,67 +40,6 @@ DEFINE_string(tablet_quorum_for_demo, "",
 namespace kudu {
 namespace tserver {
 
-// For demos, keep only a single tablet that can be ycsb
-class TemporaryTabletsForDemos {
- public:
-  explicit TemporaryTabletsForDemos(TabletServer* server, Schema schema,
-                                    const string& tablet_id)
-    : schema_(schema) {
-
-    shared_ptr<TabletPeer> peer;
-    if (server->tablet_manager()->LookupTablet(tablet_id, &peer)) {
-      CHECK(schema_.Equals(peer->tablet()->schema()))
-        << "Bad schema loaded on disk: " <<
-        peer->tablet()->schema().ToString();
-      LOG(INFO) << "Using previously-created tablet";
-    } else {
-
-      BuildQuorumFromFlagIfProvided();
-
-      CHECK_OK(server->tablet_manager()->CreateNewTablet(
-                 tablet_id, tablet_id, "", "", SchemaBuilder(schema_).Build(), quorum_, &peer));
-    }
-    tablet_ = peer->shared_tablet();
-  }
-
-  const shared_ptr<Tablet>& tablet() {
-    return tablet_;
-  }
-
- private:
-
-  void BuildQuorumFromFlagIfProvided() {
-    // If the 'tablet_quorum_for_demo' flag was specified, build the quorum
-    if (FLAGS_tablet_quorum_for_demo != "") {
-
-      vector<string> hostport_pairs;
-      SplitStringUsing(FLAGS_tablet_quorum_for_demo, ",", &hostport_pairs);
-      CHECK(!hostport_pairs.empty() && hostport_pairs.size() >= 2 && hostport_pairs.size() <= 3)
-      << kQuorumFlagFormat << "\nwas: " << FLAGS_tablet_quorum_for_demo;
-
-      int count = 0;
-      BOOST_FOREACH(const string& hostport_triple, hostport_pairs) {
-        vector<string> name_host_and_port;
-        SplitStringUsing(hostport_triple, ":", &name_host_and_port);
-        CHECK(name_host_and_port.size() == 3) << kQuorumFlagFormat;
-        QuorumPeerPB* peer = quorum_.add_peers();
-        peer->set_permanent_uuid(name_host_and_port[0]);
-        HostPortPB* hostport = peer->mutable_last_known_addr();
-        hostport->set_host(name_host_and_port[1]);
-        hostport->set_port(atoi(name_host_and_port[2].c_str()));
-        peer->set_role(count++ == 0 ? QuorumPeerPB::LEADER : QuorumPeerPB::FOLLOWER);
-      }
-      quorum_.set_local(false);
-    }
-  }
-
-  Schema schema_;
-  QuorumPB quorum_;
-
-  shared_ptr<Tablet> tablet_;
-
-  DISALLOW_COPY_AND_ASSIGN(TemporaryTabletsForDemos);
-};
 
 // Pick the next tablet to perform some maintenance on. This simply round-robins
 // through all the tablets in the server. It may return NULL if there are no
@@ -192,21 +122,6 @@ static int TabletServerMain(int argc, char** argv) {
   TabletServer server(opts);
   LOG(INFO) << "Initializing tablet server...";
   CHECK_OK(server.Init());
-
-  // TODO: Twitter does not use the client and the java api for YCSB is in progress
-  //       so they both relies on a fixed tablet-id to be created.
-  //       Remove this stuff once everything is switched to the client create/open api.
-  LOG(INFO) << "Setting up demo tablets...";
-  const string& id = FLAGS_tablet_server_tablet_id;
-  Schema schema;
-  if (id == kYCSBTabletId) {
-    schema = kudu::CreateYCSBSchema();
-  } else {
-    LOG(WARNING) << "Unknown tablet_server_tablet_id: " << id;
-  }
-  if (schema.initialized()) {
-    TemporaryTabletsForDemos demo_setup(&server, schema, id);
-  }
 
   // Temporary hack for demos: start threads which compact/flush the tablet.
   // Eventually this will be part of TabletServer itself, and take care of deciding
