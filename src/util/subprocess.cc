6b9e50112eb67216fab07c848f3a033aaadfab7c
Todd Lipcon <todd@cloudera.com>
2014-02-05 22:21:46 -0800

    Add a simple Subprocess class
    This is the beginnings of a subprocess class which wraps fork/exec and the
    file-descriptor handling necessary to read to/from the subprocess. This will be
    used by the external minicluster.
    
    Change-Id: I5441bef84236d67f32bbef1a0f7d214cc58cfc92
    Reviewed-on: http://gerrit.ent.cloudera.com:8080/1474
    Tested-by: jenkins
    Reviewed-by: Colin McCabe <cmccabe@cloudera.com>

---
+++ src/util/subprocess.cc
@@ -0,0 +1,197 @@
+// Copyright (c) 2014, Cloudera, inc.
+
+#include "util/subprocess.h"
+
+#include <boost/foreach.hpp>
+#include <dirent.h>
+#include <glog/logging.h>
+#include <fcntl.h>
+#include <string>
+#include <vector>
+#include <signal.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#include "gutil/once.h"
+#include "gutil/port.h"
+#include "gutil/strings/join.h"
+#include "gutil/strings/numbers.h"
+#include "util/debug-util.h"
+#include "util/errno.h"
+
+using std::string;
+using std::vector;
+
+namespace kudu {
+
+namespace {
+void DisableSigPipe() {
+  struct sigaction act;
+
+  act.sa_handler = SIG_IGN;
+  sigemptyset(&act.sa_mask);
+  act.sa_flags = 0;
+  PCHECK(sigaction(SIGPIPE, &act, NULL) == 0);
+}
+
+void EnsureSigPipeDisabled() {
+  static GoogleOnceType once = GOOGLE_ONCE_INIT;
+  GoogleOnceInit(&once, &DisableSigPipe);
+}
+
+// Close all open file descriptors other than stdin, stderr, stdout.
+void CloseNonStandardFDs() {
+#ifndef __linux__
+#error This function is Linux-specific.
+#endif
+  // This is implemented by iterating over the open file descriptors
+  // rather than using sysconf(SC_OPEN_MAX) -- the latter is error prone
+  // since it may not represent the highest open fd if the fd soft limit
+  // has changed since the process started. This should also be faster
+  // since iterating over all possible fds is likely to cause 64k+ syscalls
+  // in typical configurations.
+  //
+  // Note also that this doesn't use any of the Env utility functions, to
+  // make it as lean and mean as possible -- this runs in the subprocess
+  // after a fork, so there's some possibility that various global locks
+  // inside malloc() might be held, so allocating memory is a no-no.
+  errno = 0;
+  DIR* d = opendir("/proc/self/fd");
+  int dir_fd = dirfd(d);
+  PCHECK(d != NULL);
+
+  struct dirent64 *ent;
+  while ((ent = readdir64(d)) != NULL) {
+    uint32_t fd;
+    if (!safe_strtou32(ent->d_name, &fd)) continue;
+    if (fd >= 3 && fd != dir_fd) {
+      close(fd);
+    }
+  }
+
+  PCHECK(closedir(d) == 0);
+}
+
+} // anonymous namespace
+
+Subprocess::Subprocess(const string& exec_path,
+                       const vector<string>& argv)
+  : exec_path_(exec_path),
+    argv_(argv),
+    started_(false),
+    child_pid_(-1),
+    to_child_stdin_(-1),
+    from_child_stdout_(-1) {
+}
+
+Subprocess::~Subprocess() {
+  if (started_) {
+    LOG(WARNING) << "Child process " << child_pid_
+                 << "(" << JoinStrings(argv_, " ") << ") "
+                 << " was orphaned. Sending SIGKILL...";
+    WARN_NOT_OK(Kill(SIGKILL), "Failed to send SIGKILL");
+    int junk = 0;
+    WARN_NOT_OK(Wait(&junk), "Failed to Wait()");
+  }
+
+  if (to_child_stdin_ >= 0) {
+    close(to_child_stdin_);
+  }
+  if (from_child_stdout_ >= 0) {
+    close(from_child_stdout_);
+  }
+}
+
+Status Subprocess::Start() {
+  EnsureSigPipeDisabled();
+
+  if (argv_.size() < 1) {
+    return Status::InvalidArgument("argv must have at least one elem");
+  }
+
+  vector<char*> argv_ptrs;
+  BOOST_FOREACH(const string& arg, argv_) {
+    argv_ptrs.push_back(const_cast<char*>(arg.c_str()));
+  }
+  argv_ptrs.push_back(NULL);
+
+  // Pipe from caller process to child's stdin
+  // [0] = stdin for child, [1] = how parent writes to it
+  int child_stdin[2];
+  PCHECK(pipe2(child_stdin, O_CLOEXEC) == 0);
+
+  // Pipe from child's stdout back to caller process
+  int child_stdout[2];
+  // [0] = how parent reads from child's stdout, [1] = how child writes to it
+  PCHECK(pipe2(child_stdout, O_CLOEXEC) == 0);
+
+  int ret = fork();
+  if (ret == -1) {
+    return Status::RuntimeError("Unable to fork", ErrnoToString(errno));
+  }
+  if (ret == 0) {
+    // We are the child
+    PCHECK(dup2(child_stdin[0], STDIN_FILENO) == STDIN_FILENO);
+    PCHECK(dup2(child_stdout[1], STDOUT_FILENO) == STDOUT_FILENO);
+
+    CloseNonStandardFDs();
+
+    execv(exec_path_.c_str(), &argv_ptrs[0]);
+    PLOG(WARNING) << "Couldn't exec";
+    _exit(errno);
+  } else {
+    // We are the parent
+    child_pid_ = ret;
+
+    close(child_stdin[0]);
+    close(child_stdout[1]);
+    to_child_stdin_ = child_stdin[1];
+    from_child_stdout_ = child_stdout[0];
+  }
+
+  started_ = true;
+  return Status::OK();
+}
+
+Status Subprocess::Wait(int* ret) {
+  CHECK(started_);
+  int rc = waitpid(child_pid_, ret, 0);
+  if (rc == -1) {
+    return Status::RuntimeError("Unable to wait on child",
+                                ErrnoToString(errno),
+                                errno);
+  }
+  CHECK_EQ(rc, child_pid_);
+  child_pid_ = -1;
+  started_ = false;
+  return Status::OK();
+}
+
+Status Subprocess::Kill(int signal) {
+  CHECK(started_);
+  if (kill(child_pid_, signal) != 0) {
+    return Status::RuntimeError("Unable to kill",
+                                ErrnoToString(errno),
+                                errno);
+  }
+  return Status::OK();
+}
+
+int Subprocess::ReleaseChildStdinFd() {
+  CHECK(started_);
+  CHECK_GE(to_child_stdin_, 0);
+  int ret = to_child_stdin_;
+  to_child_stdin_ = -1;
+  return ret;
+}
+
+int Subprocess::ReleaseChildStdoutFd() {
+  CHECK(started_);
+  CHECK_GE(from_child_stdout_, 0);
+  int ret = from_child_stdout_;
+  from_child_stdout_ = -1;
+  return ret;
+}
+
+} // namespace kudu
---
+++ src/util/subprocess.h
@@ -0,0 +1,77 @@
+// Copyright (c) 2014, Cloudera, inc.
+#ifndef KUDU_UTIL_SUBPROCESS_H
+#define KUDU_UTIL_SUBPROCESS_H
+
+#include <glog/logging.h>
+#include <string>
+#include <vector>
+
+#include "gutil/macros.h"
+#include "util/status.h"
+
+namespace kudu {
+
+// Wrapper around a spawned subprocess.
+//
+// This takes care of creating pipes to/from the subprocess and offers
+// basic functionality to wait on it or send signals.
+//
+// Note that, when the Subprocess object is destructed, the child process
+// will be forcibly SIGKILLed to avoid orphaning processes.
+class Subprocess {
+ public:
+  Subprocess(const std::string& exec_path,
+             const std::vector<std::string>& argv);
+  ~Subprocess();
+
+  // Start the subprocess.
+  //
+  // Thie returns a bad Status if the fork() fails. However,
+  // note that if the executable path was incorrect such that
+  // exec() fails, this will still return Status::OK. You must
+  // use Wait() to check for failure.
+  Status Start();
+
+  // Wait for the subprocess to exit. The return value is the same as
+  // that of the waitpid() syscall.
+  Status Wait(int* ret);
+
+  // Send a signal to the subprocess.
+  // Note that this does not reap the process -- you must still Wait()
+  // in order to reap it.
+  Status Kill(int signal);
+
+  // Return the pipe fd to the child's standard input.
+  int to_child_stdin_fd() const {
+    CHECK(started_);
+    return to_child_stdin_;
+  }
+
+  // Release control of the file descriptor for the child's stdin.
+  // Writes to this FD show up on stdin in the subprocess.
+  int ReleaseChildStdinFd();
+
+  // Return the pipe fd from the child's standard output.
+  int from_child_stdout_fd() const {
+    CHECK(started_);
+    return from_child_stdout_;
+  }
+
+  // Release control of the file descriptor for the child's stdout.
+  // Writes to this FD show up on stdin in the subprocess.
+  int ReleaseChildStdoutFd();
+
+ private:
+  std::string exec_path_;
+  std::vector<std::string> argv_;
+
+  bool started_;
+  int child_pid_;
+  int to_child_stdin_;
+  int from_child_stdout_;
+
+  DISALLOW_COPY_AND_ASSIGN(Subprocess);
+};
+
+} // namespace kudu
+#endif /* KUDU_UTIL_SUBPROCESS_H */
