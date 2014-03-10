8e8b532e7dde896a7bdfe9281ad9b3087b339ee1
Jean-Daniel Cryans <jdcryans@cloudera.com>
2014-03-10 16:04:16 -0700

    Script to build kudu-master & kudu-tablet_server for the 5 nodes cluster
    Change-Id: If2f15e1ae2fe9d039f162065197e956c32ddb145
    Reviewed-on: http://gerrit.ent.cloudera.com:8080/1968
    Tested-by: jenkins
    Reviewed-by: Todd Lipcon <todd@cloudera.com>

---
+++ build-support/jenkins/build-master-ts.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Copyright (c) 2014, Cloudera, inc.
+#
+# This script is invoked from the Jenkins builds to build the kudu-master
+# and kudu-tablet_server binaries with the release build type.
+#
+
+set -e
+set -x
+# We pipe our build output to a log file with tee.
+# This bash setting ensures that the script exits if the build fails.
+set -o pipefail
+ulimit -c unlimited
+
+ROOT=$(readlink -f $(dirname "$BASH_SOURCE")/../..)
+cd $ROOT
+
+rm -f build.log
+
+thirdparty/build-if-necessary.sh
+
+export PATH=$(pwd)/thirdparty/installed/bin:$PATH
+
+rm -rf CMakeCache.txt CMakeFiles src/*/CMakeFiles
+
+cmake . -DCMAKE_BUILD_TYPE=release
+make clean
+
+NUM_PROCS=$(cat /proc/cpuinfo | grep processor | wc -l)
+
+make -j$NUM_PROCS kudu-master 2>&1 | tee build.log
+make -j$NUM_PROCS kudu-tablet_server 2>&1 | tee -a build.log
