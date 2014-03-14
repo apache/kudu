e7bf46b391ff0069d7f19f5e015abde595e5b624
Mike Percy <mpercy@cloudera.com>
2014-03-14 04:49:22 -0700

    Add static thread local implementations
    Two thread local implementations, one for block-scoped thread locals
    (inside functions) and one for static member variables of classes.
    
    The implementation / interface in both cases is based on macros.
    
    For the block-scoped implementation, usage is similar to a C++11 static
    thread_local. The BLOCK_STATIC_THREAD_LOCAL macro defines and
    initializes a thread local pointer whose implementation is allocated on
    the heap. The constructor is lazily called whenever a new thread enters
    the block, and the object's destructor is called when the thread's
    Runnable completes or the thread exits.
    
    For the static class member implementation, macros must be called to
    declare, define, and initialize the thread local. The initialization
    macro, INIT_STATIC_THREAD_LOCAL, must be called by any thread that may
    access the thread local object before that access occurs.
    
    Also added unit tests for each implementation.
    
    Change-Id: Ia674ab7826d97b90ce76e226e1f47c3fbd8c5004
    Reviewed-on: http://gerrit.ent.cloudera.com:8080/1908
    Tested-by: jenkins
    Reviewed-by: Todd Lipcon <todd@cloudera.com>

---
+++ src/util/threadlocal.cc
@@ -0,0 +1,54 @@
+// Copyright 2014 Cloudera, Inc.
+#include "util/threadlocal.h"
+
+#include <pthread.h>
+
+#include <glog/logging.h>
+
+#include "gutil/once.h"
+#include "util/errno.h"
+
+namespace kudu {
+namespace threadlocal {
+namespace internal {
+
+// One key used by the entire process to attach destructors on thread exit.
+static pthread_key_t destructors_key;
+
+// The above key must only be initialized once per process.
+static GoogleOnceType once = GOOGLE_ONCE_INIT;
+
+// Call all the destructors associated with all THREAD_LOCAL instances in this
+// thread.
+static void InvokeDestructors(void* t) {
+  PerThreadDestructorList* d = reinterpret_cast<PerThreadDestructorList*>(t);
+  while (d != NULL) {
+    d->destructor(d->arg);
+    d = d->next;
+  }
+}
+
+// This key must be initialized only once.
+static void CreateKey() {
+  int ret = pthread_key_create(&destructors_key, &InvokeDestructors);
+  // Linux supports up to 1024 keys, we will use only one for all thread locals.
+  CHECK_EQ(0, ret) << "pthread_key_create() failed, cannot add destructor to thread: "
+      << "error " << ret << ": " << ErrnoToString(ret);
+}
+
+// Adds a destructor to the list.
+void AddDestructor(PerThreadDestructorList* p) {
+  GoogleOnceInit(&once, &CreateKey);
+
+  // Returns NULL if nothing is set yet.
+  p->next = reinterpret_cast<PerThreadDestructorList*>(pthread_getspecific(destructors_key));
+  int ret = pthread_setspecific(destructors_key, p);
+  // The only time this check should fail is if we are out of memory, or if
+  // somehow key creation failed, which should be caught by the above CHECK.
+  CHECK_EQ(0, ret) << "pthread_setspecific() failed, cannot update destructor list: "
+      << "error " << ret << ": " << ErrnoToString(ret);
+}
+
+} // namespace internal
+} // namespace threadlocal
+} // namespace kudu
