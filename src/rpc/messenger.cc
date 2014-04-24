apache/kudu

commit da43dd1e814d320e5b418a99c78ad6751e2f4422

Refactor ServicePool to contain the service queue
This change allows the ServicePool to shut down while allowing Messenger
to continue to relay client messages as long as is needed.

Also, don't allow BlockingQueue to be destroyed nonempty.
This burns us in cases where we have bare pointers on the queue, which
implies ownership of those objects by the queue. Resources can leak.

Change-Id: I4f1e9a564d74bf51d13bccdf315b413479bb101b
Reviewed-on: http://gerrit.ent.cloudera.com:8080/2234
Tested-by: jenkins
Reviewed-by: Todd Lipcon <todd@cloudera.com>

src/rpc/messenger.cc
@@ -15,11 +15,13 @@
 #include <string>
 
 #include "gutil/gscoped_ptr.h"
+#include "gutil/stl_util.h"
 #include "rpc/acceptor_pool.h"
 #include "rpc/connection.h"
 #include "rpc/constants.h"
 #include "rpc/reactor.h"
 #include "rpc/rpc_header.pb.h"
+#include "rpc/rpc_service.h"
 #include "rpc/sasl_common.h"
 #include "rpc/transfer.h"
 #include "util/errno.h"
@@ -46,8 +48,7 @@ MessengerBuilder::MessengerBuilder(const std::string &name)
     connection_keepalive_time_(MonoDelta::FromSeconds(10)),
     num_reactors_(4),
     num_negotiation_threads_(4),
-    coarse_timer_granularity_(MonoDelta::FromMilliseconds(100)),
-    service_queue_length_(50) {
+    coarse_timer_granularity_(MonoDelta::FromMilliseconds(100)) {
 }
 
 MessengerBuilder& MessengerBuilder::set_connection_keepalive_time(const MonoDelta &keepalive) {
@@ -67,12 +68,6 @@ MessengerBuilder& MessengerBuilder::set_negotiation_threads(int num_negotiation_
 
 MessengerBuilder& MessengerBuilder::set_coarse_timer_granularity(const MonoDelta &granularity) {
   coarse_timer_granularity_ = granularity;
-  return *this;
-}
-
-MessengerBuilder &MessengerBuilder::set_service_queue_length(
-                                int service_queue_length) {
-  service_queue_length_ = service_queue_length;
   return *this;
 }
 
@@ -111,31 +106,29 @@ void Messenger::AllExternalReferencesDropped() {
 }
 
 void Messenger::Shutdown() {
-  {
-    boost::lock_guard<boost::mutex> guard(lock_);
-    if (closing_) {
-      return;
-    }
-    VLOG(1) << "shutting down messenger " << name_;
-    closing_ = true;
+  boost::lock_guard<boost::mutex> guard(lock_);
+  if (closing_) {
+    return;
   }
-  service_queue_.Shutdown();
+  VLOG(1) << "shutting down messenger " << name_;
+  closing_ = true;
 
-  // Drain any remaining calls that haven't been responded to.
-  InboundCall *call = NULL;
-  while (service_queue_.BlockingGet(&call)) {
-    call->RespondFailure(ErrorStatusPB::FATAL_SERVER_SHUTTING_DOWN,
-                         Status::ServiceUnavailable("Server shutting down"));
+  DCHECK(!rpc_service_) << "Unregister RPC services before shutting down Messenger";
+  rpc_service_ = NULL;
+
+  BOOST_FOREACH(const shared_ptr<AcceptorPool>& acceptor_pool, acceptor_pools_) {
+    acceptor_pool->Shutdown();
   }
+  acceptor_pools_.clear();
 
+  // Need to shut down negotiation executor before the reactors, since the
+  // reactors close the Connection sockets, and may race against the negotiation
+  // thread's blocking reads & writes.
+  negotiation_executor_->Shutdown();
 
-  BOOST_FOREACH(shared_ptr<AcceptorPool> &pool, acceptor_pools_) {
-    pool->Shutdown();
-  }
   BOOST_FOREACH(Reactor* reactor, reactors_) {
     reactor->Shutdown();
   }
-  negotiation_executor_->Shutdown();
 }
 
 Status Messenger::AddAcceptorPool(const Sockaddr &accept_addr,
@@ -154,35 +147,46 @@ Status Messenger::AddAcceptorPool(const Sockaddr &accept_addr,
   return Status::OK();
 }
 
+// Register a new RpcService to handle inbound requests.
+Status Messenger::RegisterService(const string& service_name,
+                                  const scoped_refptr<RpcService>& service) {
+  DCHECK(service);
+  boost::lock_guard<boost::mutex> guard(lock_);
+  // TODO: Key services on service name.
+  rpc_service_ = service;
+  return Status::OK();
+}
+
+// Unregister an RpcService.
+Status Messenger::UnregisterService(const string& service_name) {
+  boost::lock_guard<boost::mutex> guard(lock_);
+  if (!rpc_service_) {
+    return Status::ServiceUnavailable("Service is not registered");
+  } else {
+    rpc_service_ = NULL;
+  }
+  return Status::OK();
+}
+
 void Messenger::QueueOutboundCall(const shared_ptr<OutboundCall> &call) {
   Reactor *reactor = RemoteToReactor(call->conn_id().remote());
   reactor->QueueOutboundCall(call);
 }
 
 void Messenger::QueueInboundCall(gscoped_ptr<InboundCall> call) {
-  InboundCall *c = call.release();
+  TRACE_TO(call->trace(), "Inserting onto call queue");
 
-  TRACE_TO(c->trace(), "Inserting onto call queue");
-  // Queue message on service queue
-  QueueStatus s = service_queue_.Put(c);
-  if (PREDICT_TRUE(s == QUEUE_SUCCESS)) {
-    // NB: do not do anything with 'c' after it is successfully queued --
-    // a service thread may have already dequeued it, processed it, and
-    // responded by this point, in which case the pointer would be invalid.
+  boost::lock_guard<boost::mutex> guard(lock_);
+  if (PREDICT_FALSE(!rpc_service_)) {
+    Status s = Status::ServiceUnavailable("RPC Service is not registered",
+                                          call->ToString());
+    LOG(INFO) << s.ToString();
+    call.release()->RespondFailure(ErrorStatusPB::ERROR_NO_SUCH_SERVICE, s);
     return;
   }
 
-  if (s == QUEUE_FULL) {
-    c->RespondFailure(
-      ErrorStatusPB::ERROR_SERVER_TOO_BUSY,
-      Status::ServiceUnavailable(StringPrintf(
-                             "The service queue is full; it "
-                             "has %zd items. Transfer dropped because of backpressure.",
-                             service_queue_.max_elements())));
-  } else if (s == QUEUE_SHUTDOWN) {
-    c->RespondFailure(ErrorStatusPB::FATAL_SERVER_SHUTTING_DOWN,
-                      Status::ServiceUnavailable("Service is shutting down"));
-  }
+  // The RpcService will respond to the client on success or failure.
+  WARN_NOT_OK(rpc_service_->QueueInboundCall(call.Pass()), "Unable to handle RPC call");
 }
 
 void Messenger::RegisterInboundSocket(Socket *new_socket, const Sockaddr &remote) {
@@ -191,9 +195,8 @@ void Messenger::RegisterInboundSocket(Socket *new_socket, const Sockaddr &remote
 }
 
 Messenger::Messenger(const MessengerBuilder &bld)
-  : closing_(false),
-    service_queue_(bld.service_queue_length_),
-    name_(bld.name_),
+  : name_(bld.name_),
+    closing_(false),
     retain_self_(this) {
   if (bld.metric_ctx_) {
     metric_ctx_.reset(new MetricContext(*bld.metric_ctx_));
@@ -206,11 +209,9 @@ Messenger::Messenger(const MessengerBuilder &bld)
 }
 
 Messenger::~Messenger() {
+  boost::lock_guard<boost::mutex> guard(lock_);
   CHECK(closing_) << "Should have already shut down";
-  BOOST_FOREACH(Reactor* r, reactors_) {
-    delete r;
-  }
-  reactors_.clear();
+  STLDeleteElements(&reactors_);
 }
 
 Reactor* Messenger::RemoteToReactor(const Sockaddr &remote) {

src/rpc/messenger.h
@@ -2,7 +2,7 @@
 #ifndef KUDU_RPC_MESSENGER_H
 #define KUDU_RPC_MESSENGER_H
 
-#include <boost/thread/locks.hpp>
+#include <boost/thread/thread.hpp>
 #include <gtest/gtest.h>
 #include <gutil/gscoped_ptr.h>
 #include <stdint.h>
@@ -12,8 +12,8 @@
 #include <string>
 #include <vector>
 
+#include "gutil/ref_counted.h"
 #include "rpc/response_callback.h"
-#include "util/blocking_queue.h"
 #include "util/metrics.h"
 #include "util/monotime.h"
 #include "util/net/sockaddr.h"
@@ -28,12 +28,12 @@ class TaskExecutor;
 namespace rpc {
 
 class AcceptorPool;
-class Connection;
 class InboundCall;
 class Messenger;
 class OutboundCall;
 class Reactor;
 class ReactorThread;
+class RpcService;
 
 struct AcceptorPoolInfo {
  public:
@@ -71,10 +71,6 @@ class MessengerBuilder {
   // Set the granularity with which connections are checked for keepalive.
   MessengerBuilder &set_coarse_timer_granularity(const MonoDelta &granularity);
 
-  // Set the maximum number of inbound messages that can be queued before we
-  // start rejecting them.
-  MessengerBuilder &set_service_queue_length(int service_queue_length);
-
   // Set metric context for use by RPC systems.
   MessengerBuilder &set_metric_context(const MetricContext& metric_ctx);
 
@@ -87,7 +83,6 @@ class MessengerBuilder {
   int num_reactors_;
   int num_negotiation_threads_;
   MonoDelta coarse_timer_granularity_;
-  size_t service_queue_length_;
   gscoped_ptr<MetricContext> metric_ctx_;
 };
 
@@ -126,6 +121,13 @@ class Messenger {
   Status AddAcceptorPool(const Sockaddr &accept_addr, int num_threads,
                          std::tr1::shared_ptr<AcceptorPool>* pool);
 
+  // Register a new RpcService to handle inbound requests.
+  Status RegisterService(const std::string& service_name,
+                         const scoped_refptr<RpcService>& service);
+
+  // Unregister currently-registered RpcService.
+  Status UnregisterService(const std::string& service_name);
+
   // Queue a call for transmission. This will pick the appropriate reactor,
   // and enqueue a task on that reactor to assign and send the call.
   void QueueOutboundCall(const std::tr1::shared_ptr<OutboundCall> &call);
@@ -137,10 +139,6 @@ class Messenger {
   void RegisterInboundSocket(Socket *new_socket, const Sockaddr &remote);
 
   TaskExecutor* negotiation_executor() const { return negotiation_executor_.get(); }
-
-  BlockingQueue<InboundCall*>& service_queue() {
-    return service_queue_;
-  }
 
   std::string name() const {
     return name_;
@@ -167,20 +165,22 @@ class Messenger {
   // any references. See 'retain_self_' for more info.
   void AllExternalReferencesDropped();
 
-  // protects closing_, acceptor_pools_, next_call_id_, cur_time_
+  const std::string name_;
+
+  // protects closing_, acceptor_pools_, rpc_service_.
   mutable boost::mutex lock_;
 
   bool closing_;
-
-  kudu::BlockingQueue<InboundCall*> service_queue_;
-
-  const std::string name_;
 
   // Pools which are listening on behalf of this messenger.
   // Note that the user may have called Shutdown() on one of these
   // pools, so even though we retain the reference, it may no longer
   // be listening.
   acceptor_vec_t acceptor_pools_;
+
+  // RPC service that handles inbound requests.
+  // TODO: Allow multiple services to be registered.
+  scoped_refptr<RpcService> rpc_service_;
 
   std::vector<Reactor*> reactors_;
 

src/client/client-test.cc
@@ -238,7 +238,11 @@ TEST_F(ClientTest, TestMasterDown) {
   scoped_refptr<KuduTable> t;
   Status s = client_->OpenTable("other-tablet", &t);
   ASSERT_TRUE(s.IsNetworkError());
-  ASSERT_STR_CONTAINS(s.ToString(), "Connection refused");
+  const string& msg = s.ToString();
+  ASSERT_TRUE(msg.find("Connection refused") != string::npos ||
+              msg.find("EOF") != string::npos ||
+              msg.find("Connection reset") != string::npos)
+      << "Unexpected client error message: " << msg;
 }
 
 TEST_F(ClientTest, TestScan) {

src/rpc/connection.cc
@@ -134,6 +134,7 @@ void Connection::Shutdown(const Status &status) {
   read_io_.stop();
   write_io_.stop();
   is_epoll_registered_ = false;
+  WARN_NOT_OK(socket_.Close(), "Error closing socket");
 }
 
 void Connection::QueueOutbound(gscoped_ptr<OutboundTransfer> transfer) {

src/rpc/mt-rpc-test.cc
@@ -8,6 +8,7 @@
 #include <gtest/gtest.h>
 
 #include "gutil/stl_util.h"
+#include "gutil/strings/substitute.h"
 #include "util/countdown_latch.h"
 #include "util/metrics.h"
 #include "util/test_util.h"
@@ -16,6 +17,7 @@
 METRIC_DECLARE_counter(rpc_connections_accepted);
 
 using std::string;
+using strings::Substitute;
 
 namespace kudu {
 namespace rpc {
@@ -23,18 +25,21 @@ namespace rpc {
 class MultiThreadedRpcTest : public RpcTestBase {
  public:
   // Make a single RPC call.
-  void SingleCall(Sockaddr server_addr, const char* method_name,
+  void SingleCall(Sockaddr server_addr, const char* method_name, const string& thread_name,
                   Status* result, CountDownLatch* latch) {
+    SetThreadName(thread_name);
     LOG(INFO) << "Connecting to " << server_addr.ToString();
-    shared_ptr<Messenger> client_messenger(CreateMessenger("Client"));
+    shared_ptr<Messenger> client_messenger(CreateMessenger("ClientSC"));
     Proxy p(client_messenger, server_addr, GenericCalculatorService::static_service_name());
     *result = DoTestSyncCall(p, method_name);
     latch->CountDown();
   }
 
   // Make RPC calls until we see a failure.
-  void HammerServer(Sockaddr server_addr, const char* method_name, Status* last_result) {
-    shared_ptr<Messenger> client_messenger(CreateMessenger("Client"));
+  void HammerServer(Sockaddr server_addr, const char* method_name, const string& thread_name,
+                    Status* last_result) {
+    SetThreadName(thread_name);
+    shared_ptr<Messenger> client_messenger(CreateMessenger("ClientHS"));
     HammerServerWithMessenger(server_addr, method_name, last_result, client_messenger);
   }
 
@@ -74,26 +79,25 @@ TEST_F(MultiThreadedRpcTest, TestShutdownDuringService) {
   Sockaddr server_addr;
   StartTestServer(&server_addr);
 
-  gscoped_ptr<boost::thread> thread1, thread2, thread3, thread4;
-  Status status1, status2, status3, status4;
-  ASSERT_STATUS_OK(StartThread(boost::bind(&MultiThreadedRpcTest::HammerServer, this, server_addr,
-        GenericCalculatorService::kAddMethodName, &status1), &thread1));
-  ASSERT_STATUS_OK(StartThread(boost::bind(&MultiThreadedRpcTest::HammerServer, this, server_addr,
-        GenericCalculatorService::kAddMethodName, &status2), &thread2));
-  ASSERT_STATUS_OK(StartThread(boost::bind(&MultiThreadedRpcTest::HammerServer, this, server_addr,
-        GenericCalculatorService::kAddMethodName, &status3), &thread3));
-  ASSERT_STATUS_OK(StartThread(boost::bind(&MultiThreadedRpcTest::HammerServer, this, server_addr,
-        GenericCalculatorService::kAddMethodName, &status4), &thread4));
+  const int kNumThreads = 4;
+  gscoped_ptr<boost::thread> threads[kNumThreads];
+  Status statuses[kNumThreads];
+  for (int i = 0; i < kNumThreads; i++) {
+    ASSERT_STATUS_OK(StartThread(boost::bind(&MultiThreadedRpcTest::HammerServer, this,
+        server_addr, GenericCalculatorService::kAddMethodName,
+        Substitute("client-thread-$0", i), &statuses[i]), &threads[i]));
+  }
 
   usleep(50000); // 50ms
 
   // Shut down server.
+  ASSERT_STATUS_OK(server_messenger_->UnregisterService(service_name_));
+  service_pool_->Shutdown();
   server_messenger_->Shutdown();
 
-  AssertShutdown(thread1.get(), "thread1", &status1);
-  AssertShutdown(thread2.get(), "thread2", &status2);
-  AssertShutdown(thread3.get(), "thread3", &status3);
-  AssertShutdown(thread4.get(), "thread4", &status4);
+  for (int i = 0; i < kNumThreads; i++) {
+    AssertShutdown(threads[i].get(), "thread1", &statuses[i]);
+  }
 }
 
 // Test shutting down the client messenger exactly as a thread is about to start
@@ -134,10 +138,12 @@ TEST_F(MultiThreadedRpcTest, TestShutdownClientWhileCallsPending) {
 // This bogus service pool leaves the service queue full.
 class BogusServicePool : public ServicePool {
  public:
-  BogusServicePool(const shared_ptr<Messenger>& messenger, gscoped_ptr<ServiceIf> service)
-    : ServicePool(messenger, service.Pass()) {
+  BogusServicePool(gscoped_ptr<ServiceIf> service,
+                   const MetricContext& metric_ctx,
+                   size_t service_queue_length)
+    : ServicePool(service.Pass(), metric_ctx, service_queue_length) {
   }
-  virtual Status Init(int num_threads) {
+  virtual Status Init(int num_threads) OVERRIDE {
     // Do nothing
     return Status::OK();
   }
@@ -158,19 +164,24 @@ void IncrementBackpressureOrShutdown(const Status* status, int* backpressure, in
 
 // Test that we get a Service Unavailable error when we max out the incoming RPC service queue.
 TEST_F(MultiThreadedRpcTest, TestBlowOutServiceQueue) {
+  const size_t kMaxConcurrency = 2;
+
   MessengerBuilder bld("messenger1");
-  bld.set_num_reactors(2);
-  bld.set_service_queue_length(2);
+  bld.set_num_reactors(kMaxConcurrency);
   bld.set_metric_context(metric_ctx_);
   CHECK_OK(bld.Build(&server_messenger_));
 
   shared_ptr<AcceptorPool> pool;
-  ASSERT_STATUS_OK(server_messenger_->AddAcceptorPool(Sockaddr(), 2, &pool));
+  ASSERT_STATUS_OK(server_messenger_->AddAcceptorPool(Sockaddr(), kMaxConcurrency, &pool));
   Sockaddr server_addr = pool->bind_address();
 
-  gscoped_ptr<ServiceIf> impl(new GenericCalculatorService());
-  worker_pool_.reset(new BogusServicePool(server_messenger_, impl.Pass()));
-  ASSERT_STATUS_OK(worker_pool_->Init(n_worker_threads_));
+  gscoped_ptr<ServiceIf> service(new GenericCalculatorService());
+  service_name_ = service->service_name();
+  service_pool_ = new BogusServicePool(service.Pass(),
+                                      *server_messenger_->metric_context(),
+                                      kMaxConcurrency);
+  ASSERT_STATUS_OK(service_pool_->Init(n_worker_threads_));
+  server_messenger_->RegisterService(service_name_, service_pool_);
 
   gscoped_ptr<boost::thread> threads[3];
   Status status[3];
@@ -179,7 +190,9 @@ TEST_F(MultiThreadedRpcTest, TestBlowOutServiceQueue) {
     ASSERT_STATUS_OK(StartThread(
                        boost::bind(
                          &MultiThreadedRpcTest::SingleCall, this, server_addr,
-                         GenericCalculatorService::kAddMethodName, &status[i], &latch),
+                         GenericCalculatorService::kAddMethodName,
+                         Substitute("client thread $0", i),
+                         &status[i], &latch),
                        &threads[i]));;
   }
 
@@ -188,10 +201,13 @@ TEST_F(MultiThreadedRpcTest, TestBlowOutServiceQueue) {
   latch.Wait();
 
   // The rest would time out after 10 sec, but we help them along.
+  ASSERT_STATUS_OK(server_messenger_->UnregisterService(service_name_));
+  service_pool_->Shutdown();
   server_messenger_->Shutdown();
 
   for (int i = 0; i < 3; i++) {
-    ASSERT_STATUS_OK(ThreadJoiner(threads[i].get(), "thread1").warn_every_ms(500).Join());
+    ASSERT_STATUS_OK(ThreadJoiner(threads[i].get(), Substitute("client thread $0", i))
+                     .warn_every_ms(500).Join());
   }
 
   // Verify that one error was due to backpressure.
@@ -245,6 +261,8 @@ TEST_F(MultiThreadedRpcTest, TestShutdownWithIncomingConnections) {
   }
 
   // Shutdown while there are still new connections appearing.
+  ASSERT_STATUS_OK(server_messenger_->UnregisterService(service_name_));
+  service_pool_->Shutdown();
   server_messenger_->Shutdown();
 
   BOOST_FOREACH(boost::thread* t, threads) {

src/rpc/reactor.cc
@@ -90,12 +90,15 @@ void ReactorThread::ShutdownInternal() {
   for (conn_map_t::iterator c = client_conns_.begin();
        c != client_conns_.end(); c = client_conns_.begin()) {
     const shared_ptr<Connection> &conn = (*c).second;
+    VLOG(1) << name() << ": shutting down " << conn->ToString();
     conn->Shutdown(ShutdownError());
     client_conns_.erase(c);
   }
 
   // Tear down any inbound TCP connections.
+  VLOG(1) << name() << ": tearing down inbound TCP connections...";
   BOOST_FOREACH(const shared_ptr<Connection> &conn, server_conns_) {
+    VLOG(1) << name() << ": shutting down " << conn->ToString();
     conn->Shutdown(ShutdownError());
   }
   server_conns_.clear();

src/rpc/rpc-test-base.h
@@ -196,6 +196,10 @@ class RpcTestBase : public KuduTest {
   }
 
   virtual void TearDown() {
+    if (service_pool_) {
+      server_messenger_->UnregisterService(service_name_);
+      service_pool_->Shutdown();
+    }
     if (server_messenger_) {
       server_messenger_->Shutdown();
     }
@@ -277,14 +281,18 @@ class RpcTestBase : public KuduTest {
     ASSERT_STATUS_OK(server_messenger_->AddAcceptorPool(Sockaddr(), 2, &pool));
     *server_addr = pool->bind_address();
 
-    gscoped_ptr<ServiceIf> impl(new ServiceClass(metric_ctx_));
-    worker_pool_.reset(new ServicePool(server_messenger_, impl.Pass()));
-    ASSERT_STATUS_OK(worker_pool_->Init(n_worker_threads_));
+    gscoped_ptr<ServiceIf> service(new ServiceClass(metric_ctx_));
+    service_name_ = service->service_name();
+    const MetricContext& metric_ctx = *server_messenger_->metric_context();
+    service_pool_ = new ServicePool(service.Pass(), metric_ctx, 50);
+    server_messenger_->RegisterService(service_name_, service_pool_);
+    ASSERT_STATUS_OK(service_pool_->Init(n_worker_threads_));
   }
 
  protected:
+  string service_name_;
   shared_ptr<Messenger> server_messenger_;
-  gscoped_ptr<ServicePool> worker_pool_;
+  scoped_refptr<ServicePool> service_pool_;
   int n_worker_threads_;
   int n_server_reactor_threads_;
   int keepalive_time_ms_;

src/rpc/rpc_service.h
@@ -1,0 +1,26 @@
+// Copyright (c) 2014 Cloudera, Inc.
+#ifndef KUDU_RPC_SERVICE_H_
+#define KUDU_RPC_SERVICE_H_
+
+#include "gutil/ref_counted.h"
+#include "util/status.h"
+
+namespace kudu {
+namespace rpc {
+
+class InboundCall;
+
+class RpcService : public base::RefCountedThreadSafe<RpcService> {
+ public:
+  virtual ~RpcService() {}
+
+  // Enqueue a call for processing.
+  // On failure, the RpcService::QueueInboundCall() implementation is
+  // responsible for responding to the client with a failure message.
+  virtual Status QueueInboundCall(gscoped_ptr<InboundCall> call) = 0;
+};
+
+} // namespace rpc
+} // namespace kudu
+
+#endif // KUDU_RPC_SERVICE_H_

commit a699609cf349b9a627b95f5f2d3fca776a81af34

rpc: gain back some perf lost by rpc_service refactor
This fixes a ~5% regression in performance caused by
da43dd1e814d320e5b418a99c78ad6751e2f4422:

* Remove a redundant TRACE message about inserting into the
  service queue.
* Change the locking for the service_ variable to be a percpu_rwlock
  instead of a boost::mutex. Since it is very rarely mutated (only at
  startup/shutdown) this will cause less contention.

I ran rpc-bench three times before and after on c0328:

Before:
I0424 14:48:39.696794  3655 rpc-bench.cc:108] Reqs/sec:         85246.4
I0424 14:48:39.696868  3655 rpc-bench.cc:109] User CPU per req: 38.2978us
I0424 14:48:39.696882  3655 rpc-bench.cc:110] Sys CPU per req:  68.694us
I0424 14:48:49.731505  3655 rpc-bench.cc:108] Reqs/sec:         83426.1
I0424 14:48:49.731549  3655 rpc-bench.cc:109] User CPU per req: 38.4475us
I0424 14:48:49.731562  3655 rpc-bench.cc:110] Sys CPU per req:  70.0116us
I0424 14:48:59.753758  3655 rpc-bench.cc:108] Reqs/sec:         85728.4
I0424 14:48:59.753803  3655 rpc-bench.cc:109] User CPU per req: 38.237us
I0424 14:48:59.753815  3655 rpc-bench.cc:110] Sys CPU per req:  69.1735us

After:
0424 14:49:29.418010  3995 rpc-bench.cc:108] Reqs/sec:         88876.4
I0424 14:49:29.418082  3995 rpc-bench.cc:109] User CPU per req: 37.5319us
I0424 14:49:29.418097  3995 rpc-bench.cc:110] Sys CPU per req:  67.3925us
I0424 14:49:39.438143  3995 rpc-bench.cc:108] Reqs/sec:         88792.5
I0424 14:49:39.438184  3995 rpc-bench.cc:109] User CPU per req: 37.5149us
I0424 14:49:39.438196  3995 rpc-bench.cc:110] Sys CPU per req:  67.4874us
I0424 14:49:49.458145  3995 rpc-bench.cc:108] Reqs/sec:         88468.9
I0424 14:49:49.458190  3995 rpc-bench.cc:109] User CPU per req: 37.4423us
I0424 14:49:49.458200  3995 rpc-bench.cc:110] Sys CPU per req:  67.2699us

Change-Id: I758f47deab2e6d1ffb1ce5f777483cb8d100326c
Reviewed-on: http://gerrit.ent.cloudera.com:8080/2344
Reviewed-by: Michael Percy <mpercy@cloudera.com>
Tested-by: jenkins

src/rpc/messenger.cc
@@ -106,7 +106,7 @@ void Messenger::AllExternalReferencesDropped() {
 }
 
 void Messenger::Shutdown() {
-  boost::lock_guard<boost::mutex> guard(lock_);
+  boost::lock_guard<percpu_rwlock> guard(lock_);
   if (closing_) {
     return;
   }
@@ -141,7 +141,7 @@ Status Messenger::AddAcceptorPool(const Sockaddr &accept_addr,
   RETURN_NOT_OK(sock.GetSocketAddress(&remote));
   shared_ptr<AcceptorPool> acceptor_pool(new AcceptorPool(this, &sock, remote));
   RETURN_NOT_OK(acceptor_pool->Init(num_threads));
-  boost::lock_guard<boost::mutex> guard(lock_);
+  boost::lock_guard<percpu_rwlock> guard(lock_);
   acceptor_pools_.push_back(acceptor_pool);
   *pool = acceptor_pool;
   return Status::OK();
@@ -151,7 +151,7 @@ Status Messenger::AddAcceptorPool(const Sockaddr &accept_addr,
 Status Messenger::RegisterService(const string& service_name,
                                   const scoped_refptr<RpcService>& service) {
   DCHECK(service);
-  boost::lock_guard<boost::mutex> guard(lock_);
+  boost::lock_guard<percpu_rwlock> guard(lock_);
   // TODO: Key services on service name.
   rpc_service_ = service;
   return Status::OK();
@@ -159,7 +159,7 @@ Status Messenger::RegisterService(const string& service_name,
 
 // Unregister an RpcService.
 Status Messenger::UnregisterService(const string& service_name) {
-  boost::lock_guard<boost::mutex> guard(lock_);
+  boost::lock_guard<percpu_rwlock> guard(lock_);
   if (!rpc_service_) {
     return Status::ServiceUnavailable("Service is not registered");
   } else {
@@ -174,9 +174,7 @@ void Messenger::QueueOutboundCall(const shared_ptr<OutboundCall> &call) {
 }
 
 void Messenger::QueueInboundCall(gscoped_ptr<InboundCall> call) {
-  TRACE_TO(call->trace(), "Inserting onto call queue");
-
-  boost::lock_guard<boost::mutex> guard(lock_);
+  boost::shared_lock<rw_spinlock> guard(lock_.get_lock());
   if (PREDICT_FALSE(!rpc_service_)) {
     Status s = Status::ServiceUnavailable("RPC Service is not registered",
                                           call->ToString());
@@ -209,7 +207,7 @@ Messenger::Messenger(const MessengerBuilder &bld)
 }
 
 Messenger::~Messenger() {
-  boost::lock_guard<boost::mutex> guard(lock_);
+  boost::lock_guard<percpu_rwlock> guard(lock_);
   CHECK(closing_) << "Should have already shut down";
   STLDeleteElements(&reactors_);
 }

src/rpc/messenger.h
@@ -145,7 +145,7 @@ class Messenger {
   }
 
   bool closing() const {
-    boost::lock_guard<boost::mutex> guard(lock_);
+    boost::shared_lock<rw_spinlock> guard(lock_.get_lock());
     return closing_;
   }
 
@@ -168,7 +168,7 @@ class Messenger {
   const std::string name_;
 
   // protects closing_, acceptor_pools_, rpc_service_.
-  mutable boost::mutex lock_;
+  mutable percpu_rwlock lock_;
 
   bool closing_;
 