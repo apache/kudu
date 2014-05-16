4701a7a4a4a0f6c4a86f744f9b27f1a2295bf7ee
Todd Lipcon <todd@cloudera.com>
2014-05-15 21:13:47 -0700

    log-dump: add ability to decode write operations when dumping the log
    This changes the default dump format to decode the PBs for write operations.
    Also removes the unused TABLET_METADATA enum value from the log PB.
    
    Example output:
    
    0.1     REPLICATE CHANGE_CONFIG_OP
            op_type: CHANGE_CONFIG_OP change_config_request { tablet_id: "d4beb7e0a94e4616be57f450fd9ffc75" new_config { seqno: 0 peers { permanent_uuid: "2f52095518904515bebe8a75aa0eb30e" } } }
    0.2     COMMIT 0.1
            op_type: CHANGE_CONFIG_OP commited_op_id { term: 0 index: 1 } timestamp: 2 change_config_response { }
    0.3     REPLICATE WRITE_OP
            Tablet: d4beb7e0a94e4616be57f450fd9ffc75
            Consistency: NO_CONSISTENCY
            op 0: INSERT (uint64 rand_key=8558277605797851395, uint64 link_to=0, uint64 insert_ts=1400213460158075)
    0.4     REPLICATE WRITE_OP
            Tablet: d4beb7e0a94e4616be57f450fd9ffc75
            Consistency: NO_CONSISTENCY
            op 0: INSERT (uint64 rand_key=5274442192492290564, uint64 link_to=0, uint64 insert_ts=1400213460158081)
    0.5     REPLICATE WRITE_OP
            Tablet: d4beb7e0a94e4616be57f450fd9ffc75
            Consistency: NO_CONSISTENCY
            op 0: INSERT (uint64 rand_key=9181735844631528718, uint64 link_to=0, uint64 insert_ts=1400213460158139)
    0.6     REPLICATE WRITE_OP
            Tablet: d4beb7e0a94e4616be57f450fd9ffc75
            Consistency: NO_CONSISTENCY
            op 0: INSERT (uint64 rand_key=7228088743684002569, uint64 link_to=0, uint64 insert_ts=1400213460158113)
    0.7     REPLICATE WRITE_OP
            Tablet: d4beb7e0a94e4616be57f450fd9ffc75
            Consistency: NO_CONSISTENCY
            op 0: INSERT (uint64 rand_key=5879420867084272911, uint64 link_to=0, uint64 insert_ts=1400213460158143)
    0.8     REPLICATE WRITE_OP
            Tablet: d4beb7e0a94e4616be57f450fd9ffc75
            Consistency: NO_CONSISTENCY
            op 0: INSERT (uint64 rand_key=7851548082029307668, uint64 link_to=0, uint64 insert_ts=1400213460158172)
    0.9     COMMIT 0.3
            op_type: WRITE_OP commited_op_id { term: 0 index: 3 } timestamp: 5 result { ops { mutated_stores { mrs_id: 0 } } }
    
    Change-Id: Ica951a6d56ee5a74e27fc1f541ec27fa7bafa3c1
    Reviewed-on: http://gerrit.ent.cloudera.com:8080/2588
    Reviewed-by: Adar Dembo <adar@cloudera.com>
    Tested-by: jenkins

--- src/consensus/log-dump.cc
+++ src/consensus/log-dump.cc
@@ -4,7 +4,11 @@
 #include <gflags/gflags.h>
 #include <glog/logging.h>
 #include <iostream>
+#include <vector>
 
+#include "common/row_operations.h"
+#include "common/wire_protocol.h"
+#include "consensus/consensus.pb.h"
 #include "consensus/log_reader.h"
 #include "gutil/stl_util.h"
 #include "gutil/strings/numbers.h"
@@ -12,10 +16,11 @@
 #include "util/pb_util.h"
 
 DEFINE_bool(print_headers, true, "print the log segment headers");
-DEFINE_string(print_entries, "full",
+DEFINE_string(print_entries, "decoded",
               "How to print entries:\n"
               "  false|0|no = don't print\n"
-              "  true|1|yes|full = print them fully\n"
+              "  true|1|yes|decoded = print them decoded\n"
+              "  pb = print the raw protobuf\n"
               "  id = print only their ids");
 DEFINE_int32(truncate_data, 100,
              "Truncate the data fields to the given number of bytes "
@@ -23,13 +28,19 @@ DEFINE_int32(truncate_data, 100,
 namespace kudu {
 namespace log {
 
+using consensus::OperationPB;
+using consensus::OperationType;
+using consensus::ReplicateMsg;
+using tserver::WriteRequestPB;
 using std::string;
+using std::vector;
 using std::cout;
 using std::endl;
 
 enum PrintEntryType {
   DONT_PRINT,
-  PRINT_FULL,
+  PRINT_PB,
+  PRINT_DECODED,
   PRINT_ID
 };
 
@@ -37,12 +48,71 @@ static PrintEntryType ParsePrintType() {
   if (ParseLeadingBoolValue(FLAGS_print_entries.c_str(), true) == false) {
     return DONT_PRINT;
   } else if (ParseLeadingBoolValue(FLAGS_print_entries.c_str(), false) == true ||
-             FLAGS_print_entries == "full") {
-    return PRINT_FULL;
+             FLAGS_print_entries == "decoded") {
+    return PRINT_DECODED;
+  } else if (FLAGS_print_entries == "pb") {
+    return PRINT_PB;
   } else if (FLAGS_print_entries == "id") {
     return PRINT_ID;
   } else {
     LOG(FATAL) << "Unknown value for --print_entries: " << FLAGS_print_entries;
+  }
+}
+
+void PrintIdOnly(const LogEntryPB& entry) {
+  cout << entry.operation().id().term() << "." << entry.operation().id().index() << "\t";
+  if (entry.operation().has_commit()) {
+    cout << "COMMIT " << entry.operation().commit().commited_op_id().term()
+         << "." << entry.operation().commit().commited_op_id().index();
+  } else if (entry.operation().has_replicate()) {
+    cout << "REPLICATE "
+         << OperationType_Name(entry.operation().replicate().op_type());
+  } else {
+    cout << "UNKNOWN";
+  }
+  cout << endl;
+}
+
+void PrintDecodedWriteRequestPB(const string& indent,
+                                const WriteRequestPB& write) {
+  Schema s;
+  CHECK_OK(SchemaFromPB(write.schema(), &s));
+  Arena arena(32 * 1024, 1024 * 1024);
+  RowOperationsPBDecoder dec(&write.row_operations(), &s, &s, &arena);
+  vector<DecodedRowOperation> ops;
+  CHECK_OK(dec.DecodeOperations(&ops));
+
+  cout << indent << "Tablet: " << write.tablet_id() << endl;
+  cout << indent << "Consistency: "
+       << ExternalConsistencyMode_Name(write.external_consistency_mode()) << endl;
+  if (write.has_propagated_timestamp()) {
+    cout << indent << "Propagated TS: " << write.propagated_timestamp() << endl;
+  }
+
+  int i = 0;
+  BOOST_FOREACH(const DecodedRowOperation& op, ops) {
+    cout << indent << "op " << (i++) << ": " << op.ToString(s) << endl;
+  }
+}
+
+void PrintDecoded(const LogEntryPB& entry) {
+  CHECK_EQ(entry.type(), log::OPERATION);
+
+  PrintIdOnly(entry);
+
+  const string indent = "\t";
+  if (entry.operation().has_replicate()) {
+    // We can actually decode REPLICATE messages.
+
+    const ReplicateMsg& replicate = entry.operation().replicate();
+    if (replicate.op_type() == consensus::WRITE_OP) {
+      PrintDecodedWriteRequestPB(indent, replicate.write_request());
+    } else {
+      cout << indent << replicate.ShortDebugString() << endl;
+    }
+  } else if (entry.operation().has_commit()) {
+    // For COMMIT we'll just dump the PB
+    cout << indent << entry.operation().commit().ShortDebugString() << endl;
   }
 }
 
@@ -58,24 +128,17 @@ void PrintSegment(LogReader* reader,
   if (print_type == DONT_PRINT) return;
 
   BOOST_FOREACH(LogEntryPB* entry, entries) {
-    if (FLAGS_truncate_data > 0) {
-      pb_util::TruncateFields(entry, FLAGS_truncate_data);
-    }
 
-    if (print_type == PRINT_FULL) {
-      cout << "Entry:\n" << entry->DebugString();
-    } else if (print_type == PRINT_ID) {
-      cout << entry->operation().id().term() << "." << entry->operation().id().index() << "\t";
-      if (entry->operation().has_commit()) {
-        cout << "COMMIT " << entry->operation().commit().commited_op_id().term()
-             << "." << entry->operation().commit().commited_op_id().index();
-      } else if (entry->operation().has_replicate()) {
-        cout << "REPLICATE "
-             << OperationType_Name(entry->operation().replicate().op_type());
-      } else {
-        cout << "UNKNOWN";
+    if (print_type == PRINT_PB) {
+      if (FLAGS_truncate_data > 0) {
+        pb_util::TruncateFields(entry, FLAGS_truncate_data);
       }
-      cout << endl;
+
+      cout << "Entry:\n" << entry->DebugString();
+    } else if (print_type == PRINT_DECODED) {
+      PrintDecoded(*entry);
+    } else if (print_type == PRINT_ID) {
+      PrintIdOnly(*entry);
     }
   }
 }
