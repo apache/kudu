apache/kudu

commit cf3bea1189a37ac1086209d59880627c07a466e7

Reduced memory overhead for knapsack solver, in turn optimizing performance.
Used reverse-ordering for maximum-weight-obtained per item found for
knapsack solver. This allows us to use one column keeping track of the
maximum weight per knapsack capacity (0..W) instead of a previous and
a current column.

Performance improved according to the knapsack_solver-test.cc benchmark
(activated in release mode). Improvement on my T530 was from 1.327s to
0.558s in real time.

Change-Id: I82ce483f47ae65d27f41464fcfd45d0ebb7f22a3
Reviewed-on: http://gerrit.ent.cloudera.com:8080/3048
Tested-by: jenkins
Reviewed-by: Todd Lipcon <todd@cloudera.com>

src/util/knapsack_solver.h
@@ -59,7 +59,7 @@ class KnapsackSolver {
   bool ProcessNext();
 
   // Returns the current best solution after the most recent ProcessNext
-  // call. *solution is a pair of (weight used, value obtained).
+  // call. *solution is a pair of (knapsack weight used, value obtained).
   solution_type GetSolution();
 
   // Trace the path of item indexes used to achieve the given best
@@ -70,37 +70,33 @@ class KnapsackSolver {
  private:
 
   // The state kept by the DP algorithm.
-  template<typename value_type>
   class KnapsackBlackboard {
    public:
+    typedef std::pair<int, value_type> solution_type;
     KnapsackBlackboard() :
       n_items_(0),
-      n_weights_(0) {
+      n_weights_(0),
+      cur_item_idx_(0),
+      best_solution_(0, 0) {
     }
 
-    void Resize(int n_items, int max_weight);
+    void ResizeAndClear(int n_items, int max_weight);
 
-    value_type &at_prev_column(int weight) {
+    // Current maximum value at the given weight
+    value_type &max_at(int weight) {
       DCHECK_GE(weight, 0);
       DCHECK_LT(weight, n_weights_);
-      return prev_column_[weight];
+      return max_value_[weight];
     }
 
-    value_type &at_cur_column(int weight) {
-      DCHECK_GE(weight, 0);
-      DCHECK_LT(weight, n_weights_);
-      return cur_column_[weight];
-    }
+    // Consider the next item to be put into the knapsack
+    // Moves the "state" of the solution forward
+    void Advance(value_type new_val, int new_wt);
 
-    void Advance() {
-      cur_column_.swap(prev_column_);
-    }
+    // How many items have been considered
+    int current_item_index() const { return cur_item_idx_; }
 
-    void ClearCurrentColumn() {
-      cur_column_.assign(cur_column_.size(), 0);
-    }
-
-    bool taken(int item, int weight) {
+    bool item_taken(int item, int weight) const {
       DCHECK_GE(weight, 0);
       DCHECK_LT(weight, n_weights_);
       DCHECK_GE(item, 0);
@@ -108,43 +104,38 @@ class KnapsackSolver {
       return item_taken_[index(item, weight)];
     }
 
-    void mark_taken(int item, int weight) {
+    solution_type best_solution() { return best_solution_; }
+
+    bool done() { return cur_item_idx_ == n_items_; }
+
+   private:
+    void MarkTaken(int item, int weight) {
       item_taken_[index(item, weight)] = true;
     }
 
-    std::pair<int, value_type> BestSolution() {
-      value_type max_value = 0;
-      int max_value_weight = 0;
-      for (int w = 0; w < n_weights_; w++) {
-        value_type val = at_cur_column(w);
-        if (val > max_value) {
-          max_value = val;
-          max_value_weight = w;
-        }
-      }
-      return std::make_pair(max_value_weight, max_value);
-    }
-
-   private:
     // If the dynamic programming matrix has more than this number of cells,
     // then warn.
     static const int kWarnDimension = 10000000;
 
-    size_t index(int item, int weight) {
+    size_t index(int item, int weight) const {
       return n_weights_ * item + weight;
     }
 
-    std::vector<value_type> prev_column_;
-    std::vector<value_type> cur_column_;
-    std::vector<bool> item_taken_;
+    // vector with maximum value at the i-th position meaning that it is
+    // the maximum value you can get given a knapsack of weight capacity i
+    // while only considering items 0..cur_item_idx_-1
+    std::vector<value_type> max_value_;
+    std::vector<bool> item_taken_; // TODO: record difference vectors?
     size_t n_items_, n_weights_;
+    int cur_item_idx_;
+    // Best current solution
+    solution_type best_solution_;
 
     DISALLOW_COPY_AND_ASSIGN(KnapsackBlackboard);
   };
 
-  KnapsackBlackboard<value_type> bb_;
+  KnapsackBlackboard bb_;
   const std::vector<item_type>* items_;
-  int cur_item_idx_;
   int knapsack_capacity_;
 
   DISALLOW_COPY_AND_ASSIGN(KnapsackSolver);
@@ -156,42 +147,18 @@ inline void KnapsackSolver<Traits>::Reset(int knapsack_capacity,
   DCHECK_GE(knapsack_capacity, 0);
   items_ = items;
   knapsack_capacity_ = knapsack_capacity;
-  bb_.Resize(items->size(), knapsack_capacity);
-  bb_.ClearCurrentColumn();
-  cur_item_idx_ = 0;
+  bb_.ResizeAndClear(items->size(), knapsack_capacity);
 }
 
 template<class Traits>
 inline bool KnapsackSolver<Traits>::ProcessNext() {
-  if (cur_item_idx_ >= items_->size()) {
-    return false;
-  }
+  if (bb_.done()) return false;
 
-  bb_.Advance();
-  const item_type& item = (*items_)[cur_item_idx_];
+  const item_type& item = (*items_)[bb_.current_item_index()];
   int item_weight = Traits::get_weight(item);
   value_type item_value = Traits::get_value(item);
+  bb_.Advance(item_value, item_weight);
 
-  int j;
-  for (j = 0; j < item_weight && j <= knapsack_capacity_; j++) {
-    bb_.at_cur_column(j) = bb_.at_prev_column(j);
-    // No need to set "taken" to false -- we clear to false at the start
-    // in Resize()
-  }
-  for (; j <= knapsack_capacity_; j++) {
-    value_type val_if_taken = bb_.at_prev_column(j - item_weight) + item_value;
-    value_type val_if_skipped = bb_.at_prev_column(j);
-
-    if (val_if_taken > val_if_skipped) {
-      bb_.at_cur_column(j) = val_if_taken;
-      bb_.mark_taken(cur_item_idx_, j);
-    } else {
-      bb_.at_cur_column(j) = val_if_skipped;
-      // No need to set false -- we clear to false at the start.
-    }
-  }
-
-  cur_item_idx_++;
   return true;
 }
 
@@ -200,7 +167,6 @@ inline void KnapsackSolver<Traits>::Solve(std::vector<item_type> &items,
                                           size_t knapsack_capacity,
                                           std::vector<size_t>* chosen_items,
                                           value_type* optimal_value) {
-
   Reset(knapsack_capacity, &items);
 
   while (ProcessNext()) {
@@ -213,7 +179,7 @@ inline void KnapsackSolver<Traits>::Solve(std::vector<item_type> &items,
 
 template<class Traits>
 inline typename KnapsackSolver<Traits>::solution_type KnapsackSolver<Traits>::GetSolution() {
-  return bb_.BestSolution();
+  return bb_.best_solution();
 }
 
 template<class Traits>
@@ -223,8 +189,8 @@ inline void KnapsackSolver<Traits>::TracePath(const solution_type& best,
   // Retrace back which set of items corresponded to this value.
   int w = best.first;
   chosen_items->clear();
-  for (int k = cur_item_idx_ - 1; k >= 0; k--) {
-    if (bb_.taken(k, w)) {
+  for (int k = bb_.current_item_index() - 1; k >= 0; k--) {
+    if (bb_.item_taken(k, w)) {
       const item_type& taken = (*items_)[k];
       chosen_items->push_back(k);
       w -= Traits::get_weight(taken);
@@ -234,9 +200,8 @@ inline void KnapsackSolver<Traits>::TracePath(const solution_type& best,
 }
 
 template<class Traits>
-template<class value_type>
-void KnapsackSolver<Traits>::KnapsackBlackboard<value_type>::Resize(
-    int n_items, int max_weight) {
+void KnapsackSolver<Traits>::KnapsackBlackboard::ResizeAndClear(int n_items,
+                                                                int max_weight) {
   CHECK_GT(n_items, 0);
   CHECK_GE(max_weight, 0);
 
@@ -244,9 +209,7 @@ void KnapsackSolver<Traits>::KnapsackBlackboard<value_type>::Resize(
   // 0 to max_weight. This avoids having to subtract 1 every time
   // we index into the array.
   n_weights_ = max_weight + 1;
-
-  prev_column_.resize(n_weights_);
-  cur_column_.resize(n_weights_);
+  max_value_.resize(n_weights_);
 
   size_t dimension = index(n_items, n_weights_);
   if (dimension > kWarnDimension) {
@@ -254,8 +217,38 @@ void KnapsackSolver<Traits>::KnapsackBlackboard<value_type>::Resize(
                  << " is large: may be inefficient!";
   }
   item_taken_.resize(dimension);
-  std::fill(item_taken_.begin(), item_taken_.end(), false);
   n_items_ = n_items;
+
+  // Clear
+  std::fill(max_value_.begin(), max_value_.end(), 0);
+  std::fill(item_taken_.begin(), item_taken_.end(), false);
+  best_solution_ = std::make_pair(0, 0);
+
+  cur_item_idx_ = 0;
+}
+
+template<class Traits>
+void KnapsackSolver<Traits>::KnapsackBlackboard::Advance(value_type new_val, int new_wt) {
+  // Use the dynamic programming formula:
+  // Define mv(i, j) as maximum value considering items 0..i-1 with knapsack weight j
+  // Then:
+  // if j - weight(i) >= 0, then:
+  // mv(i, j) = max(mv(i-1, j), mv(i-1, j-weight(i)) + value(j))
+  // else mv(i, j) = mv(i-1, j)
+  // Since the recursive formula requires an access of j-weight(i), we go in reverse.
+  for (int j = n_weights_ - 1; j >= new_wt ; --j) {
+    int val_if_taken = max_value_[j - new_wt] + new_val;
+    if (max_value_[j] < val_if_taken) {
+      max_value_[j] = val_if_taken;
+      MarkTaken(cur_item_idx_, j);
+      // Check if new solution found
+      if (best_solution_.second < val_if_taken) {
+        best_solution_ = std::make_pair(j, val_if_taken);
+      }
+    }
+  }
+
+  cur_item_idx_++;
 }
 
 } // namespace kudu

commit 0aa8aec34e66a38bb1f61254d26477e6527d51c7

compaction: fix bug in knapsack solver which broke budgeted compaction
In cf3bea1189a37ac1086209d59880627c07a466e7, KnapsackBlackboard::Advance()
accidentally introduced a typecast from the knapsack "value" type to integer.
This didn't cause tests to fail, because the tests used an integer value type,
so it was equivalent. However, in real usage, the value_type is a double,
so this actually broke quite a bit of stuff.

We didn't catch it quickly because the compaction policy code lacked any
proper unit tests. This patch adds a new unit test for the compaction policy
based on a specific instance that I saw breaking (i.e not compacting) in a
real server situation.

This patch also changes the knapsack solver test to use double values, to more
closely track what we do in real life.

As a happy side effect of this change, it appears that the knapsack solver
benchmark runs marginally faster by avoiding the type casting back and forth
between int and double.

Change-Id: I58e676f26f8bb8865132454d544727745b3c02e1
Reviewed-on: http://gerrit.ent.cloudera.com:8080/3415
Tested-by: Todd Lipcon <todd@cloudera.com>
Reviewed-by: Vladimir Feinberg <vladimir.feinberg@cloudera.com>

src/tablet/CMakeLists.txt
@@ -58,6 +58,7 @@ target_link_libraries(tablet
 set(KUDU_TEST_LINK_LIBS tablet ${KUDU_MIN_TEST_LIBS})
 ADD_KUDU_TEST(tablet-test)
 ADD_KUDU_TEST(mt-tablet-test RUN_SERIAL true)
+ADD_KUDU_TEST(compaction_policy-test)
 ADD_KUDU_TEST(diskrowset-test)
 ADD_KUDU_TEST(mt-diskrowset-test RUN_SERIAL true)
 ADD_KUDU_TEST(memrowset-test)

src/tablet/mock-rowsets.h
@@ -1,0 +1,151 @@
+// Copyright (c) 2013, Cloudera, inc.
+#ifndef KUDU_TABLET_MOCK_ROWSETS_H
+#define KUDU_TABLET_MOCK_ROWSETS_H
+
+#include <string>
+#include <vector>
+
+#include "gutil/strings/substitute.h"
+#include "server/metadata.h"
+#include "tablet/rowset.h"
+
+namespace kudu {
+namespace tablet {
+
+// Mock implementation of RowSet which just aborts on every call.
+class MockRowSet : public RowSet {
+ public:
+  virtual Status CheckRowPresent(const RowSetKeyProbe &probe, bool *present,
+                                 ProbeStats* stats) const OVERRIDE {
+    LOG(FATAL) << "Unimplemented";
+    return Status::OK();
+  }
+  virtual Status MutateRow(Timestamp timestamp,
+                           const RowSetKeyProbe &probe,
+                           const RowChangeList &update,
+                           const consensus::OpId& op_id_,
+                           ProbeStats* stats,
+                           OperationResultPB *result) OVERRIDE {
+    LOG(FATAL) << "Unimplemented";
+    return Status::OK();
+  }
+  virtual RowwiseIterator *NewRowIterator(const Schema* projection,
+                                          const MvccSnapshot &snap) const OVERRIDE {
+    LOG(FATAL) << "Unimplemented";
+    return NULL;
+  }
+  virtual CompactionInput *NewCompactionInput(const Schema* projection,
+                                              const MvccSnapshot &snap) const OVERRIDE {
+    LOG(FATAL) << "Unimplemented";
+    return NULL;
+  }
+  virtual Status CountRows(rowid_t *count) const OVERRIDE {
+    LOG(FATAL) << "Unimplemented";
+    return Status::OK();
+  }
+  virtual string ToString() const OVERRIDE {
+    LOG(FATAL) << "Unimplemented";
+    return "";
+  }
+  virtual Status DebugDump(vector<string> *lines = NULL) OVERRIDE {
+    LOG(FATAL) << "Unimplemented";
+    return Status::OK();
+  }
+  virtual Status Delete() {
+    LOG(FATAL) << "Unimplemented";
+    return Status::OK();
+  }
+  virtual uint64_t EstimateOnDiskSize() const OVERRIDE {
+    LOG(FATAL) << "Unimplemented";
+    return 0;
+  }
+  virtual boost::mutex *compact_flush_lock() OVERRIDE {
+    LOG(FATAL) << "Unimplemented";
+    return NULL;
+  }
+  virtual shared_ptr<metadata::RowSetMetadata> metadata() OVERRIDE {
+    LOG(FATAL) << "Unimplemented";
+    return shared_ptr<metadata::RowSetMetadata>(
+      reinterpret_cast<metadata::RowSetMetadata *>(NULL));
+  }
+  virtual Status AlterSchema(const Schema& schema) OVERRIDE {
+    LOG(FATAL) << "Unimplemented";
+    return Status::OK();
+  }
+  virtual const Schema &schema() const OVERRIDE {
+    LOG(FATAL) << "Unimplemented";
+  }
+
+  virtual size_t DeltaMemStoreSize() const OVERRIDE {
+    LOG(FATAL) << "Unimplemented";
+    return 0;
+  }
+
+  virtual size_t CountDeltaStores() const OVERRIDE {
+    LOG(FATAL) << "Unimplemented";
+    return 0;
+  }
+
+  virtual Status FlushDeltas() OVERRIDE {
+    LOG(FATAL) << "Unimplemented";
+    return Status::OK();
+  }
+
+  virtual Status MinorCompactDeltaStores() OVERRIDE {
+    LOG(FATAL) << "Unimplemented";
+    return Status::OK();
+  }
+
+  virtual bool IsAvailableForCompaction() OVERRIDE {
+    return true;
+  }
+};
+
+// Mock which implements GetBounds() with constant provided bonuds.
+class MockDiskRowSet : public MockRowSet {
+ public:
+  MockDiskRowSet(string first_key, string last_key, int size = 1000000)
+    : first_key_(first_key),
+      last_key_(last_key),
+      size_(size) {
+  }
+
+  virtual Status GetBounds(Slice *min_encoded_key,
+                           Slice *max_encoded_key) const OVERRIDE {
+    *min_encoded_key = Slice(first_key_);
+    *max_encoded_key = Slice(last_key_);
+    return Status::OK();
+  }
+
+  virtual uint64_t EstimateOnDiskSize() const OVERRIDE {
+    return size_;
+  }
+
+  virtual std::string ToString() const {
+    return strings::Substitute("mock[$0, $1]",
+                               Slice(first_key_).ToDebugString(),
+                               Slice(last_key_).ToDebugString());
+  }
+
+ private:
+  const string first_key_;
+  const string last_key_;
+  const uint64_t size_;
+};
+
+// Mock which acts like a MemRowSet and has no known bounds.
+class MockMemRowSet : public MockRowSet {
+ public:
+  virtual Status GetBounds(Slice *min_encoded_key,
+                           Slice *max_encoded_key) const OVERRIDE {
+    return Status::NotSupported("");
+  }
+
+ private:
+  const string first_key_;
+  const string last_key_;
+};
+
+} // namespace tablet
+} // namespace kudu
+#endif /* KUDU_TABLET_MOCK_ROWSETS_H */

src/tablet/rowset_tree.cc
@@ -91,6 +91,8 @@ Status RowSetTree::Reset(const RowSetVector &rowsets) {
                    << s.ToString();
       return s;
     }
+    DCHECK_LE(min_key.compare(max_key), 0)
+      << "Rowset min must be <= max: " << rs->ToString();
     // Load into key endpoints.
     endpoints.push_back(RSEndpoint(rsit->rowset, START, min_key));
     endpoints.push_back(RSEndpoint(rsit->rowset, STOP, max_key));

src/util/knapsack_solver.h
@@ -237,7 +237,7 @@ void KnapsackSolver<Traits>::KnapsackBlackboard::Advance(value_type new_val, int
   // else mv(i, j) = mv(i-1, j)
   // Since the recursive formula requires an access of j-weight(i), we go in reverse.
   for (int j = n_weights_ - 1; j >= new_wt ; --j) {
-    int val_if_taken = max_value_[j - new_wt] + new_val;
+    value_type val_if_taken = max_value_[j - new_wt] + new_val;
     if (max_value_[j] < val_if_taken) {
       max_value_[j] = val_if_taken;
       MarkTaken(cur_item_idx_, j);

src/tablet/compaction_policy-test.cc
@@ -1,0 +1,37 @@
+// Copyright (c) 2013, Cloudera, inc.
+
+#include <gtest/gtest.h>
+#include <tr1/unordered_set>
+
+#include "util/test_util.h"
+#include "tablet/mock-rowsets.h"
+#include "tablet/rowset.h"
+#include "tablet/rowset_tree.h"
+#include "tablet/compaction_policy.h"
+
+using std::tr1::unordered_set;
+
+namespace kudu {
+namespace tablet {
+
+// Simple test for budgeted compaction: with three rowsets which
+// mostly overlap, and an high budget, they should all be selected.
+TEST(TestCompactionPolicy, TestBudgetedSelection) {
+  RowSetVector vec;
+  vec.push_back(shared_ptr<RowSet>(new MockDiskRowSet("C", "c")));
+  vec.push_back(shared_ptr<RowSet>(new MockDiskRowSet("B", "a")));
+  vec.push_back(shared_ptr<RowSet>(new MockDiskRowSet("A", "b")));
+
+  RowSetTree tree;
+  ASSERT_STATUS_OK(tree.Reset(vec));
+
+  const int kBudgetMb = 1000; // enough to select all
+  BudgetedCompactionPolicy policy(kBudgetMb);
+
+  std::tr1::unordered_set<RowSet*> picked;
+  ASSERT_OK(policy.PickRowSets(tree, &picked));
+  ASSERT_EQ(3, picked.size());
+}
+
+} // namespace tablet
+} // namespace kudu

src/tablet/rowset_tree-test.cc
@@ -8,135 +8,18 @@
 #include <stdio.h>
 
 #include "gutil/map-util.h"
-#include "server/metadata.h"
+#include "tablet/mock-rowsets.h"
+#include "tablet/rowset.h"
 #include "tablet/rowset_tree.h"
 #include "util/stopwatch.h"
 #include "util/test_util.h"
 
 using std::string;
 using std::tr1::unordered_set;
-using kudu::metadata::RowSetMetadata;
 
 namespace kudu { namespace tablet {
 
 class TestRowSetTree : public KuduTest {
-};
-
-// Mock implementation of RowSet which just aborts on every call.
-class MockRowSet : public RowSet {
- public:
-  virtual Status CheckRowPresent(const RowSetKeyProbe &probe, bool *present,
-                                 ProbeStats* stats) const OVERRIDE {
-    LOG(FATAL) << "Unimplemented";
-    return Status::OK();
-  }
-  virtual Status MutateRow(Timestamp timestamp,
-                           const RowSetKeyProbe &probe,
-                           const RowChangeList &update,
-                           const consensus::OpId& op_id_,
-                           ProbeStats* stats,
-                           OperationResultPB *result) OVERRIDE {
-    LOG(FATAL) << "Unimplemented";
-    return Status::OK();
-  }
-  virtual RowwiseIterator *NewRowIterator(const Schema* projection,
-                                          const MvccSnapshot &snap) const OVERRIDE {
-    LOG(FATAL) << "Unimplemented";
-    return NULL;
-  }
-  virtual CompactionInput *NewCompactionInput(const Schema* projection,
-                                              const MvccSnapshot &snap) const OVERRIDE {
-    LOG(FATAL) << "Unimplemented";
-    return NULL;
-  }
-  virtual Status CountRows(rowid_t *count) const OVERRIDE {
-    LOG(FATAL) << "Unimplemented";
-    return Status::OK();
-  }
-  virtual string ToString() const OVERRIDE {
-    LOG(FATAL) << "Unimplemented";
-    return "";
-  }
-  virtual Status DebugDump(vector<string> *lines = NULL) OVERRIDE {
-    LOG(FATAL) << "Unimplemented";
-    return Status::OK();
-  }
-  virtual Status Delete() {
-    LOG(FATAL) << "Unimplemented";
-    return Status::OK();
-  }
-  virtual uint64_t EstimateOnDiskSize() const OVERRIDE {
-    LOG(FATAL) << "Unimplemented";
-    return 0;
-  }
-  virtual boost::mutex *compact_flush_lock() OVERRIDE {
-    LOG(FATAL) << "Unimplemented";
-    return NULL;
-  }
-  virtual shared_ptr<RowSetMetadata> metadata() OVERRIDE {
-    LOG(FATAL) << "Unimplemented";
-    return shared_ptr<RowSetMetadata>(reinterpret_cast<RowSetMetadata *>(NULL));
-  }
-  virtual Status AlterSchema(const Schema& schema) OVERRIDE {
-    LOG(FATAL) << "Unimplemented";
-    return Status::OK();
-  }
-  virtual const Schema &schema() const OVERRIDE {
-    LOG(FATAL) << "Unimplemented";
-  }
-
-  virtual size_t DeltaMemStoreSize() const OVERRIDE {
-    LOG(FATAL) << "Unimplemented";
-    return 0;
-  }
-
-  virtual size_t CountDeltaStores() const OVERRIDE {
-    LOG(FATAL) << "Unimplemented";
-    return 0;
-  }
-
-  virtual Status FlushDeltas() OVERRIDE {
-    LOG(FATAL) << "Unimplemented";
-    return Status::OK();
-  }
-
-  virtual Status MinorCompactDeltaStores() OVERRIDE {
-    LOG(FATAL) << "Unimplemented";
-    return Status::OK();
-  }
-};
-
-// Mock which implements GetBounds() with constant provided bonuds.
-class MockDiskRowSet : public MockRowSet {
- public:
-  MockDiskRowSet(string first_key, string last_key)
-    : first_key_(first_key),
-      last_key_(last_key) {
-  }
-
-  virtual Status GetBounds(Slice *min_encoded_key,
-                           Slice *max_encoded_key) const OVERRIDE {
-    *min_encoded_key = Slice(first_key_);
-    *max_encoded_key = Slice(last_key_);
-    return Status::OK();
-  }
-
- private:
-  const string first_key_;
-  const string last_key_;
-};
-
-// Mock which acts like a MemRowSet and has no known bounds.
-class MockMemRowSet : public MockRowSet {
- public:
-  virtual Status GetBounds(Slice *min_encoded_key,
-                           Slice *max_encoded_key) const OVERRIDE {
-    return Status::NotSupported("");
-  }
-
- private:
-  const string first_key_;
-  const string last_key_;
 };
 
 namespace {

src/util/knapsack_solver-test.cc
@@ -19,18 +19,18 @@ class TestKnapsack : public KuduTest {
 // The real code will be solving knapsack over RowSet objects --
 // using simple value/weight pairs in the tests makes it standalone.
 struct TestItem {
-  TestItem(int v, int w)
+  TestItem(double v, int w)
     : value(v), weight(w) {
   }
 
-  int value;
+  double value;
   int weight;
 };
 
 // A traits class to adapt the knapsack solver to TestItem.
 struct TestItemTraits {
   typedef TestItem item_type;
-  typedef int value_type;
+  typedef double value_type;
   static int get_weight(const TestItem &item) {
     return item.weight;
   }
@@ -43,7 +43,9 @@ struct TestItemTraits {
 static void GenerateRandomItems(int n_items, int max_weight,
                                 vector<TestItem> *out) {
   for (int i = 0; i < n_items; i++) {
-    out->push_back(TestItem(random() % 10000, random() % max_weight));
+    double value = 10000.0 / (random() % 10000 + 1);
+    int weight = random() % max_weight;
+    out->push_back(TestItem(value, weight));
   }
 }
 
@@ -69,29 +71,29 @@ TEST_F(TestKnapsack, Basics) {
   in.push_back(TestItem(100, 1));
 
   vector<int> out;
-  int max_val;
+  double max_val;
 
   // For 1 weight, pick item 2
   solver.Solve(in, 1, &out, &max_val);
-  ASSERT_EQ(125, max_val);
+  ASSERT_DOUBLE_EQ(125, max_val);
   ASSERT_EQ("2", JoinInts(out));
   out.clear();
 
   // For 2 weight, pick item 1, 2
   solver.Solve(in, 2, &out, &max_val);
-  ASSERT_EQ(110 + 125, max_val);
+  ASSERT_DOUBLE_EQ(110 + 125, max_val);
   ASSERT_EQ("2,1", JoinInts(out));
   out.clear();
 
   // For 3 weight, pick item 0
   solver.Solve(in, 3, &out, &max_val);
-  ASSERT_EQ(500, max_val);
+  ASSERT_DOUBLE_EQ(500, max_val);
   ASSERT_EQ("0", JoinInts(out));
   out.clear();
 
   // For 10 weight, pick all.
   solver.Solve(in, 10, &out, &max_val);
-  ASSERT_EQ(500 + 110 + 125 + 100, max_val);
+  ASSERT_DOUBLE_EQ(500 + 110 + 125 + 100, max_val);
   ASSERT_EQ("3,2,1,0", JoinInts(out));
   out.clear();
 }
@@ -110,18 +112,18 @@ TEST_F(TestKnapsack, Randomized) {
     vector<TestItem> in;
     vector<int> out;
     GenerateRandomItems(kNumItems, kMaxWeight, &in);
-    int max_val;
+    double max_val;
     int max_weight = random() % kMaxWeight;
     solver.Solve(in, max_weight, &out, &max_val);
 
     // Verify that the max_val is equal to the sum of the chosen items' values.
-    int sum_val = 0;
+    double sum_val = 0;
     int sum_weight = 0;
     BOOST_FOREACH(int i, out) {
       sum_val += in[i].value;
       sum_weight += in[i].weight;
     }
-    ASSERT_EQ(max_val, sum_val);
+    ASSERT_DOUBLE_EQ(max_val, sum_val);
     ASSERT_LE(sum_weight, max_weight);
   }
 }
@@ -141,7 +143,7 @@ TEST_F(TestKnapsack, Benchmark) {
     vector<int> out;
     for (int i = 0; i < kNumTrials; i++) {
       out.clear();
-      int max_val;
+      double max_val;
       solver.Solve(in, random() % kMaxWeight, &out, &max_val);
     }
   }