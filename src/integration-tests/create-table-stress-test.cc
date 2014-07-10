40406e1fe516e92960dd7f3ab66e68888bba4656
Mike Percy <mpercy@cloudera.com>
2014-07-09 23:37:05 -0700

    Fix bug in create-table-stress-test, make more debuggable
    Add a bunch of informational and timing logging to the test and use
    ASSERT where possible in the test instead of CHECK.
    
    Also, fix some test issues including:
    1. Number of tablets created is one too many.
    2. "Middle" split calculation is off by one.
    
    Change-Id: Ie65d4f83400f8b423added6719042e6bb6ac6275
    Reviewed-on: http://gerrit.ent.cloudera.com:8080/3556
    Tested-by: jenkins
    Reviewed-by: Todd Lipcon <todd@cloudera.com>

--- src/integration-tests/create-table-stress-test.cc
+++ src/integration-tests/create-table-stress-test.cc
@@ -14,6 +14,7 @@
 #include "master/mini_master.h"
 #include "master/master-test-util.h"
 #include "rpc/messenger.h"
+#include "util/stopwatch.h"
 #include "util/test_util.h"
 
 using std::tr1::shared_ptr;
@@ -29,11 +30,11 @@ using kudu::master::MasterServiceProxy;
 
 DECLARE_int32(heartbeat_interval_ms);
 DECLARE_bool(log_preallocate_segments);
+DEFINE_int32(num_test_tablets, 100, "Number of tablets for stress test");
 
 namespace kudu {
 
 const char* kTableName = "test_table";
-const int kNumTablets = 100;
 
 // Temporarily disabled while working on KUDU-234.
 class CreateTableStressTest : public KuduTest {
@@ -73,7 +74,7 @@ class CreateTableStressTest : public KuduTest {
     cluster_->Shutdown();
   }
 
-  void CreateBigTable(const string& table_name);
+  void CreateBigTable(const string& table_name, int num_tablets);
 
  protected:
   shared_ptr<KuduClient> client_;
@@ -82,10 +83,11 @@ class CreateTableStressTest : public KuduTest {
   KuduSchema schema_;
 };
 
-void CreateTableStressTest::CreateBigTable(const string& table_name) {
+void CreateTableStressTest::CreateBigTable(const string& table_name, int num_tablets) {
   vector<string> keys;
-  int num_splits = kNumTablets - 1; // 1 split = 2 tablets
-  for (int i = num_splits; i >= 0; i--) {
+  int num_splits = num_tablets - 1; // 4 tablets == 3 splits.
+  // Let the "k_0" keys end up in the first split; start splitting at 1.
+  for (int i = 1; i <= num_splits; i++) {
     keys.push_back(StringPrintf("k_%05d", i));
   }
 
@@ -96,17 +98,16 @@ void CreateTableStressTest::CreateBigTable(const string& table_name) {
                         .WaitAssignment(false)));
 }
 
-
 TEST_F(CreateTableStressTest, CreateBigTable) {
   if (!AllowSlowTests()) {
     LOG(INFO) << "Skipping slow test";
     return;
   }
   string table_name = "test_table";
-  ASSERT_NO_FATAL_FAILURE(CreateBigTable(table_name));
+  ASSERT_NO_FATAL_FAILURE(CreateBigTable(table_name, FLAGS_num_test_tablets));
   master::GetTableLocationsResponsePB resp;
   ASSERT_STATUS_OK(WaitForRunningTabletCount(cluster_->mini_master(), table_name,
-                                             kNumTablets, &resp));
+                                             FLAGS_num_test_tablets, &resp));
   LOG(INFO) << "Created table successfully!";
   // Use std::cout instead of log, since these responses are large and log
   // messages have a max size.
@@ -122,7 +123,7 @@ TEST_F(CreateTableStressTest, RestartMasterDuringCreation) {
   }
 
   string table_name = "test_table";
-  ASSERT_NO_FATAL_FAILURE(CreateBigTable(table_name));
+  ASSERT_NO_FATAL_FAILURE(CreateBigTable(table_name, FLAGS_num_test_tablets));
 
   for (int i = 0; i < 3; i++) {
     usleep(500);
@@ -131,7 +132,7 @@ TEST_F(CreateTableStressTest, RestartMasterDuringCreation) {
 
   master::GetTableLocationsResponsePB resp;
   Status s = WaitForRunningTabletCount(cluster_->mini_master(), table_name,
-                                       kNumTablets, &resp);
+                                       FLAGS_num_test_tablets, &resp);
   if (!s.ok()) {
     cluster_->mini_master()->master()->catalog_manager()->DumpState(&std::cerr);
     CHECK_OK(s);
@@ -143,18 +144,27 @@ TEST_F(CreateTableStressTest, TestGetTableLocationsOptions) {
     LOG(INFO) << "Skipping slow test";
     return;
   }
+
   string table_name = "test_table";
-  ASSERT_NO_FATAL_FAILURE(CreateBigTable(table_name));
+  LOG(INFO) << CURRENT_TEST_NAME() << ": Step 1. Creating big table " << table_name << " ...";
+  LOG_TIMING(INFO, "creating big table") {
+    ASSERT_NO_FATAL_FAILURE(CreateBigTable(table_name, FLAGS_num_test_tablets));
+  }
+
+  master::GetTableLocationsRequestPB req;
   master::GetTableLocationsResponsePB resp;
 
   // Make sure the table is completely created before we start poking.
-  CHECK_OK(WaitForRunningTabletCount(cluster_->mini_master(), table_name,
-                                     kNumTablets, &resp));
-
-  master::GetTableLocationsRequestPB req;
+  LOG(INFO) << CURRENT_TEST_NAME() << ": Step 2. Waiting for creation of big table "
+            << table_name << " to complete...";
+  LOG_TIMING(INFO, "waiting for creation of big table") {
+    ASSERT_OK(WaitForRunningTabletCount(cluster_->mini_master(), table_name,
+                                       FLAGS_num_test_tablets, &resp));
+  }
 
   // Test asking for 0 tablets, should fail
-  {
+  LOG(INFO) << CURRENT_TEST_NAME() << ": Step 3. Asking for zero tablets...";
+  LOG_TIMING(INFO, "asking for zero tablets") {
     req.Clear();
     resp.Clear();
     req.mutable_table()->set_table_name(table_name);
@@ -164,40 +174,77 @@ TEST_F(CreateTableStressTest, TestGetTableLocationsOptions) {
   }
 
   // Ask for one, get one, verify
-  {
+  LOG(INFO) << CURRENT_TEST_NAME() << ": Step 4. Asking for one tablet...";
+  LOG_TIMING(INFO, "asking for one tablet") {
     req.Clear();
     resp.Clear();
     req.mutable_table()->set_table_name(table_name);
     req.set_max_returned_locations(1);
-    CHECK_OK(cluster_->mini_master()->master()->catalog_manager()->GetTableLocations(&req, &resp));
+    ASSERT_OK(cluster_->mini_master()->master()->catalog_manager()->GetTableLocations(&req, &resp));
     ASSERT_EQ(resp.tablet_locations_size(), 1);
     // empty since it's the first
     ASSERT_EQ(resp.tablet_locations(0).start_key(), "");
-    ASSERT_EQ(resp.tablet_locations(0).end_key(), "k_00000");
+    ASSERT_EQ(resp.tablet_locations(0).end_key(), "k_00001");
   }
 
-  int half_tablets = kNumTablets / 2;
-  string start_key_middle = StringPrintf("k_%05d", half_tablets);
+  int half_tablets = FLAGS_num_test_tablets / 2;
   // Ask for half of them, get that number back
-  {
+  LOG(INFO) << CURRENT_TEST_NAME() << ": Step 5. Asking for half the tablets...";
+  LOG_TIMING(INFO, "asking for half the tablets") {
     req.Clear();
     resp.Clear();
     req.mutable_table()->set_table_name(table_name);
     req.set_max_returned_locations(half_tablets);
-    CHECK_OK(cluster_->mini_master()->master()->catalog_manager()->GetTableLocations(&req, &resp));
-    ASSERT_EQ(resp.tablet_locations_size(), half_tablets);
+    ASSERT_OK(cluster_->mini_master()->master()->catalog_manager()->GetTableLocations(&req, &resp));
+    ASSERT_EQ(half_tablets, resp.tablet_locations_size());
   }
 
+  // Ask for all of them, get that number back
+  LOG(INFO) << CURRENT_TEST_NAME() << ": Step 6. Asking for all the tablets...";
+  LOG_TIMING(INFO, "asking for all the tablets") {
+    req.Clear();
+    resp.Clear();
+    req.mutable_table()->set_table_name(table_name);
+    req.set_max_returned_locations(FLAGS_num_test_tablets);
+    ASSERT_OK(cluster_->mini_master()->master()->catalog_manager()->GetTableLocations(&req, &resp));
+    ASSERT_EQ(FLAGS_num_test_tablets, resp.tablet_locations_size());
+  }
+
+  LOG(INFO) << "========================================================";
+  LOG(INFO) << "Tables and tablets:";
+  LOG(INFO) << "========================================================";
+  std::vector<scoped_refptr<master::TableInfo> > tables;
+  cluster_->mini_master()->master()->catalog_manager()->GetAllTables(&tables);
+  BOOST_FOREACH(const scoped_refptr<master::TableInfo>& table_info, tables) {
+    LOG(INFO) << "Table: " << table_info->ToString();
+    std::vector<scoped_refptr<master::TabletInfo> > tablets;
+    table_info->GetAllTablets(&tablets);
+    BOOST_FOREACH(const scoped_refptr<master::TabletInfo>& tablet_info, tablets) {
+      const master::SysTabletsEntryPB& metadata = tablet_info->metadata().state().pb;
+      LOG(INFO) << "  Tablet: " << tablet_info->ToString()
+                << " { start_key: "
+                << ((metadata.has_start_key()) ? metadata.start_key() : "<< none >>")
+                << ", end_key: "
+                << ((metadata.has_end_key()) ? metadata.end_key() : "<< none >>") << ", "
+                << "running = " << tablet_info->metadata().state().is_running() << " }";
+    }
+    ASSERT_EQ(FLAGS_num_test_tablets, tablets.size());
+  }
+  LOG(INFO) << "========================================================";
+
   // Get a single tablet in the middle, make sure we get that one back
-  {
+  string start_key_middle = StringPrintf("k_%05d", half_tablets - 1);
+  LOG(INFO) << "Start key middle: " << start_key_middle;
+  LOG(INFO) << CURRENT_TEST_NAME() << ": Step 7. Asking for single middle tablet...";
+  LOG_TIMING(INFO, "asking for single middle tablet") {
     req.Clear();
     resp.Clear();
     req.mutable_table()->set_table_name(table_name);
     req.set_max_returned_locations(1);
     req.set_start_key(start_key_middle);
-    CHECK_OK(cluster_->mini_master()->master()->catalog_manager()->GetTableLocations(&req, &resp));
-    ASSERT_EQ(resp.tablet_locations_size(), 1);
-    ASSERT_EQ(resp.tablet_locations(0).start_key(), start_key_middle);
+    ASSERT_OK(cluster_->mini_master()->master()->catalog_manager()->GetTableLocations(&req, &resp));
+    ASSERT_EQ(1, resp.tablet_locations_size()) << "Response: [" << resp.DebugString() << "]";
+    ASSERT_EQ(start_key_middle, resp.tablet_locations(0).start_key());
   }
 }
 
