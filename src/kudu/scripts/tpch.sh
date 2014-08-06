c875ce554f95d697fed6a79cc1e62647961f17b8
Mike Percy <mpercy@cloudera.com>
2014-08-05 17:24:07 -0700

    Refactor tpch.sh script
    This allows running the script locally, without running on Jenkins or
    writing to a MySQL database. Environment variables are used to override
    the default parameters, which are kept the same for now.
    
    Also, only build the tpch1 target when running make, which should speed
    up the build, and do various bits of cleanup.
    
    I tested this locally using the following command line:
    LINEITEM_TBL_PATH=lineitem.tbl KUDU_DATA_DIR=/tmp/kudutpch1-mpercy TPCH_NUM_QUERY_ITERS=2 ./src/kudu/scripts/tpch.sh
    
    (I have a copy of the TPC-H lineitem.tbl file in my kudu source dir.)
    
    Change-Id: I9907844de649561b69a89214011f829860afe7ad
    Reviewed-on: http://gerrit.sjc.cloudera.com:8080/3776
    Tested-by: jenkins
    Reviewed-by: Jean-Daniel Cryans <jdcryans@cloudera.com>

--- src/kudu/scripts/tpch.sh
+++ src/kudu/scripts/tpch.sh
@@ -4,14 +4,65 @@
 #
 # Run tpch benchmark and write results to the DB.
 #
-# Expects to find the following environment variables set:
+# Expects to find the following Jenkins environment variables set:
 # - JOB_NAME
 # - BUILD_NUMBER
+# If these are not set, the script will still run but will not record
+# the results in the MySQL database. Instead, it will output results
+# into .tsv files in the kudu source root directory. This is useful for
+# running this benchmark locally for testing / dev purposes.
+#
+# Optional environment variables to override (defaults set for Jenkins):
+# - LINEITEM_TBL_PATH: Path to lineitem.tbl from the TPC-H suite.
+# - KUDU_DATA_DIR: Directory to use for data storage.
+# - TPCH_NUM_QUERY_ITERS: Number of TPC-H query iterations to run.
 #
 # Jenkins job: http://sandbox.jenkins.cloudera.com/job/kudu-tpch1
 ########################################################################
 
+##########################################################
+# Constants
+##########################################################
+ROOT=$(readlink -f $(dirname $0)/../../..)
+OUTDIR=$ROOT/build/tpch
+
+##########################################################
+# Overridable params
+##########################################################
+LINEITEM_TBL_PATH=${LINEITEM_TBL_PATH:-/home/jdcryans/lineitem.tbl}
+KUDU_DATA_DIR=${KUDU_DATA_DIR:-/data/2/tmp/kudutpch1-jenkins}
+TPCH_NUM_QUERY_ITERS=${TPCH_NUM_QUERY_ITERS:-5}
+
+##########################################################
+# Functions
+##########################################################
+record_result() {
+  local RECORD_STATS_SCRIPT=$ROOT/src/kudu/scripts/write-jobs-stats-to-mysql.py
+  local TEST_NAME=$1
+  local ITER=$2
+  local VALUE=$3
+  if [ -n "$JOB_NAME" ]; then
+    # Jenkins.
+    python $RECORD_STATS_SCRIPT $JOB_NAME $BUILD_NUMBER $TEST_NAME $ITER $VALUE
+  else
+    # Running locally.
+    local STATS_FILE="$OUTDIR/tpch-$TEST_NAME.tsv"
+    echo -e "${TEST_NAME}\t${ITER}\t${VALUE}" >> "$STATS_FILE"
+  fi
+}
+
+##########################################################
+# Main
+##########################################################
+if [ $TPCH_NUM_QUERY_ITERS -lt 2 ]; then
+  echo "Error: TPCH_NUM_QUERY_ITERS must be 2 or greater"
+  exit 1
+fi
+
+cd $ROOT
+
 # Set up environment
+set -o pipefail
 ulimit -m $[3000*1000]
 ulimit -c unlimited # gather core dumps
 
@@ -22,7 +73,7 @@ if [ -f "$TOOLCHAIN" ]; then
 fi
 
 # Build thirdparty
-thirdparty/build-if-necessary.sh
+$ROOT/thirdparty/build-if-necessary.sh
 
 # PATH=<thirdparty_stuff>:<toolchain_stuff>:$PATH
 export PATH=$(pwd)/thirdparty/installed/bin:$PATH
@@ -37,39 +88,32 @@ export LD_BIND_NOW=1
 
 cmake . -DCMAKE_BUILD_TYPE=${BUILD_TYPE}
 make clean
-# clean up before we run
-rm -Rf /tmp/kudutpch1-$UID
-mkdir /tmp/kudutpch1-$UID
 
 NUM_PROCS=$(cat /proc/cpuinfo | grep processor | wc -l)
-make -j${NUM_PROCS} 2>&1 | tee build.log
+make -j${NUM_PROCS} tpch1 2>&1 | tee build.log
 
-export TBL_PATH=/home/jdcryans/lineitem.tbl
-export SCRIPT_PATH=src/kudu/scripts/write-jobs-stats-to-mysql.py
+# Warming up the OS buffer.
+cat $LINEITEM_TBL_PATH > /dev/null
+cat $LINEITEM_TBL_PATH > /dev/null
 
-# warming up the OS buffer
-cat $TBL_PATH > /dev/null
-cat $TBL_PATH > /dev/null
+rm -Rf $KUDU_DATA_DIR   # Clean up data dir.
+mkdir -p $OUTDIR        # Create log file output dir.
 
-TPCH_OUTDIR=/data/2/tmp/kudutpch1-jenkins
-rm -rf $TPCH_OUTDIR
-./build/release/tpch1 -logtostderr=1 -tpch_path_to_data=$TBL_PATH -mini_cluster_base_dir=$TPCH_OUTDIR -tpch_num_query_iterations=5 &> benchmark.log
+./build/release/tpch1 -logtostderr=1 \
+                      -tpch_path_to_data=$LINEITEM_TBL_PATH \
+                      -mini_cluster_base_dir=$KUDU_DATA_DIR \
+                      -tpch_num_query_iterations=$TPCH_NUM_QUERY_ITERS \
+                      >$OUTDIR/benchmark.log 2>&1
 
-cat benchmark.log
-INSERT_TIME=$(grep "Time spent loading" benchmark.log | \
+cat $OUTDIR/benchmark.log
+INSERT_TIME=$(grep "Time spent loading" $OUTDIR/benchmark.log | \
     perl -pe 's/.*Time spent loading: real ([0-9\.]+)s.*/\1/')
-QUERY_TIME_1=$(grep "iteration # 1" benchmark.log | \
-    perl -pe 's/.*iteration # 1: real ([0-9\.]+)s.*/\1/')
-QUERY_TIME_2=$(grep "iteration # 2" benchmark.log | \
-    perl -pe 's/.*iteration # 2: real ([0-9\.]+)s.*/\1/')
-QUERY_TIME_3=$(grep "iteration # 3" benchmark.log | \
-    perl -pe 's/.*iteration # 3: real ([0-9\.]+)s.*/\1/')
-QUERY_TIME_4=$(grep "iteration # 4" benchmark.log | \
-    perl -pe 's/.*iteration # 4: real ([0-9\.]+)s.*/\1/')
+record_result insert_1gb 1 $INSERT_TIME
 
-python $SCRIPT_PATH $JOB_NAME $BUILD_NUMBER insert_1gb 1 $INSERT_TIME
-
-python $SCRIPT_PATH $JOB_NAME $BUILD_NUMBER query_1_1gb 1 $QUERY_TIME_1
-python $SCRIPT_PATH $JOB_NAME $BUILD_NUMBER query_1_1gb 2 $QUERY_TIME_2
-python $SCRIPT_PATH $JOB_NAME $BUILD_NUMBER query_1_1gb 3 $QUERY_TIME_3
-python $SCRIPT_PATH $JOB_NAME $BUILD_NUMBER query_1_1gb 4 $QUERY_TIME_4
+# We do not record the first iteration (#0) because we want to record the
+# in-cache performance.
+for iter in $(seq 1 $(expr $TPCH_NUM_QUERY_ITERS - 1)); do
+  QUERY_TIME=$(grep "iteration # $iter" $OUTDIR/benchmark.log | \
+      perl -pe "s/.*iteration # $iter: real ([0-9\.]+)s.*/\1/")
+  record_result query_1_1gb $iter $QUERY_TIME
+done
