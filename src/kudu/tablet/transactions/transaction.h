65b5dc12b5da72bac0bebbd759e6b40332d80c22
David Alves <david.alves@cloudera.com>
2014-09-03 21:46:58 -0700

    Add a TransactionCompletionCallback that takes a StatusCallback
    Change-Id: Ica5d488e7c0cbe789583bf26af3dd80d5451c800
    Reviewed-on: http://gerrit.sjc.cloudera.com:8080/4161
    Tested-by: jenkins
    Reviewed-by: Todd Lipcon <todd@cloudera.com>

--- src/kudu/tablet/transactions/transaction.h
+++ src/kudu/tablet/transactions/transaction.h
@@ -317,6 +317,20 @@ class LatchTransactionCompletionCallback : public TransactionCompletionCallback 
   ResponsePB* response_;
 };
 
+// A transaction completion callback that takes a StatusCallback and simply
+// calls it with the transaction status when it completes.
+class StatusTransactionCompletionCallback : public TransactionCompletionCallback {
+ public:
+  explicit StatusTransactionCompletionCallback(const StatusCallback& callback)
+   : callback_(callback) {
+  }
+
+  virtual void TransactionCompleted() OVERRIDE {
+    callback_.Run(status());
+  }
+ private:
+  StatusCallback callback_;
+};
 
 }  // namespace tablet
 }  // namespace kudu
