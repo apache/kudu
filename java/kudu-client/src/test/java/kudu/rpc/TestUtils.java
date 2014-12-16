ffc30579f908f9cb606e705e86110ba3035ccf45
Alex Feinberg <af@cloudera.com>
2014-12-15 16:29:45 -0800

    [java client] Support sending SIGSTOP/SIGCONT in Java tests
    Uses reflection to get the pid of a Process and then executes
    the "kill" command to send the specified signal. Only works on
    UNIX.
    
    TODO: Consider using a library like JNR Posix for this.
    
    Change-Id: Ib2095562f40ff9af4e86673655b6f49e9675d9e6
    Reviewed-on: http://gerrit.sjc.cloudera.com:8080/5583
    Reviewed-by: Jean-Daniel Cryans <jdcryans@cloudera.com>
    Tested-by: jenkins

--- java/kudu-client/src/test/java/kudu/rpc/TestUtils.java
+++ java/kudu-client/src/test/java/kudu/rpc/TestUtils.java
@@ -2,13 +2,22 @@
 // Confidential Cloudera Information: Covered by NDA.
 package kudu.rpc;
 
+import com.google.common.base.Joiner;
+import com.google.common.collect.ImmutableSet;
+
 import java.io.IOException;
+import java.lang.reflect.Field;
 import java.net.ServerSocket;
+import java.util.Set;
 
 /**
  * A grouping of methods that help unit testing.
  */
 public class TestUtils {
+
+  // Used by pidOfProcess()
+  private static String UNIX_PROCESS_CLS_NAME =  "java.lang.UNIXProcess";
+  private static Set VALID_SIGNALS =  ImmutableSet.of("STOP", "CONT", "TERM", "KILL");
 
   /**
    * Finds the next free port, starting with the one passed. Keep in mind the
@@ -31,5 +40,65 @@ public class TestUtils {
       return i;
     }
     throw new IOException("Ran out of ports.");
+  }
+
+  /**
+   * Gets the pid of a specified process. Relies on reflection and only works on
+   * UNIX process, not guaranteed to work on JDKs other than Oracle and OpenJDK.
+   * @param proc The specified process.
+   * @return The process UNIX pid.
+   * @throws IllegalArgumentException If the process is not a UNIXProcess.
+   * @throws Exception If there are other getting the pid via reflection.
+   */
+  static int pidOfProcess(Process proc) throws Exception {
+    Class<?> procCls = proc.getClass();
+    if (!procCls.getName().equals(UNIX_PROCESS_CLS_NAME)) {
+      throw new IllegalArgumentException("stopProcess() expects objects of class " +
+          UNIX_PROCESS_CLS_NAME + ", but " + procCls.getName() + " was passed in instead!");
+    }
+    Field pidField = procCls.getDeclaredField("pid");
+    pidField.setAccessible(true);
+    return (Integer) pidField.get(proc);
+  }
+
+  /**
+   * Send a code specified by its string representation to the specified process.
+   * TODO: Use a JNR/JNR-Posix instead of forking the JVM to exec "kill".
+   * @param proc The specified process.
+   * @param sig The string representation of the process (e.g., STOP for SIGSTOP).
+   * @throws IllegalArgumentException If the signal type is not supported.
+   * @throws IllegalStateException If we are unable to send the specified signal.
+   */
+  static void signalProcess(Process proc, String sig) throws Exception {
+    if (!VALID_SIGNALS.contains(sig)) {
+      throw new IllegalArgumentException(sig + " is not a supported signal, only " +
+              Joiner.on(",").join(VALID_SIGNALS) + " are supported");
+    }
+    int pid = pidOfProcess(proc);
+    int rv = Runtime.getRuntime()
+            .exec(String.format("kill -%s %d", sig, pid))
+            .waitFor();
+    if (rv != 0) {
+      throw new IllegalStateException(String.format("unable to send SIG%s to process %s(pid=%d): " +
+              "expected return code from kill, but got %d instead", sig, proc, pid, rv));
+    }
+  }
+
+  /**
+   * Pause the specified process by sending a SIGSTOP using the kill command.
+   * @param proc The specified process.
+   * @throws Exception If error prevents us from pausing the process.
+   */
+  static void pauseProcess(Process proc) throws Exception {
+    signalProcess(proc, "STOP");
+  }
+
+  /**
+   * Resumes the specified process by sending a SIGCONT using the kill command.
+   * @param proc The specified process.
+   * @throws Exception If error prevents us from resuming the process.
+   */
+  static void resumeProcess(Process proc) throws Exception {
+    signalProcess(proc, "CONT");
   }
 }
