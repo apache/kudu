a1384694138e64b988ce0b1368e23bc8944b55ae
Todd Lipcon <todd@cloudera.com>
2014-12-17 22:36:26 -0800

    benchmarks: ensure CPU governor is set to 'performance'
    Locally I was able to reproduce that various benchmarks have higher variance
    when the CPU governor is set to 'ondemand'. I also noticed that our benchmark
    boxes are running with 'ondemand' as the default.  This changes the benchmark
    script to reset the CPU governor to 'performance' before running.
    
    Change-Id: I08809b59401e7c8dda7ed0f313cddac3cbadc137
    Reviewed-on: http://gerrit.sjc.cloudera.com:8080/5624
    Reviewed-by: Adar Dembo <adar@cloudera.com>
    Tested-by: Todd Lipcon <todd@cloudera.com>

--- src/kudu/scripts/benchmarks.sh
+++ src/kudu/scripts/benchmarks.sh
@@ -71,6 +71,32 @@ usage_and_die() {
   exit 1
 }
 
+# Ensure that the CPU governor is set to a particular governor, outputting
+# the prior governor on stdout.
+#
+# Without this, some of our tests end up having higher variance due to
+# changing CPU speed during the test.
+#
+# Assumes that all CPUs are set to the same governor.
+ensure_cpu_scaling() {
+  target_governor=$1
+  old_governor=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor)
+  for cpu_dir in /sys/devices/system/cpu/cpu[0-9]*/ ; do
+    governor_file=$cpu_dir/cpufreq/scaling_governor
+    governor=$(cat $governor_file)
+    if [ "$governor" != "$target_governor" ]; then
+      >&2 echo "CPU $cpu_dir not in '$target_governor' mode. Attempting to change"
+      echo $target_governor | sudo tee $governor_file > /dev/null
+      if [ $? -ne 0 ]; then
+        >&2 echo Could not set $target_governor governor!
+        >&2 echo Perhaps you need passwordless sudo for this user
+        exit 1
+      fi
+    fi
+  done
+  echo $old_governor
+}
+
 record_result() {
   local BUILD_IDENTIFIER=$1
   local TEST_NAME=$2
@@ -562,6 +588,13 @@ fi
 # Set up environment.
 ulimit -m $[3000*1000]
 ulimit -c unlimited   # gather core dumps
+
+# Set CPU governor, and restore it on exit.
+old_governor=$(ensure_cpu_scaling performance)
+restore_governor() {
+  ensure_cpu_scaling $old_governor >/dev/null
+}
+trap restore_governor EXIT
 
 # Create output directories if needed.
 [ -d "$LOGDIR" ] || mkdir -p "$LOGDIR"
