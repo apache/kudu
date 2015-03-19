c9ad4daf5ca22aa9e65180015b1cbc4323df3120
Todd Lipcon <todd@cloudera.com>
2015-03-19 10:49:22 -0700

    Add annotations for ASAN to gutil
    Change-Id: I6e4b393ef0a6389d41ea9eb2087a6a2b039afeb7
    Reviewed-on: http://gerrit.sjc.cloudera.com:8080/6194
    Tested-by: jenkins
    Reviewed-by: Adar Dembo <adar@cloudera.com>

--- src/kudu/gutil/dynamic_annotations.h
+++ src/kudu/gutil/dynamic_annotations.h
@@ -572,6 +572,52 @@ int RunningOnValgrind(void);
  */
 double ValgrindSlowdown(void);
 
+
+/* AddressSanitizer annotations from LLVM asan_interface.h */
+
+// Marks memory region [addr, addr+size) as unaddressable.
+// This memory must be previously allocated by the user program. Accessing
+// addresses in this region from instrumented code is forbidden until
+// this region is unpoisoned. This function is not guaranteed to poison
+// the whole region - it may poison only subregion of [addr, addr+size) due
+// to ASan alignment restrictions.
+// Method is NOT thread-safe in the sense that no two threads can
+// (un)poison memory in the same memory region simultaneously.
+void __asan_poison_memory_region(void const volatile *addr, size_t size);
+// Marks memory region [addr, addr+size) as addressable.
+// This memory must be previously allocated by the user program. Accessing
+// addresses in this region is allowed until this region is poisoned again.
+// This function may unpoison a superregion of [addr, addr+size) due to
+// ASan alignment restrictions.
+// Method is NOT thread-safe in the sense that no two threads can
+// (un)poison memory in the same memory region simultaneously.
+void __asan_unpoison_memory_region(void const volatile *addr, size_t size);
+
+// User code should use macros instead of functions.
+#if defined(__SANITIZE_ADDRESS__) || defined(ADDRESS_SANITIZER)
+#define ASAN_POISON_MEMORY_REGION(addr, size)   \
+  __asan_poison_memory_region((addr), (size))
+#define ASAN_UNPOISON_MEMORY_REGION(addr, size) \
+  __asan_unpoison_memory_region((addr), (size))
+#else
+#define ASAN_POISON_MEMORY_REGION(addr, size)   \
+  ((void)(addr), (void)(size))
+#define ASAN_UNPOISON_MEMORY_REGION(addr, size) \
+  ((void)(addr), (void)(size))
+#endif
+
+// Sets the callback to be called right before death on error.
+// Passing 0 will unset the callback.
+void __asan_set_death_callback(void (*callback)(void));
+
+#if defined(__SANITIZE_ADDRESS__) || defined(ADDRESS_SANITIZER)
+#define ASAN_SET_DEATH_CALLBACK(cb)   \
+  __asan_set_death_callback((cb))
+#else
+#define ASAN_SET_DEATH_CALLBACK(cb)   \
+  ((void)(cb))
+#endif
+
 #ifdef __cplusplus
 }
 #endif
