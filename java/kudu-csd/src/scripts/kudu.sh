ba36b98f0c14c8e4c03e39468ef3dc1837c8b3df
Dave Wang <dsw@cloudera.com>
2015-04-16 22:16:29 -0700

    KUDU-696. Kudu CSD
    Basic start/stop functionality and process-level metrics are provided.
    
    Ability to specify data directories, as well as integration with the
    role/host rules engine are still needed.  Both of those depend on
    further work to be done in CM.
    
    Change-Id: I4e721e2e96e7acdf028c921edaf68694ad673e28
    Reviewed-on: http://gerrit.sjc.cloudera.com:8080/6353
    Tested-by: jenkins
    Reviewed-by: Adar Dembo <adar@cloudera.com>

---
+++ java/kudu-csd/src/scripts/kudu.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+##
+#
+# Licensed to Cloudera, Inc. under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  Cloudera, Inc. licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##
+
+set -x
+
+# Time marker for both stderr and stdout
+date 1>&2
+
+export KUDU_HOME=${KUDU_HOME:-/usr/lib/kudu}
+
+CMD=$1
+LOG_DIR=$2
+MASTER_FILE=$CONF_DIR/$3
+DEFAULT_NUM_REPLICAS=$4
+shift 2
+
+echo "KUDU_HOME: $KUDU_HOME"
+echo "CONF_DIR: $CONF_DIR"
+echo "CMD: $CMD"
+echo "LOG_DIR: $LOG_DIR"
+echo "MASTER_FILE: $MASTER_FILE"
+echo "DEFAULT_NUM_REPLICAS: $DEFAULT_NUM_REPLICAS"
+
+function log {
+  timestamp=$(date)
+  echo "$timestamp: $1"       #stdout
+  echo "$timestamp: $1" 1>&2; #stderr
+}
+
+# Reads a line in the format "$host:$key=$value", setting those variables.
+function readconf {
+  local conf
+  IFS=':' read host conf <<< "$1"
+  IFS='=' read key value <<< "$conf"
+}
+
+if [ -f "$MASTER_FILE" ]; then
+  MASTER_IPS=
+  for line in $(cat "$MASTER_FILE")
+  do
+    readconf "$line"
+    case $key in
+      server.address)
+        # Fall back to the host only if there's no defined value.
+        if [ -n "$value" ]; then
+          actual_value="$value"
+        else
+          actual_value="$host"
+        fi
+
+        # Append to comma-separated MASTER_IPS.
+        if [ -n "$MASTER_IPS" ]; then
+          MASTER_IPS="${MASTER_IPS},"
+        fi
+        MASTER_IPS="${MASTER_IPS}${actual_value}"
+        ;;
+    esac
+  done
+  log "Found master(s) on $MASTER_IPS"
+fi
+
+if [ "$CMD" = "master" ]; then
+  # Only pass --master_quorum if there's more than one master.
+  #
+  # Need to use [[ ]] for regex support.
+  if [[ "$MASTER_IPS" =~ , ]]; then
+    MASTER_QUORUM="--master_quorum=$MASTER_IPS"
+  fi
+
+  exec "$KUDU_HOME/bin/kudu-master" \
+    --log_dir="$LOG_DIR" \
+    $MASTER_QUORUM \
+    --default_num_replicas=$DEFAULT_NUM_REPLICAS
+elif [ "$CMD" = "tserver" ]; then
+  exec "$KUDU_HOME/bin/kudu-tablet_server" \
+    --log_dir="$LOG_DIR" \
+    --tablet_server_master_addrs="$MASTER_IPS"
+else
+  log "Unknown command: $CMD"
+  exit 2
+fi
