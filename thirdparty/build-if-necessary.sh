5831908d822bd7911adb889b2c45f0ddf4e75a2d
Adar Dembo <adar@cloudera.com>
2015-07-31 19:34:21 -0700

    thirdparty: delete the cached git hash immediately upon rebuilding
    Imagine a sequence of three builds:
    1. Thirdparty's git hash is foo. We've never built thirdparty, so we do that
       first and write 'foo' to .build-hash. The rest of the build completes.
    2. Thirdparty's git hash is bar. We compare 'bar' to 'foo' and start
       rebuilding thirdparty. Halfway through, the build is aborted. Now
       thirdparty/installed contains a mix of stuff from 'foo' and 'bar', and
       .build-hash is still 'foo'.
    3. Thirdparty's git hash is, once again, foo. We compare 'foo' to 'foo';
       they match, so we don't rebuild thirdparty. The rest of the build fails
       because thirdparty/installed is inconsistent.
    
    This actually happened on a (surprisingly) long-lived build slave, so let's
    prevent it from happening again by removing .build-hash before running
    anything that might touch thirdparty/installed.
    
    A side effect is that if you build locally, notice that your thirdparty is
    rebuilding, and interrupt it, you'll need to pass NO_REBUILD_THIRDPARTY=1
    into subsequent builds until .build-hash is regenerated. That's annoying,
    but worth it to fix this bug.
    
    Change-Id: I6785f5f99095e88c5d22c63450787b282cc04678
    Reviewed-on: http://gerrit.sjc.cloudera.com:8080/7306
    Tested-by: jenkins
    Reviewed-by: Todd Lipcon <todd@cloudera.com>

--- thirdparty/build-if-necessary.sh
+++ thirdparty/build-if-necessary.sh
@@ -3,7 +3,7 @@
 #
 # Script which downloads and builds the thirdparty dependencies
 # only if necessary (i.e if they have changed or the local repository
-# is dirty)
+# is dirty).
 
 set -e
 set -o pipefail
@@ -18,18 +18,20 @@ NEEDS_BUILD=
 CUR_THIRDPARTY_HASH=$(cd .. && git ls-tree -d HEAD thirdparty | awk '{print $3}')
 LAST_BUILD_HASH=$(cat .build-hash || :)
 if [ "$CUR_THIRDPARTY_HASH" != "$LAST_BUILD_HASH" ]; then
-  echo The git repository has changed since thirdparty was last
-  echo downloaded/built. Requiring rebuild.
+  echo "Rebuilding thirdparty: the repository has changed since thirdparty was last built."
+  echo "Old git hash: $LAST_BUILD_HASH"
+  echo "New build hash: $CUR_THIRDPARTY_HASH"
   NEEDS_BUILD=1
 else
   # Determine whether the developer has any local changes
   if ! ( git diff --quiet .  && git diff --cached --quiet . ) ; then
-    echo There are local changes in the git repo. Requiring rebuild.
+    echo "Rebuilding thirdparty: There are local changes in the repository."
     NEEDS_BUILD=1
   fi
 fi
 
 if [ -n "$NEEDS_BUILD" ]; then
+  rm -f .build-hash
   ./download-thirdparty.sh
   ./build-thirdparty.sh
   echo $CUR_THIRDPARTY_HASH > .build-hash
