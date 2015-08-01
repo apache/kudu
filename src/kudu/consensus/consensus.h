apache/kudu

commit 4a98d4b0ce54bda87ff3f8f4f63940a8d998fb63

Only change TabletPeer state to RUNNING after consensus starts
This should fix a source of flakiness in alter_table-randomized-test
where we see the following assertion failure:

F0725 01:32:48.067261 12247 local_consensus.cc:100] Check failed: state_ >= kConfiguring (1 vs. 2)

This race occurred if an RPC arrived for a tablet after the peer state switched
to RUNNING, but before Consensus was ready to service requests.

Change-Id: I2a3d129c02c2ceba44addf017d04db905389985f
Reviewed-on: http://gerrit.sjc.cloudera.com:8080/7231
Reviewed-by: David Alves <david.alves@cloudera.com>
Tested-by: jenkins

src/kudu/tablet/tablet_peer.cc
@@ -175,18 +175,12 @@ Status TabletPeer::Start(const ConsensusBootstrapInfo& bootstrap_info) {
 
   VLOG(2) << "RaftConfig before starting: " << consensus_->CommittedConfig().DebugString();
 
-  // TODO we likely should only change the state after starting consensus
-  // but if we do that a lot of tests fail. We can't include Consensus::Start()
-  // with the lock either or we'd get a deadlock. We should investigate why
-  // this is happening and move the state change to the right place but this
-  // should be good enough for now.
+  RETURN_NOT_OK(consensus_->Start(bootstrap_info));
   {
     boost::lock_guard<simple_spinlock> lock(lock_);
     CHECK_EQ(state_, BOOTSTRAPPING);
     state_ = RUNNING;
   }
-
-  RETURN_NOT_OK(consensus_->Start(bootstrap_info));
 
   return Status::OK();
 }
@@ -415,7 +409,11 @@ Status TabletPeer::GetGCableDataSize(int64_t* retention_size) const {
 }
 
 Status TabletPeer::StartReplicaTransaction(const scoped_refptr<ConsensusRound>& round) {
-  RETURN_NOT_OK(CheckRunning());
+  {
+    boost::lock_guard<simple_spinlock> lock(lock_);
+    CHECK(state_ == RUNNING || state_ == BOOTSTRAPPING)
+      << "Bad state: " << TabletStatePB_Name(state_);
+  }
 
   consensus::ReplicateMsg* replicate_msg = round->replicate_msg();
   DCHECK(replicate_msg->has_timestamp());

commit b28ba7870df9b2a2a34e0b7de4c178e775b9afe2

KUDU-891. Fix occasional timeout while creating tables
This should fix a flakiness seen in client-test where table creation
would time out.

The bug turned out to be a race condition introduced in
4a98d4b0ce54bda87ff3f8f4f63940a8d998fb63, which changed TabletPeer to
transition state_from BOOTSTRAPPING to RUNNING only after Consensus was
started.

In most cases, Consensus would mark the tablet peer dirty in Start(), but the
heartbeat wouldn't be generated until some tens of milliseconds later (when the
heartbeat thread got scheduled). So, this would give the TabletPeer time to
switch from BOOTSTRAPPING to RUNNING before the tablet report was generated,
and we'd almost always report the RUNNING state to the master.

It's possible, though, that the TabletPeer::Start() thread would get context
switched after starting consensus but before changing the state to RUNNING. In
this case, we could heartbeat the BOOTSTRAPPING state. In that case, the
master would decide that the tablet was stuck bootstrapping, and thus that
the table was still in the process of being created.

client-test hit this problem more than most because it sets the heartbeat
interval to 10ms vs the longer default, increasing the chances that a heartbeat
would be sent between Consensus starting and the state transitioning.

The fix is simple: TabletPeer::Start() has to mark the tablet dirty after
changing its state. In most cases this won't generate a double heartbeat, since
MarkDirty() is idempotent in the case that the heartbeat hasn't been sent yet.

After moving this call to TabletPeer, there's no longer a need to call it in
LocalConsensus, so I removed it from there (and the Consensus interface).

In trying to debug this, I also noticed that TSTabletManager was taking a
read lock in one spot where it should have taken a write lock, and made that
one-line change here too.

There's no new unit test for this since it's a race that's hard to reproduce,

src/kudu/consensus/consensus.h
@@ -234,10 +234,6 @@ class Consensus : public RefCountedThreadSafe<Consensus> {
   // TODO Remove once we have solid election.
   virtual Status GetLastReceivedOpId(OpId* id) { return Status::NotFound("Not implemented."); }
 
-  // Marks the configuration as dirty.
-  // TODO: Make this private if possible.
-  virtual void MarkDirty() = 0;
-
  protected:
   friend class RefCountedThreadSafe<Consensus>;
   friend class tablet::TabletPeer;

src/kudu/consensus/local_consensus.cc
@@ -30,15 +30,13 @@ LocalConsensus::LocalConsensus(const ConsensusOptions& options,
                                const string& peer_uuid,
                                const scoped_refptr<server::Clock>& clock,
                                ReplicaTransactionFactory* txn_factory,
-                               Log* log,
-                               const Closure& mark_dirty_clbk)
+                               Log* log)
     : peer_uuid_(peer_uuid),
       options_(options),
       cmeta_(cmeta.Pass()),
       txn_factory_(DCHECK_NOTNULL(txn_factory)),
       log_(DCHECK_NOTNULL(log)),
       clock_(clock),
-      mark_dirty_clbk_(mark_dirty_clbk),
       state_(kInitializing),
       next_op_id_index_(-1) {
   CHECK(cmeta_) << "Passed ConsensusMetadata object is NULL";
@@ -68,9 +66,7 @@ Status LocalConsensus::Start(const ConsensusBootstrapInfo& info) {
 
     state_ = kRunning;
   }
-
   TRACE("Consensus started");
-  MarkDirty();
   return Status::OK();
 }
 
@@ -155,10 +151,6 @@ Status LocalConsensus::Update(const ConsensusRequestPB* request,
 Status LocalConsensus::RequestVote(const VoteRequestPB* request,
                                    VoteResponsePB* response) {
   return Status::NotSupported("LocalConsensus does not support RequestVote() calls.");
-}
-
-void LocalConsensus::MarkDirty() {
-  mark_dirty_clbk_.Run();
 }
 
 ConsensusStatePB LocalConsensus::CommittedConsensusState() const {

src/kudu/consensus/local_consensus.h
@@ -37,8 +37,7 @@ class LocalConsensus : public Consensus {
                           const std::string& peer_uuid,
                           const scoped_refptr<server::Clock>& clock,
                           ReplicaTransactionFactory* txn_factory,
-                          log::Log* log,
-                          const Closure& mark_dirty_clbk);
+                          log::Log* log);
 
   virtual Status Start(const ConsensusBootstrapInfo& info) OVERRIDE;
 
@@ -68,8 +67,6 @@ class LocalConsensus : public Consensus {
 
   virtual void DumpStatusHtml(std::ostream& out) const OVERRIDE;
 
-  virtual void MarkDirty() OVERRIDE;
-
   //
   //  NOT IMPLEMENTED IN LOCAL CONSENSUS
   //
@@ -96,7 +93,6 @@ class LocalConsensus : public Consensus {
   ReplicaTransactionFactory* const txn_factory_;
   log::Log* const log_;
   const scoped_refptr<server::Clock> clock_;
-  const Closure mark_dirty_clbk_;
 
   // Protects 'state_' and 'next_op_id_index_'.
   mutable simple_spinlock lock_;

src/kudu/consensus/raft_consensus.h
@@ -143,8 +143,6 @@ class RaftConsensus : public Consensus,
 
   virtual Status GetLastReceivedOpId(OpId* id) OVERRIDE;
 
-  virtual void MarkDirty() OVERRIDE;
-
  protected:
   // Trigger that a non-Transaction ConsensusRound has finished replication.
   // If the replication was successful, an status will be OK. Otherwise, it
@@ -361,6 +359,10 @@ class RaftConsensus : public Consensus,
 
   // Handle when the term has advanced beyond the current term.
   Status HandleTermAdvanceUnlocked(ConsensusTerm new_term);
+
+  // Asynchronously (on thread_pool_) notify the tablet peer that the consensus configuration
+  // has changed, thus reporting it back to the master.
+  void MarkDirty();
 
   // Threadpool for constructing requests to peers, handling RPC callbacks,
   // etc.

src/kudu/tablet/tablet_peer.cc
@@ -143,8 +143,7 @@ Status TabletPeer::Init(const shared_ptr<Tablet>& tablet,
                                           meta_->fs_manager()->uuid(),
                                           clock_,
                                           this,
-                                          log_.get(),
-                                          mark_dirty_clbk_));
+                                          log_.get()));
     } else {
       consensus_ = RaftConsensus::Create(options,
                                          cmeta.Pass(),
@@ -183,6 +182,9 @@ Status TabletPeer::Start(const ConsensusBootstrapInfo& bootstrap_info) {
     CHECK_EQ(state_, BOOTSTRAPPING);
     state_ = RUNNING;
   }
+
+  // Because we changed the tablet state, we need to re-report the tablet to the master.
+  mark_dirty_clbk_.Run();
 
   return Status::OK();
 }

src/kudu/tserver/ts_tablet_manager.cc
@@ -621,7 +621,7 @@ void TSTabletManager::GenerateFullTabletReport(TabletReportPB* report) {
 }
 
 void TSTabletManager::MarkTabletReportAcknowledged(const TabletReportPB& report) {
-  boost::shared_lock<rw_spinlock> shared_lock(lock_);
+  boost::lock_guard<rw_spinlock> l(lock_);
 
   int32_t acked_seq = report.sequence_number();
   CHECK_LT(acked_seq, next_report_seq_);