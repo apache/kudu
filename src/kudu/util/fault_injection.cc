27c0ef875e733cb91418c62aaeddc56ca7610d90
Todd Lipcon <todd@cloudera.com>
2015-08-06 19:48:13 -0700

    KUDU-942. Disable core dumps when crashing due to fault injection
    This adds a simple call to setrlimit to disable core dumps when crashing due to
    an injected fault. This should prevent spewing core files to the disk in tests
    where we inject faults and repeatedly restart servers (for example,
    RaftConsensusITest.InsertWithCrashNodes).
    
    I manually tested this by running the above-mentioned test with `ulimit -c
    unlimited`, and ctrl-Cing the test case just after the fault was injected.
    Without this patch, I saw a core file. With it, I didn't.
    
    Change-Id: I5bb340942ff11b91aa682588ff1c90b4da1ab9d9
    Reviewed-on: http://gerrit.sjc.cloudera.com:8080/7382
    Reviewed-by: Adar Dembo <adar@cloudera.com>
    Tested-by: jenkins

--- src/kudu/util/fault_injection.cc
+++ src/kudu/util/fault_injection.cc
@@ -3,6 +3,9 @@
 
 #include "kudu/util/fault_injection.h"
 
+#include <sys/resource.h>
+#include <sys/time.h>
+
 #include "kudu/gutil/once.h"
 #include "kudu/util/debug/leakcheck_disabler.h"
 #include "kudu/util/random.h"
@@ -32,6 +35,14 @@ void DoMaybeFault(const char* fault_str, double fraction) {
   if (PREDICT_TRUE(g_random->NextDoubleFraction() >= fraction)) {
     return;
   }
+
+  // Disable core dumps -- it's not useful to get a core dump when we're
+  // purposefully crashing, and some tests cause lots of server crashes
+  // in a loop. This avoids filling up the disk with useless cores.
+  struct rlimit lim;
+  PCHECK(getrlimit(RLIMIT_CORE, &lim) == 0);
+  lim.rlim_cur = 0;
+  PCHECK(setrlimit(RLIMIT_CORE, &lim) == 0);
 
   LOG(FATAL) << "Injected fault: " << fault_str;
 }
