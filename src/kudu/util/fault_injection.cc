d3fc252a12f08ba56ad68e97240ec1d2390788fc
Todd Lipcon <todd@cloudera.com>
2015-08-10 12:53:28 -0700

    fault_injection: set coredump_filter to not dump entire address space into abrt
    On systems where we have 'abrtd' configured, core dumps are piped into the abrt
    core processor regardless of the RLIMIT_CORE setting on the process. This
    changes the coredump_filter to make the piped core minimal.
    
    This sped up delete_table-test from 120s to 20s on a system with abrt
    configured.
    
    Change-Id: Ie740097034a96015ea138710b705310dda5920e5
    Reviewed-on: http://gerrit.sjc.cloudera.com:8080/7403
    Reviewed-by: Adar Dembo <adar@cloudera.com>
    Tested-by: jenkins

--- src/kudu/util/fault_injection.cc
+++ src/kudu/util/fault_injection.cc
@@ -3,6 +3,7 @@
 
 #include "kudu/util/fault_injection.h"
 
+#include <fcntl.h>
 #include <sys/resource.h>
 #include <sys/time.h>
 
@@ -43,6 +44,17 @@ void DoMaybeFault(const char* fault_str, double fraction) {
   PCHECK(getrlimit(RLIMIT_CORE, &lim) == 0);
   lim.rlim_cur = 0;
   PCHECK(setrlimit(RLIMIT_CORE, &lim) == 0);
+
+  // Set coredump_filter to not dump any parts of the address space.
+  // Although the above disables core dumps to files, if core_pattern
+  // is set to a pipe rather than a file, it's not sufficient. Setting
+  // this pattern results in piping a very minimal dump into the core
+  // processor (eg abrtd), thus speeding up the crash.
+  int f = open("/proc/self/coredump_filter", O_WRONLY);
+  if (f >= 0) {
+    write(f, "00000000", 8);
+    close(f);
+  }
 
   LOG(FATAL) << "Injected fault: " << fault_str;
 }
