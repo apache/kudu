17e56c3ee0e8d87c463ff1874e44441b47fcfa33
Todd Lipcon <todd@cloudera.com>
2015-09-24 15:28:12 -0700

    Update master branch to 0.6.0-SNAPSHOT
    Now that we created a branch for 0.5.0, we should bump our dev builds
    to 0.6.0-SNAPSHOT so that we don't confuse the two.
    
    Note that I didn't update the docs to say 0.6.0 yet -- I figure we can
    continue to use 'master' to build docs for now so that less stuff is
    shifting around.
    
    Change-Id: I2171a7ae75fa54ef46e6630d624cc363a90133db
    Reviewed-on: http://gerrit.sjc.cloudera.com:8080/8496
    Tested-by: jenkins
    Reviewed-by: Michael Percy <mpercy@cloudera.com>

--- java/kudu-csd/generate_mdl.py
+++ java/kudu-csd/generate_mdl.py
@@ -128,7 +128,7 @@ def main():
 
   output = dict(
     name="KUDU",
-    version="0.5.0",
+    version="0.6.0",
     metricDefinitions=[],
     nameForCrossEntityAggregateMetrics="kudus",
     roles=[
