9d25ab4dac842f228b8fbc258af2054e74a63512
Mike Percy <mpercy@cloudera.com>
2015-09-24 01:59:34 -0700

    Add ASL 2.0 header comments to source files
    These changes were automatically performed by a Perl script based on
    some regular expressions. This doesn't cover all of the files that have
    a Cloudera copyright notice, but it covers 95% of them.
    
    I will follow up with another couple of patches to deal with the files
    that my regular expressions couldn't easily handle.
    
    Change-Id: I86863d9195449e5788ac8cb235ce2f3b720484a4
    Reviewed-on: http://gerrit.sjc.cloudera.com:8080/8473
    Tested-by: Michael Percy <mpercy@cloudera.com>
    Reviewed-by: Todd Lipcon <todd@cloudera.com>

--- src/kudu/util/debug_ref_counted.h
+++ src/kudu/util/debug_ref_counted.h
@@ -1,5 +1,16 @@
-// Copyright (c) 2014 Cloudera, Inc.
-// Confidential Cloudera Information: Covered by NDA.
+// Copyright 2014 Cloudera, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 #ifndef KUDU_UTIL_DEBUG_REF_COUNTED_H_
 #define KUDU_UTIL_DEBUG_REF_COUNTED_H_
