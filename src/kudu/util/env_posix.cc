apache/kudu

commit 40c3606a736b8b97ffbdce443929b93c2bf402d2

Canonicalize test temp directory path
The /tmp directory on OS X is a symlink. This causes issues with a few tests
that compare the temp dir path against a canonicalized version.

Change-Id: Ib38aeb9083ca577bcd6a40aad1b4febfdb54b23e
Reviewed-on: http://gerrit.cloudera.org:8080/1333
Reviewed-by: Mike Percy <mpercy@cloudera.com>
Tested-by: Internal Jenkins

src/kudu/util/env_posix.cc
@@ -1221,17 +1221,19 @@ class PosixEnv : public Env {
   }
 
   virtual Status GetTestDirectory(std::string* result) OVERRIDE {
+    string dir;
     const char* env = getenv("TEST_TMPDIR");
     if (env && env[0] != '\0') {
-      *result = env;
+      dir = env;
     } else {
       char buf[100];
       snprintf(buf, sizeof(buf), "/tmp/kudutest-%d", static_cast<int>(geteuid()));
-      *result = buf;
+      dir = buf;
     }
     // Directory may already exist
     ignore_result(CreateDir(*result));
-    return Status::OK();
+    // /tmp may be a symlink, so canonicalize the path.
+    return Canonicalize(dir, result);
   }
 
   virtual uint64_t gettid() OVERRIDE {

commit 9dbc789c0f1a6f2a0048c91752a44ff5990d5120

Fix test tmp dir bug
40c3606a736b8b97 introduced a bug where the test temporary directory would not
be created automatically.

Change-Id: Ie0a007fc7fa697cf33e4bd694c8d558e9071d739
Reviewed-on: http://gerrit.cloudera.org:8080/1353
Reviewed-by: Todd Lipcon <todd@cloudera.com>
Tested-by: Internal Jenkins

src/kudu/util/env_posix.cc
@@ -1231,7 +1231,7 @@ class PosixEnv : public Env {
       dir = buf;
     }
     // Directory may already exist
-    ignore_result(CreateDir(*result));
+    ignore_result(CreateDir(dir));
     // /tmp may be a symlink, so canonicalize the path.
     return Canonicalize(dir, result);
   }