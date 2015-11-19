9ceeb66ec8de9147bbb25c9a4a63248d28f4e03c
Todd Lipcon <todd@cloudera.com>
2015-11-18 21:14:48 -0800

    rpc: improve docs and remove unused fields
    The RPC system inherited some protobufs from Hadoop and HBase which were never
    used. Since no version ever read or wrote them, it's safe to remove.
    
    Some other fields only existed in commented-out form, so I removed them as
    well.
    
    The one interesting field is SaslAuth.method, which was marked 'required' and
    always set to "". I changed it to optional, but kept the code which sets the
    field, so that new servers will continue to work against old clients which
    expect the field to be required. However, by making it optional, we can at
    some point remove it once we stop needing to support compatibility with
    Kudu releases prior to 0.7.0.
    
    I additionally cleaned up and improved the documentation, removed various
    non-Kudu-related information from the proto definition, and clarified some
    confusing points.
    
    Change-Id: I8526322d37f329697db7a95aa34b7816e9a28512
    Reviewed-on: http://gerrit.cloudera.org:8080/1493
    Tested-by: Internal Jenkins
    Reviewed-by: Jean-Daniel Cryans
    Reviewed-by: Mike Percy <mpercy@cloudera.com>

--- src/kudu/rpc/sasl_server.cc
+++ src/kudu/rpc/sasl_server.cc
@@ -291,7 +291,11 @@ Status SaslServer::SendNegotiateResponse(const set<string>& server_mechs) {
 
   BOOST_FOREACH(const string& mech, server_mechs) {
     SaslMessagePB::SaslAuth* auth = response.add_auths();
-    auth->set_method(""); // TODO: Support "method" in some meaningful way.
+
+    // The 'method' field is deprecated, but older versions of Kudu marked it 'required'.
+    // So, we have to set it to something to keep compatibility. At some point, we can
+    // consider removing it and breaking compatibility with Kudu <=0.6.
+    auth->set_method("");
     auth->set_mechanism(mech);
   }
 
