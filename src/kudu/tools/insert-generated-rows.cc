772fb0e9efce14840d857b3cc3985f7082b90792
Todd Lipcon <todd@cloudera.com>
2016-01-06 21:11:07 -0800

    Update Apache license text based on the ASF policy
    This commit uses an automated script[1] to update the license text to match the
    ASF Source Header and Copyright Notice Policy. This does not yet handle updating
    thirdparty sources like gutil/ to fully comply.
    
    [1] https://gist.github.com/anonymous/ff71292094362fc5c594
    [2] http://www.apache.org/legal/src-headers.html#3party
    
    Change-Id: I16a89f062ffddd859dce72e3a26e72798fe7c745
    Reviewed-on: http://gerrit.cloudera.org:8080/1726
    Reviewed-by: Adar Dembo <adar@cloudera.com>
    Tested-by: Internal Jenkins

--- src/kudu/tools/insert-generated-rows.cc
+++ src/kudu/tools/insert-generated-rows.cc
@@ -1,15 +1,19 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+//   http://www.apache.org/licenses/LICENSE-2.0
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
 //
 // Simple tool to insert "random junk" rows into an arbitrary table.
 // First column is in ascending order, the rest are random data.
