apache/kudu

commit 65b7e5b2cc5c9e7fde64e6658a6334094a739403

KUDU-25: tool to dump contents of protobuf container files
This commit introduces pbc-dump (binary is kudu-pbc-dump), a tool to dump
all contents of protobuf container files.

A technique for parsing any protobuf message is described in the PB
documentation[1]. I followed the general gist, taking advantage of the fact
that, when writing container files, any FileDescriptorProto can be had by
calling msg->GetDescriptor()->file(). This means we don't need to go through
protoc, which is far more laborious (ask protoc to emit FileDescriptorSets,
ship them with Kudu, and load them at runtime).

Now, each container needs to include its PB type information. I opted to add
this via a "supplemental header" PB. This lets us take advantage of the
existing PB parsing code in pb_util, and gives us a place to add more
container metadata in the future.

Lastly, the PB type now describes the contents of the container, so I
changed the container "magic" string to be more generic.

1. https://developers.google.com/protocol-buffers/docs/techniques#self-description

Change-Id: I7449e2896845aeb801a3028ebe0f946e329e98ce
Reviewed-on: http://gerrit.sjc.cloudera.com:8080/5763
Tested-by: jenkins
Reviewed-by: Todd Lipcon <todd@cloudera.com>

java/kudu-client/pom.xml
@@ -78,6 +78,11 @@
                     <checkStaleness>true</checkStaleness>
                     <protoSourceRoot>${project.basedir}/../../src</protoSourceRoot>
                     <protocExecutable>${project.basedir}/dev-support/build-proto.sh</protocExecutable>
+                    <additionalProtoPathElements>
+                      <additionalProtoPathElement>
+                        ${project.basedir}/../../thirdparty/installed/include
+                      </additionalProtoPathElement>
+                    </additionalProtoPathElements>
                 </configuration>
                 <executions>
                     <execution>

cmake_modules/FindKRPC.cmake
@@ -69,12 +69,15 @@ function(KRPC_GENERATE SRCS HDRS)
       OUTPUT "${SERVICE_CC}" "${SERVICE_H}" "${PROXY_CC}" "${PROXY_H}"
              "${PROTO_CC_OUT}" "${PROTO_H_OUT}"
       COMMAND  ${PROTOBUF_PROTOC_EXECUTABLE}
-      ARGS --plugin=$<TARGET_FILE:protoc-gen-krpc>
-           --plugin=$<TARGET_FILE:protoc-gen-insertions>
+      ARGS --plugin $<TARGET_FILE:protoc-gen-krpc>
+           --plugin $<TARGET_FILE:protoc-gen-insertions>
            --cpp_out ${ARG_BINARY_ROOT}
            --krpc_out ${ARG_BINARY_ROOT}
            --insertions_out ${ARG_BINARY_ROOT}
-           --proto_path ${ARG_SOURCE_ROOT} ${EXTRA_PROTO_PATH_ARGS} ${ABS_FIL}
+           --proto_path ${ARG_SOURCE_ROOT}
+           # Used to find built-in .proto files (e.g. FileDescriptorProto)
+           --proto_path ${PROTOBUF_INCLUDE_DIR}
+           ${EXTRA_PROTO_PATH_ARGS} ${ABS_FIL}
       DEPENDS ${ABS_FIL} protoc-gen-krpc protoc-gen-insertions
       COMMENT "Running protocol buffer compiler with KRPC plugin on ${FIL}"
       VERBATIM)

cmake_modules/FindProtobuf.cmake
@@ -120,10 +120,13 @@ function(PROTOBUF_GENERATE_CPP SRCS HDRS)
       OUTPUT "${PROTO_CC_OUT}" "${PROTO_H_OUT}"
       COMMAND  ${PROTOBUF_PROTOC_EXECUTABLE}
       ARGS
-        --plugin=$<TARGET_FILE:protoc-gen-insertions>
+        --plugin $<TARGET_FILE:protoc-gen-insertions>
         --cpp_out ${ARG_BINARY_ROOT}
         --insertions_out ${ARG_BINARY_ROOT}
-        --proto_path ${ARG_SOURCE_ROOT} ${EXTRA_PROTO_PATH_ARGS} ${ABS_FIL}
+        --proto_path ${ARG_SOURCE_ROOT}
+        # Used to find built-in .proto files (e.g. FileDescriptorProto)
+        --proto_path ${PROTOBUF_INCLUDE_DIR}
+        ${EXTRA_PROTO_PATH_ARGS} ${ABS_FIL}
       DEPENDS ${ABS_FIL} protoc-gen-insertions
       COMMENT "Running C++ protocol buffer compiler on ${FIL}"
       VERBATIM )

src/kudu/consensus/consensus_meta.cc
@@ -17,8 +17,6 @@ using metadata::QuorumPB;
 using std::string;
 using strings::Substitute;
 
-static const char* kConsensusMetadataMagicNumber = "kconmeta";
-
 Status ConsensusMetadata::Create(FsManager* fs_manager,
                                  const string& tablet_id,
                                  QuorumPB& quorum,
@@ -38,7 +36,6 @@ Status ConsensusMetadata::Load(FsManager* fs_manager,
   gscoped_ptr<ConsensusMetadata> cmeta(new ConsensusMetadata(fs_manager, tablet_id));
   RETURN_NOT_OK(pb_util::ReadPBContainerFromPath(fs_manager->env(),
                                         fs_manager->GetConsensusMetadataPath(tablet_id),
-                                        kConsensusMetadataMagicNumber,
                                         cmeta->mutable_pb()));
   cmeta_out->swap(cmeta);
   return Status::OK();
@@ -63,7 +60,7 @@ Status ConsensusMetadata::Flush() {
 
   string meta_file_path = fs_manager_->GetConsensusMetadataPath(tablet_id_);
   RETURN_NOT_OK_PREPEND(pb_util::WritePBContainerToPath(
-      fs_manager_->env(), meta_file_path, kConsensusMetadataMagicNumber, pb_,
+      fs_manager_->env(), meta_file_path, pb_,
       // We use FLAGS_log_force_fsync_all here because the consensus metadata is
       // essentially an extension of the primary durability mechanism of the
       // consensus subsystem: the WAL. Using the same flag ensures that the WAL

src/kudu/fs/fs_manager.cc
@@ -5,7 +5,7 @@
 
 #include <boost/foreach.hpp>
 #include <glog/logging.h>
-#include <google/protobuf/message_lite.h>
+#include <google/protobuf/message.h>
 #include <iostream>
 #include <tr1/memory>
 
@@ -31,7 +31,7 @@ DEFINE_bool(enable_data_block_fsync, true,
 DEFINE_string(block_manager, "file", "Which block manager to use for storage. "
               "The only valid option is 'file'.");
 
-using google::protobuf::MessageLite;
+using google::protobuf::Message;
 using strings::Substitute;
 using std::tr1::shared_ptr;
 using kudu::fs::CreateBlockOptions;
@@ -51,8 +51,6 @@ const char *FsManager::kMasterBlockDirName = "master-blocks";
 const char *FsManager::kDataDirName = "data";
 const char *FsManager::kCorruptedSuffix = ".corrupted";
 const char *FsManager::kInstanceMetadataFileName = "instance";
-const char *FsManager::kInstanceMetadataMagicNumber = "kuduinst";
-const char *FsManager::kTabletSuperBlockMagicNumber = "ksuprblk";
 const char *FsManager::kConsensusMetadataDirName = "consensus-meta";
 
 FsManager::FsManager(Env *env, const string& root_path)
@@ -76,7 +74,7 @@ void FsManager::InitBlockManager() {
 Status FsManager::Open() {
   gscoped_ptr<InstanceMetadataPB> pb(new InstanceMetadataPB);
   RETURN_NOT_OK(pb_util::ReadPBContainerFromPath(env_, GetInstanceMetadataPath(),
-                                                 kInstanceMetadataMagicNumber, pb.get()));
+                                                 pb.get()));
   metadata_.reset(pb.release());
   RETURN_NOT_OK(block_manager_->Open());
   LOG(INFO) << "Opened local filesystem: " << root_path_
@@ -125,7 +123,7 @@ Status FsManager::CreateAndWriteInstanceMetadata() {
 
   // The instance metadata is written effectively once per TS, so the
   // durability cost is negligible.
-  RETURN_NOT_OK(pb_util::WritePBContainerToPath(env_, path, kInstanceMetadataMagicNumber,
+  RETURN_NOT_OK(pb_util::WritePBContainerToPath(env_, path,
                                                 new_instance, pb_util::SYNC));
   LOG(INFO) << "Generated new instance metadata in path " << path << ":\n"
             << new_instance.DebugString();
@@ -294,22 +292,22 @@ Status FsManager::CreateBlockDir(const BlockId& block_id) {
 //
 // TODO: Route through BlockManager, but that means potentially supporting block renaming.
 
-Status FsManager::WriteMetadataBlock(const BlockId& block_id, const MessageLite& msg) {
+Status FsManager::WriteMetadataBlock(const BlockId& block_id, const Message& msg) {
   RETURN_NOT_OK(CreateBlockDir(block_id));
   VLOG(1) << "Writing Metadata Block: " << block_id.ToString();
 
   // Write the new metadata file
   shared_ptr<WritableFile> wfile;
   string path = GetBlockPath(block_id);
-  return pb_util::WritePBContainerToPath(env_, path, kTabletSuperBlockMagicNumber, msg,
+  return pb_util::WritePBContainerToPath(env_, path, msg,
       FLAGS_enable_data_block_fsync ? pb_util::SYNC : pb_util::NO_SYNC);
 }
 
-Status FsManager::ReadMetadataBlock(const BlockId& block_id, MessageLite *msg) {
+Status FsManager::ReadMetadataBlock(const BlockId& block_id, Message* msg) {
   VLOG(1) << "Reading Metadata Block " << block_id.ToString();
 
   string path = GetBlockPath(block_id);
-  Status s = pb_util::ReadPBContainerFromPath(env_, path, kTabletSuperBlockMagicNumber, msg);
+  Status s = pb_util::ReadPBContainerFromPath(env_, path, msg);
   if (s.IsNotFound()) {
     return s;
   }

src/kudu/fs/fs_manager.h
@@ -17,7 +17,7 @@ DECLARE_bool(enable_data_block_fsync);
 
 namespace google {
 namespace protobuf {
-class MessageLite;
+class Message;
 } // namespace protobuf
 } // namespace google
 
@@ -97,9 +97,9 @@ class FsManager {
   // ==========================================================================
 
   Status WriteMetadataBlock(const BlockId& block_id,
-                            const google::protobuf::MessageLite& msg);
+                            const google::protobuf::Message& msg);
   Status ReadMetadataBlock(const BlockId& block_id,
-                           google::protobuf::MessageLite *msg);
+                           google::protobuf::Message *msg);
 
   // ==========================================================================
   //  on-disk path

src/kudu/tablet/tablet_metadata.cc
@@ -25,7 +25,6 @@ namespace kudu {
 namespace tablet {
 
 const int64 kNoDurableMemStore = -1;
-static const char* kMasterBlockMagicNumber = "kmstrblk";
 
 // ============================================================================
 //  Tablet Metadata
@@ -89,7 +88,7 @@ Status TabletMetadata::OpenMasterBlock(Env* env,
                                        const string& master_block_path,
                                        const string& expected_tablet_id,
                                        TabletMasterBlockPB* master_block) {
-  RETURN_NOT_OK(pb_util::ReadPBContainerFromPath(env, master_block_path, kMasterBlockMagicNumber,
+  RETURN_NOT_OK(pb_util::ReadPBContainerFromPath(env, master_block_path,
                                                  master_block));
   if (expected_tablet_id != master_block->tablet_id()) {
     LOG_AND_RETURN(ERROR, Status::Corruption(
@@ -103,7 +102,7 @@ Status TabletMetadata::OpenMasterBlock(Env* env,
 Status TabletMetadata::PersistMasterBlock(FsManager* fs_manager,
                                           const TabletMasterBlockPB& pb) {
   string path = fs_manager->GetMasterBlockPath(pb.tablet_id());
-  return pb_util::WritePBContainerToPath(fs_manager->env(), path, kMasterBlockMagicNumber, pb,
+  return pb_util::WritePBContainerToPath(fs_manager->env(), path, pb,
       FLAGS_enable_data_block_fsync ? pb_util::SYNC : pb_util::NO_SYNC);
 }
 

src/kudu/tools/CMakeLists.txt
@@ -61,6 +61,11 @@ target_link_libraries(kudu-ksck
   ksck
 )
 
+add_executable(kudu-pbc-dump pbc-dump.cc)
+target_link_libraries(kudu-pbc-dump
+  ${LINK_LIBS}
+)
+
 set(KUDU_TEST_LINK_LIBS
   ksck
   integration-tests

commit 7a0244c8c539dd800b7269c32a6826d2fdad43d9

java: fix fallback to system protoc
This used to work, but broke in commit 65b7e5b with the introduction of
additionalProtoPathelements. The new approach uses an automatically
activated maven profile which obviates the need for the protoc wrapper
script.

Change-Id: I9f307844b455667f87b56df4e12b27b39344aa71
Reviewed-on: http://gerrit.cloudera.org:8080/2321
Reviewed-by: Jean-Daniel Cryans
Tested-by: Adar Dembo <adar@cloudera.com>

java/kudu-client/dev-support/build-proto.sh
@@ -1,36 +1,0 @@
-#!/bin/bash
-#
-#
-# Licensed to the Apache Software Foundation (ASF) under one
-# or more contributor license agreements.  See the NOTICE file
-# distributed with this work for additional information
-# regarding copyright ownership.  The ASF licenses this file
-# to you under the Apache License, Version 2.0 (the
-# "License"); you may not use this file except in compliance
-# with the License.  You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-##
-# Script to find and run protoc to generate protocol buf files.
-# Should be used exclusively by Maven.
-#
-
-KUDU_DIR=`dirname $0`/../../..
-PROTOC_BIN=$KUDU_DIR/thirdparty/installed-deps/bin/protoc
-if [ ! -f "$PROTOC_BIN" ] ; then
-  if which protoc > /dev/null; then
-    PROTOC_BIN=`which protoc`
-  else
-    echo 'Error: protoc is missing from the 3rd party folder and on the PATH'
-    exit 1
-  fi
-fi
-
-$PROTOC_BIN "$@"

java/kudu-client/pom.xml
@@ -110,7 +110,11 @@
                          http://sergei-ivanov.github.io/maven-protoc-plugin/compile-mojo.html -->
                     <checkStaleness>true</checkStaleness>
                     <protoSourceRoot>${project.basedir}/../../src</protoSourceRoot>
-                    <protocExecutable>${project.basedir}/dev-support/build-proto.sh</protocExecutable>
+                    <protocExecutable>
+                      ${project.basedir}/../../thirdparty/installed-deps/bin/protoc
+                    </protocExecutable>
+                    <!-- Where we'll look for built-in .proto files such as
+                         google/protobuf/descriptor.proto. -->
                     <additionalProtoPathElements>
                       <additionalProtoPathElement>
                         ${project.basedir}/../../thirdparty/installed-deps/include
@@ -299,4 +303,33 @@
           </plugins>
         </pluginManagement>
     </build>
+
+    <profiles>
+      <!-- If thirdparty's protoc can't be found, this profile will activate and
+           use the system's protoc during the build. -->
+      <profile>
+        <id>protoc-system</id>
+        <activation>
+          <file>
+            <missing>${basedir}/../../thirdparty/installed-deps/bin/protoc</missing>
+          </file>
+        </activation>
+        <build>
+          <plugins>
+            <plugin>
+              <groupId>com.google.protobuf.tools</groupId>
+              <artifactId>maven-protoc-plugin</artifactId>
+              <version>${maven-protoc-plugin.version}</version>
+              <configuration>
+                <protocExecutable>protoc</protocExecutable>
+                <additionalProtoPathElements>
+                  <additionalProtoPathElement>/usr/include</additionalProtoPathElement>
+                  <additionalProtoPathElement>/usr/local/include</additionalProtoPathElement>
+                </additionalProtoPathElements>
+              </configuration>
+            </plugin>
+          </plugins>
+        </build>
+      </profile>
+    </profiles>
 </project>