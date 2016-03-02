cd3aa0ee636ce7008750922d5b55620aab03a97e
Todd Lipcon <todd@apache.org>
2016-03-01 18:36:31 -0800

    KUDU-1347. Improve licensing documentation
    This addresses a few issues and suggestions raised on our
    0.7.0 release candidate vote:
    
    - Many of the licenses previously mentioned in LICENSE.txt do not actually
      require that they be reproduced in binary distributions. Since we already
      include the license text in the source headers themselves, we can just
      refer the reader to the source.
    
      For those licenses which require some form of notice or attribution in
      binary distributions, we continue to copy-paste the license text into
      the LICENSE.txt file.
    
    - Copy the CMake BSD license notice into the FindProtobuf.cmake file.
      Even though we substantially rewrote it, it could be considered a
      derived work.
    
    - Relocate the copyright notices for Slice.java and Slices.java into the
      source header for those files, so that we can remove the extra copy of the
      Apache license from LICENSE.txt.
    
      Similarly, relocate the authorship information for HdrHistogram into the
      source code. Since it's a public domain library, it doesn't require
      any attribution.
    
    - Remove a stray Cloudera copyright notice from python/Makefile
    
    - Add a copy of the Boost license to the thirdparty/boost_uuid/ directory
      and reference it from the top-level LICENSE instead of copying it. This
      license does not require attribution in binary distributions.
    
    - Fix the reference to the WebRTC code to include random.h (not just
      random-util.cc)
    
    Change-Id: I95e5a86128e677839c84e209ba4f7c910c33517d
    Reviewed-on: http://gerrit.cloudera.org:8080/2417
    Reviewed-by: Jean-Daniel Cryans
    Tested-by: Todd Lipcon <todd@apache.org>

--- cmake_modules/FindProtobuf.cmake
+++ cmake_modules/FindProtobuf.cmake
@@ -14,6 +14,51 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
+#
+#=============================================================================
+# This file is heavily modified/rewritten from FindProtobuf.cmake from the
+# CMake project:
+#
+#   Copyright 2011 Kirill A. Korinskiy <catap@catap.ru>
+#   Copyright 2009 Kitware, Inc.
+#   Copyright 2009 Philip Lowman <philip@yhbt.com>
+#   Copyright 2008 Esben Mose Hansen, Ange Optimization ApS
+#
+#   Distributed under the OSI-approved BSD License (the "License"):
+#
+#   CMake - Cross Platform Makefile Generator
+#   Copyright 2000-2015 Kitware, Inc.
+#   Copyright 2000-2011 Insight Software Consortium
+#   All rights reserved.
+#
+#   Redistribution and use in source and binary forms, with or without
+#   modification, are permitted provided that the following conditions
+#   are met:
+#
+#   * Redistributions of source code must retain the above copyright
+#     notice, this list of conditions and the following disclaimer.
+#
+#   * Redistributions in binary form must reproduce the above copyright
+#     notice, this list of conditions and the following disclaimer in the
+#     documentation and/or other materials provided with the distribution.
+#
+#   * Neither the names of Kitware, Inc., the Insight Software Consortium,
+#     nor the names of their contributors may be used to endorse or promote
+#     products derived from this software without specific prior written
+#     permission.
+#
+#   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+#   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+#   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+#   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+#   HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+#   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+#   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+#   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+#   THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+#   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+#   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#=============================================================================
 
 #########
 # Local rewrite of the protobuf support in cmake.
@@ -61,22 +106,6 @@
 #          libraries, those libraries should depend on these targets
 #          in order to "serialize" the protoc invocations
 #  ====================================================================
-
-#=============================================================================
-# Copyright 2011 Kirill A. Korinskiy <catap@catap.ru>
-# Copyright 2009 Kitware, Inc.
-# Copyright 2009 Philip Lowman <philip@yhbt.com>
-# Copyright 2008 Esben Mose Hansen, Ange Optimization ApS
-#
-# Distributed under the OSI-approved BSD License (the "License");
-# see accompanying file Copyright.txt for details.
-#
-# This software is distributed WITHOUT ANY WARRANTY; without even the
-# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-# See the License for more information.
-#=============================================================================
-# (To distributed this file outside of CMake, substitute the full
-#  License text for the above reference.)
 
 function(PROTOBUF_GENERATE_CPP SRCS HDRS TGTS)
   if(NOT ARGN)
--- java/kudu-client/src/main/java/org/kududb/util/Slice.java
+++ java/kudu-client/src/main/java/org/kududb/util/Slice.java
@@ -12,6 +12,9 @@
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
  * License for the specific language governing permissions and limitations
  * under the License.
+ *
+ * Copyright 2011 Dain Sundstrom <dain@iq80.com>
+ * Copyright 2011 FuseSource Corp. http://fusesource.com
  */
 package org.kududb.util;
 
