51463bcf6cca7e99439bb796a35d43560bfc9bcc
Todd Lipcon <todd@apache.org>
2016-03-08 09:58:15 -0800

    Remove incorrectly applied or doubled ASF license headers
    The source header policy[1] says that you should not prepend the Apache
    License when distributing work that is derived from projects under other
    licenses. We have bits and pieces of code from LevelDB and RocksDB (BSD
    license) which we'd previously prepended the ASF license to, but instead
    should just keep the original license.
    
    I briefly checked the 'git blame' on these files to see the extent to which
    we'd modified them, and in some cases it was around 40-50% of the lines of
    code, but none was rewritten to the extent that it's worth starting a discussion
    on the subtleties of "major" vs "minor" modifications.
    
    This also removes some cases in which we'd applied the Apache license twice
    to the same piece of code -- in all cases in code we'd imported from Impala
    at some point along the line after it was Apache licensed.
    
    Change-Id: I8d8e781b672764a84792203b09e85c67e535dd11
    Reviewed-on: http://gerrit.cloudera.org:8080/2484
    Reviewed-by: Mike Percy <mpercy@apache.org>
    Tested-by: Kudu Jenkins

--- src/kudu/util/cache.h
+++ src/kudu/util/cache.h
@@ -1,6 +1,6 @@
 // Copyright (c) 2011 The LevelDB Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file. See the AUTHORS file for names of contributors.
+// found in the LICENSE file.
 //
 // A Cache is an interface that maps keys to values.  It has internal
 // synchronization and may be safely accessed concurrently from
