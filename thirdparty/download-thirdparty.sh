637b2e62cac8571879710b4442b31e85eec4bfef
bruceSz <zsyuyizhang@gmail.com>
2016-04-10 12:56:32 +0800

    add -L option to curl to enable curl to follow redirects
    Change-Id: I2c5bedf93d69cfe4efbc91785a7bce81026c12c9
    Author: bruceSz(zsyuyizhang@gmail.com)
    Reviewed-on: http://gerrit.cloudera.org:8080/2743
    Reviewed-by: Todd Lipcon <todd@apache.org>
    Tested-by: Kudu Jenkins

--- thirdparty/download-thirdparty.sh
+++ thirdparty/download-thirdparty.sh
@@ -56,7 +56,7 @@ fetch_and_expand() {
       echo "Archive $FILENAME already exists. Not re-downloading archive."
     else
       echo "Fetching $FILENAME from $FULL_URL"
-      curl -O "$FULL_URL"
+      curl -L -O "$FULL_URL"
     fi
 
     echo "Unpacking $FILENAME"
