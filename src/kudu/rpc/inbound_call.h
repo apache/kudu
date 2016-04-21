apache/kudu

commit 38e16b71b881ad1f276da918cae8977a383906a5

KUDU-1410 (part 3): rpc: look up the RpcMethodInfo from the reactor
This makes the reactor code look up the RpcMethodInfo associated with a call,
and moves the latency histogram collection code into the InboundCall class
instead of RpcContext. This cleans up RpcContext so now it is only responsible
for user-facing APIs and not responsible for collecting metrics.

This change may also be useful in the future if we want to start queueing calls
differently based on the particular method being invoked. We now have an easy
way for the RPC service definition to propagate up appropriate attributes
all the way to the reactor.

Change-Id: I1bed4917ca05f46c829d1733270bfba3ec20d699
Reviewed-on: http://gerrit.cloudera.org:8080/2796
Reviewed-by: Adar Dembo <adar@cloudera.com>
Tested-by: Kudu Jenkins

src/kudu/rpc/inbound_call.h
@@ -49,6 +49,7 @@ namespace rpc {
 class Connection;
 class DumpRunningRpcsRequestPB;
 class RpcCallInProgressPB;
+struct RpcMethodInfo;
 class RpcSidecar;
 class UserCredentials;
 
@@ -132,6 +133,19 @@ class InboundCall {
 
   Trace* trace();
 
+  // Associate this call with a particular method that will be invoked
+  // by the service.
+  void set_method_info(RpcMethodInfo* info) {
+    method_info_ = info;
+  }
+
+  // Return the method associated with this call. This is set just before
+  // the call is enqueued onto the service queue, and therefore may be
+  // 'nullptr' for much of the lifecycle of a call.
+  RpcMethodInfo* method_info() {
+    return method_info_;
+  }
+
   // When this InboundCall was received (instantiated).
   // Should only be called once on a given instance.
   // Not thread-safe. Should only be called by the current "owner" thread.
@@ -142,12 +156,6 @@ class InboundCall {
   // and should only be called once on a given instance.
   // Not thread-safe. Should only be called by the current "owner" thread.
   void RecordHandlingStarted(scoped_refptr<Histogram> incoming_queue_time);
-
-  // When RPC call Handle() completed execution on the server side.
-  // Updates the Histogram with time elapsed since the call was started,
-  // and should only be called once on a given instance.
-  // Not thread-safe. Should only be called by the current "owner" thread.
-  void RecordHandlingCompleted(scoped_refptr<Histogram> handler_run_time);
 
   // Return true if the deadline set by the client has already elapsed.
   // In this case, the server may stop processing the call, since the
@@ -183,6 +191,12 @@ class InboundCall {
   // Also can be configured to log _all_ RPC traces for help debugging.
   void LogTrace() const;
 
+  // When RPC call Handle() completed execution on the server side.
+  // Updates the Histogram with time elapsed since the call was started,
+  // and should only be called once on a given instance.
+  // Not thread-safe. Should only be called by the current "owner" thread.
+  void RecordHandlingCompleted();
+
   // The connection on which this inbound call arrived.
   scoped_refptr<Connection> conn_;
 
@@ -216,6 +230,11 @@ class InboundCall {
   // Proto service this calls belongs to. Used for routing.
   // This field is filled in when the inbound request header is parsed.
   RemoteMethod remote_method_;
+
+  // After the method has been looked up within the service, this is filled in
+  // to point to the information about this method. Acts as a pointer back to
+  // per-method info such as tracing.
+  RpcMethodInfo* method_info_;
 
   DISALLOW_COPY_AND_ASSIGN(InboundCall);
 };

src/kudu/rpc/protoc-gen-krpc.cc
@@ -454,7 +454,7 @@ class CodeGenerator : public ::google::protobuf::compiler::CodeGenerator {
               "    unique_ptr<RpcMethodInfo> mi(new RpcMethodInfo());\n"
               "    mi->req_prototype.reset(new $request$());\n"
               "    mi->resp_prototype.reset(new $response$());\n"
-              "    mi->metrics.handler_latency =\n"
+              "    mi->handler_latency_histogram =\n"
               "        METRIC_handler_latency_$rpc_full_name_plainchars$.Instantiate(entity);\n"
               "    mi->func = [this](const Message* req, Message* resp, RpcContext* ctx) {\n"
               "      this->$rpc_name$(static_cast<const $request$*>(req),\n"

src/kudu/rpc/service_if.h
@@ -38,11 +38,8 @@ class Histogram;
 namespace rpc {
 
 class InboundCall;
+class RemoteMethod;
 class RpcContext;
-
-struct RpcMethodMetrics {
-  scoped_refptr<Histogram> handler_latency;
-};
 
 // Generated services define an instance of this class for each
 // method that they implement. The generic server code implemented
@@ -55,7 +52,7 @@ struct RpcMethodInfo {
   std::unique_ptr<google::protobuf::Message> req_prototype;
   std::unique_ptr<google::protobuf::Message> resp_prototype;
 
-  RpcMethodMetrics metrics;
+  scoped_refptr<Histogram> handler_latency_histogram;
 
   // The actual function to be called.
   std::function<void(const google::protobuf::Message* req,
@@ -75,6 +72,14 @@ class ServiceIf {
   // specific feature flag.
   virtual bool SupportsFeature(uint32_t feature) const;
 
+  // Look up the method being requested by the remote call.
+  //
+  // If this returns nullptr, then certain functionality like
+  // metrics collection will not be performed for this call.
+  virtual RpcMethodInfo* LookupMethod(const RemoteMethod& method) {
+    return nullptr;
+  }
+
  protected:
   bool ParseParam(InboundCall* call, google::protobuf::Message* message);
   void RespondBadMethod(InboundCall* call);
@@ -91,6 +96,8 @@ class GeneratedServiceIf : public ServiceIf {
   //
   // If no such method is found, responds with an error.
   void Handle(InboundCall* incoming) override;
+
+  RpcMethodInfo* LookupMethod(const RemoteMethod& method) override;
 
  protected:
   // For each method, stores the relevant information about how to handle the

src/kudu/rpc/inbound_call.cc
@@ -25,6 +25,7 @@
 #include "kudu/rpc/rpc_introspection.pb.h"
 #include "kudu/rpc/rpc_sidecar.h"
 #include "kudu/rpc/serialization.h"
+#include "kudu/rpc/service_if.h"
 #include "kudu/util/debug/trace_event.h"
 #include "kudu/util/flag_tags.h"
 #include "kudu/util/metrics.h"
@@ -49,7 +50,8 @@ namespace rpc {
 InboundCall::InboundCall(Connection* conn)
   : conn_(conn),
     sidecars_deleter_(&sidecars_),
-    trace_(new Trace) {
+    trace_(new Trace),
+    method_info_(nullptr) {
   RecordCallReceived();
 }
 
@@ -135,7 +137,7 @@ void InboundCall::Respond(const MessageLite& response,
   TRACE_EVENT_ASYNC_END1("rpc", "InboundCall", this,
                          "method", remote_method_.method_name());
   TRACE_TO(trace_, "Queueing $0 response", is_success ? "success" : "failure");
-
+  RecordHandlingCompleted();
   LogTrace();
   conn_->QueueResponseForCall(gscoped_ptr<InboundCall>(this));
 }
@@ -262,12 +264,20 @@ void InboundCall::RecordHandlingStarted(scoped_refptr<Histogram> incoming_queue_
       timing_.time_handled.GetDeltaSince(timing_.time_received).ToMicroseconds());
 }
 
-void InboundCall::RecordHandlingCompleted(scoped_refptr<Histogram> handler_run_time) {
-  DCHECK(handler_run_time != nullptr);
+void InboundCall::RecordHandlingCompleted() {
   DCHECK(!timing_.time_completed.Initialized());  // Protect against multiple calls.
   timing_.time_completed = MonoTime::Now(MonoTime::FINE);
-  handler_run_time->Increment(
-      timing_.time_completed.GetDeltaSince(timing_.time_handled).ToMicroseconds());
+
+  if (!timing_.time_handled.Initialized()) {
+    // Sometimes we respond to a call before we begin handling it (e.g. due to queue
+    // overflow, etc). These cases should not be counted against the histogram.
+    return;
+  }
+
+  if (method_info_) {
+    method_info_->handler_latency_histogram->Increment(
+        timing_.time_completed.GetDeltaSince(timing_.time_handled).ToMicroseconds());
+  }
 }
 
 bool InboundCall::ClientTimedOut() const {

src/kudu/rpc/messenger.cc
@@ -218,6 +218,8 @@ void Messenger::QueueInboundCall(gscoped_ptr<InboundCall> call) {
     return;
   }
 
+  call->set_method_info((*service)->LookupMethod(call->remote_method()));
+
   // The RpcService will respond to the client on success or failure.
   WARN_NOT_OK((*service)->QueueInboundCall(std::move(call)), "Unable to handle RPC call");
 }

src/kudu/rpc/rpc_context.cc
@@ -69,12 +69,10 @@ scoped_refptr<debug::ConvertableToTraceFormat> TracePb(const Message& msg) {
 
 RpcContext::RpcContext(InboundCall *call,
                        const google::protobuf::Message *request_pb,
-                       google::protobuf::Message *response_pb,
-                       RpcMethodMetrics metrics)
+                       google::protobuf::Message *response_pb)
   : call_(CHECK_NOTNULL(call)),
     request_pb_(request_pb),
-    response_pb_(response_pb),
-    metrics_(metrics) {
+    response_pb_(response_pb) {
   VLOG(4) << call_->remote_method().service_name() << ": Received RPC request for "
           << call_->ToString() << ":" << std::endl << request_pb_->DebugString();
   TRACE_EVENT_ASYNC_BEGIN2("rpc_call", "RPC", this,
@@ -86,7 +84,6 @@ RpcContext::~RpcContext() {
 }
 
 void RpcContext::RespondSuccess() {
-  call_->RecordHandlingCompleted(metrics_.handler_latency);
   VLOG(4) << call_->remote_method().service_name() << ": Sending RPC success response for "
           << call_->ToString() << ":" << std::endl << response_pb_->DebugString();
   TRACE_EVENT_ASYNC_END2("rpc_call", "RPC", this,
@@ -97,7 +94,6 @@ void RpcContext::RespondSuccess() {
 }
 
 void RpcContext::RespondFailure(const Status &status) {
-  call_->RecordHandlingCompleted(metrics_.handler_latency);
   VLOG(4) << call_->remote_method().service_name() << ": Sending RPC failure response for "
           << call_->ToString() << ": " << status.ToString();
   TRACE_EVENT_ASYNC_END2("rpc_call", "RPC", this,
@@ -109,7 +105,6 @@ void RpcContext::RespondFailure(const Status &status) {
 }
 
 void RpcContext::RespondRpcFailure(ErrorStatusPB_RpcErrorCodePB err, const Status& status) {
-  call_->RecordHandlingCompleted(metrics_.handler_latency);
   VLOG(4) << call_->remote_method().service_name() << ": Sending RPC failure response for "
           << call_->ToString() << ": " << status.ToString();
   TRACE_EVENT_ASYNC_END2("rpc_call", "RPC", this,
@@ -121,7 +116,6 @@ void RpcContext::RespondRpcFailure(ErrorStatusPB_RpcErrorCodePB err, const Statu
 
 void RpcContext::RespondApplicationError(int error_ext_id, const std::string& message,
                                          const Message& app_error_pb) {
-  call_->RecordHandlingCompleted(metrics_.handler_latency);
   if (VLOG_IS_ON(4)) {
     ErrorStatusPB err;
     InboundCall::ApplicationErrorToPB(error_ext_id, message, app_error_pb, &err);

src/kudu/rpc/rpc_context.h
@@ -63,8 +63,7 @@ class RpcContext {
   // and is not a public API.
   RpcContext(InboundCall *call,
              const google::protobuf::Message *request_pb,
-             google::protobuf::Message *response_pb,
-             RpcMethodMetrics metrics);
+             google::protobuf::Message *response_pb);
 
   ~RpcContext();
 
@@ -179,7 +178,6 @@ class RpcContext {
   InboundCall* const call_;
   const gscoped_ptr<const google::protobuf::Message> request_pb_;
   const gscoped_ptr<google::protobuf::Message> response_pb_;
-  RpcMethodMetrics metrics_;
 };
 
 } // namespace rpc

src/kudu/rpc/rpc_service.h
@@ -23,6 +23,8 @@
 namespace kudu {
 namespace rpc {
 
+class RemoteMethod;
+struct RpcMethodInfo;
 class InboundCall;
 
 class RpcService : public RefCountedThreadSafe<RpcService> {
@@ -33,6 +35,10 @@ class RpcService : public RefCountedThreadSafe<RpcService> {
   // On failure, the RpcService::QueueInboundCall() implementation is
   // responsible for responding to the client with a failure message.
   virtual Status QueueInboundCall(gscoped_ptr<InboundCall> call) = 0;
+
+  virtual RpcMethodInfo* LookupMethod(const RemoteMethod& method) {
+    return nullptr;
+  }
 };
 
 } // namespace rpc

commit b6bab20922def643c9b57edef7313ea940dda068

Fix use-after free in accessing InboundCall's MethodInfo
This fixes a regression from 38e16b71b881ad1f276da918cae8977a383906a5 in which
a call is being responded to after the underlying service has shut down. In
this case, the call was holding on to a raw pointer to the RpcMethodInfo
object, and that object was being destroyed when the service stopped.

The fix here is to make it reference-counted. This has a slight penalty of some
extra atomic refcount operations, but any other fix would likely be much more
complex.

No new test since client-test was already failing nearly 50% in ASAN/TSAN
builds.

Change-Id: I30304fe6bd2074578e5cfc09ee18ef616d43e687
Reviewed-on: http://gerrit.cloudera.org:8080/2833
Tested-by: Kudu Jenkins
Reviewed-by: Adar Dembo <adar@cloudera.com>

src/kudu/rpc/inbound_call.h
@@ -26,6 +26,7 @@
 #include "kudu/gutil/macros.h"
 #include "kudu/gutil/ref_counted.h"
 #include "kudu/rpc/remote_method.h"
+#include "kudu/rpc/service_if.h"
 #include "kudu/rpc/rpc_header.pb.h"
 #include "kudu/rpc/transfer.h"
 #include "kudu/util/faststring.h"
@@ -147,15 +148,15 @@ class InboundCall {
 
   // Associate this call with a particular method that will be invoked
   // by the service.
-  void set_method_info(RpcMethodInfo* info) {
-    method_info_ = info;
+  void set_method_info(scoped_refptr<RpcMethodInfo> info) {
+    method_info_ = std::move(info);
   }
 
   // Return the method associated with this call. This is set just before
   // the call is enqueued onto the service queue, and therefore may be
   // 'nullptr' for much of the lifecycle of a call.
   RpcMethodInfo* method_info() {
-    return method_info_;
+    return method_info_.get();
   }
 
   // When this InboundCall was received (instantiated).
@@ -242,7 +243,7 @@ class InboundCall {
   // After the method has been looked up within the service, this is filled in
   // to point to the information about this method. Acts as a pointer back to
   // per-method info such as tracing.
-  RpcMethodInfo* method_info_;
+  scoped_refptr<RpcMethodInfo> method_info_;
 
   DISALLOW_COPY_AND_ASSIGN(InboundCall);
 };

src/kudu/rpc/protoc-gen-krpc.cc
@@ -451,7 +451,7 @@ class CodeGenerator : public ::google::protobuf::compiler::CodeGenerator {
 
         Print(printer, *subs,
               "  {\n"
-              "    unique_ptr<RpcMethodInfo> mi(new RpcMethodInfo());\n"
+              "    scoped_refptr<RpcMethodInfo> mi(new RpcMethodInfo());\n"
               "    mi->req_prototype.reset(new $request$());\n"
               "    mi->resp_prototype.reset(new $response$());\n"
               "    mi->handler_latency_histogram =\n"

src/kudu/rpc/service_if.h
@@ -45,7 +45,7 @@ class RpcContext;
 // method that they implement. The generic server code implemented
 // by GeneratedServiceIf look up the RpcMethodInfo in order to handle
 // each RPC.
-struct RpcMethodInfo {
+struct RpcMethodInfo : public RefCountedThreadSafe<RpcMethodInfo> {
   // Prototype protobufs for requests and responses.
   // These are empty protobufs which are cloned in order to provide an
   // instance for each request.
@@ -104,7 +104,7 @@ class GeneratedServiceIf : public ServiceIf {
   // call. Methods are inserted by the constructor of the generated subclass.
   // After construction, this map is accessed by multiple threads and therefore
   // must not be modified.
-  std::unordered_map<std::string, std::unique_ptr<RpcMethodInfo>> methods_by_name_;
+  std::unordered_map<std::string, scoped_refptr<RpcMethodInfo>> methods_by_name_;
 };
 
 } // namespace rpc