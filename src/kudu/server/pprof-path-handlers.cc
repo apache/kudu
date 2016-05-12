apache/kudu

commit d20b44704033d46be76ce4bfa55df09251b45520

simplify pprof cpu profiler path handling
This code was previously failing to compile with GCC 5.2 with -std=c++11.

Change-Id: I77f1be3778ff417ca464b085f29a7913dffa1770
Reviewed-on: http://gerrit.cloudera.org:8080/1469
Tested-by: Internal Jenkins
Reviewed-by: Dan Burkert <dan@danburkert.com>

src/kudu/server/pprof-path-handlers.cc
@@ -122,13 +122,12 @@ static void PprofCpuProfileHandler(const Webserver::WebRequest& req, stringstrea
   if (it != req.parsed_args.end()) {
     seconds = atoi(it->second.c_str());
   }
-  ostringstream tmp_prof_file_name;
   // Build a temporary file name that is hopefully unique.
-  tmp_prof_file_name << "/tmp/kudu_cpu_profile." << getpid() << "." << rand();
-  ProfilerStart(tmp_prof_file_name.str().c_str());
+  string tmp_prof_file_name = strings::Substitute("/tmp/kudu_cpu_profile/$0.$1", getpid(), rand());
+  ProfilerStart(tmp_prof_file_name.c_str());
   SleepFor(MonoDelta::FromSeconds(seconds));
   ProfilerStop();
-  ifstream prof_file(tmp_prof_file_name.str().c_str(), std::ios::in);
+  ifstream prof_file(tmp_prof_file_name.c_str(), std::ios::in);
   if (!prof_file.is_open()) {
     (*output) << "Unable to open cpu profile: " << tmp_prof_file_name;
     return;

commit 999e7c908c5fd522d686451942a9f62106598bdd

pprof: fix tmp filename path construction
The pprof handler was trying to store the profile data in
/tmp/kudu_cpu_profile/<pid>.<rand>. The kudu_cpu_profile directory is unlikely
to exist, which makes the path handler fail.

This seems to have been an accidental regression in
d20b44704033d46be76ce4bfa55df09251b45520.

This restores the prior behavior of /tmp/kudu_cpu_profile.<pid>.<rand>

Change-Id: I555037cb2ee59ef3735e209816def23d8bed856b
Reviewed-on: http://gerrit.cloudera.org:8080/3029
Tested-by: Kudu Jenkins
Reviewed-by: Adar Dembo <adar@cloudera.com>

src/kudu/server/pprof-path-handlers.cc
@@ -112,7 +112,7 @@ static void PprofCpuProfileHandler(const Webserver::WebRequest& req, stringstrea
     seconds = atoi(it->second.c_str());
   }
   // Build a temporary file name that is hopefully unique.
-  string tmp_prof_file_name = strings::Substitute("/tmp/kudu_cpu_profile/$0.$1", getpid(), rand());
+  string tmp_prof_file_name = strings::Substitute("/tmp/kudu_cpu_profile.$0.$1", getpid(), rand());
   ProfilerStart(tmp_prof_file_name.c_str());
   SleepFor(MonoDelta::FromSeconds(seconds));
   ProfilerStop();