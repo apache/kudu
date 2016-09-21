80923d6904f80d8e7d56cc7eee2c11e88b9329bd
David Alves <dralves@apache.org>
2016-09-21 10:30:46 -0700

    Patch gperftools to be able to work on macOS Sierra/XCode 8
    Apparently gperftools needs to be patched for tcmalloc to work
    with the new clang version that ships with XCode 8, but the fix
    hasn't landed yet.
    
    This patch fixes that and was copied verbatim from:
    https://github.com/gperftools/gperftools/issues/827
    
    Note that this fix is virtually identical to the one merged into
    jemalloc, which had the same problem:
    https://github.com/jemalloc/jemalloc/pull/427/commits/19c9a3e828ed46f1576521c264640e60bd0cb01f
    
    Finally it's noteworthy that this is an macOS/OS X only
    fix and, as such, doesn't affect released binaries.
    
    Change-Id: Ie00d9fad36d1746aa94da3b1711353110da42d95
    Reviewed-on: http://gerrit.cloudera.org:8080/4495
    Reviewed-by: Adar Dembo <adar@cloudera.com>
    Tested-by: Kudu Jenkins

--- thirdparty/download-thirdparty.sh
+++ thirdparty/download-thirdparty.sh
@@ -124,7 +124,7 @@ fi
 # Check that the gperftools patch has been applied.
 # If you add or remove patches, bump the patchlevel below to ensure
 # that any new Jenkins builds pick up your patches.
-GPERFTOOLS_PATCHLEVEL=2
+GPERFTOOLS_PATCHLEVEL=3
 delete_if_wrong_patchlevel $GPERFTOOLS_DIR $GPERFTOOLS_PATCHLEVEL
 if [ ! -d $GPERFTOOLS_DIR ]; then
   fetch_and_expand gperftools-${GPERFTOOLS_VERSION}.tar.gz
@@ -132,6 +132,7 @@ if [ ! -d $GPERFTOOLS_DIR ]; then
   pushd $GPERFTOOLS_DIR
   patch -p1 < $TP_DIR/patches/gperftools-Change-default-TCMALLOC_TRANSFER_NUM_OBJ-to-40.patch
   patch -p1 < $TP_DIR/patches/gperftools-hook-mi_force_unlock-on-OSX-instead-of-pthread_atfork.patch
+  patch -p1 < $TP_DIR/patches/gperftools-issue-827-add_get_default_zone_to_osx_libc_override.patch
   touch patchlevel-$GPERFTOOLS_PATCHLEVEL
   autoreconf -fvi
   popd
