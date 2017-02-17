6098da159dc04e8637d1ccf06349ddf5d03bbe46
Dan Burkert <danburkert@apache.org>
2017-02-16 23:58:04 -0800

    [security] add --rpc_tls_ciphers flag
    This new flag allows for precise control over the TLS cipher suite
    preference list to use for RPC connections on the server and kudu CLI
    tool. This is a relatively common security configuration option on
    systems which include TLS encryption.
    
    I also took this opportunity to change our default cipher suite list to
    match the Mozilla "modern compatibility" recommendation[1], plus some
    cipher suites necessary to remain compatible with RHEL 6.5. This
    revealed that we are not properly supporting ECDHE ciphers, so I enabled
    those as well.
    
    [1] https://wiki.mozilla.org/Security/Server_Side_TLS#Recommended_configurations
    
    Change-Id: I050e2295041a98fe2c3118c6258b910423bd3816
    Reviewed-on: http://gerrit.cloudera.org:8080/6055
    Reviewed-by: Alexey Serbin <aserbin@cloudera.com>
    Tested-by: Kudu Jenkins
    Reviewed-by: Todd Lipcon <todd@apache.org>

--- src/kudu/security/tls_context.cc
+++ src/kudu/security/tls_context.cc
@@ -42,6 +42,26 @@ DEFINE_int32(ipki_server_key_size, 2048,
              "is used for TLS connections to and from clients and other servers.");
 TAG_FLAG(ipki_server_key_size, experimental);
 
+DEFINE_string(rpc_tls_ciphers,
+              // This is the "modern compatibility" cipher list of the Mozilla Security
+              // Server Side TLS recommendations, accessed Feb. 2017, with the addition of
+              // the non ECDH/DH AES cipher suites from the "intermediate compatibility"
+              // list. These additional ciphers maintain compatibility with RHEL 6.5 and
+              // below. The DH AES ciphers are not included since we are not configured to
+              // use DH key agreement.
+              "ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:"
+              "ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:"
+              "ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:"
+              "ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA384:"
+              "ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA256"
+              "AES256-GCM-SHA384:AES128-GCM-SHA256:"
+              "AES256-SHA256:AES128-SHA256:"
+              "AES256-SHA:AES128-SHA",
+              "The cipher suite preferences to use for TLS-secured RPC connections. "
+              "Uses the OpenSSL cipher preference list format. See man (1) ciphers "
+              "for more information.");
+TAG_FLAG(rpc_tls_ciphers, advanced);
+
 namespace kudu {
 namespace security {
 
@@ -85,9 +105,31 @@ Status TlsContext::Init() {
                       SSL_OP_NO_SSLv2 | SSL_OP_NO_SSLv3 |
                       SSL_OP_NO_COMPRESSION);
 
+  OPENSSL_RET_NOT_OK(
+      SSL_CTX_set_cipher_list(ctx_.get(), FLAGS_rpc_tls_ciphers.c_str()),
+      "failed to set TLS ciphers");
+
+  // Enable ECDH curves. For OpenSSL 1.1.0 and up, this is done automatically.
+#ifndef OPENSSL_NO_ECDH
+#if OPENSSL_VERSION_NUMBER < 0x10002000L
+  // OpenSSL 1.0.1 and below only support setting a single ECDH curve at once.
+  // We choose prime256v1 because it's the first curve listed in the "modern
+  // compatibility" section of the Mozilla Server Side TLS recommendations,
+  // accessed Feb. 2017.
+  c_unique_ptr<EC_KEY> ecdh { EC_KEY_new_by_curve_name(NID_X9_62_prime256v1), &EC_KEY_free };
+  OPENSSL_RET_IF_NULL(ecdh, "failed to create prime256v1 curve");
+  OPENSSL_RET_NOT_OK(SSL_CTX_set_tmp_ecdh(ctx_.get(), ecdh.get()),
+                     "failed to set ECDH curve");
+#elif OPENSSL_VERSION_NUMBER < 0x10100000L
+  // OpenSSL 1.0.2 provides the set_ecdh_auto API which internally figures out
+  // the best curve to use.
+  OPENSSL_RET_NOT_OK(SSL_CTX_set_ecdh_auto(ctx_.get(), 1),
+                     "failed to configure ECDH support");
+#endif
+#endif
+
   // TODO(PKI): is it possible to disable client-side renegotiation? it seems there
   // have been various CVEs related to this feature that we don't need.
-  // TODO(PKI): set desired cipher suites?
   return Status::OK();
 }
 
