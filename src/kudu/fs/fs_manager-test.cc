apache/kudu

commit c26e09ef510f2fd4cfab7cde0bb811127ee14b7c

fs_manager: optimize tmp file deletion
In a run of dense_node-itest, the bulk of CPU time[1] was spent
canonicalizing paths while cleaning up temporary files. This patch optimizes
that in two ways:
- Stop canonicalizing paths. We already canonicalize the WAL and data dir
  roots; that's good enough for admin-provided symlinks.
- Split WAL and data root cleaning, parallelizing the latter through the
  DataDirManager.

1. Though admittedly the majority of wall clock time was waiting on IO.

Change-Id: I07830f0cc3fd5da847361607c62a369c39e677d2
Reviewed-on: http://gerrit.cloudera.org:8080/6837
Reviewed-by: Todd Lipcon <todd@apache.org>
Tested-by: Adar Dembo <adar@cloudera.com>

src/kudu/fs/fs_manager.cc
@@ -234,12 +234,10 @@ void FsManager::InitBlockManager() {
 Status FsManager::Open(FsReport* report) {
   RETURN_NOT_OK(Init());
 
-  // Remove leftover tmp files and fix permissions.
-  if (!read_only_) {
-    CleanTmpFiles();
-    CheckAndFixPermissions();
-  }
-
+  // Load and verify the instance metadata files.
+  //
+  // Done first to minimize side effects in the case that the configured roots
+  // are not yet initialized on disk.
   for (const string& root : canonicalized_all_fs_roots_) {
     gscoped_ptr<InstanceMetadataPB> pb(new InstanceMetadataPB);
     RETURN_NOT_OK(pb_util::ReadPBContainerFromPath(env_, GetInstanceMetadataPath(root),
@@ -251,6 +249,15 @@ Status FsManager::Open(FsReport* report) {
           "Mismatched UUIDs across filesystem roots: $0 vs. $1",
           metadata_->uuid(), pb->uuid()));
     }
+  }
+
+  // Remove leftover temporary files from the WAL root and fix permissions.
+  //
+  // Temporary files in the data directory roots will be removed by the block
+  // manager.
+  if (!read_only_) {
+    CleanTmpFilesInWalRoot();
+    CheckAndFixPermissions();
   }
 
   LOG_TIMING(INFO, "opening block manager") {
@@ -466,56 +473,10 @@ string FsManager::GetWalSegmentFileName(const string& tablet_id,
                                               StringPrintf("%09" PRIu64, sequence_number)));
 }
 
-void FsManager::CleanTmpFiles() {
+void FsManager::CleanTmpFilesInWalRoot() {
   DCHECK(!read_only_);
-  string canonized_path;
-  vector<string> children;
-  unordered_set<string> checked_dirs;
-  stack<string> paths;
-  for (const string& root : canonicalized_all_fs_roots_) {
-    paths.push(root);
-  }
-
-  while (!paths.empty()) {
-    string path = paths.top();
-    paths.pop();
-
-    Status s = env_->GetChildren(path, &children);
-    if (s.ok()) {
-      for (const string& child : children) {
-        if (child == "." || child == "..") continue;
-
-        // Canonicalize in case of symlinks
-        s = env_->Canonicalize(JoinPathSegments(path, child), &canonized_path);
-        if (!s.ok()) {
-          LOG(WARNING) << "Unable to get the real path: " << s.ToString();
-          continue;
-        }
-
-        bool is_directory;
-        s = env_->IsDirectory(canonized_path, &is_directory);
-        if (!s.ok()) {
-          LOG(WARNING) << "Unable to get information about file: " << s.ToString();
-          continue;
-        }
-
-        if (is_directory) {
-          // Check if we didn't handle this path yet
-          if (!ContainsKey(checked_dirs, canonized_path)) {
-            checked_dirs.insert(canonized_path);
-            paths.push(canonized_path);
-          }
-        } else if (child.find(kTmpInfix) != string::npos) {
-          s = env_->DeleteFile(canonized_path);
-          if (!s.ok()) {
-            LOG(WARNING) << "Unable to delete tmp file: " << s.ToString();
-          }
-        }
-      }
-    } else {
-      LOG(WARNING) << "Unable to read directory: " << s.ToString();
-    }
-  }
+  WARN_NOT_OK(env_util::DeleteTmpFilesRecursively(env_, canonicalized_wal_fs_root_),
+              "Error while deleting tmp files");
 }
 
 void FsManager::CheckAndFixPermissions() {

src/kudu/fs/fs_manager.h
@@ -244,9 +244,10 @@ class FsManager {
                           const std::string& path,
                           const std::vector<std::string>& objects);
 
-  // Deletes temporary files left from previous execution (e.g., after a crash).
+  // Deletes leftover temporary files in canonicalized_wal_fs_root_.
+  //
   // Logs warnings in case of errors.
-  void CleanTmpFiles();
+  void CleanTmpFilesInWalRoot();
 
   // Checks that the permissions of the root data directories conform to the
   // configured umask, and tightens them as necessary if they do not.

src/kudu/fs/data_dirs.cc
@@ -135,6 +135,14 @@ Status CheckHolePunch(Env* env, const string& path) {
   return Status::OK();
 }
 
+// Wrapper for env_util::DeleteTmpFilesRecursively that is suitable for parallel
+// execution on a data directory's thread pool (which requires the return value
+// be void).
+void DeleteTmpFilesRecursively(Env* env, const string& path) {
+  WARN_NOT_OK(env_util::DeleteTmpFilesRecursively(env, path),
+              "Error while deleting temp files");
+}
+
 } // anonymous namespace
 
 #define GINIT(x) x(METRIC_##x.Instantiate(entity, 0))
@@ -359,6 +367,14 @@ Status DataDirManager::Open(int max_data_dirs, LockMode mode) {
   RETURN_NOT_OK_PREPEND(PathInstanceMetadataFile::CheckIntegrity(instances),
                         Substitute("Could not verify integrity of files: $0",
                                    JoinStrings(paths_, ",")));
+
+  // Use the per-dir thread pools to delete temporary files in parallel.
+  for (const auto& dd : dds) {
+    dd->ExecClosure(Bind(&DeleteTmpFilesRecursively, env_, dd->dir()));
+  }
+  for (const auto& dd : dds) {
+    dd->WaitOnClosures();
+  }
 
   // Build uuid index and data directory maps.
   UuidIndexMap dd_by_uuid_idx;

src/kudu/util/env_util.cc
@@ -26,6 +26,7 @@
 #include <gflags/gflags.h>
 #include <glog/logging.h>
 
+#include "kudu/gutil/bind.h"
 #include "kudu/gutil/map-util.h"
 #include "kudu/gutil/strings/numbers.h"
 #include "kudu/gutil/strings/split.h"
@@ -236,6 +237,31 @@ Status DeleteExcessFilesByPattern(Env* env, const string& pattern, int max_match
   }
 
   return Status::OK();
+}
+
+// Callback for DeleteTmpFilesRecursively().
+//
+// Tests 'basename' for the Kudu-specific tmp file infix, and if found,
+// deletes the file.
+static Status DeleteTmpFilesRecursivelyCb(Env* env,
+                                          Env::FileType file_type,
+                                          const string& dirname,
+                                          const string& basename) {
+  if (file_type != Env::FILE_TYPE) {
+    // Skip directories.
+    return Status::OK();
+  }
+
+  if (basename.find(kTmpInfix) != string::npos) {
+    string filename = JoinPathSegments(dirname, basename);
+    WARN_NOT_OK(env->DeleteFile(filename),
+                Substitute("Failed to remove temporary file $0", filename));
+  }
+  return Status::OK();
+}
+
+Status DeleteTmpFilesRecursively(Env* env, const string& path) {
+  return env->Walk(path, Env::PRE_ORDER, Bind(&DeleteTmpFilesRecursivelyCb, env));
 }
 
 ScopedFileDeleter::ScopedFileDeleter(Env* env, std::string path)

src/kudu/util/env_util.h
@@ -74,6 +74,12 @@ Status CopyFile(Env* env, const std::string& source_path, const std::string& des
 // defined which file will be deleted first.
 Status DeleteExcessFilesByPattern(Env* env, const std::string& pattern, int max_matches);
 
+// Traverses 'path' recursively and deletes all files matching the special Kudu
+// tmp file infix. Does not follow symlinks.
+//
+// Deletion errors generate warnings but do not halt the traversal.
+Status DeleteTmpFilesRecursively(Env* env, const std::string& path);
+
 // Deletes a file or directory when this object goes out of scope.
 //
 // The deletion may be cancelled by calling .Cancel().

commit f3f75ab5a409ab2c2faf646cd70de3c7fb35f914

fs_manager: clean tmp files from all special directories
Commit c26e09e regressed tmp file cleanup in that the tablet-meta and cmeta
directories were no longer cleaned. This is because I assumed that the
cleanup done in DataDirManager::Open would include these directories (they
are typically found in the first data root), but I forgot that in the
DataDirManager, a data directory is the data root plus a "/data" suffix.

This patch fixes that by explicitly including all "special" directories in
the FsManager's cleanup routine.

Change-Id: Ia6340ad72a6996869e7a83d0d66c165dfe2173c8
Reviewed-on: http://gerrit.cloudera.org:8080/7144
Tested-by: Kudu Jenkins
Reviewed-by: Todd Lipcon <todd@apache.org>

src/kudu/fs/fs_manager.cc
@@ -258,7 +258,7 @@ Status FsManager::Open(FsReport* report) {
   // Temporary files in the data directory roots will be removed by the block
   // manager.
   if (!read_only_) {
-    CleanTmpFilesInWalRoot();
+    CleanTmpFiles();
     CheckAndFixPermissions();
   }
 
@@ -479,10 +479,14 @@ string FsManager::GetWalSegmentFileName(const string& tablet_id,
                                               StringPrintf("%09" PRIu64, sequence_number)));
 }
 
-void FsManager::CleanTmpFilesInWalRoot() {
+void FsManager::CleanTmpFiles() {
   DCHECK(!read_only_);
-  WARN_NOT_OK(env_util::DeleteTmpFilesRecursively(env_, canonicalized_wal_fs_root_),
-              "Error while deleting tmp files");
+  for (const auto& s : { GetWalsRootDir(),
+                         GetTabletMetadataDir(),
+                         GetConsensusMetadataDir() }) {
+    WARN_NOT_OK(env_util::DeleteTmpFilesRecursively(env_, s),
+                Substitute("Error deleting tmp files in $0", s));
+  }
 }
 
 void FsManager::CheckAndFixPermissions() {

src/kudu/fs/fs_manager.h
@@ -250,10 +250,11 @@ class FsManager {
                           const std::string& path,
                           const std::vector<std::string>& objects);
 
-  // Deletes leftover temporary files in canonicalized_wal_fs_root_.
+  // Deletes leftover temporary files in all "special" top-level directories
+  // (e.g. WAL root directory).
   //
   // Logs warnings in case of errors.
-  void CleanTmpFilesInWalRoot();
+  void CleanTmpFiles();
 
   // Checks that the permissions of the root data directories conform to the
   // configured umask, and tightens them as necessary if they do not.

src/kudu/fs/fs_manager-test.cc
@@ -256,6 +256,12 @@ TEST_F(FsManagerTestBase, TestTmpFilesCleanup) {
   tmp_path = JoinPathSegments(fs_manager()->GetDataRootDirs()[0], "data1.kudutmp.file");
   ASSERT_OK(env_util::OpenFileForWrite(fs_manager()->env(), tmp_path, &tmp_writer));
 
+  tmp_path = JoinPathSegments(fs_manager()->GetConsensusMetadataDir(), "12345.kudutmp.asdfg");
+  ASSERT_OK(env_util::OpenFileForWrite(fs_manager()->env(), tmp_path, &tmp_writer));
+
+  tmp_path = JoinPathSegments(fs_manager()->GetTabletMetadataDir(), "12345.kudutmp.asdfg");
+  ASSERT_OK(env_util::OpenFileForWrite(fs_manager()->env(), tmp_path, &tmp_writer));
+
   // Not a misprint here: checking for just ".kudutmp" as well
   tmp_path = JoinPathSegments(fs_manager()->GetDataRootDirs()[1], "data2.kudutmp");
   ASSERT_OK(env_util::OpenFileForWrite(fs_manager()->env(), tmp_path, &tmp_writer));
@@ -272,11 +278,13 @@ TEST_F(FsManagerTestBase, TestTmpFilesCleanup) {
   ASSERT_EQ(0, symlink_error);
 
   vector<string> lookup_dirs = fs_manager()->GetDataRootDirs();
-  lookup_dirs.push_back(fs_manager()->GetWalsRootDir());
+  lookup_dirs.emplace_back(fs_manager()->GetWalsRootDir());
+  lookup_dirs.emplace_back(fs_manager()->GetConsensusMetadataDir());
+  lookup_dirs.emplace_back(fs_manager()->GetTabletMetadataDir());
 
   size_t n_tmp_files = 0;
   ASSERT_OK(CountTmpFiles(fs_manager()->env(), lookup_dirs, &n_tmp_files));
-  ASSERT_EQ(4, n_tmp_files);
+  ASSERT_EQ(6, n_tmp_files);
 
   // Opening fs_manager should remove tmp files
   ASSERT_OK(fs_manager()->Open());