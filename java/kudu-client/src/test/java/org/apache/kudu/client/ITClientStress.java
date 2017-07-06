ac2a2852ed6b593f9ea6a56190b2e1bea4122964
Todd Lipcon <todd@apache.org>
2017-07-05 17:50:50 -0700

    java: refactor ITClientStress for more reusable code
    Change-Id: I263b5681b52df0fa496cce2f58c0605c66810e64
    Reviewed-on: http://gerrit.cloudera.org:8080/7360
    Tested-by: Kudu Jenkins
    Reviewed-by: Jean-Daniel Cryans <jdcryans@apache.org>
    Reviewed-by: Alexey Serbin <aserbin@cloudera.com>

--- java/kudu-client/src/test/java/org/apache/kudu/client/ITClientStress.java
+++ java/kudu-client/src/test/java/org/apache/kudu/client/ITClientStress.java
@@ -18,19 +18,67 @@ package org.apache.kudu.client;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 import java.io.Closeable;
+import java.io.IOException;
+import java.util.concurrent.Callable;
 import java.util.concurrent.SynchronousQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicReference;
 
 import com.google.common.base.Stopwatch;
+import com.google.common.base.Supplier;
+
 import org.junit.Test;
 
 import org.apache.kudu.util.CapturingLogAppender;
 
 public class ITClientStress extends BaseKuduTest {
+
+  private void runTasks(int numThreads, int secondsToRun,
+      Supplier<Callable<Void>> taskFactory) throws InterruptedException, IOException {
+    // Capture any exception thrown by the tasks.
+    final AtomicReference<Throwable> thrown = new AtomicReference<>(null);
+
+    // Setup a pool with synchronous handoff.
+    SynchronousQueue<Runnable> queue = new SynchronousQueue<>();
+    ThreadPoolExecutor pool = new ThreadPoolExecutor(
+        numThreads, numThreads, 100, TimeUnit.MILLISECONDS,
+        queue, new ThreadPoolExecutor.CallerRunsPolicy());
+
+    // Capture logs so we can check that no exceptions are logged.
+    CapturingLogAppender cla = new CapturingLogAppender();
+    try (Closeable c = cla.attach()){
+      Stopwatch s = Stopwatch.createStarted();
+      while (s.elapsed(TimeUnit.SECONDS) < secondsToRun &&
+          thrown.get() == null) {
+        final Callable<Void> task = taskFactory.get();
+        // Wrap the task so that if it throws an exception, we stop and
+        // fail the test.
+        Runnable wrapped = new Runnable() {
+          @Override
+          public void run() {
+            try {
+              task.call();
+            } catch (Throwable t) {
+              thrown.set(t);
+            }
+          }
+        };
+        pool.submit(wrapped);
+      }
+    } finally {
+      pool.shutdown();
+      assertTrue(pool.awaitTermination(10, TimeUnit.SECONDS));
+      assertNull(thrown.get());
+    }
+    assertFalse("log contained NPE",
+        cla.getAppendedText().contains("NullPointerException"));
+    assertFalse("log contained SSLException",
+        cla.getAppendedText().contains("SSLException"));
+  }
 
   /**
    * Regression test for KUDU-1963. This simulates the behavior of the
@@ -44,45 +92,23 @@ public class ITClientStress extends BaseKuduTest {
     final int NUM_THREADS = 80;
     createFourTabletsTableWithNineRows(TABLE_NAME);
 
-    // Setup a pool with synchronous handoff.
-    SynchronousQueue<Runnable> queue = new SynchronousQueue<>();
-    ThreadPoolExecutor pool = new ThreadPoolExecutor(
-        NUM_THREADS, NUM_THREADS, 100, TimeUnit.MILLISECONDS,
-        queue, new ThreadPoolExecutor.CallerRunsPolicy());
-
-    // Capture any exception thrown by the tasks.
-    final AtomicReference<Throwable> thrown = new AtomicReference<>(null);
-
-    // Capture logs so we can check that no exceptions are logged.
-    CapturingLogAppender cla = new CapturingLogAppender();
-    try (Closeable c = cla.attach()) {
-      // Submit tasks for the prescribed amount of time.
-      Stopwatch s = Stopwatch.createStarted();
-      while (s.elapsed(TimeUnit.SECONDS) < SECONDS_TO_RUN &&
-             thrown.get() == null) {
-        pool.submit(new Runnable() {
+    runTasks(NUM_THREADS, SECONDS_TO_RUN, new Supplier<Callable<Void>>() {
+      @Override
+      public Callable<Void> get() {
+        return new Callable<Void>() {
           @Override
-          public void run() {
+          public Void call() throws Exception {
             try (AsyncKuduClient client =
                   new AsyncKuduClient.AsyncKuduClientBuilder(masterAddresses)
                   .defaultAdminOperationTimeoutMs(DEFAULT_SLEEP)
                   .build()) {
               KuduTable t = client.openTable(TABLE_NAME).join();
               new KuduScanToken.KuduScanTokenBuilder(client, t).build();
-            } catch (Throwable t) {
-              thrown.set(t);
             }
+            return null;
           }
-        });
+        };
       }
-      pool.shutdown();
-      pool.awaitTermination(10, TimeUnit.SECONDS);
-    }
-    assertNull(thrown.get());
-    assertFalse("log contained NPE",
-        cla.getAppendedText().contains("NullPointerException"));
-    assertFalse("log contained SSLException",
-        cla.getAppendedText().contains("SSLException"));
+    });
   }
-
 }
