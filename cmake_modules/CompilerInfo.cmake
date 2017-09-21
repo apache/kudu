315a464c192d04e37f75a84aaceae22cc8f4425f
Dan Burkert <danburkert@apache.org>
2017-09-21 12:24:31 -0700

    Add support for clang from XCode 9 in macOS
    Change-Id: Ib636deee77471e60c02e90028a0e5cc0f303b45a
    Reviewed-on: http://gerrit.cloudera.org:8080/8119
    Reviewed-by: Alexey Serbin <aserbin@cloudera.com>
    Tested-by: Kudu Jenkins
    Reviewed-by: David Ribeiro Alves <davidralves@gmail.com>

--- cmake_modules/CompilerInfo.cmake
+++ cmake_modules/CompilerInfo.cmake
@@ -42,6 +42,19 @@ elseif("${COMPILER_VERSION_FULL}" MATCHES ".*clang-8")
   set(COMPILER_FAMILY "clang")
   set(COMPILER_VERSION "3.8.0svn")
 
+# clang on Mac OS X, XCode 9.
+#
+# TODO(dan): 4.0.0svn is just a guess, because Apple has not (as of the time of
+# writing) released the sources [1]. [2] and [3] are good resources for
+# determining the upstream Clang version as well.
+#
+# [1]: https://opensource.apple.com/
+# [2]: https://gist.github.com/yamaya/2924292
+# [3]: https://gist.github.com/epipping/ef8b02b0cfaec4a5ebf3a57092145a3f
+elseif("${COMPILER_VERSION_FULL}" MATCHES ".*clang-9")
+  set(COMPILER_FAMILY "clang")
+  set(COMPILER_VERSION "4.0.0svn")
+
 # gcc
 elseif("${COMPILER_VERSION_FULL}" MATCHES ".*gcc version.*")
   set(COMPILER_FAMILY "gcc")
