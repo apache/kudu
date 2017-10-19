c7fa4aaf704e4035843bc8fe20e5ecada2ed1acc
Will Berkeley <wdberkeley@apache.org>
2017-10-19 13:34:02 -0700

    KUDU-2170 Masters can start with duplicates specified
    Users can specify duplicate masters in --master_addresses, where a
    duplicate includes duplicated names and the same server known by
    different names. This doesn't cause problems as far as I know, but it
    looks scary because, for example, the web ui shows extra masters listed in
    /masters. This is because /masters shows one master per result from
    ListMasters. This patch deduplicates the output of ListMasters by UUID.
    
    It also warns on start if there are exact string duplicates in
    --master_addresses, at the time the master compares on-disk and
    configured master addresses, but before the masters are ready to
    resolve UUIDs.
    
    Also, the error message when on-disk and configured masters don't match
    is confusing. This patch makes it easy to understand.
    
    Change-Id: I5047ccf92dde5377da18a1a84e105a951fa5b48c
    Reviewed-on: http://gerrit.cloudera.org:8080/8341
    Reviewed-by: Adar Dembo <adar@cloudera.com>
    Tested-by: Kudu Jenkins
    Reviewed-by: Andrew Wong <awong@cloudera.com>

--- src/kudu/master/sys_catalog.cc
+++ src/kudu/master/sys_catalog.cc
@@ -24,6 +24,7 @@
 #include <memory>
 #include <ostream>
 #include <set>
+#include <string>
 #include <type_traits>
 #include <utility>
 #include <vector>
@@ -196,6 +197,11 @@ Status SysCatalogTable::Load(FsManager *fs_manager) {
     for (const auto& hp : master_->opts().master_addresses) {
       peer_addrs_from_opts.insert(hp.ToString());
     }
+    if (peer_addrs_from_opts.size() < master_->opts().master_addresses.size()) {
+      LOG(WARNING) << Substitute("Found duplicates in --master_addresses: "
+                                 "the unique set of addresses is $0",
+                                 JoinStrings(peer_addrs_from_opts, ", "));
+    }
     set<string> peer_addrs_from_disk;
     for (const auto& p : cstate.committed_config().peers()) {
       HostPort hp;
@@ -210,8 +216,11 @@ Status SysCatalogTable::Load(FsManager *fs_manager) {
                                   std::back_inserter(symm_diff));
     if (!symm_diff.empty()) {
       string msg = Substitute(
-          "on-disk and provided master lists are different: $0",
-          JoinStrings(symm_diff, " "));
+          "on-disk master list ($0) and provided master list ($1) differ. "
+          "Their symmetric difference is: $2",
+          JoinStrings(peer_addrs_from_opts, ", "),
+          JoinStrings(peer_addrs_from_disk, ", "),
+          JoinStrings(symm_diff, ", "));
       return Status::InvalidArgument(msg);
     }
   }
