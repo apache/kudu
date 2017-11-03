84c7e9328a373d55f0329faf9fa02bafcb554931
Todd Lipcon <todd@apache.org>
2017-11-03 12:39:25 -0700

    [security] test and fixes for TLS socket EINTR issues
    SSL_{read,write}() can return SSL_ERROR_WANT_{READ,WRITE}
    correspondingly when signal interrupts recv()/send() calls even if
    SSL_MODE_AUTO_RETRY is set in the TLS context.  To handle that
    properly in Socket::Blocking{Recv,Write}() methods, return
    NetworkError() with appropriate POSIX error code from
    TlsSocket::{Recv,Write}().
    
    As a by-product, this changelist fixes flakiness in TestUniqueClientIds
    scenario of the ClientStressTest test and other flaky tests which failed
    with errors like below:
    
      Bad status: IO error: Could not connect to the cluster: \
        Client connection negotiation failed: client connection to \
        IP:port: Read zero bytes on a blocking Recv() call: \
        Transferred 0 of 4 bytes
    
    Prior to this fix, the test failure ratio observed with dist-test
    for TSAN builds was about 6% in multiple 1K runs.  After the fix,
    no failures observed.
    
    Change-Id: Ibec9049186f79f1c43295e4735538ed7ba4e516e
    Reviewed-on: http://gerrit.cloudera.org:8080/8462
    Tested-by: Kudu Jenkins
    Reviewed-by: Dan Burkert <danburkert@apache.org>
    Reviewed-by: Alexey Serbin <aserbin@cloudera.com>
    (cherry picked from commit 18e024cf8bcaea192efb63780802cc4c799bbb9c)
    (cherry picked from commit d98c24fa69ccea676cdf50857ebdff8fe866f751)
    Reviewed-on: http://gerrit.cloudera.org:8080/8601
    Tested-by: Alexey Serbin <aserbin@cloudera.com>
    Reviewed-by: Todd Lipcon <todd@apache.org>

--- src/kudu/util/net/socket.cc
+++ src/kudu/util/net/socket.cc
@@ -27,6 +27,7 @@
 
 #include <limits>
 #include <string>
+#include <type_traits>
 
 #include <glog/logging.h>
 
@@ -54,6 +55,14 @@ DEFINE_bool(socket_inject_short_recvs, false,
             "requested");
 TAG_FLAG(socket_inject_short_recvs, hidden);
 TAG_FLAG(socket_inject_short_recvs, unsafe);
+
+// TODO(todd) consolidate with other copies of this!
+// Retry on EINTR for functions like read() that return -1 on error.
+#define RETRY_ON_EINTR(err, expr) do { \
+  static_assert(std::is_signed<decltype(err)>::value == true, \
+                #err " must be a signed integer"); \
+  (err) = (expr); \
+} while ((err) == -1 && errno == EINTR)
 
 namespace kudu {
 
@@ -337,20 +346,25 @@ Status Socket::Accept(Socket *new_conn, Sockaddr *remote, int flags) {
   if (flags & FLAG_NONBLOCKING) {
     accept_flags |= SOCK_NONBLOCK;
   }
-  new_conn->Reset(::accept4(fd_, (struct sockaddr*)&addr,
-                  &olen, accept_flags));
-  if (new_conn->GetFd() < 0) {
+  int fd = -1;
+  RETRY_ON_EINTR(fd, accept4(fd_, (struct sockaddr*)&addr,
+                             &olen, accept_flags));
+  if (fd < 0) {
     int err = errno;
     return Status::NetworkError(std::string("accept4(2) error: ") +
                                 ErrnoToString(err), Slice(), err);
   }
+  new_conn->Reset(fd);
+
 #else
-  new_conn->Reset(::accept(fd_, (struct sockaddr*)&addr, &olen));
-  if (new_conn->GetFd() < 0) {
+  int fd = -1;
+  RETRY_ON_EINTR(fd, accept(fd_, (struct sockaddr*)&addr, &olen));
+  if (fd < 0) {
     int err = errno;
     return Status::NetworkError(std::string("accept(2) error: ") +
                                 ErrnoToString(err), Slice(), err);
   }
+  new_conn->Reset(fd);
   RETURN_NOT_OK(new_conn->SetNonBlocking(flags & FLAG_NONBLOCKING));
   RETURN_NOT_OK(new_conn->SetCloseOnExec());
 #endif // defined(__linux__)
@@ -507,7 +521,8 @@ Status Socket::Recv(uint8_t *buf, int32_t amt, int32_t *nread) {
   }
 
   DCHECK_GE(fd_, 0);
-  int res = ::recv(fd_, buf, amt, 0);
+  int res;
+  RETRY_ON_EINTR(res, recv(fd_, buf, amt, 0));
   if (res <= 0) {
     if (res == 0) {
       return Status::NetworkError("Recv() got EOF from remote", Slice(), ESHUTDOWN);
