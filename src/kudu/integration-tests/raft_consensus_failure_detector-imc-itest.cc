apache/kudu

commit 28a671365d6d38da966481daf937b3776e3d4852

consensus: support changing between VOTER and NON_VOTER
This patch implements changing from VOTER to NON_VOTER and vice-versa.

Added an integration test scenario for NON_VOTER --> VOTER,
VOTER --> NON_VOTER changes and other use cases.

Change-Id: I978cbed683b0e95a903f2bd2b57496aee71cb33a
Reviewed-on: http://gerrit.cloudera.org:8080/8297
Tested-by: Kudu Jenkins
Reviewed-by: Todd Lipcon <todd@apache.org>

src/kudu/consensus/raft_consensus.cc
@@ -316,7 +316,7 @@ Status RaftConsensus::Start(const ConsensusBootstrapInfo& info,
       }
     }
 
-    // Now assume "follower" duties.
+    // Now assume non-leader replica duties.
     RETURN_NOT_OK(BecomeReplicaUnlocked(fd_initial_delta));
 
     SetStateUnlocked(kRunning);
@@ -585,15 +585,9 @@ Status RaftConsensus::BecomeReplicaUnlocked(boost::optional<MonoDelta> fd_delta)
                                  << ToStringUnlocked();
   ClearLeaderUnlocked();
 
-  if (consensus::IsVoterRole(cmeta_->active_role())) {
-    // A voter should run failure detector, if not a leader.
-    EnableFailureDetector(std::move(fd_delta));
-  } else {
-    // A non-voter should not start leader elections. The leader failure
-    // detector should be re-enabled once the non-voter replica is promoted
-    // to voter replica.
-    DisableFailureDetector();
-  }
+  // Enable/disable leader failure detection if becoming VOTER/NON_VOTER replica
+  // correspondingly.
+  ToggleFailureDetector(std::move(fd_delta));
 
   // Now that we're a replica, we can allow voting for other nodes.
   withhold_votes_until_ = MonoTime::Min();
@@ -1648,8 +1642,31 @@ Status RaftConsensus::ChangeConfig(const ChangeConfigRequestPB& req,
 
       // TODO(mpercy): Support changing between VOTER and NON_VOTER.
       case CHANGE_REPLICA_TYPE:
+        if (server.member_type() == RaftPeerPB::UNKNOWN_MEMBER_TYPE) {
+          return Status::InvalidArgument("Cannot change replica type to UNKNOWN_MEMBER_TYPE");
+        }
+        if (server_uuid == peer_uuid()) {
+          return Status::InvalidArgument(
+              Substitute("Cannot change the replica type of peer $0 because it is the leader. "
+                         "Force another leader to be elected to modify the type of this replica. "
+                         "Consensus state: $1",
+                         server_uuid,
+                         SecureShortDebugString(cmeta_->ToConsensusStatePB())));
+        }
+        RaftPeerPB* peer_pb;
+        RETURN_NOT_OK(GetRaftConfigMember(&new_config, server_uuid, &peer_pb));
+        // Validate the changes.
+        if (ReplicaTypesEqual(*peer_pb, server)) {
+          return Status::InvalidArgument("Cannot change replica type to same type");
+        }
+        peer_pb->set_member_type(server.member_type());
+        // TODO(mpercy): Copy over replica intentions when implemented.
+        break;
+
       default:
-        return Status::NotSupported("Role change is not yet implemented.");
+        return Status::NotSupported(Substitute(
+            "$0: unsupported type of configuration change",
+            ChangeConfigType_Name(type)));
     }
 
     RETURN_NOT_OK(ReplicateConfigChangeUnlocked(
@@ -2401,6 +2418,10 @@ void RaftConsensus::CompleteConfigChangeRoundUnlocked(ConsensusRound* round, con
       LOG_WITH_PREFIX_UNLOCKED(INFO) << "Aborting config change with OpId "
                                      << op_id << ": " << status.ToString();
       cmeta_->clear_pending_config();
+
+      // Disable leader failure detection if transitioning from VOTER to
+      // NON_VOTER and vice versa.
+      ToggleFailureDetector();
     } else {
       LOG_WITH_PREFIX_UNLOCKED(INFO)
           << "Skipping abort of non-pending config change with OpId "
@@ -2448,7 +2469,6 @@ void RaftConsensus::CompleteConfigChangeRoundUnlocked(ConsensusRound* round, con
   }
 }
 
-
 void RaftConsensus::EnableFailureDetector(boost::optional<MonoDelta> delta) {
   if (PREDICT_TRUE(FLAGS_enable_leader_failure_detection)) {
     failure_detector_->Start(std::move(delta));
@@ -2458,6 +2478,18 @@ void RaftConsensus::EnableFailureDetector(boost::optional<MonoDelta> delta) {
 void RaftConsensus::DisableFailureDetector() {
   if (PREDICT_TRUE(FLAGS_enable_leader_failure_detection)) {
     failure_detector_->Stop();
+  }
+}
+
+void RaftConsensus::ToggleFailureDetector(boost::optional<MonoDelta> delta) {
+  if (IsRaftConfigVoter(peer_uuid(), cmeta_->ActiveConfig())) {
+    // A non-leader voter replica should run failure detector.
+    EnableFailureDetector(std::move(delta));
+  } else {
+    // A non-voter should not start leader elections. The leader failure
+    // detector should be re-enabled once the non-voter replica is promoted
+    // to voter replica.
+    DisableFailureDetector();
   }
 }
 
@@ -2583,6 +2615,11 @@ Status RaftConsensus::SetPendingConfigUnlocked(const RaftConfigPB& new_config) {
         << "New pending config: " << SecureShortDebugString(new_config);
   }
   cmeta_->set_pending_config(new_config);
+
+  // Disable leader failure detection if transitioning from VOTER to NON_VOTER
+  // and vice versa.
+  ToggleFailureDetector();
+
   return Status::OK();
 }
 

src/kudu/consensus/raft_consensus.h
@@ -563,6 +563,14 @@ class RaftConsensus : public std::enable_shared_from_this<RaftConsensus>,
   // If the failure detector is already disabled, has no effect.
   void DisableFailureDetector();
 
+  // Disable leader failure detector if transitioning from VOTER to NON_VOTER,
+  // and vice versa. The decision is based on the type of membership of the
+  // peer in the active Raft configuration.
+  //
+  // If it's time to enable the leader failure detection, the specified
+  // 'delta' value is used as described in EnableFailureDetector()'s comment.
+  void ToggleFailureDetector(boost::optional<MonoDelta> delta = boost::none);
+
   // "Reset" the failure detector to indicate leader activity.
   //
   // When this is called a failure is guaranteed not to be detected before

src/kudu/consensus/quorum_util-test.cc
@@ -179,6 +179,16 @@ TEST(QuorumUtilTest, TestIsRaftConfigVoter) {
   ASSERT_FALSE(IsRaftConfigVoter("B", config));
   ASSERT_FALSE(IsRaftConfigVoter("C", config));
   ASSERT_FALSE(IsRaftConfigVoter(no_member_type_peer_uuid, config));
+
+  RaftPeerPB* peer_a;
+  ASSERT_OK(GetRaftConfigMember(&config, "A", &peer_a));
+  RaftPeerPB* peer_b;
+  ASSERT_OK(GetRaftConfigMember(&config, "B", &peer_b));
+  ASSERT_FALSE(ReplicaTypesEqual(*peer_a, *peer_b));
+  ASSERT_TRUE(ReplicaTypesEqual(*peer_b, *peer_b));
+  RaftPeerPB* peer_c;
+  ASSERT_OK(GetRaftConfigMember(&config, "C", &peer_c));
+  ASSERT_FALSE(ReplicaTypesEqual(*peer_b, *peer_c));
 }
 
 } // namespace consensus

src/kudu/consensus/quorum_util.cc
@@ -98,6 +98,12 @@ bool RemoveFromRaftConfig(RaftConfigPB* config, const string& uuid) {
   return true;
 }
 
+bool ReplicaTypesEqual(const RaftPeerPB& peer1, const RaftPeerPB& peer2) {
+  // TODO(mpercy): Include comparison of replica intentions once they are
+  // implemented.
+  return peer1.member_type() == peer2.member_type();
+}
+
 int CountVoters(const RaftConfigPB& config) {
   int voters = 0;
   for (const RaftPeerPB& peer : config.peers()) {

src/kudu/consensus/quorum_util.h
@@ -56,6 +56,10 @@ Status GetRaftConfigLeader(ConsensusStatePB* cstate, RaftPeerPB** peer_pb);
 // Returns true on success.
 bool RemoveFromRaftConfig(RaftConfigPB* config, const std::string& uuid);
 
+// Returns true iff the two peers have equivalent replica types and associated
+// options.
+bool ReplicaTypesEqual(const RaftPeerPB& peer1, const RaftPeerPB& peer2);
+
 // Counts the number of voters in the configuration.
 int CountVoters(const RaftConfigPB& config);
 

src/kudu/integration-tests/cluster_itest_util.cc
@@ -738,7 +738,40 @@ Status RemoveServer(const TServerDetails* leader,
   rpc.set_timeout(timeout);
   RETURN_NOT_OK(leader->consensus_proxy->ChangeConfig(req, &resp, &rpc));
   if (resp.has_error()) {
-    if (error_code) *error_code = resp.error().code();
+    if (error_code) {
+      *error_code = resp.error().code();
+    }
+    return StatusFromPB(resp.error().status());
+  }
+  return Status::OK();
+}
+
+Status ChangeReplicaType(const TServerDetails* leader,
+                         const std::string& tablet_id,
+                         const TServerDetails* target_replica,
+                         RaftPeerPB::MemberType replica_type,
+                         const MonoDelta& timeout,
+                         const boost::optional<int64_t>& cas_config_index,
+                         tserver::TabletServerErrorPB::Code* error_code) {
+  ChangeConfigRequestPB req;
+  req.set_dest_uuid(leader->uuid());
+  req.set_tablet_id(tablet_id);
+  req.set_type(consensus::CHANGE_REPLICA_TYPE);
+  if (cas_config_index) {
+    req.set_cas_config_opid_index(*cas_config_index);
+  }
+  RaftPeerPB* peer = req.mutable_server();
+  peer->set_permanent_uuid(target_replica->uuid());
+  peer->set_member_type(replica_type);
+
+  ChangeConfigResponsePB resp;
+  RpcController rpc;
+  rpc.set_timeout(timeout);
+  RETURN_NOT_OK(leader->consensus_proxy->ChangeConfig(req, &resp, &rpc));
+  if (resp.has_error()) {
+    if (error_code) {
+      *error_code = resp.error().code();
+    }
     return StatusFromPB(resp.error().status());
   }
   return Status::OK();

src/kudu/integration-tests/cluster_itest_util.h
@@ -288,6 +288,16 @@ Status RemoveServer(const TServerDetails* leader,
                     const boost::optional<int64_t>& cas_config_index = boost::none,
                     tserver::TabletServerErrorPB::Code* error_code = nullptr);
 
+// Change type of the given replica to the specified type.
+// The RPC request is sent to 'leader'.
+Status ChangeReplicaType(const TServerDetails* leader,
+                         const std::string& tablet_id,
+                         const TServerDetails* target_replica,
+                         consensus::RaftPeerPB::MemberType replica_type,
+                         const MonoDelta& timeout,
+                         const boost::optional<int64_t>& cas_config_index = boost::none,
+                         tserver::TabletServerErrorPB::Code* error_code = nullptr);
+
 // Get the list of tablets from the remote server.
 Status ListTablets(const TServerDetails* ts,
                    const MonoDelta& timeout,

commit 1c9e424c251d6b47fa8a161a94358634fbd13549

KUDU-2229. consensus: Leader should not start FD
This patch fixes a log spam issue caused by commit
28a671365d6d38da966481daf937b3776e3d4852 that erroneously enables the
failure detector on the leader when a configuration takes place. That
bug had no consensus safety implications, but it did generate a large
volume of confusing log messages in a neverending loop.

This patch also includes some minor updates to InternalMiniCluster for
convenience purposes.

Change-Id: Ie2ec9c5499e8e4c1659333bd53dd2d7f6dae81f5
Reviewed-on: http://gerrit.cloudera.org:8080/8711
Reviewed-by: Alexey Serbin <aserbin@cloudera.com>
Tested-by: Kudu Jenkins

src/kudu/consensus/raft_consensus.cc
@@ -599,7 +599,7 @@ Status RaftConsensus::BecomeReplicaUnlocked(boost::optional<MonoDelta> fd_delta)
 
   // Enable/disable leader failure detection if becoming VOTER/NON_VOTER replica
   // correspondingly.
-  ToggleFailureDetector(std::move(fd_delta));
+  UpdateFailureDetectorState(std::move(fd_delta));
 
   // Now that we're a replica, we can allow voting for other nodes.
   withhold_votes_until_ = MonoTime::Min();
@@ -2610,7 +2610,7 @@ void RaftConsensus::CompleteConfigChangeRoundUnlocked(ConsensusRound* round, con
 
       // Disable leader failure detection if transitioning from VOTER to
       // NON_VOTER and vice versa.
-      ToggleFailureDetector();
+      UpdateFailureDetectorState();
     } else {
       LOG_WITH_PREFIX_UNLOCKED(INFO)
           << "Skipping abort of non-pending config change with OpId "
@@ -2670,14 +2670,15 @@ void RaftConsensus::DisableFailureDetector() {
   }
 }
 
-void RaftConsensus::ToggleFailureDetector(boost::optional<MonoDelta> delta) {
-  if (IsRaftConfigVoter(peer_uuid(), cmeta_->ActiveConfig())) {
-    // A non-leader voter replica should run failure detector.
+void RaftConsensus::UpdateFailureDetectorState(boost::optional<MonoDelta> delta) {
+  const auto& uuid = peer_uuid();
+  if (uuid != cmeta_->leader_uuid() &&
+      cmeta_->IsVoterInConfig(uuid, ACTIVE_CONFIG)) {
+    // A voter that is not the leader should run the failure detector.
     EnableFailureDetector(std::move(delta));
   } else {
-    // A non-voter should not start leader elections. The leader failure
-    // detector should be re-enabled once the non-voter replica is promoted
-    // to voter replica.
+    // Otherwise, the local peer should not start leader elections
+    // (e.g. if it is the leader, a non-voter, a non-participant, etc).
     DisableFailureDetector();
   }
 }
@@ -2805,9 +2806,7 @@ Status RaftConsensus::SetPendingConfigUnlocked(const RaftConfigPB& new_config) {
   }
   cmeta_->set_pending_config(new_config);
 
-  // Disable leader failure detection if transitioning from VOTER to NON_VOTER
-  // and vice versa.
-  ToggleFailureDetector();
+  UpdateFailureDetectorState();
 
   return Status::OK();
 }

src/kudu/consensus/raft_consensus.h
@@ -158,6 +158,11 @@ class RaftConsensus : public std::enable_shared_from_this<RaftConsensus>,
   // Returns Status::TimedOut if the role is not LEADER within 'timeout'.
   Status WaitUntilLeaderForTests(const MonoDelta& timeout);
 
+  // Return a copy of the failure detector instance. Only for use in tests.
+  std::shared_ptr<rpc::PeriodicTimer> GetFailureDetectorForTests() const {
+    return failure_detector_;
+  }
+
   // Implement a LeaderStepDown() request.
   Status StepDown(LeaderStepDownResponsePB* resp);
 
@@ -582,13 +587,15 @@ class RaftConsensus : public std::enable_shared_from_this<RaftConsensus>,
   // If the failure detector is already disabled, has no effect.
   void DisableFailureDetector();
 
-  // Disable leader failure detector if transitioning from VOTER to NON_VOTER,
-  // and vice versa. The decision is based on the type of membership of the
-  // peer in the active Raft configuration.
+  // Enables or disables the failure detector based on the role of the local
+  // peer in the active config. If the local peer a VOTER, but not the leader,
+  // then failure detection will be enabled. If the local peer is the leader,
+  // or a NON_VOTER, then failure detection will be disabled.
   //
-  // If it's time to enable the leader failure detection, the specified
-  // 'delta' value is used as described in EnableFailureDetector()'s comment.
-  void ToggleFailureDetector(boost::optional<MonoDelta> delta = boost::none);
+  // See EnableFailureDetector() for an explanation of the 'delta' parameter,
+  // which is used if it is determined that the failure detector should be
+  // enabled.
+  void UpdateFailureDetectorState(boost::optional<MonoDelta> delta = boost::none);
 
   // "Reset" the failure detector to indicate leader activity.
   //

src/kudu/mini-cluster/external_mini_cluster.h
@@ -232,9 +232,7 @@ class ExternalMiniCluster : public MiniCluster {
   // Return ExternalTabletServer given its UUID. If not found, returns NULL.
   ExternalTabletServer* tablet_server_by_uuid(const std::string& uuid) const;
 
-  // Return the index of the ExternalTabletServer that has the given 'uuid', or
-  // -1 if no such UUID can be found.
-  int tablet_server_index_by_uuid(const std::string& uuid) const;
+  int tablet_server_index_by_uuid(const std::string& uuid) const override;
 
   // Return all tablet servers and masters.
   std::vector<ExternalDaemon*> daemons() const;

src/kudu/mini-cluster/internal_mini_cluster.cc
@@ -230,6 +230,24 @@ MiniTabletServer* InternalMiniCluster::mini_tablet_server(int idx) const {
   return mini_tablet_servers_[idx].get();
 }
 
+MiniTabletServer* InternalMiniCluster::mini_tablet_server_by_uuid(const string& uuid) const {
+  for (const auto& ts : mini_tablet_servers_) {
+    if (ts->uuid() == uuid) {
+      return ts.get();
+    }
+  }
+  return nullptr;
+}
+
+int InternalMiniCluster::tablet_server_index_by_uuid(const std::string& uuid) const {
+  for (int i = 0; i < mini_tablet_servers_.size(); i++) {
+    if (mini_tablet_servers_[i]->uuid() == uuid) {
+      return i;
+    }
+  }
+  return -1;
+}
+
 vector<HostPort> InternalMiniCluster::master_rpc_addrs() const {
   if (opts_.num_masters == 1) {
     const auto& addr = CHECK_NOTNULL(mini_master(0))->bound_rpc_addr();

src/kudu/mini-cluster/internal_mini_cluster.h
@@ -141,6 +141,11 @@ class InternalMiniCluster : public MiniCluster {
   // 'idx' must be between 0 and 'num_tablet_servers' -1.
   tserver::MiniTabletServer* mini_tablet_server(int idx) const;
 
+  // Returns the TabletServer with uuid 'uuid', or nullptr if not found.
+  tserver::MiniTabletServer* mini_tablet_server_by_uuid(const std::string& uuid) const;
+
+  int tablet_server_index_by_uuid(const std::string& uuid) const override;
+
   int num_tablet_servers() const override {
     return mini_tablet_servers_.size();
   }

src/kudu/mini-cluster/mini_cluster.h
@@ -129,6 +129,10 @@ class MiniCluster {
 
   virtual std::vector<HostPort> master_rpc_addrs() const = 0;
 
+  // Return the index of the tablet server that has the given 'uuid', or
+  // -1 if no such UUID can be found.
+  virtual int tablet_server_index_by_uuid(const std::string& uuid) const = 0;
+
   // Create a client configured to talk to this cluster. 'builder' may contain
   // override options for the client. The master address will be overridden to
   // talk to the running master(s). If 'builder' is a nullptr, default options

src/kudu/rpc/periodic.cc
@@ -117,6 +117,11 @@ void PeriodicTimer::SnoozeUnlocked(boost::optional<MonoDelta> next_task_delta) {
   next_task_time_ = MonoTime::Now() + *next_task_delta;
 }
 
+bool PeriodicTimer::started() const {
+  std::lock_guard<simple_spinlock> l(lock_);
+  return started_;
+}
+
 MonoDelta PeriodicTimer::GetMinimumPeriod() {
   // Given jitter percentage J and period P, this returns (1-J)*P, which is
   // the lowest possible jittered value.

src/kudu/rpc/periodic.h
@@ -127,7 +127,7 @@ class PeriodicTimer : public std::enable_shared_from_this<PeriodicTimer> {
   // otherwise the task is not guaranteed to run in a timely manner.
   //
   // Note: Snooze() is not additive. That is, if called at time X and again at
-  // time X + P/2, the timer is snoozed until X+P/2, not X+2P.
+  // time X + P/2, the timer is snoozed until X+P/2+P, not X+2P.
   //
   // Does nothing if the timer is stopped.
   void Snooze(boost::optional<MonoDelta> next_task_delta = boost::none);
@@ -140,6 +140,9 @@ class PeriodicTimer : public std::enable_shared_from_this<PeriodicTimer> {
   //
   // Does nothing if the timer is already stopped.
   void Stop();
+
+  // Returns true iff the failure detected has been started.
+  bool started() const;
 
  private:
   FRIEND_TEST(PeriodicTimerTest, TestCallbackRestartsTimer);

src/kudu/integration-tests/CMakeLists.txt
@@ -86,9 +86,10 @@ ADD_KUDU_TEST(master-stress-test RESOURCE_LOCK "master-rpc-ports")
 ADD_KUDU_TEST(multidir_cluster-itest)
 ADD_KUDU_TEST(open-readonly-fs-itest)
 ADD_KUDU_TEST(raft_config_change-itest)
-ADD_KUDU_TEST(raft_consensus-itest RUN_SERIAL true)
 ADD_KUDU_TEST(raft_consensus_election-itest)
+ADD_KUDU_TEST(raft_consensus_failure_detector-imc-itest)
 ADD_KUDU_TEST(raft_consensus_nonvoter-itest)
+ADD_KUDU_TEST(raft_consensus-itest RUN_SERIAL true)
 ADD_KUDU_TEST(registration-test RESOURCE_LOCK "master-web-port")
 ADD_KUDU_TEST(security-faults-itest)
 ADD_KUDU_TEST(security-itest)

src/kudu/integration-tests/raft_consensus_failure_detector-imc-itest.cc
@@ -1,0 +1,144 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+#include <memory>
+#include <ostream>
+#include <string>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+#include <glog/logging.h>
+#include <gtest/gtest.h>
+
+#include "kudu/consensus/metadata.pb.h"
+#include "kudu/consensus/raft_consensus.h"
+#include "kudu/gutil/ref_counted.h"
+#include "kudu/integration-tests/cluster_itest_util.h"
+#include "kudu/integration-tests/internal_mini_cluster-itest-base.h"
+#include "kudu/integration-tests/test_workload.h"
+#include "kudu/mini-cluster/internal_mini_cluster.h"
+#include "kudu/rpc/periodic.h"
+#include "kudu/tablet/tablet_replica.h"
+#include "kudu/tserver/mini_tablet_server.h"
+#include "kudu/tserver/tablet_server.h"
+#include "kudu/tserver/ts_tablet_manager.h"
+#include "kudu/util/monotime.h"
+#include "kudu/util/status.h"
+#include "kudu/util/test_macros.h"
+#include "kudu/util/test_util.h"
+
+using kudu::consensus::RaftPeerPB;
+using kudu::itest::WaitForServersToAgree;
+using kudu::tablet::TabletReplica;
+using std::string;
+using std::vector;
+
+namespace kudu {
+
+class RaftConsensusFailureDetectorIMCTest : public MiniClusterITestBase {
+};
+
+// Ensure that the failure detector is enabled for non-leader voters, and
+// disabled for leaders and non-voters. Ensure this persists after a
+// configuration change.
+// Regression test for KUDU-2229.
+TEST_F(RaftConsensusFailureDetectorIMCTest, TestFailureDetectorActivation) {
+  if (!AllowSlowTests()) {
+    LOG(WARNING) << "Skipping test in fast-test mode.";
+    return;
+  }
+
+  const MonoDelta kTimeout = MonoDelta::FromSeconds(30);
+
+  const int kNumReplicas = 3;
+  NO_FATALS(StartCluster(/*num_tablet_servers=*/ kNumReplicas + 1));
+  TestWorkload workload(cluster_.get());
+  workload.Setup();
+
+  // Identify the tablet id and locate the replicas.
+  string tablet_id;
+  itest::TabletReplicaMap replica_map_;
+  string missing_replica_uuid;
+  ASSERT_EVENTUALLY([&] {
+    replica_map_.clear();
+    missing_replica_uuid.clear();
+    for (int i = 0; i < cluster_->num_tablet_servers(); i++) {
+      auto* ts = cluster_->mini_tablet_server(i);
+      auto tablet_ids = ts->ListTablets();
+      if (tablet_ids.empty()) {
+        missing_replica_uuid = ts->uuid();
+        continue;
+      }
+      ASSERT_EQ(1, tablet_ids.size());
+      if (!tablet_id.empty()) {
+        ASSERT_EQ(tablet_id, tablet_ids[0]);
+      }
+      tablet_id = tablet_ids[0];
+      replica_map_.emplace(tablet_id, ts_map_[ts->uuid()]);
+    }
+    ASSERT_EQ(kNumReplicas, replica_map_.count(tablet_id));
+    ASSERT_FALSE(tablet_id.empty());
+  });
+
+  // Wait until tablets are running.
+  auto range = replica_map_.equal_range(tablet_id);
+  for (auto it = range.first; it != range.second; ++it) {
+    auto ts = it->second;
+    ASSERT_OK(WaitUntilTabletRunning(ts, tablet_id, kTimeout));
+  }
+  // Elect a leader.
+  string leader_uuid = range.first->second->uuid();
+  ASSERT_OK(StartElection(ts_map_[leader_uuid], tablet_id, kTimeout));
+  ASSERT_OK(WaitUntilLeader(ts_map_[leader_uuid], tablet_id, kTimeout));
+
+  auto active_ts_map = ts_map_;
+  ASSERT_EQ(1, active_ts_map.erase(missing_replica_uuid));
+
+  // Ensure all servers are up to date.
+  ASSERT_OK(WaitForServersToAgree(kTimeout, active_ts_map, tablet_id,
+                                  /*minimum_index=*/ 1));
+
+  // Ensure that the failure detector activation state is consistent with the
+  // rule that only non-leader VOTER replicas should have failure detection
+  // enabled. Leaders and NON_VOTER replicas should not enable the FD.
+  auto validate_failure_detector_status = [&](const itest::TabletServerMap& ts_map) {
+    for (const auto& entry : ts_map) {
+      const auto& uuid = entry.first;
+      auto mini_ts = cluster_->mini_tablet_server_by_uuid(uuid);
+      vector<scoped_refptr<TabletReplica>> replicas;
+      mini_ts->server()->tablet_manager()->GetTabletReplicas(&replicas);
+      ASSERT_EQ(1, replicas.size());
+      const auto& replica = replicas[0];
+      ASSERT_EQ(replica->consensus()->role() == RaftPeerPB::FOLLOWER,
+                replica->consensus()->GetFailureDetectorForTests()->started());
+    }
+  };
+
+  NO_FATALS(validate_failure_detector_status(active_ts_map));
+
+  // Add a new non-voter.
+  ASSERT_OK(AddServer(ts_map_[leader_uuid], tablet_id,
+                      ts_map_[missing_replica_uuid],
+                      RaftPeerPB::NON_VOTER, kTimeout));
+  ASSERT_OK(WaitForServersToAgree(kTimeout, ts_map_, tablet_id,
+                                  /*minimum_index=*/ 2));
+
+  NO_FATALS(validate_failure_detector_status(ts_map_));
+}
+
+} // namespace kudu