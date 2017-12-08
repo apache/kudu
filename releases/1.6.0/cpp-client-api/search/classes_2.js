751c7afb5ad296229a734abd1c60e36903e6c581
Mike Percy <mpercy@apache.org>
2017-12-07 22:30:01 -0800

    Updating web site for Kudu 1.6.0 release
    Change-Id: Ic6718fb049880fe69464e73950166b5168859ab1
    Reviewed-on: http://gerrit.cloudera.org:8080/8799
    Reviewed-by: Mike Percy <mpercy@apache.org>
    Tested-by: Mike Percy <mpercy@apache.org>

---
+++ releases/1.6.0/cpp-client-api/search/classes_2.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['monodelta',['MonoDelta',['../classkudu_1_1MonoDelta.html',1,'kudu']]],
+  ['monotime',['MonoTime',['../classkudu_1_1MonoTime.html',1,'kudu']]]
+];
---
+++ releases/1.6.0/cpp-client-api/search/classes_5.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['slice',['Slice',['../classkudu_1_1Slice.html',1,'kudu']]],
+  ['slicemap',['SliceMap',['../structkudu_1_1SliceMap.html',1,'kudu']]],
+  ['status',['Status',['../classkudu_1_1Status.html',1,'kudu']]]
+];
