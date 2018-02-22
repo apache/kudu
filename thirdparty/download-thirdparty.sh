1ae56048b8ad62f1f7d11a4e6bdd290f9c753191
Todd Lipcon <todd@apache.org>
2018-02-22 00:28:59 -0800

    thirdparty: patch tcmalloc to improve AllocLarge performance
    This pulls in an upstream tcmalloc pull request[1] which addresses O(n)
    behavior in the large allocation path. Without this patch, large
    allocations (>=1MB) do a linear scan of all large spans of free heap
    to find the best fit, which is very expensive especially as the heap
    grows more fragmented over time.
    
    I tested this using YCSB workload C (100% random read) on an in-memory dataset
    (10M rows on 6 nodes). As noted in KUDU-1465, scanners currently always
    allocate 1MB buffers as a starting point even if the scan will only return a
    small amount of data. These 1M allocations aggravate the AllocLarge path
    and without this patch, AllocLarge was the top CPU consumer.
    
    Prior to this fix, the workload managed about 20k reads/second. With the
    fix, the workload averaged around 85k reads/second.
    
    We should still fix KUDU-1465 to allocate smaller buffers, since small
    allocations will always be faster (and less wasteful), but with this tcmalloc
    fix, the performance issue is much less pronounced. The fix will also likely
    help any other places that we might be making large allocations.
    
    [1] https://github.com/gperftools/gperftools/pull/960
    
    Change-Id: I4abffbd1cb02f99ebda1628d98e5c342ccb7f0f9
    Reviewed-on: http://gerrit.cloudera.org:8080/9392
    Tested-by: Kudu Jenkins
    Reviewed-by: Dan Burkert <dan@cloudera.com>

--- thirdparty/download-thirdparty.sh
+++ thirdparty/download-thirdparty.sh
@@ -124,12 +124,13 @@ if [ ! -d $GFLAGS_SOURCE ]; then
   fetch_and_expand gflags-${GFLAGS_VERSION}.tar.gz
 fi
 
-GPERFTOOLS_PATCHLEVEL=2
+GPERFTOOLS_PATCHLEVEL=4
 delete_if_wrong_patchlevel $GPERFTOOLS_SOURCE $GPERFTOOLS_PATCHLEVEL
 if [ ! -d $GPERFTOOLS_SOURCE ]; then
   fetch_and_expand gperftools-${GPERFTOOLS_VERSION}.tar.gz
 
   pushd $GPERFTOOLS_SOURCE
+  patch -p1 < $TP_DIR/patches/gperftools-Implemented-O-log-n-searching-among-large-spans.patch
   patch -p1 < $TP_DIR/patches/gperftools-Replace-namespace-base-with-namespace-tcmalloc.patch
   patch -p1 < $TP_DIR/patches/gperftools-sized-alloc-build-fix.patch
   touch patchlevel-$GPERFTOOLS_PATCHLEVEL
