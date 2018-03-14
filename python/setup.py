706b3c3b186b5d196d396fbcb66b8ad005084b25
Grant Henke <granthenke@gmail.com>
2018-03-14 09:06:13 -0500

    Update version to 1.8.0-SNAPSHOT
    Change-Id: I6552ec90cab4de52a88b1d3df7170ec6f3623140
    Reviewed-on: http://gerrit.cloudera.org:8080/9628
    Reviewed-by: Todd Lipcon <todd@apache.org>
    Tested-by: Todd Lipcon <todd@apache.org>

--- python/setup.py
+++ python/setup.py
@@ -37,7 +37,7 @@ if Cython.__version__ < '0.21.0':
     raise Exception('Please upgrade to Cython 0.21.0 or newer')
 
 MAJOR = 1
-MINOR = 7
+MINOR = 8
 MICRO = 0
 VERSION = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
 ISRELEASED = True
