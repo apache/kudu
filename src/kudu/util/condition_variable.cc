apache/kudu

commit 038c3ebe77f41e3d2ebfc1d08ea8cf849edb2a5d

condition_variable: rework timed waiting mechanisms and associated cleanup
This patch reworks ConditionVariable::TimedWait into two new methods:
1. WaitFor(delta), equivalent to TimedWait.
2. WaitUntil(deadline), waits for a deadline to elapse.

Besides the improved ergonomics of using WaitUntil() when all one has on
hand is a deadline, WaitUntil() also yields more precise timeouts when
dealing with spurious wakeups while waiting in a loop; the WaitFor methods
in CountDownLatch and ThreadPool stand to benefit from this.

I also cleaned up some dead portability code in condition_variable.cc.

Change-Id: Ie2d3d0d9d84c3b1a76f3efc8ae706ddcaa455630
Reviewed-on: http://gerrit.cloudera.org:8080/9572
Tested-by: Kudu Jenkins
Reviewed-by: Todd Lipcon <todd@apache.org>

src/kudu/util/condition_variable.cc
@@ -26,39 +26,23 @@ ConditionVariable::ConditionVariable(Mutex* user_lock)
 #endif
 {
   int rv = 0;
-  // http://crbug.com/293736
-  // NaCl doesn't support monotonic clock based absolute deadlines.
-  // On older Android platform versions, it's supported through the
-  // non-standard pthread_cond_timedwait_monotonic_np. Newer platform
-  // versions have pthread_condattr_setclock.
-  // Mac can use relative time deadlines.
-#if !defined(__APPLE__) && !defined(OS_NACL) && \
-      !(defined(OS_ANDROID) && defined(HAVE_PTHREAD_COND_TIMEDWAIT_MONOTONIC))
+#if defined(__APPLE__)
+  rv = pthread_cond_init(&condition_, nullptr);
+#else
+  // On Linux we can't use relative times like on macOS; reconfiguring the
+  // condition variable to use the monotonic clock means we can use support
+  // WaitFor with our MonoTime implementation.
   pthread_condattr_t attrs;
   rv = pthread_condattr_init(&attrs);
   DCHECK_EQ(0, rv);
   pthread_condattr_setclock(&attrs, CLOCK_MONOTONIC);
   rv = pthread_cond_init(&condition_, &attrs);
   pthread_condattr_destroy(&attrs);
-#else
-  rv = pthread_cond_init(&condition_, nullptr);
 #endif
   DCHECK_EQ(0, rv);
 }
 
 ConditionVariable::~ConditionVariable() {
-#if defined(OS_MACOSX)
-  // This hack is necessary to avoid a fatal pthreads subsystem bug in the
-  // Darwin kernel. https://codereview.chromium.org/1323293005/
-  {
-    Mutex lock;
-    MutexLock l(lock);
-    struct timespec ts;
-    ts.tv_sec = 0;
-    ts.tv_nsec = 1;
-    pthread_cond_timedwait_relative_np(&condition_, lock.native_handle, &ts);
-  }
-#endif
   int rv = pthread_cond_destroy(&condition_);
   DCHECK_EQ(0, rv);
 }
@@ -75,54 +59,55 @@ void ConditionVariable::Wait() const {
 #endif
 }
 
-bool ConditionVariable::TimedWait(const MonoDelta& max_time) const {
+bool ConditionVariable::WaitUntil(const MonoTime& until) const {
   ThreadRestrictions::AssertWaitAllowed();
 
-  // Negative delta means we've already timed out.
-  int64_t nsecs = max_time.ToNanoseconds();
-  if (nsecs < 0) {
+  // Have we already timed out?
+  if (MonoTime::Now() > until) {
     return false;
   }
 
-  struct timespec relative_time;
-  max_time.ToTimeSpec(&relative_time);
+#if !defined(NDEBUG)
+  user_lock_->CheckHeldAndUnmark();
+#endif
+
+  struct timespec absolute_time;
+  until.ToTimeSpec(&absolute_time);
+  int rv = pthread_cond_timedwait(&condition_, user_mutex_, &absolute_time);
+  DCHECK(rv == 0 || rv == ETIMEDOUT)
+    << "unexpected pthread_cond_timedwait return value: " << rv;
+
+#if !defined(NDEBUG)
+  user_lock_->CheckUnheldAndMark();
+#endif
+  return rv == 0;
+}
+
+bool ConditionVariable::WaitFor(const MonoDelta& delta) const {
+  ThreadRestrictions::AssertWaitAllowed();
+
+  // Negative delta means we've already timed out.
+  int64_t nsecs = delta.ToNanoseconds();
+  if (nsecs < 0) {
+    return false;
+  }
 
 #if !defined(NDEBUG)
   user_lock_->CheckHeldAndUnmark();
 #endif
 
 #if defined(__APPLE__)
+  struct timespec relative_time;
+  delta.ToTimeSpec(&relative_time);
   int rv = pthread_cond_timedwait_relative_np(
       &condition_, user_mutex_, &relative_time);
 #else
   // The timeout argument to pthread_cond_timedwait is in absolute time.
   struct timespec absolute_time;
-#if defined(OS_NACL)
-  // See comment in constructor for why this is different in NaCl.
-  struct timeval now;
-  gettimeofday(&now, NULL);
-  absolute_time.tv_sec = now.tv_sec;
-  absolute_time.tv_nsec = now.tv_usec * MonoTime::kNanosecondsPerMicrosecond;
-#else
-  struct timespec now;
-  clock_gettime(CLOCK_MONOTONIC, &now);
-  absolute_time.tv_sec = now.tv_sec;
-  absolute_time.tv_nsec = now.tv_nsec;
-#endif
-
-  absolute_time.tv_sec += relative_time.tv_sec;
-  absolute_time.tv_nsec += relative_time.tv_nsec;
-  absolute_time.tv_sec += absolute_time.tv_nsec / MonoTime::kNanosecondsPerSecond;
-  absolute_time.tv_nsec %= MonoTime::kNanosecondsPerSecond;
-  DCHECK_GE(absolute_time.tv_sec, now.tv_sec);  // Overflow paranoia
-
-#if defined(OS_ANDROID) && defined(HAVE_PTHREAD_COND_TIMEDWAIT_MONOTONIC)
-  int rv = pthread_cond_timedwait_monotonic_np(
-      &condition_, user_mutex_, &absolute_time);
-#else
+  MonoTime deadline = MonoTime::Now() + delta;
+  deadline.ToTimeSpec(&absolute_time);
   int rv = pthread_cond_timedwait(&condition_, user_mutex_, &absolute_time);
-#endif  // OS_ANDROID && HAVE_PTHREAD_COND_TIMEDWAIT_MONOTONIC
-#endif  // __APPLE__
+#endif
 
   DCHECK(rv == 0 || rv == ETIMEDOUT)
     << "unexpected pthread_cond_timedwait return value: " << rv;

src/kudu/master/catalog_manager.cc
@@ -457,7 +457,7 @@ class CatalogManagerBgTasks {
     MutexLock lock(lock_);
     if (closing_) return;
     if (!pending_updates_) {
-      cond_.TimedWait(MonoDelta::FromMilliseconds(msec));
+      cond_.WaitFor(MonoDelta::FromMilliseconds(msec));
     }
     pending_updates_ = false;
   }

src/kudu/server/diagnostics_log.cc
@@ -193,7 +193,7 @@ void DiagnosticsLog::RunThread() {
 
   while (!stop_) {
     MonoTime next_log = wakeups.top().first;
-    wake_.TimedWait(next_log - MonoTime::Now());
+    wake_.WaitUntil(next_log);
 
     string reason;
     WakeupType what;

src/kudu/tserver/heartbeater.cc
@@ -564,14 +564,10 @@ void Heartbeater::Thread::RunThread() {
     // or for the signal to shut down.
     {
       MutexLock l(mutex_);
-      while (true) {
-        MonoDelta remaining = next_heartbeat - MonoTime::Now();
-        if (remaining.ToMilliseconds() <= 0 ||
-            heartbeat_asap_ ||
-            !should_run_) {
-          break;
-        }
-        cond_.TimedWait(remaining);
+      while (next_heartbeat > MonoTime::Now() &&
+          !heartbeat_asap_ &&
+          should_run_) {
+        cond_.WaitUntil(next_heartbeat);
       }
 
       heartbeat_asap_ = false;

src/kudu/tserver/scanners.cc
@@ -119,7 +119,7 @@ void ScannerManager::RunRemovalThread() {
       if (shutdown_) {
         return;
       }
-      shutdown_cv_.TimedWait(MonoDelta::FromMicroseconds(FLAGS_scanner_gc_check_interval_us));
+      shutdown_cv_.WaitFor(MonoDelta::FromMicroseconds(FLAGS_scanner_gc_check_interval_us));
     }
     RemoveExpiredScanners();
   }

src/kudu/util/async_logger.cc
@@ -114,7 +114,7 @@ void AsyncLogger::RunThread() {
   MutexLock l(lock_);
   while (state_ == RUNNING || active_buf_->needs_flush_or_write()) {
     while (!active_buf_->needs_flush_or_write() && state_ == RUNNING) {
-      if (!wake_flusher_cond_.TimedWait(MonoDelta::FromSeconds(FLAGS_logbufsecs))) {
+      if (!wake_flusher_cond_.WaitFor(MonoDelta::FromSeconds(FLAGS_logbufsecs))) {
         // In case of wait timeout, force it to flush regardless whether there is anything enqueued.
         active_buf_->flush = true;
       }

src/kudu/util/blocking_queue.h
@@ -26,6 +26,7 @@
 #include "kudu/gutil/basictypes.h"
 #include "kudu/gutil/gscoped_ptr.h"
 #include "kudu/util/condition_variable.h"
+#include "kudu/util/monotime.h"
 #include "kudu/util/mutex.h"
 #include "kudu/util/status.h"
 
@@ -131,7 +132,7 @@ class BlockingQueue {
       }
       if (!deadline.Initialized()) {
         not_empty_.Wait();
-      } else if (PREDICT_FALSE(!not_empty_.TimedWait(deadline - MonoTime::Now()))) {
+      } else if (PREDICT_FALSE(!not_empty_.WaitUntil(deadline))) {
         return Status::TimedOut("");
       }
     }

src/kudu/util/condition_variable.h
@@ -72,6 +72,7 @@
 namespace kudu {
 
 class MonoDelta;
+class MonoTime;
 class Mutex;
 
 class ConditionVariable {
@@ -85,10 +86,15 @@ class ConditionVariable {
   // sleep, and the reacquires it when it is signaled.
   void Wait() const;
 
+  // Like Wait(), but only waits up to a certain point in time.
+  //
+  // Returns true if we were Signal()'ed, or false if we reached 'until'.
+  bool WaitUntil(const MonoTime& until) const;
+
   // Like Wait(), but only waits up to a limited amount of time.
   //
-  // Returns true if we were Signal()'ed, or false if 'max_time' elapsed.
-  bool TimedWait(const MonoDelta& max_time) const;
+  // Returns true if we were Signal()'ed, or false if 'delta' elapsed.
+  bool WaitFor(const MonoDelta& delta) const;
 
   // Broadcast() revives all waiting threads.
   void Broadcast();

commit a469506115fbc13c1fe835ae9c1a75a5544bf64e

condition_variable: fix fallout from commit 038c3eb for macOS
Normally pthread_cond_timedwait expects wallclock time in the 'abstime'
parameter, and on macOS, it's not possible to change that to monotonic time.
As a result, commit 038c3eb caused all sorts of timing issues on macOS for
any code doing deadline-based waiting on condition variables. As an example,
ThreadJoiner configured to wait in 1000ms increments would instead use 10us
increments. This patch fixes this by switching back to relative time
waiting, as was done in ConditionVariable::TimedWait prior to that commit.

I tried doing the same on Linux so as to chain WaitUntil into WaitFor, but
that leads to overflows when the deadline is Max() due to a subtraction and
then an addition of potentially different values of Now().

Change-Id: If33329376af7a350e66c1d26c7bb57d7e5416236
Reviewed-on: http://gerrit.cloudera.org:8080/9695
Reviewed-by: Dan Burkert <danburkert@apache.org>
Tested-by: Adar Dembo <adar@cloudera.com>
Reviewed-by: Todd Lipcon <todd@apache.org>

src/kudu/util/condition_variable.cc
@@ -63,7 +63,8 @@ bool ConditionVariable::WaitUntil(const MonoTime& until) const {
   ThreadRestrictions::AssertWaitAllowed();
 
   // Have we already timed out?
-  if (MonoTime::Now() > until) {
+  MonoTime now = MonoTime::Now();
+  if (now > until) {
     return false;
   }
 
@@ -71,9 +72,20 @@ bool ConditionVariable::WaitUntil(const MonoTime& until) const {
   user_lock_->CheckHeldAndUnmark();
 #endif
 
+#if defined(__APPLE__)
+  // macOS does not provide a way to configure pthread_cond_timedwait() to use
+  // monotonic clocks, so we must convert the deadline into a delta and perform
+  // a relative wait.
+  MonoDelta delta = until - now;
+  struct timespec relative_time;
+  delta.ToTimeSpec(&relative_time);
+  int rv = pthread_cond_timedwait_relative_np(
+      &condition_, user_mutex_, &relative_time);
+#else
   struct timespec absolute_time;
   until.ToTimeSpec(&absolute_time);
   int rv = pthread_cond_timedwait(&condition_, user_mutex_, &absolute_time);
+#endif
   DCHECK(rv == 0 || rv == ETIMEDOUT)
     << "unexpected pthread_cond_timedwait return value: " << rv;
 