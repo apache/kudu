b531da26acf23d6f2969c66d458d598d56d37bd5
Mike Percy <mpercy@apache.org>
2018-03-23 16:10:57 -0700

    consensus: Revert terminal state enforcement of FAILED_UNRECOVERABLE
    In patch 7b88a42332540ace18ea033439ceac0e7cb68411 FAILED_UNRECOVERABLE
    was noted as a terminal state. Todd pointed out that there are certain
    corner cases where that may not be the case, such as a replica that
    started up on a temporarily-failed or read-only disk, and then was
    restarted. In such a case, if the leader was previously unable to evict
    that node because of a lack of a majority, the affected replica may
    revert to a HEALTHY state.
    
    Change-Id: I2f858af8c578340f7a32b95702b5da7bbe81a675
    Reviewed-on: http://gerrit.cloudera.org:8080/9794
    Reviewed-by: Alexey Serbin <aserbin@cloudera.com>
    Tested-by: Kudu Jenkins

--- src/kudu/consensus/consensus_queue.cc
+++ src/kudu/consensus/consensus_queue.cc
@@ -531,14 +531,6 @@ void PeerMessageQueue::UpdatePeerHealthUnlocked(TrackedPeer* peer) {
   }
 
   bool changed = overall_health_status != peer->last_overall_health_status;
-  if (changed) {
-    // FAILED_UNRECOVERABLE is a terminal state; it's illegal to transition out of it.
-    DCHECK_NE(HealthReportPB::FAILED_UNRECOVERABLE, peer->last_overall_health_status)
-        << "Transitioning out of FAILED_UNRECOVERABLE health status should not be possible. "
-        << "old health status: "
-        << HealthReportPB::HealthStatus_Name(peer->last_overall_health_status) << ", "
-        << "new health status: " << HealthReportPB::HealthStatus_Name(overall_health_status);
-  }
   peer->last_overall_health_status = overall_health_status;
 
   if (FLAGS_raft_prepare_replacement_before_eviction) {
