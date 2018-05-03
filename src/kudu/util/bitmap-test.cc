0061f32d157dacd0cc1e6af341d6d5bf1e82378c
Adar Dembo <adar@cloudera.com>
2018-05-03 15:40:38 -0700

    bitmap-test: fix dangling-else warnings
    This squelches the following two warnings:
    
      [396/750] Building CXX object src/kudu/util/CMakeFiles/bitmap-test.dir/bitmap-test.cc.o
      ../../src/kudu/util/bitmap-test.cc: In member function ‘virtual void kudu::TestBitMap_TestFindBit_Test::TestBody()’:
      ../../src/kudu/util/bitmap-test.cc:187:10: warning: suggest explicit braces to avoid ambiguous ‘else’ [-Wdangling-else]
             if (expect_set_found) ASSERT_EQ(expected_set_idx, idx);
                ^
      ../../src/kudu/util/bitmap-test.cc:194:10: warning: suggest explicit braces to avoid ambiguous ‘else’ [-Wdangling-else]
             if (expect_zero_found) ASSERT_EQ(expected_zero_idx, idx);
                ^
    
    Change-Id: I0cf567b69c39958acb0c6fc7aa110d38f7b718ef
    Reviewed-on: http://gerrit.cloudera.org:8080/10305
    Reviewed-by: Alexey Serbin <aserbin@cloudera.com>
    Tested-by: Kudu Jenkins
    Reviewed-by: Todd Lipcon <todd@apache.org>

--- src/kudu/util/bitmap-test.cc
+++ src/kudu/util/bitmap-test.cc
@@ -184,14 +184,18 @@ TEST(TestBitMap, TestFindBit) {
       size_t expected_set_idx = (offset + !(offset & 3));
       bool expect_set_found = (expected_set_idx < num_bits);
       ASSERT_EQ(expect_set_found, res);
-      if (expect_set_found) ASSERT_EQ(expected_set_idx, idx);
+      if (expect_set_found) {
+        ASSERT_EQ(expected_set_idx, idx);
+      }
 
       // Find a zero bit
       res = BitmapFindFirstZero(bm, offset, num_bits, &idx);
       size_t expected_zero_idx = offset + ((offset & 3) ? (4 - (offset & 3)) : 0);
       bool expect_zero_found = (expected_zero_idx < num_bits);
       ASSERT_EQ(expect_zero_found, res);
-      if (expect_zero_found) ASSERT_EQ(expected_zero_idx, idx);
+      if (expect_zero_found) {
+        ASSERT_EQ(expected_zero_idx, idx);
+      }
     }
   }
 }
