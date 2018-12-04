9b80a98bdab5870421614ae3b2e18a766112ad3d
xiang88 <38744875+wqxiang88@users.noreply.github.com>
2018-12-04 16:50:22 +0800

    Update heartbeater.h

--- src/kudu/tserver/heartbeater.h
+++ src/kudu/tserver/heartbeater.h
@@ -14,7 +14,6 @@
 // KIND, either express or implied.  See the License for the
 // specific language governing permissions and limitations
 // under the License.
-
 #ifndef KUDU_TSERVER_HEARTBEATER_H
 #define KUDU_TSERVER_HEARTBEATER_H
 
