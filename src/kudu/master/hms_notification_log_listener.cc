9660d43069b0cda587da81232b363fda3858194e
Andrew Wong <awong@apache.org>
2019-04-26 15:47:26 -0700

    hms: mark latency flag as hidden
    Change-Id: Ib11848a262a1d9329a3981c7f7eafb99134fae75
    Reviewed-on: http://gerrit.cloudera.org:8080/13135
    Tested-by: Andrew Wong <awong@cloudera.com>
    Reviewed-by: Adar Dembo <adar@cloudera.com>
    Reviewed-by: Alexey Serbin <aserbin@cloudera.com>

--- src/kudu/master/hms_notification_log_listener.cc
+++ src/kudu/master/hms_notification_log_listener.cc
@@ -60,6 +60,7 @@ TAG_FLAG(hive_metastore_notification_log_batch_size, runtime);
 DEFINE_uint32(hive_metastore_notification_log_poll_inject_latency_ms, 0,
               "Inject latency into the inner polling loop of the Hive Metastore"
               "notification log listener. Only takes effect during unit tests.");
+TAG_FLAG(hive_metastore_notification_log_poll_inject_latency_ms, hidden);
 TAG_FLAG(hive_metastore_notification_log_poll_inject_latency_ms, unsafe);
 TAG_FLAG(hive_metastore_notification_log_poll_inject_latency_ms, runtime);
 
