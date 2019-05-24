apache/kudu

commit 0114ce1d0ebbec41eef971be341432a0f8f28a76

[java] Upgrade to log4j 2
This patch upgrades from log4j 1.2.17 to 2.11.2 and
includes any neccessary changes to support the
upgrade.

Log4j 2.x has been out for 5 years now and log4j1.x has
been marked as EOL as of August 2015.

The changes required to support Log4j 2.x are test only
because we use Slf4j to abstract away our concrete
logger implementation and allow users to use any
logging implementaton they like.

Change-Id: Ic4e14a3a25c36d5f47d07f06c1c0e68dad42bc66
Reviewed-on: http://gerrit.cloudera.org:8080/13304
Tested-by: Kudu Jenkins
Reviewed-by: Adar Dembo <adar@cloudera.com>

java/kudu-test-utils/src/main/java/org/apache/kudu/test/CapturingLogAppender.java
@@ -20,11 +20,11 @@ import java.io.Closeable;
 import java.io.IOException;
 
 import com.google.common.base.Throwables;
-import org.apache.log4j.AppenderSkeleton;
-import org.apache.log4j.Layout;
-import org.apache.log4j.Logger;
-import org.apache.log4j.SimpleLayout;
-import org.apache.log4j.spi.LoggingEvent;
+import org.apache.logging.log4j.core.LogEvent;
+import org.apache.logging.log4j.core.LoggerContext;
+import org.apache.logging.log4j.core.appender.AbstractAppender;
+import org.apache.logging.log4j.core.config.Property;
+import org.apache.logging.log4j.core.layout.PatternLayout;
 import org.apache.yetus.audience.InterfaceAudience;
 import org.apache.yetus.audience.InterfaceStability;
 
@@ -35,26 +35,25 @@ import org.apache.yetus.audience.InterfaceStability;
  */
 @InterfaceAudience.Private
 @InterfaceStability.Unstable
-public class CapturingLogAppender extends AppenderSkeleton {
-  private static final Layout LAYOUT = new SimpleLayout();
+public class CapturingLogAppender extends AbstractAppender {
+  // This is the standard layout used in Kudu tests.
+  private static final PatternLayout LAYOUT = PatternLayout.newBuilder()
+      .withPattern("%d{HH:mm:ss.SSS} [%p - %t] (%F:%L) %m%n")
+      .build();
 
   private StringBuilder appended = new StringBuilder();
 
-  @Override
-  public void close() {
+  public CapturingLogAppender() {
+    super("CapturingLogAppender", /* filter */ null, LAYOUT,
+        /* ignoreExceptions */ true, Property.EMPTY_ARRAY);
   }
 
   @Override
-  public boolean requiresLayout() {
-    return false;
-  }
-
-  @Override
-  protected void append(LoggingEvent event) {
-    appended.append(LAYOUT.format(event));
-    if (event.getThrowableInformation() != null) {
-      appended.append(Throwables.getStackTraceAsString(
-          event.getThrowableInformation().getThrowable())).append("\n");
+  public void append(LogEvent event) {
+    appended.append(getLayout().toSerializable(event));
+    if (event.getThrown() != null) {
+      appended.append(Throwables.getStackTraceAsString(event.getThrown()));
+      appended.append("\n");
     }
   }
 
@@ -75,11 +74,11 @@ public class CapturingLogAppender extends AppenderSkeleton {
    * </code>
    */
   public Closeable attach() {
-    Logger.getRootLogger().addAppender(this);
+    LoggerContext.getContext(false).getRootLogger().addAppender(this);
     return new Closeable() {
       @Override
       public void close() throws IOException {
-        Logger.getRootLogger().removeAppender(CapturingLogAppender.this);
+        LoggerContext.getContext(false).getRootLogger().removeAppender(CapturingLogAppender.this);
       }
     };
   }

java/kudu-test-utils/src/main/java/org/apache/kudu/test/CapturingToFileLogAppender.java
@@ -27,12 +27,11 @@ import java.io.Writer;
 import java.util.zip.GZIPOutputStream;
 
 import com.google.common.base.Throwables;
-import org.apache.commons.io.IOUtils;
-import org.apache.log4j.AppenderSkeleton;
-import org.apache.log4j.Layout;
-import org.apache.log4j.Logger;
-import org.apache.log4j.PatternLayout;
-import org.apache.log4j.spi.LoggingEvent;
+import org.apache.logging.log4j.core.LogEvent;
+import org.apache.logging.log4j.core.LoggerContext;
+import org.apache.logging.log4j.core.appender.AbstractAppender;
+import org.apache.logging.log4j.core.config.Property;
+import org.apache.logging.log4j.core.layout.PatternLayout;
 import org.apache.yetus.audience.InterfaceAudience;
 import org.apache.yetus.audience.InterfaceStability;
 
@@ -52,10 +51,11 @@ import static java.nio.charset.StandardCharsets.UTF_8;
  */
 @InterfaceAudience.Private
 @InterfaceStability.Unstable
-public class CapturingToFileLogAppender extends AppenderSkeleton implements AutoCloseable {
+public class CapturingToFileLogAppender extends AbstractAppender implements AutoCloseable {
   // This is the standard layout used in Kudu tests.
-  private static final Layout LAYOUT = new PatternLayout(
-      "%d{HH:mm:ss.SSS} [%p - %t] (%F:%L) %m%n");
+  private static final PatternLayout LAYOUT = PatternLayout.newBuilder()
+      .withPattern("%d{HH:mm:ss.SSS} [%p - %t] (%F:%L) %m%n")
+      .build();
 
   private File outputFile;
   private Writer outputFileWriter;
@@ -69,33 +69,41 @@ public class CapturingToFileLogAppender extends AppenderSkeleton implements Auto
    * @param useGzip whether to gzip-compress messages when appended
    */
   public CapturingToFileLogAppender(boolean useGzip) throws IOException {
+    super("CapturingToFileLogAppender", /* filter */ null, LAYOUT,
+        /* ignoreExceptions */ true, Property.EMPTY_ARRAY);
     outputFile = File.createTempFile("captured_output", ".txt.gz");
-    try {
-      OutputStream os = new FileOutputStream(outputFile.getPath());
-      try {
-        if (useGzip) {
-          os = new GZIPOutputStream(os);
-        }
-
-        // As per the recommendation in OutputStreamWriter's Javadoc, we wrap in a
-        // BufferedWriter to buffer up character conversions.
-        outputFileWriter = new BufferedWriter(new OutputStreamWriter(os, UTF_8));
-      } catch (Throwable t) {
-        IOUtils.closeQuietly(os);
-        throw t;
-      }
+    try (OutputStream os = createOutputStream(useGzip)) {
+      // As per the recommendation in OutputStreamWriter's Javadoc, we wrap in a
+      // BufferedWriter to buffer up character conversions.
+      outputFileWriter = new BufferedWriter(new OutputStreamWriter(os, UTF_8));
     } catch (Throwable t) {
       outputFile.delete();
       throw t;
     }
   }
 
+  private OutputStream createOutputStream(boolean useGzip) throws IOException {
+    OutputStream os = new FileOutputStream(outputFile.getPath());
+    if (useGzip) {
+      try {
+        os = new GZIPOutputStream(os);
+      } catch (IOException ex) {
+        os.close();
+        throw ex;
+      }
+    }
+    return os;
+  }
+
   @Override
   public void close() {
     // Just do the cleanup; we don't care about exceptions/logging.
-
     if (outputFileWriter != null) {
-      IOUtils.closeQuietly(outputFileWriter);
+      try {
+        outputFileWriter.close();
+      } catch (final IOException ioe) {
+        // ignored
+      }
       outputFileWriter = null;
     }
     if (outputFile != null) {
@@ -105,18 +113,12 @@ public class CapturingToFileLogAppender extends AppenderSkeleton implements Auto
   }
 
   @Override
-  public boolean requiresLayout() {
-    return false;
-  }
-
-  @Override
-  protected void append(LoggingEvent event) {
+  public void append(LogEvent event) {
     assert outputFileWriter != null;
     try {
-      outputFileWriter.write(LAYOUT.format(event));
-      if (event.getThrowableInformation() != null) {
-        outputFileWriter.write(Throwables.getStackTraceAsString(
-            event.getThrowableInformation().getThrowable()));
+      outputFileWriter.write(LAYOUT.toSerializable(event));
+      if (event.getThrown() != null) {
+        outputFileWriter.write(Throwables.getStackTraceAsString(event.getThrown()));
         outputFileWriter.write("\n");
       }
     } catch (IOException e) {
@@ -166,11 +168,12 @@ public class CapturingToFileLogAppender extends AppenderSkeleton implements Auto
    * </code>
    */
   public Closeable attach() {
-    Logger.getRootLogger().addAppender(this);
+    LoggerContext.getContext(false).getRootLogger().addAppender(this);
     return new Closeable() {
       @Override
       public void close() throws IOException {
-        Logger.getRootLogger().removeAppender(CapturingToFileLogAppender.this);
+        LoggerContext.getContext(false).getRootLogger()
+            .removeAppender(CapturingToFileLogAppender.this);
       }
     };
   }

java/gradle/dependencies.gradle
@@ -44,7 +44,7 @@ versions += [
     jetty          : "9.4.15.v20190215",
     jsr305         : "3.0.2",
     junit          : "4.12",
-    log4j          : "1.2.17",
+    log4j          : "2.11.2",
     mockito        : "2.25.1",
     murmur         : "1.0.0",
     netty          : "3.10.6.Final",
@@ -103,7 +103,8 @@ libs += [
     jettyServlet         : "org.eclipse.jetty:jetty-servlet:$versions.jetty",
     jsr305               : "com.google.code.findbugs:jsr305:$versions.jsr305",
     junit                : "junit:junit:$versions.junit",
-    log4j                : "log4j:log4j:$versions.log4j",
+    log4j                : "org.apache.logging.log4j:log4j-1.2-api:$versions.log4j",
+    log4jSlf4jImpl       : "org.apache.logging.log4j:log4j-slf4j-impl:$versions.log4j",
     mockitoCore          : "org.mockito:mockito-core:$versions.mockito",
     murmur               : "com.sangupta:murmur:$versions.murmur",
     netty                : "io.netty:netty:$versions.netty",
@@ -117,7 +118,6 @@ libs += [
     scalatest            : "org.scalatest:scalatest_$versions.scalaBase:$versions.scalatest",
     scopt                : "com.github.scopt:scopt_$versions.scalaBase:$versions.scopt",
     slf4jApi             : "org.slf4j:slf4j-api:$versions.slf4j",
-    slf4jLog4j12         : "org.slf4j:slf4j-log4j12:$versions.slf4j",
     sparkAvro            : "org.apache.spark:spark-avro_$versions.scalaBase:$versions.spark",
     sparkCore            : "org.apache.spark:spark-core_$versions.scalaBase:$versions.spark",
     sparkSql             : "org.apache.spark:spark-sql_$versions.scalaBase:$versions.spark",

java/kudu-backup-tools/build.gradle
@@ -44,8 +44,8 @@ dependencies {
   testCompile project(path: ":kudu-test-utils", configuration: "shadow")
   testCompile libs.junit
   testCompile libs.log4j
+  testCompile libs.log4jSlf4jImpl
   testCompile libs.scalatest
-  testCompile libs.slf4jLog4j12
 }
 
 // Add protobuf files to the proto source set.

java/kudu-backup-tools/src/test/resources/log4j2.properties
@@ -15,9 +15,18 @@
 # specific language governing permissions and limitations
 # under the License.
 
-log4j.rootLogger = INFO, out
-log4j.appender.out = org.apache.log4j.ConsoleAppender
-log4j.appender.out.layout = org.apache.log4j.PatternLayout
-log4j.appender.out.layout.ConversionPattern = %d{HH:mm:ss.SSS} [%p - %t] (%F:%L) %m%n
+status = error
+name = PropertiesConfig
+appenders = console
 
-log4j.logger.org.apache.kudu = DEBUG
+appender.console.type = Console
+appender.console.name = STDOUT
+appender.console.layout.type = PatternLayout
+appender.console.layout.pattern = %d{HH:mm:ss.SSS} [%p - %t] (%F:%L) %m%n
+
+rootLogger.level = debug
+rootLogger.appenderRefs = stdout
+rootLogger.appenderRef.stdout.ref = STDOUT
+
+logger.kudu.name = org.apache.kudu
+logger.kudu.level = debug

java/kudu-backup/build.gradle
@@ -43,8 +43,8 @@ dependencies {
   testCompile project(path: ":kudu-spark", configuration: "test")
   testCompile libs.junit
   testCompile libs.log4j
+  testCompile libs.log4jSlf4jImpl
   testCompile libs.scalatest
-  testCompile libs.slf4jLog4j12
 }
 
 // Adjust the artifact name to match the maven build.

java/kudu-backup/src/test/resources/log4j2.properties
@@ -15,9 +15,18 @@
 # specific language governing permissions and limitations
 # under the License.
 
-log4j.rootLogger = INFO, out
-log4j.appender.out = org.apache.log4j.ConsoleAppender
-log4j.appender.out.layout = org.apache.log4j.PatternLayout
-log4j.appender.out.layout.ConversionPattern = %d{HH:mm:ss.SSS} [%p - %t] (%F:%L) %m%n
+status = error
+name = PropertiesConfig
+appenders = console
 
-log4j.logger.org.apache.kudu = DEBUG
+appender.console.type = Console
+appender.console.name = STDOUT
+appender.console.layout.type = PatternLayout
+appender.console.layout.pattern = %d{HH:mm:ss.SSS} [%p - %t] (%F:%L) %m%n
+
+rootLogger.level = debug
+rootLogger.appenderRefs = stdout
+rootLogger.appenderRef.stdout.ref = STDOUT
+
+logger.kudu.name = org.apache.kudu
+logger.kudu.level = debug

java/kudu-client-tools/build.gradle
@@ -33,5 +33,5 @@ dependencies {
   testCompile libs.commonsIo
   testCompile libs.junit
   testCompile libs.log4j
-  testCompile libs.slf4jLog4j12
+  testCompile libs.log4jSlf4jImpl
 }

commit 37e53bf1d99e2ff44f0288ed65321f9004736813

java: fix CapturingToFileLogAppender
It was broken in commit 0114ce1d0: the try-with-resources in the constructor
closed the OutputStream out from under the BufferedWriter.

I added a regression test for this.

Change-Id: I7fa14df5cd4025236ec398acf4973520d9dea082
Reviewed-on: http://gerrit.cloudera.org:8080/13418
Reviewed-by: Hao Hao <hao.hao@cloudera.com>
Tested-by: Adar Dembo <adar@cloudera.com>

java/kudu-test-utils/src/main/java/org/apache/kudu/test/CapturingLogAppender.java
@@ -18,6 +18,7 @@ package org.apache.kudu.test;
 
 import java.io.Closeable;
 import java.io.IOException;
+import java.util.Random;
 
 import com.google.common.base.Throwables;
 import org.apache.logging.log4j.core.LogEvent;
@@ -44,8 +45,15 @@ public class CapturingLogAppender extends AbstractAppender {
   private StringBuilder appended = new StringBuilder();
 
   public CapturingLogAppender() {
-    super("CapturingLogAppender", /* filter */ null, LAYOUT,
-        /* ignoreExceptions */ true, Property.EMPTY_ARRAY);
+    // Appender name must be unique so that attaching/detaching works correctly
+    // when multiple capturing appenders are used recursively.
+    super(String.format("CapturingToFileLogAppender-%d", new Random().nextInt()),
+          /* filter */ null, LAYOUT, /* ignoreExceptions */ true, Property.EMPTY_ARRAY);
+
+    // If we don't call start(), we get an ugly log error:
+    //
+    // ERROR Attempted to append to non-started appender CapturingToFileLogAppender
+    start();
   }
 
   @Override

java/kudu-test-utils/src/main/java/org/apache/kudu/test/CapturingToFileLogAppender.java
@@ -24,6 +24,7 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
+import java.util.Random;
 import java.util.zip.GZIPOutputStream;
 
 import com.google.common.base.Throwables;
@@ -69,17 +70,33 @@ public class CapturingToFileLogAppender extends AbstractAppender implements Auto
    * @param useGzip whether to gzip-compress messages when appended
    */
   public CapturingToFileLogAppender(boolean useGzip) throws IOException {
-    super("CapturingToFileLogAppender", /* filter */ null, LAYOUT,
-        /* ignoreExceptions */ true, Property.EMPTY_ARRAY);
+    // Appender name must be unique so that attaching/detaching works correctly
+    // when multiple capturing appenders are used recursively.
+    super(String.format("CapturingToFileLogAppender-%d", new Random().nextInt()),
+          /* filter */ null, LAYOUT, /* ignoreExceptions */ true, Property.EMPTY_ARRAY);
+
     outputFile = File.createTempFile("captured_output", ".txt.gz");
-    try (OutputStream os = createOutputStream(useGzip)) {
-      // As per the recommendation in OutputStreamWriter's Javadoc, we wrap in a
-      // BufferedWriter to buffer up character conversions.
-      outputFileWriter = new BufferedWriter(new OutputStreamWriter(os, UTF_8));
+    try {
+      OutputStream os = createOutputStream(useGzip);
+      try {
+        // As per the recommendation in OutputStreamWriter's Javadoc, we wrap in
+        // a BufferedWriter to buffer up character conversions.
+        outputFileWriter = new BufferedWriter(new OutputStreamWriter(os, UTF_8));
+      } catch (Throwable t) {
+        os.close();
+      }
     } catch (Throwable t) {
       outputFile.delete();
       throw t;
     }
+
+    // If we don't call start(), we get an ugly log error:
+    //
+    // ERROR Attempted to append to non-started appender CapturingToFileLogAppender
+    //
+    // It doesn't throw anything so there's no reason to include it in the above
+    // try/catch monstrosity.
+    start();
   }
 
   private OutputStream createOutputStream(boolean useGzip) throws IOException {

java/kudu-test-utils/src/test/java/org/apache/kudu/test/TestCapturingToFileLogAppender.java
@@ -1,0 +1,141 @@
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. See accompanying LICENSE file.
+ */
+package org.apache.kudu.test;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import com.google.common.base.Joiner;
+
+import org.apache.kudu.test.junit.RetryRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.zip.GZIPInputStream;
+
+public class TestCapturingToFileLogAppender {
+  private static final Logger LOG =
+      LoggerFactory.getLogger(TestCapturingToFileLogAppender.class);
+
+  private static final String MAGIC_STRING = "hello world!";
+
+  @Rule
+  public RetryRule retryRule = new RetryRule();
+
+  private String readAllFromBufferedReader(BufferedReader br) throws IOException {
+      List<String> output = new ArrayList<>();
+      for (String line = br.readLine(); line != null; line = br.readLine()) {
+        output.add(line);
+      }
+      return Joiner.on("\n").join(output);
+  }
+
+  private String readAllFromFile(File fileName) throws IOException {
+    try (InputStream fis = new FileInputStream(fileName);
+         Reader isr = new InputStreamReader(fis, UTF_8);
+         BufferedReader br = new BufferedReader(isr)) {
+      return readAllFromBufferedReader(br);
+    }
+  }
+
+  private String readAllFromGzippedFile(File fileName) throws IOException {
+    try (InputStream fis = new FileInputStream(fileName);
+         InputStream gzis = new GZIPInputStream(fis);
+         Reader isr = new InputStreamReader(gzis, UTF_8);
+         BufferedReader br = new BufferedReader(isr)) {
+      return readAllFromBufferedReader(br);
+    }
+  }
+
+  @Test
+  public void testLog() throws IOException {
+    File outputFile;
+    try (CapturingToFileLogAppender capturer =
+         new CapturingToFileLogAppender(/*useGzip=*/ false)) {
+      outputFile = capturer.getOutputFile();
+      assertTrue(outputFile.exists());
+
+      // Log a magic string and flush the output file.
+      try (Closeable c = capturer.attach()) {
+        LOG.info(MAGIC_STRING);
+      }
+      capturer.finish();
+
+      // Read the magic string out of the output file.
+      String captured = readAllFromFile(outputFile);
+      assertNotNull(captured);
+      assertTrue(captured.contains(MAGIC_STRING));
+    }
+    assertFalse(outputFile.exists());
+  }
+
+  @Test
+  public void testLogGzipped() throws IOException {
+    File outputFile;
+    try (CapturingToFileLogAppender capturer =
+         new CapturingToFileLogAppender(/*useGzip=*/ true)) {
+      outputFile = capturer.getOutputFile();
+      assertTrue(outputFile.exists());
+
+      // Log a magic string and flush the output file.
+      try (Closeable c = capturer.attach()) {
+        LOG.info(MAGIC_STRING);
+      }
+      capturer.finish();
+
+      // Read the magic string out of the output file.
+      String captured = readAllFromGzippedFile(outputFile);
+      assertNotNull(captured);
+      assertTrue(captured.contains(MAGIC_STRING));
+    }
+    assertFalse(outputFile.exists());
+  }
+
+  @Test
+  public void testLogException() throws IOException {
+    File outputFile;
+    try (CapturingToFileLogAppender capturer =
+         new CapturingToFileLogAppender(/*useGzip=*/ false)) {
+      outputFile = capturer.getOutputFile();
+      assertTrue(outputFile.exists());
+
+      // Log a magic string and flush the output file.
+      try (Closeable c = capturer.attach()) {
+        LOG.error("Saw exception", new Exception(MAGIC_STRING));
+      }
+      capturer.finish();
+
+      // Read the magic string out of the output file.
+      String captured = readAllFromFile(outputFile);
+      assertNotNull(captured);
+      assertTrue(captured.contains("java.lang.Exception: " + MAGIC_STRING));
+    }
+    assertFalse(outputFile.exists());    
+  }
+}