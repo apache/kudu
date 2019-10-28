apache/kudu

commit 4343fb746766b166f5532d36afbe903fdf092b95

KUDU-1938 Add C++ client support for VARCHAR pt 2
Change-Id: Ifaf609565b0a0a87d6e645cd3ac14c0965af5ba8
Reviewed-on: http://gerrit.cloudera.org:8080/13869
Reviewed-by: Grant Henke <granthenke@apache.org>
Reviewed-by: Adar Dembo <adar@cloudera.com>
Tested-by: Kudu Jenkins

src/kudu/client/schema.h
@@ -60,6 +60,8 @@ class MetaCacheEntry;
 class WriteRpc;
 } // namespace internal
 
+class KuduColumnSchema;
+class KuduColumnSpec;
 class KuduSchema;
 class KuduValue;
 
@@ -82,6 +84,12 @@ class KUDU_EXPORT KuduColumnTypeAttributes {
   ///   The scale of a decimal column.
   KuduColumnTypeAttributes(int8_t precision, int8_t scale);
 
+  /// Create a KuduColumnTypeAttributes object
+  ///
+  /// @param [in] length
+  ///   The maximum length of a VARCHAR column in characters.
+  explicit KuduColumnTypeAttributes(uint16_t length);
+
   ~KuduColumnTypeAttributes();
 
   /// @name Assign/copy KuduColumnTypeAttributes.
@@ -100,7 +108,16 @@ class KUDU_EXPORT KuduColumnTypeAttributes {
   /// @return Scale for the column type.
   int8_t scale() const;
 
+  /// @return Length for the column type.
+  uint16_t length() const;
+
  private:
+  friend KuduColumnSchema;
+  friend KuduColumnSpec;
+  friend KuduSchema;
+
+  KuduColumnTypeAttributes(int8_t precision, int8_t scale, uint16_t length);
+
   class KUDU_NO_EXPORT Data;
   // Owned.
   Data* data_;
@@ -189,6 +206,7 @@ class KUDU_EXPORT KuduColumnSchema {
     BINARY = 8,
     UNIXTIME_MICROS = 9,
     DECIMAL = 10,
+    VARCHAR = 11,
     TIMESTAMP = UNIXTIME_MICROS //!< deprecated, use UNIXTIME_MICROS
   };
 
@@ -380,6 +398,24 @@ class KUDU_EXPORT KuduColumnSpec {
   ///   Desired scale to set.
   /// @return Pointer to the modified object.
   KuduColumnSpec* Scale(int8_t scale);
+  ///@}
+
+  /// @name Operation only relevant for VARCHAR columns.
+  ///
+  ///@{
+  /// Set the length for a column.
+  ///
+  /// Clients can specify a length for VARCHAR columns.
+  /// Length represents the maximum length of a VARCHAR column in
+  /// characters.
+  ///
+  /// The length must be greater than 0 and less than 65536.
+  /// If no length is provided a default length of 65535 is used.
+  ///
+  /// @param [in] length
+  ///   Desired length to set.
+  /// @return Pointer to the modified object.
+  KuduColumnSpec* Length(uint16_t length);
   ///@}
 
   /// @name Operations only relevant for Create Table

src/kudu/client/predicate-test.cc
@@ -19,6 +19,7 @@
 #include <cstdint>
 #include <limits>
 #include <memory>
+#include <ostream>
 #include <string>
 #include <vector>
 
@@ -26,10 +27,10 @@
 #include <gtest/gtest.h>
 
 #include "kudu/client/client.h"
-#include "kudu/client/shared_ptr.h"
 #include "kudu/client/scan_batch.h"
 #include "kudu/client/scan_predicate.h"
 #include "kudu/client/schema.h"
+#include "kudu/client/shared_ptr.h"
 #include "kudu/client/value.h"
 #include "kudu/client/write_op.h"
 #include "kudu/common/partial_row.h"
@@ -74,7 +75,19 @@ class PredicateTest : public KuduTest {
     {
       KuduSchemaBuilder builder;
       builder.AddColumn("key")->NotNull()->Type(KuduColumnSchema::INT64)->PrimaryKey();
-      builder.AddColumn("value")->Type(value_type);
+      switch (value_type) {
+        case KuduColumnSchema::VARCHAR:
+          builder.AddColumn("value")->Type(value_type)
+            ->Length(40);
+          break;
+        case KuduColumnSchema::DECIMAL:
+          builder.AddColumn("value")->Type(value_type)
+            ->Precision(kMaxDecimal128Precision)->Scale(2);
+          break;
+        default:
+          builder.AddColumn("value")->Type(value_type);
+          break;
+      }
       CHECK_OK(builder.Build(&schema));
     }
     unique_ptr<client::KuduTableCreator> table_creator(client_->NewTableCreator());
@@ -387,9 +400,8 @@ class PredicateTest : public KuduTest {
   }
 
   // Check string predicates against the specified table.
-  void CheckStringPredicates(shared_ptr<KuduTable> table) {
+  void CheckStringPredicates(const shared_ptr<KuduTable>& table, vector<string> values) {
 
-    vector<string> values = CreateStringValues();
     ASSERT_EQ(values.size() + 1, CountRows(table, {}));
 
     // Add some additional values to check against.
@@ -1118,8 +1130,16 @@ TEST_F(PredicateTest, TestDecimalPredicates) {
   ASSERT_EQ(1, CountRows(table, { table->NewIsNullPredicate("value") }));
 }
 
-TEST_F(PredicateTest, TestStringPredicates) {
-  shared_ptr<KuduTable> table = CreateAndOpenTable(KuduColumnSchema::STRING);
+class ParameterizedPredicateTest : public PredicateTest,
+  public ::testing::WithParamInterface<KuduColumnSchema::DataType> {};
+
+INSTANTIATE_TEST_CASE_P(, ParameterizedPredicateTest,
+                        ::testing::Values(KuduColumnSchema::STRING,
+                                          KuduColumnSchema::BINARY,
+                                          KuduColumnSchema::VARCHAR));
+
+TEST_P(ParameterizedPredicateTest, TestIndirectDataPredicates) {
+  shared_ptr<KuduTable> table = CreateAndOpenTable(GetParam());
   shared_ptr<KuduSession> session = CreateSession();
 
   vector<string> values = CreateStringValues();
@@ -1127,7 +1147,20 @@ TEST_F(PredicateTest, TestStringPredicates) {
   for (const string& value : values) {
       unique_ptr<KuduInsert> insert(table->NewInsert());
       ASSERT_OK(insert->mutable_row()->SetInt64("key", i++));
-      ASSERT_OK(insert->mutable_row()->SetStringNoCopy("value", value));
+      switch (GetParam()) {
+        case KuduColumnSchema::STRING:
+          ASSERT_OK(insert->mutable_row()->SetStringNoCopy("value", value));
+          break;
+        case KuduColumnSchema::BINARY:
+          ASSERT_OK(insert->mutable_row()->SetBinaryNoCopy("value", value));
+          break;
+        case KuduColumnSchema::VARCHAR:
+          ASSERT_OK(insert->mutable_row()->SetVarchar("value", value));
+          break;
+        default:
+          LOG(FATAL) << "Invalid type";
+          break;
+      }
       ASSERT_OK(session->Apply(insert.release()));
   }
   unique_ptr<KuduInsert> null_insert(table->NewInsert());
@@ -1136,28 +1169,7 @@ TEST_F(PredicateTest, TestStringPredicates) {
   ASSERT_OK(session->Apply(null_insert.release()));
   ASSERT_OK(session->Flush());
 
-  CheckStringPredicates(table);
-}
-
-TEST_F(PredicateTest, TestBinaryPredicates) {
-  shared_ptr<KuduTable> table = CreateAndOpenTable(KuduColumnSchema::BINARY);
-  shared_ptr<KuduSession> session = CreateSession();
-
-  vector<string> values = CreateStringValues();
-  int i = 0;
-  for (const string& value : values) {
-      unique_ptr<KuduInsert> insert(table->NewInsert());
-      ASSERT_OK(insert->mutable_row()->SetInt64("key", i++));
-      ASSERT_OK(insert->mutable_row()->SetBinaryNoCopy("value", value));
-      ASSERT_OK(session->Apply(insert.release()));
-  }
-  unique_ptr<KuduInsert> null_insert(table->NewInsert());
-  ASSERT_OK(null_insert->mutable_row()->SetInt64("key", i++));
-  ASSERT_OK(null_insert->mutable_row()->SetNull("value"));
-  ASSERT_OK(session->Apply(null_insert.release()));
-  ASSERT_OK(session->Flush());
-
-  CheckStringPredicates(table);
+  CheckStringPredicates(table, values);
 }
 
 } // namespace client

src/kudu/client/scan_batch.cc
@@ -166,6 +166,10 @@ Status KuduScanBatch::RowPtr::GetBinary(const Slice& col_name, Slice* val) const
   return Get<TypeTraits<BINARY> >(col_name, val);
 }
 
+Status KuduScanBatch::RowPtr::GetVarchar(const Slice& col_name, Slice* val) const {
+  return Get<TypeTraits<VARCHAR> >(col_name, val);
+}
+
 Status KuduScanBatch::RowPtr::GetBool(int col_idx, bool* val) const {
   return Get<TypeTraits<BOOL> >(col_idx, val);
 }
@@ -204,6 +208,10 @@ Status KuduScanBatch::RowPtr::GetString(int col_idx, Slice* val) const {
 
 Status KuduScanBatch::RowPtr::GetBinary(int col_idx, Slice* val) const {
   return Get<TypeTraits<BINARY> >(col_idx, val);
+}
+
+Status KuduScanBatch::RowPtr::GetVarchar(int col_idx, Slice* val) const {
+  return Get<TypeTraits<VARCHAR> >(col_idx, val);
 }
 
 template<typename T>
@@ -277,6 +285,9 @@ template
 Status KuduScanBatch::RowPtr::Get<TypeTraits<BINARY> >(const Slice& col_name, Slice* val) const;
 
 template
+Status KuduScanBatch::RowPtr::Get<TypeTraits<VARCHAR> >(const Slice& col_name, Slice* val) const;
+
+template
 Status KuduScanBatch::RowPtr::Get<TypeTraits<BOOL> >(int col_idx, bool* val) const;
 
 template
@@ -308,6 +319,9 @@ Status KuduScanBatch::RowPtr::Get<TypeTraits<STRING> >(int col_idx, Slice* val) 
 
 template
 Status KuduScanBatch::RowPtr::Get<TypeTraits<BINARY> >(int col_idx, Slice* val) const;
+
+template
+Status KuduScanBatch::RowPtr::Get<TypeTraits<VARCHAR> >(int col_idx, Slice* val) const;
 
 template
 Status KuduScanBatch::RowPtr::Get<TypeTraits<DECIMAL32> >(int col_idx, int32_t* val) const;

src/kudu/client/scan_batch.h
@@ -251,9 +251,9 @@ class KUDU_EXPORT KuduScanBatch::RowPtr {
 #endif
   ///@}
 
-  /// @name Getters for string/binary column by column name.
+  /// @name Getters for string/binary/varchar column by column name.
   ///
-  /// Get the string/binary value for a column by its name.
+  /// Get the string/binary/varchar value for a column by its name.
   ///
   /// @param [in] col_name
   ///   Name of the column.
@@ -270,11 +270,12 @@ class KUDU_EXPORT KuduScanBatch::RowPtr {
   ///@{
   Status GetString(const Slice& col_name, Slice* val) const WARN_UNUSED_RESULT;
   Status GetBinary(const Slice& col_name, Slice* val) const WARN_UNUSED_RESULT;
+  Status GetVarchar(const Slice& col_name, Slice* val) const WARN_UNUSED_RESULT;
   ///@}
 
-  /// @name Getters for string/binary column by column index.
+  /// @name Getters for string/binary/varchar column by column index.
   ///
-  /// Get the string/binary value for a column by its index.
+  /// Get the string/binary/varchar value for a column by its index.
   ///
   /// These methods are faster than their name-based counterparts
   /// since using indices avoids a hashmap lookup, so index-based getters
@@ -295,6 +296,7 @@ class KUDU_EXPORT KuduScanBatch::RowPtr {
   ///@{
   Status GetString(int col_idx, Slice* val) const WARN_UNUSED_RESULT;
   Status GetBinary(int col_idx, Slice* val) const WARN_UNUSED_RESULT;
+  Status GetVarchar(int col_idx, Slice* val) const WARN_UNUSED_RESULT;
   ///@}
 
   /// Get the column's row data.

src/kudu/client/schema-internal.h
@@ -47,13 +47,15 @@ KuduColumnSchema::DataType FromInternalDataType(kudu::DataType type);
 
 class KuduColumnTypeAttributes::Data {
  public:
-  Data(int8_t precision, int8_t scale)
+  Data(int8_t precision, int8_t scale, uint16_t length)
       : precision(precision),
-        scale(scale) {
+        scale(scale),
+        length(length) {
   }
 
   int8_t precision;
   int8_t scale;
+  uint16_t length;
 };
 
 class KuduColumnSpec::Data {
@@ -75,6 +77,7 @@ class KuduColumnSpec::Data {
   boost::optional<KuduColumnSchema::DataType> type;
   boost::optional<int8_t> precision;
   boost::optional<int8_t> scale;
+  boost::optional<uint16_t> length;
   boost::optional<KuduColumnStorageAttributes::EncodingType> encoding;
   boost::optional<KuduColumnStorageAttributes::CompressionType> compression;
   boost::optional<int32_t> block_size;

src/kudu/client/schema.cc
@@ -39,6 +39,7 @@
 #include "kudu/gutil/macros.h"
 #include "kudu/gutil/map-util.h"
 #include "kudu/gutil/strings/substitute.h"
+#include "kudu/util/char_util.h"
 #include "kudu/util/compression/compression.pb.h"
 #include "kudu/util/decimal_util.h"
 #include "kudu/util/slice.h"
@@ -126,6 +127,7 @@ kudu::DataType ToInternalDataType(KuduColumnSchema::DataType type,
     case KuduColumnSchema::UNIXTIME_MICROS: return kudu::UNIXTIME_MICROS;
     case KuduColumnSchema::FLOAT: return kudu::FLOAT;
     case KuduColumnSchema::DOUBLE: return kudu::DOUBLE;
+    case KuduColumnSchema::VARCHAR: return kudu::VARCHAR;
     case KuduColumnSchema::STRING: return kudu::STRING;
     case KuduColumnSchema::BINARY: return kudu::BINARY;
     case KuduColumnSchema::BOOL: return kudu::BOOL;
@@ -152,6 +154,7 @@ KuduColumnSchema::DataType FromInternalDataType(kudu::DataType type) {
     case kudu::UNIXTIME_MICROS: return KuduColumnSchema::UNIXTIME_MICROS;
     case kudu::FLOAT: return KuduColumnSchema::FLOAT;
     case kudu::DOUBLE: return KuduColumnSchema::DOUBLE;
+    case kudu::VARCHAR: return KuduColumnSchema::VARCHAR;
     case kudu::STRING: return KuduColumnSchema::STRING;
     case kudu::BINARY: return KuduColumnSchema::BINARY;
     case kudu::BOOL: return KuduColumnSchema::BOOL;
@@ -167,7 +170,7 @@ KuduColumnSchema::DataType FromInternalDataType(kudu::DataType type) {
 ////////////////////////////////////////////////////////////
 
 KuduColumnTypeAttributes::KuduColumnTypeAttributes()
-    : data_(new Data(0, 0)) {
+    : data_(new Data(0, 0, 0)) {
 }
 
 KuduColumnTypeAttributes::KuduColumnTypeAttributes(const KuduColumnTypeAttributes& other)
@@ -176,7 +179,15 @@ KuduColumnTypeAttributes::KuduColumnTypeAttributes(const KuduColumnTypeAttribute
 }
 
 KuduColumnTypeAttributes::KuduColumnTypeAttributes(int8_t precision, int8_t scale)
-    : data_(new Data(precision, scale)) {
+    : data_(new Data(precision, scale, 0)) {
+}
+
+KuduColumnTypeAttributes::KuduColumnTypeAttributes(uint16_t length)
+    : data_(new Data(0, 0, length)) {
+}
+
+KuduColumnTypeAttributes::KuduColumnTypeAttributes(int8_t precision, int8_t scale, uint16_t length)
+    : data_(new Data(precision, scale, length)) {
 }
 
 KuduColumnTypeAttributes::~KuduColumnTypeAttributes() {
@@ -202,6 +213,10 @@ int8_t KuduColumnTypeAttributes::precision() const {
 
 int8_t KuduColumnTypeAttributes::scale() const {
   return data_->scale;
+}
+
+uint16_t KuduColumnTypeAttributes::length() const {
+  return data_->length;
 }
 
 ////////////////////////////////////////////////////////////
@@ -257,6 +272,11 @@ KuduColumnSpec* KuduColumnSpec::Scale(int8_t scale) {
   return this;
 }
 
+KuduColumnSpec* KuduColumnSpec::Length(uint16_t length) {
+  data_->length = length;
+  return this;
+}
+
 KuduColumnSpec* KuduColumnSpec::PrimaryKey() {
   data_->primary_key = true;
   return this;
@@ -303,46 +323,78 @@ Status KuduColumnSpec::ToColumnSchema(KuduColumnSchema* col) const {
     return Status::InvalidArgument("no type provided for column", data_->name);
   }
 
-  if (data_->type.value() == KuduColumnSchema::DECIMAL) {
-    if (!data_->precision) {
-      return Status::InvalidArgument("no precision provided for decimal column", data_->name);
-    }
-    if (data_->precision.value() < kMinDecimalPrecision ||
-        data_->precision.value() > kMaxDecimalPrecision) {
-      return Status::InvalidArgument(
-          strings::Substitute("precision must be between $0 and $1",
-                              kMinDecimalPrecision,
-                              kMaxDecimalPrecision), data_->name);
-    }
-    if (data_->scale) {
-      if (data_->scale.value() < kMinDecimalScale) {
-        return Status::InvalidArgument(
-            strings::Substitute("scale is less than the minimum value of $0",
-                                kMinDecimalScale), data_->name);
+  switch (data_->type.value()) {
+    case KuduColumnSchema::DECIMAL:
+      if (!data_->precision) {
+        return Status::InvalidArgument("no precision provided for decimal column", data_->name);
       }
-      if (data_->scale.value() > data_->precision.value()) {
+      if (data_->precision.value() < kMinDecimalPrecision ||
+          data_->precision.value() > kMaxDecimalPrecision) {
         return Status::InvalidArgument(
-            strings::Substitute("scale is greater than the precision value of",
-                                data_->precision.value()), data_->name);
+            strings::Substitute("precision must be between $0 and $1",
+              kMinDecimalPrecision,
+              kMaxDecimalPrecision), data_->name);
       }
-    }
-  } else {
-    if (data_->precision) {
-      return Status::InvalidArgument(
-          strings::Substitute("precision is not valid on a $0 column",
-                              data_->type.value()), data_->name);
-    }
-    if (data_->scale) {
-      return Status::InvalidArgument(
-          strings::Substitute("scale is not valid on a $0 column",
-                              data_->type.value()), data_->name);
-    }
+      if (data_->scale) {
+        if (data_->scale.value() < kMinDecimalScale) {
+          return Status::InvalidArgument(
+              strings::Substitute("scale is less than the minimum value of $0",
+                kMinDecimalScale), data_->name);
+        }
+        if (data_->scale.value() > data_->precision.value()) {
+          return Status::InvalidArgument(
+              strings::Substitute("scale is greater than the precision value of",
+                data_->precision.value()), data_->name);
+        }
+      }
+      if (data_->length) {
+        return Status::InvalidArgument(
+            strings::Substitute("length is not applicable for column $0",
+              data_->type.value()), data_->name);
+      }
+      break;
+    case KuduColumnSchema::VARCHAR:
+      if (data_->length.value() < kMinVarcharLength ||
+          data_->length.value() > kMaxVarcharLength) {
+        return Status::InvalidArgument(
+            strings::Substitute("length must be between $0 and $1",
+                                kMinVarcharLength,
+                                kMaxVarcharLength), data_->name);
+      }
+      if (data_->precision) {
+        return Status::InvalidArgument(
+            strings::Substitute("precision is not valid on a $0 column",
+                                data_->type.value()), data_->name);
+      }
+      if (data_->scale) {
+        return Status::InvalidArgument(
+            strings::Substitute("scale is not valid on a $0 column",
+                                data_->type.value()), data_->name);
+      }
+      break;
+    default:
+      if (data_->precision) {
+        return Status::InvalidArgument(
+            strings::Substitute("precision is not valid on a $0 column",
+                                data_->type.value()), data_->name);
+      }
+      if (data_->scale) {
+        return Status::InvalidArgument(
+            strings::Substitute("scale is not valid on a $0 column",
+                                data_->type.value()), data_->name);
+      }
+      if (data_->length) {
+        return Status::InvalidArgument(
+            strings::Substitute("length is not valid on a $0 column",
+                                data_->type.value()), data_->name);
+      }
   }
 
-  int8_t precision = (data_->precision) ? data_->precision.value() : 0;
-  int8_t scale = (data_->scale) ? data_->scale.value() : kDefaultDecimalScale;
+  int8_t precision = data_->precision ? data_->precision.value() : 0;
+  int8_t scale = data_->scale ? data_->scale.value() : kDefaultDecimalScale;
+  uint16_t length = data_->length ? data_->length.value() : 0;
 
-  KuduColumnTypeAttributes type_attrs(precision, scale);
+  KuduColumnTypeAttributes type_attrs(precision, scale, length);
   DataType internal_type = ToInternalDataType(data_->type.value(), type_attrs);
   bool nullable = data_->nullable ? data_->nullable.value() : true;
 
@@ -575,6 +627,8 @@ string KuduColumnSchema::DataTypeToString(DataType type) {
       return "UNIXTIME_MICROS";
     case DECIMAL:
       return "DECIMAL";
+    case VARCHAR:
+      return "VARCHAR";
   }
   LOG(FATAL) << "Unhandled type " << type;
 }
@@ -593,6 +647,7 @@ KuduColumnSchema::KuduColumnSchema(const string &name,
   ColumnTypeAttributes type_attr_private;
   type_attr_private.precision = type_attributes.precision();
   type_attr_private.scale = type_attributes.scale();
+  type_attr_private.length = type_attributes.length();
   col_ = new ColumnSchema(name, ToInternalDataType(type, type_attributes),
                           is_nullable,
                           default_value, default_value, attr_private,
@@ -647,7 +702,8 @@ KuduColumnSchema::DataType KuduColumnSchema::type() const {
 
 KuduColumnTypeAttributes KuduColumnSchema::type_attributes() const {
   ColumnTypeAttributes type_attributes = DCHECK_NOTNULL(col_)->type_attributes();
-  return KuduColumnTypeAttributes(type_attributes.precision, type_attributes.scale);
+  return KuduColumnTypeAttributes(type_attributes.precision, type_attributes.scale,
+                                  type_attributes.length);
 }
 
 const string& KuduColumnSchema::comment() const {
@@ -712,7 +768,8 @@ KuduColumnSchema KuduSchema::Column(size_t idx) const {
   KuduColumnStorageAttributes attrs(FromInternalEncodingType(col.attributes().encoding),
                                     FromInternalCompressionType(col.attributes().compression));
 #pragma GCC diagnostic pop
-  KuduColumnTypeAttributes type_attrs(col.type_attributes().precision, col.type_attributes().scale);
+  KuduColumnTypeAttributes type_attrs(col.type_attributes().precision, col.type_attributes().scale,
+                                      col.type_attributes().length);
   return KuduColumnSchema(col.name(), FromInternalDataType(col.type_info()->type()),
                           col.is_nullable(), col.read_default_value(),
                           attrs, type_attrs, col.comment());

src/kudu/client/value.cc
@@ -154,6 +154,7 @@ Status KuduValue::Data::CheckTypeAndGetPointer(const string& col_name,
 
     case kudu::BINARY:
     case kudu::STRING:
+    case kudu::VARCHAR:
       RETURN_NOT_OK(CheckAndPointToString(col_name, val_void));
       break;
 

src/kudu/integration-tests/CMakeLists.txt
@@ -62,6 +62,7 @@ ADD_KUDU_TEST(alter_table-test PROCESSORS 3)
 ADD_KUDU_TEST(auth_token_expire-itest)
 ADD_KUDU_TEST(authz_token-itest PROCESSORS 2)
 ADD_KUDU_TEST(catalog_manager_tsk-itest PROCESSORS 2)
+ADD_KUDU_TEST(varchar-itest)
 ADD_KUDU_TEST(client_failover-itest)
 ADD_KUDU_TEST(client-negotiation-failover-itest)
 ADD_KUDU_TEST(client-stress-test

commit ec8102cd3cccd3af2f5c042518c9283447c96eef

schema.h: fix compilation with old gcc
This file fails to compile with the system gcc found on CentOS 6.6
(version 4.4.7) with the following error:

  kudu/client/schema.h:115: error: a class-key must be used when declaring a friend
  kudu/client/schema.h:115: error: friend declaration does not name a class or function
  kudu/client/schema.h:116: error: a class-key must be used when declaring a friend
  kudu/client/schema.h:116: error: friend declaration does not name a class or function
  kudu/client/schema.h:117: error: a class-key must be used when declaring a friend
  kudu/client/schema.h:117: error: friend declaration does not name a class or function

It's a regression from commit 4343fb746. What's perplexing is that while
it's reliably showing up in the dist-test dashboard as a
client_examples-test.sh failure, it's not reliably reproducible. When I'm
logged into a CentOS 6.6 machine I can repro the failed g++ attempt, but if
I run the failed test itself, it passes.

Change-Id: I5916501fc52910bcc0e843726a1d684be6d56417
Reviewed-on: http://gerrit.cloudera.org:8080/14557
Reviewed-by: Alexey Serbin <aserbin@cloudera.com>
Tested-by: Kudu Jenkins

src/kudu/client/schema.h
@@ -112,9 +112,9 @@ class KUDU_EXPORT KuduColumnTypeAttributes {
   uint16_t length() const;
 
  private:
-  friend KuduColumnSchema;
-  friend KuduColumnSpec;
-  friend KuduSchema;
+  friend class KuduColumnSchema;
+  friend class KuduColumnSpec;
+  friend class KuduSchema;
 
   KuduColumnTypeAttributes(int8_t precision, int8_t scale, uint16_t length);
 