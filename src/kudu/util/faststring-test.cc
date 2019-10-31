a035869886bb5de37cde7b2bf48bdd3def3123ff
lingbin <lingbinlb@gmail.com>
2019-10-31 22:11:26 +0800

    [util] Fix comment of faststring
    1. Under any case, capacity() is greater than or equal to
       the data length. If the current length is shorter than
       kInitialCapacity, after call shrink_to_fit(),
       capacity() will go down to kInitialCapacity.
    2. The caller must be responsible for releasing the pointer
       release() returns.
    
    Now In `GrowByAtLeast()`, to_reserve (i.e. `len_ + count`)
    is usually larger than we really need. This commit also
    calculate an exact value when we try to expand the faststring.
    
    To be clearer, this commit rename `GrowByAtLeast()` to
    `GrowToAtLeast()`, and make the newcapacity as its argument.
    
    Note: If the capacity needs to grow, it still expand by
    at least 50% each time.
    
    Change-Id: Ie4bd4f31216fdf5cfbf70526f1358824fbda225a
    Reviewed-on: http://gerrit.cloudera.org:8080/14604
    Reviewed-by: Adar Dembo <adar@cloudera.com>
    Tested-by: Adar Dembo <adar@cloudera.com>

--- src/kudu/util/faststring-test.cc
+++ src/kudu/util/faststring-test.cc
@@ -18,6 +18,7 @@
 #include <algorithm>
 #include <cstring>
 #include <memory>
+#include <string>
 
 #include <gtest/gtest.h>
 
@@ -25,6 +26,9 @@
 #include "kudu/util/random.h"
 #include "kudu/util/random_util.h"
 #include "kudu/util/test_util.h"
+
+using std::string;
+using std::unique_ptr;
 
 namespace kudu {
 class FaststringTest : public KuduTest {};
@@ -48,7 +52,7 @@ TEST_F(FaststringTest, TestShrinkToFit_SmallerThanInitialCapacity) {
 TEST_F(FaststringTest, TestShrinkToFit_Random) {
   Random r(GetRandomSeed32());
   int kMaxSize = faststring::kInitialCapacity * 2;
-  std::unique_ptr<char[]> random_bytes(new char[kMaxSize]);
+  unique_ptr<char[]> random_bytes(new char[kMaxSize]);
   RandomString(random_bytes.get(), kMaxSize, &r);
 
   faststring s;
@@ -59,6 +63,63 @@ TEST_F(FaststringTest, TestShrinkToFit_Random) {
     s.shrink_to_fit();
     ASSERT_EQ(0, memcmp(s.data(), random_bytes.get(), new_size));
     ASSERT_EQ(std::max<int>(faststring::kInitialCapacity, new_size), s.capacity());
+  }
+}
+
+TEST_F(FaststringTest, TestPushBack) {
+  faststring s;
+  for (int i = 0; i < faststring::kInitialCapacity * 2; ++i) {
+    s.push_back('a');
+    ASSERT_LE(s.size(), s.capacity());
+    ASSERT_EQ(i + 1, s.size());
+    if (i + 1 <= faststring::kInitialCapacity) {
+      ASSERT_EQ(s.capacity(), faststring::kInitialCapacity);
+    }
+  }
+}
+
+TEST_F(FaststringTest, TestAppend_Simple) {
+  faststring s;
+  ASSERT_EQ(s.capacity(), faststring::kInitialCapacity);
+  ASSERT_EQ(s.size(), 0);
+
+  // append empty string
+  s.append("");
+  ASSERT_EQ(s.capacity(), faststring::kInitialCapacity);
+  ASSERT_EQ(s.size(), 0);
+
+  // len_ < kInitialCapacity
+  s.append("hello");
+  ASSERT_EQ(s.capacity(), faststring::kInitialCapacity);
+  ASSERT_EQ(s.size(), 5);
+
+  // len_ > kInitialCapacity
+  string tmp(faststring::kInitialCapacity, 'a');
+  s.append(tmp);
+  ASSERT_GT(s.capacity(), faststring::kInitialCapacity);
+  ASSERT_EQ(s.size(), 5 + faststring::kInitialCapacity);
+}
+
+TEST_F(FaststringTest, TestAppend_ExponentiallyExpand) {
+  size_t initial_capacity = faststring::kInitialCapacity / 2;
+  string tmp1(initial_capacity, 'a');
+
+  {
+    // Less than 150% after expansion
+    faststring s;
+    string tmp2(faststring::kInitialCapacity - 1, 'a');
+    s.append(tmp1);
+    s.append(tmp2);
+    ASSERT_EQ(s.capacity(), faststring::kInitialCapacity * 3 / 2);
+  }
+
+  {
+    // More than 150% after expansion
+    faststring s;
+    string tmp2(faststring::kInitialCapacity + 1, 'a');
+    s.append(tmp1);
+    s.append(tmp2);
+    ASSERT_GT(s.capacity(), faststring::kInitialCapacity * 3 / 2);
   }
 }
 
