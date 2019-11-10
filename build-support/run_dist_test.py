f769f1db64076809486ec4bb6b4f39ff6c5bb1b7
Adar Dembo <adar@cloudera.com>
2019-11-10 01:12:35 -0800

    mini_chronyd: switch to using symbolic links
    The one gotcha is that the symbolic links won't exist a priori in a remote
    dist-test machine, which causes MiniChronyd::GetPath to fail to find the
    chrony binaries. To fix this, run_dist_test.py must recreate the links.
    
    Change-Id: Id24d97d2badfee3e121f01a5280a2879513782de
    Reviewed-on: http://gerrit.cloudera.org:8080/14684
    Tested-by: Kudu Jenkins
    Reviewed-by: Alexey Serbin <aserbin@cloudera.com>

--- build-support/run_dist_test.py
+++ build-support/run_dist_test.py
@@ -153,9 +153,17 @@ def main():
   env['SENTRY_HOME'] = glob.glob(os.path.join(ROOT, "thirdparty/src/sentry-*"))[0]
   env['JAVA_HOME'] = glob.glob("/usr/lib/jvm/java-1.8.0-*")[0]
 
+  # Restore the symlinks to the chrony binaries; tests expect to find them in
+  # same directory as the test binaries themselves.
+  for bin_path in glob.glob(os.path.join(ROOT, "build/*/bin")):
+    os.symlink(os.path.join(ROOT, "thirdparty/installed/common/bin/chronyc"),
+               os.path.join(bin_path, "chronyc"))
+    os.symlink(os.path.join(ROOT, "thirdparty/installed/common/sbin/chronyd"),
+               os.path.join(bin_path, "chronyd"))
+
   env['LD_LIBRARY_PATH'] = ":".join(
     [os.path.join(ROOT, "build/dist-test-system-libs/")] +
-    glob.glob(os.path.abspath((os.path.join(ROOT, "build/*/lib")))))
+    glob.glob(os.path.abspath(os.path.join(ROOT, "build/*/lib"))))
 
   # Don't pollute /tmp in dist-test setting. If a test crashes, the dist-test slave
   # will clear up our working directory but won't be able to find and clean up things
