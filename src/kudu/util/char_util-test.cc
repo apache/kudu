d5ac2f2e6f1003ff2f5163631e2be2f88f5c97d5
Attila Bukor <abukor@apache.org>
2019-11-11 23:10:03 +0100

    KUDU-1938 Make UTF-8 truncation faster pt 1
    This commit adds a fast path for ASCII strings where if the MSB is a
    0-bit on each byte in a chunk of string it advances the counter and the
    iterator by the chunk size. This way if a chunk contains only ASCII
    characters there's no need to count each individual character.
    
    Thanks to Todd Lipcon for the initial idea and Zoltan Chovan and Istvan
    Farmosi for the brainstorming and the help in figuring out how this
    should be done.
    
    Before:
    
    [ RUN      ] CharUtilTest.StressTestUtf8
    [       OK ] CharUtilTest.StressTestUtf8 (6698 ms)
    [ RUN      ] CharUtilTest.StressTestAscii
    [       OK ] CharUtilTest.StressTestAscii (6161 ms)
    
    After:
    
    [ RUN      ] CharUtilTest.StressTestUtf8
    [       OK ] CharUtilTest.StressTestUtf8 (7746 ms)
    [ RUN      ] CharUtilTest.StressTestAscii
    [       OK ] CharUtilTest.StressTestAscii (1028 ms)
    
    Change-Id: Iebb98e18a3619029d9b0bc224c7dead89a3d7374
    Reviewed-on: http://gerrit.cloudera.org:8080/14353
    Reviewed-by: Adar Dembo <adar@cloudera.com>
    Tested-by: Kudu Jenkins

---
+++ src/kudu/util/char_util-test.cc
@@ -0,0 +1,103 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+#include "kudu/util/char_util.h"
+
+#include <cstdint>
+#include <memory>
+
+#include <gtest/gtest.h>
+
+#include "kudu/util/env.h"
+#include "kudu/util/faststring.h"
+#include "kudu/util/path_util.h"
+#include "kudu/util/slice.h"
+#include "kudu/util/test_util.h"
+
+using std::unique_ptr;
+
+namespace kudu {
+
+class CharUtilTest : public KuduTest {
+ protected:
+  Slice data_utf8_;
+  Slice data_ascii_;
+
+  void SetUp() override {
+    ReadFileToString(env_, JoinPathSegments(GetTestExecutableDirectory(),
+                                           "testdata/char_truncate_utf8.txt"),
+                     &string_utf8_);
+    data_utf8_ = Slice(string_utf8_);
+    ReadFileToString(env_, JoinPathSegments(GetTestExecutableDirectory(),
+                                           "testdata/char_truncate_ascii.txt"),
+                     &string_ascii_);
+    data_ascii_ = Slice(string_ascii_);
+  }
+
+  unique_ptr<const uint8_t[]> Truncate(const Slice& slice, int length, Slice* result) {
+    *result = UTF8Truncate(slice, length);
+    return std::unique_ptr<const uint8_t[]>(result->data());
+  }
+
+  void StressTest(const Slice& slice, int length) {
+    for (int i = 0; i < kNumCycles_; ++i) {
+      Slice result;
+      auto ptr = Truncate(slice, length, &result);
+      ASSERT_FALSE(result.empty());
+    }
+  }
+
+ private:
+  const int kNumCycles_ = 1000000;
+  faststring string_utf8_;
+  faststring string_ascii_;
+};
+
+TEST_F(CharUtilTest, CorrectnessTestUtf8) {
+  Slice result;
+  auto ptr = Truncate(data_utf8_, 9756, &result);
+  ASSERT_EQ(10549, result.size());
+
+  ptr = Truncate(data_utf8_, 10549, &result);
+  ASSERT_EQ(10550, result.size());
+}
+
+TEST_F(CharUtilTest, CorrectnessTestAscii) {
+  Slice result;
+  auto ptr = Truncate(data_ascii_, 9756, &result);
+  ASSERT_EQ(9756, result.size());
+  ptr = Truncate(data_ascii_, 10549, &result);
+  ASSERT_EQ(10549, result.size());
+}
+
+TEST_F(CharUtilTest, CorrectnessTestIncompleteUtf8) {
+  Slice result;
+  Slice test_data = "aaaa\xf3";
+
+  auto ptr = Truncate(test_data, 5, &result);
+  ASSERT_EQ(test_data, result);
+}
+
+TEST_F(CharUtilTest, StressTestUtf8) {
+  StressTest(data_utf8_, 9000);
+}
+
+TEST_F(CharUtilTest, StressTestAscii) {
+  StressTest(data_ascii_, 9000);
+}
+
+} // namespace kudu
--- src/kudu/util/char_util.cc
+++ src/kudu/util/char_util.cc
@@ -17,25 +17,37 @@
 
 #include "kudu/util/char_util.h"
 
-#include <string.h>
+#include <algorithm>
+#include <cstring>
 
 namespace kudu {
 
 Slice UTF8Truncate(Slice val, size_t max_utf8_length) {
   size_t num_utf8_chars = 0;
+  const uint8_t* str;
+  const uint8_t* start;
+  str = start = val.data();
   size_t num_bytes = 0;
-  auto str = val.data();
-  for (auto i = 0; i < val.size(); ++i) {
-    num_utf8_chars += (*str++ & 0xc0) != 0x80;
-    num_bytes++;
-    if (num_utf8_chars > max_utf8_length) {
-      num_bytes--;
-      num_utf8_chars--;
-      break;
+  size_t size = val.size();
+  while (num_bytes < size) {
+    // If the next chunk of bytes are all ASCII we can fast path them.
+    if (size - num_bytes >= 8 &&
+        max_utf8_length - num_utf8_chars >= 8 &&
+        (*(reinterpret_cast<const int64_t*>(str)) & 0x8080808080808080) == 0) {
+      num_utf8_chars += 8;
+      num_bytes += 8;
+    } else {
+      num_utf8_chars += (*str++ & 0xc0) != 0x80;
+      num_bytes++;
+      if (num_utf8_chars > max_utf8_length) {
+        num_bytes--;
+        num_utf8_chars--;
+        break;
+      }
     }
+    str = start + num_bytes;
   }
-  // as num_bytes <= val.size() we can use that to allocate the new slice data
-  // and copy the first num_bytes from val.data() to it.
+  num_bytes = std::min<size_t>(size, num_bytes);
   auto relocated = new uint8_t[num_bytes];
   memcpy(relocated, val.data(), num_bytes);
   return Slice(relocated, num_bytes);
