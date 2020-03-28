apache/kudu

commit f37e7a6e0a249d32568707bdd2f5305c3663e0b3

thread: simplify Thread::Create API
I set out to remove boost::bind from this API, but after that I found it
cleaner to force callers to create the lambdas rather than retain the
Thread::Create multi-arg overloads.

Change-Id: I581378b093a8d2e8536b7b07cca5520fdaa7672d
Reviewed-on: http://gerrit.cloudera.org:8080/15448
Reviewed-by: Alexey Serbin <aserbin@cloudera.com>
Reviewed-by: Andrew Wong <awong@cloudera.com>
Tested-by: Andrew Wong <awong@cloudera.com>

src/kudu/util/debug-util-test.cc
@@ -19,12 +19,15 @@
 #ifdef __linux__
 #include <link.h>
 #endif
+#include "kudu/util/debug-util.h"
+
 #include <unistd.h>
 
 #include <algorithm>
 #include <csignal>
 #include <cstddef>
 #include <cstdint>
+#include <initializer_list>
 #include <memory>
 #include <ostream>
 #include <string>
@@ -39,7 +42,6 @@
 #include "kudu/gutil/walltime.h"
 #include "kudu/util/array_view.h"
 #include "kudu/util/countdown_latch.h"
-#include "kudu/util/debug-util.h"
 #include "kudu/util/kernel_stack_watchdog.h"
 #include "kudu/util/monotime.h"
 #include "kudu/util/scoped_cleanup.h"
@@ -107,12 +109,12 @@ TEST_F(DebugUtilTest, TestStackTraceMainThread) {
 TEST_F(DebugUtilTest, TestSignalStackTrace) {
   CountDownLatch l(1);
   scoped_refptr<Thread> t;
-  ASSERT_OK(Thread::Create("test", "test thread", &SleeperThread, &l, &t));
-  auto cleanup_thr = MakeScopedCleanup([&]() {
-      // Allow the thread to finish.
-      l.CountDown();
-      t->Join();
-    });
+  ASSERT_OK(Thread::Create("test", "test thread", [&l]() { SleeperThread(&l); }, &t));
+  SCOPED_CLEANUP({
+    // Allow the thread to finish.
+    l.CountDown();
+    t->Join();
+  });
 
   // We have to loop a little bit because it takes a little while for the thread
   // to start up and actually call our function.
@@ -193,16 +195,17 @@ TEST_F(DebugUtilTest, TestSnapshot) {
   CountDownLatch l(1);
   vector<scoped_refptr<Thread>> threads(kNumThreads);
   for (int i = 0; i < kNumThreads; i++) {
-    ASSERT_OK(Thread::Create("test", "test thread", &SleeperThread, &l, &threads[i]));
+    ASSERT_OK(Thread::Create("test", "test thread",
+                             [&l]() { SleeperThread(&l); }, &threads[i]));
   }
 
   SCOPED_CLEANUP({
-      // Allow the thread to finish.
-      l.CountDown();
-      for (auto& t : threads) {
-        t->Join();
-      }
-    });
+    // Allow the thread to finish.
+    l.CountDown();
+    for (auto& t : threads) {
+      t->Join();
+    }
+  });
 
   StackTraceSnapshot snap;
   ASSERT_OK(snap.SnapshotAllStacks());
@@ -233,12 +236,12 @@ TEST_F(DebugUtilTest, TestSnapshot) {
 TEST_F(DebugUtilTest, Benchmark) {
   CountDownLatch l(1);
   scoped_refptr<Thread> t;
-  ASSERT_OK(Thread::Create("test", "test thread", &SleeperThread, &l, &t));
+  ASSERT_OK(Thread::Create("test", "test thread", [&l]() { SleeperThread(&l); }, &t));
   SCOPED_CLEANUP({
-      // Allow the thread to finish.
-      l.CountDown();
-      t->Join();
-    });
+    // Allow the thread to finish.
+    l.CountDown();
+    t->Join();
+  });
 
   for (bool symbolize : {false, true}) {
     MonoTime end_time = MonoTime::Now() + MonoDelta::FromSeconds(1);
@@ -356,14 +359,15 @@ TEST_P(RaceTest, TestStackTraceRaces) {
   DangerousOp op = GetParam();
   CountDownLatch l(1);
   scoped_refptr<Thread> t;
-  ASSERT_OK(Thread::Create("test", "test thread", &DangerousOperationThread, op, &l, &t));
+  ASSERT_OK(Thread::Create("test", "test thread",
+                           [op, &l]() { DangerousOperationThread(op, &l); }, &t));
   SCOPED_CLEANUP({
-      // Allow the thread to finish.
-      l.CountDown();
-      // Crash if we can't join the thread after a reasonable amount of time.
-      // That probably indicates a deadlock.
-      CHECK_OK(ThreadJoiner(t.get()).give_up_after_ms(10000).Join());
-    });
+    // Allow the thread to finish.
+    l.CountDown();
+    // Crash if we can't join the thread after a reasonable amount of time.
+    // That probably indicates a deadlock.
+    CHECK_OK(ThreadJoiner(t.get()).give_up_after_ms(10000).Join());
+  });
   MonoTime end_time = MonoTime::Now() + MonoDelta::FromSeconds(1);
   while (MonoTime::Now() < end_time) {
     StackTrace trace;
@@ -399,12 +403,12 @@ TEST_F(DebugUtilTest, TestTimeouts) {
 
   CountDownLatch l(1);
   scoped_refptr<Thread> t;
-  ASSERT_OK(Thread::Create("test", "test thread", &SleeperThread, &l, &t));
-  auto cleanup_thr = MakeScopedCleanup([&]() {
-      // Allow the thread to finish.
-      l.CountDown();
-      t->Join();
-    });
+  ASSERT_OK(Thread::Create("test", "test thread", [&l]() { SleeperThread(&l); }, &t));
+  SCOPED_CLEANUP({
+    // Allow the thread to finish.
+    l.CountDown();
+    t->Join();
+  });
 
   // First, time a few stack traces to determine how long a non-timed-out stack
   // trace takes.

src/kudu/clock/builtin_ntp.cc
@@ -630,7 +630,7 @@ Status BuiltInNtp::InitImpl() {
   RETURN_NOT_OK_PREPEND(socket_.SetRecvTimeout(MonoDelta::FromSeconds(0.5)),
                         "could not set socket recv timeout");
   RETURN_NOT_OK_PREPEND(
-      Thread::Create("ntp", "ntp client", &BuiltInNtp::PollThread, this, &thread_),
+      Thread::Create("ntp", "ntp client", [this]() { this->PollThread(); }, &thread_),
       "could not start NTP client thread");
   return Status::OK();
 }

src/kudu/hms/hms_catalog.cc
@@ -130,7 +130,7 @@ Status HmsCatalog::Start(HmsClientVerifyKuduSyncConfig verify_service_config) {
   RETURN_NOT_OK(ha_client_.Start(std::move(addresses), std::move(options)));
 
   RETURN_NOT_OK(Thread::Create("hms_catalog", "fetch_uuid",
-                               &HmsCatalog::LoopInitializeUuid, this,
+                               [this](){ this->LoopInitializeUuid(); },
                                &uuid_initializing_thread_));
   return Status::OK();
 }

src/kudu/master/catalog_manager.cc
@@ -578,7 +578,7 @@ class CatalogManagerBgTasks {
 
 Status CatalogManagerBgTasks::Init() {
   RETURN_NOT_OK(kudu::Thread::Create("catalog manager", "bgtasks",
-      &CatalogManagerBgTasks::Run, this, &thread_));
+                                     [this]() { this->Run(); }, &thread_));
   return Status::OK();
 }
 

src/kudu/master/hms_notification_log_listener.cc
@@ -92,7 +92,7 @@ HmsNotificationLogListenerTask::~HmsNotificationLogListenerTask() {
 Status HmsNotificationLogListenerTask::Init() {
   CHECK(!thread_) << "HmsNotificationLogListenerTask is already initialized";
   return kudu::Thread::Create("catalog manager", "hms-notification-log-listener",
-                              &HmsNotificationLogListenerTask::RunLoop, this, &thread_);
+                              [this]() { this->RunLoop(); }, &thread_);
 }
 
 void HmsNotificationLogListenerTask::Shutdown() {

src/kudu/rpc/acceptor_pool.cc
@@ -84,7 +84,7 @@ Status AcceptorPool::Start(int num_threads) {
   for (int i = 0; i < num_threads; i++) {
     scoped_refptr<kudu::Thread> new_thread;
     Status s = kudu::Thread::Create("acceptor pool", "acceptor",
-        &AcceptorPool::RunThread, this, &new_thread);
+                                    [this]() { this->RunThread(); }, &new_thread);
     if (!s.ok()) {
       Shutdown();
       return s;

src/kudu/rpc/reactor.cc
@@ -185,7 +185,8 @@ Status ReactorThread::Init() {
   ev_set_invoke_pending_cb(loop_, &ReactorThread::InvokePendingCb);
 
   // Create Reactor thread.
-  return kudu::Thread::Create("reactor", "rpc reactor", &ReactorThread::RunThread, this, &thread_);
+  return kudu::Thread::Create("reactor", "rpc reactor",
+                              [this]() { this->RunThread(); }, &thread_);
 }
 
 void ReactorThread::InvokePendingCb(struct ev_loop* loop) {

src/kudu/rpc/result_tracker.cc
@@ -459,8 +459,8 @@ void ResultTracker::FailAndRespond(const RequestIdPB& request_id,
 
 void ResultTracker::StartGCThread() {
   CHECK(!gc_thread_);
-  CHECK_OK(Thread::Create("server", "result-tracker", &ResultTracker::RunGCThread,
-                          this, &gc_thread_));
+  CHECK_OK(Thread::Create("server", "result-tracker",
+                          [this]() { this->RunGCThread(); }, &gc_thread_));
 }
 
 void ResultTracker::RunGCThread() {

commit 5ce3a022f99ee3f99e086292487e390e3334d247

KUDU-3093: fix DebugUtilTest.TestSignalStackTrace in RELEASE mode
I think commit f37e7a6e0 regressed this test when Kudu is built with a more
modern compiler, due to the opportunity for more inlining.

Change-Id: I0bcd76f42b70bfc77ab18e8163a367f89eaad3e9
Reviewed-on: http://gerrit.cloudera.org:8080/15572
Tested-by: Kudu Jenkins
Reviewed-by: Alexey Serbin <aserbin@cloudera.com>

src/kudu/util/debug-util-test.cc
@@ -119,9 +119,14 @@ TEST_F(DebugUtilTest, TestSignalStackTrace) {
 
   // We have to loop a little bit because it takes a little while for the thread
   // to start up and actually call our function.
+  //
+  // Note: due to RELEASE build inlining, we need to make sure to pick a stack
+  // frame that isn't optimized away.
+  static constexpr const char* kTestThreadStackFrame =
+      "kudu::ConditionVariable::WaitUntil()";
   ASSERT_EVENTUALLY([&]() {
-      ASSERT_STR_CONTAINS(DumpThreadStack(t->tid()), "SleeperThread");
-    });
+    ASSERT_STR_CONTAINS(DumpThreadStack(t->tid()), kTestThreadStackFrame);
+  });
 
   // Test that we can change the signal and that the stack traces still work,
   // on the new signal.
@@ -135,7 +140,7 @@ TEST_F(DebugUtilTest, TestSignalStackTrace) {
   ASSERT_FALSE(IsSignalHandlerRegistered(SIGUSR2));
 
   // Stack traces should work using the new handler.
-  ASSERT_STR_CONTAINS(DumpThreadStack(t->tid()), "SleeperThread");
+  ASSERT_STR_CONTAINS(DumpThreadStack(t->tid()), kTestThreadStackFrame);
 
   // Switch back to SIGUSR2 and ensure it changes back.
   ASSERT_OK(SetStackTraceSignal(SIGUSR2));
@@ -143,7 +148,7 @@ TEST_F(DebugUtilTest, TestSignalStackTrace) {
   ASSERT_FALSE(IsSignalHandlerRegistered(SIGHUP));
 
   // Stack traces should work using the new handler.
-  ASSERT_STR_CONTAINS(DumpThreadStack(t->tid()), "SleeperThread");
+  ASSERT_STR_CONTAINS(DumpThreadStack(t->tid()), kTestThreadStackFrame);
 
   // Register our own signal handler on SIGHUP, and ensure that
   // we get a bad Status if we try to use it.