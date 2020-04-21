apache/kudu

commit fbde23718211f9912067d33fd09b4177263263a1

Add basic support for UNIX domain sockets
This adds support to Sockaddr to represent UNIX domain sockets, and
fixes some code in the RPC stack and Socket classes that previously
assumed sockaddr_in.

This updates rpc-test to run all of the cases over both TCP and UNIX
sockets. It also adds a simple performance test of running RPC over a
UNIX domain socket.

The performance difference is small but measurable. The test has a lot
of variability depending on whether the reactor threads, test thread,
and RPC worker thread get scheduled to the same core, different
hyperthreads of the same core, same vs different NUMA nodes, etc. To
eliminate that variability I ran the performance test using 'taskset -c
1' to pin to a single CPU:

I0409 15:15:50.290123 42405 rpc-test.cc:1529] Connecting to 0.0.0.0:33635
I0409 15:15:50.646266 42405 rpc-test.cc:1543] Sending 1024MB via tcp socket: real 0.355s        user 0.110s     sys 0.245s
I0409 15:15:51.048060 42405 rpc-test.cc:1543] Sending 1024MB via tcp socket: real 0.402s        user 0.106s     sys 0.295s
I0409 15:15:51.455765 42405 rpc-test.cc:1543] Sending 1024MB via tcp socket: real 0.408s        user 0.120s     sys 0.287s
I0409 15:15:51.861732 42405 rpc-test.cc:1543] Sending 1024MB via tcp socket: real 0.406s        user 0.112s     sys 0.293s
I0409 15:15:52.268314 42405 rpc-test.cc:1543] Sending 1024MB via tcp socket: real 0.407s        user 0.119s     sys 0.287s
[       OK ] Parameters/TestRpc.TestPerformanceBySocketType/NoSSL_TCP (1987 ms)
[ RUN      ] Parameters/TestRpc.TestPerformanceBySocketType/NoSSL_UnixSocket
I0409 15:15:52.271142 42405 rpc-test.cc:1529] Connecting to unix:@kudu-test-42405
I0409 15:15:52.605296 42405 rpc-test.cc:1543] Sending 1024MB via unix socket: real 0.333s       user 0.098s     sys 0.235s
I0409 15:15:52.934170 42405 rpc-test.cc:1543] Sending 1024MB via unix socket: real 0.329s       user 0.107s     sys 0.222s

src/kudu/security/tls_socket.cc
@@ -44,6 +44,15 @@ namespace security {
 TlsSocket::TlsSocket(int fd, c_unique_ptr<SSL> ssl)
     : Socket(fd),
       ssl_(std::move(ssl)) {
+  use_cork_ = true;
+  if (fd >= 0) {
+    int dom;
+    socklen_t len = sizeof(dom);
+    if (getsockopt(fd, SOL_SOCKET, SO_DOMAIN, &dom, &len) == 0 &&
+        dom == AF_UNIX) {
+      use_cork_ = false;
+    }
+  }
 }
 
 TlsSocket::~TlsSocket() {
@@ -87,7 +96,9 @@ Status TlsSocket::Writev(const struct ::iovec *iov, int iov_len, int64_t *nwritt
   CHECK(ssl_);
   *nwritten = 0;
   // Allows packets to be aggresively be accumulated before sending.
-  RETURN_NOT_OK(SetTcpCork(1));
+  if (use_cork_) {
+    RETURN_NOT_OK(SetTcpCork(1));
+  }
   Status write_status = Status::OK();
   for (int i = 0; i < iov_len; ++i) {
     int32_t frame_size = iov[i].iov_len;
@@ -100,7 +111,9 @@ Status TlsSocket::Writev(const struct ::iovec *iov, int iov_len, int64_t *nwritt
     *nwritten += bytes_written;
     if (bytes_written < frame_size) break;
   }
-  RETURN_NOT_OK(SetTcpCork(0));
+  if (use_cork_) {
+    RETURN_NOT_OK(SetTcpCork(0));
+}
   // If we did manage to write something, but not everything, due to a temporary socket
   // error, then we should still return an OK status indicating a successful _partial_
   // write.

src/kudu/hms/hms_client-test.cc
@@ -408,7 +408,7 @@ TEST_F(HmsClientTest, TestHmsConnect) {
   // Listening, but not accepting socket.
   Sockaddr listening;
   Socket listening_socket;
-  ASSERT_OK(listening_socket.Init(0));
+  ASSERT_OK(listening_socket.Init(loopback.family(), 0));
   ASSERT_OK(listening_socket.BindAndListen(loopback, 1));
   listening_socket.GetSocketAddress(&listening);
   ASSERT_TRUE(start_client(listening).IsTimedOut());
@@ -416,7 +416,7 @@ TEST_F(HmsClientTest, TestHmsConnect) {
   // Bound, but not listening socket.
   Sockaddr bound;
   Socket bound_socket;
-  ASSERT_OK(bound_socket.Init(0));
+  ASSERT_OK(bound_socket.Init(loopback.family(), 0));
   ASSERT_OK(bound_socket.Bind(loopback));
   bound_socket.GetSocketAddress(&bound);
   ASSERT_TRUE(start_client(bound).IsNetworkError());
@@ -424,7 +424,7 @@ TEST_F(HmsClientTest, TestHmsConnect) {
   // Unbound socket.
   Sockaddr unbound;
   Socket unbound_socket;
-  ASSERT_OK(unbound_socket.Init(0));
+  ASSERT_OK(unbound_socket.Init(loopback.family(), 0));
   ASSERT_OK(unbound_socket.Bind(loopback));
   unbound_socket.GetSocketAddress(&unbound);
   ASSERT_OK(unbound_socket.Close());

src/kudu/mini-cluster/mini_cluster.cc
@@ -66,7 +66,7 @@ Status MiniCluster::ReserveDaemonSocket(DaemonType type,
   RETURN_NOT_OK(sock_addr.ParseString(ip, 0));
 
   unique_ptr<Socket> sock(new Socket());
-  RETURN_NOT_OK(sock->Init(0));
+  RETURN_NOT_OK(sock->Init(sock_addr.family(), 0));
   RETURN_NOT_OK(sock->SetReuseAddr(true));
   RETURN_NOT_OK(sock->SetReusePort(true));
   RETURN_NOT_OK(sock->Bind(sock_addr));

src/kudu/rpc/CMakeLists.txt
@@ -132,6 +132,6 @@ ADD_KUDU_TEST(periodic-test)
 ADD_KUDU_TEST(reactor-test)
 ADD_KUDU_TEST(request_tracker-test)
 ADD_KUDU_TEST(rpc-bench RUN_SERIAL true)
-ADD_KUDU_TEST(rpc-test)
+ADD_KUDU_TEST(rpc-test NUM_SHARDS 8)
 ADD_KUDU_TEST(rpc_stub-test)
 ADD_KUDU_TEST(service_queue-test RUN_SERIAL true)

src/kudu/rpc/acceptor_pool.cc
@@ -160,12 +160,14 @@ void AcceptorPool::RunThread() {
                                     << THROTTLE_MSG;
       continue;
     }
-    s = new_sock.SetNoDelay(true);
-    if (!s.ok()) {
-      KLOG_EVERY_N_SECS(WARNING, 1) << "Acceptor with remote = " << remote.ToString()
-          << " failed to set TCP_NODELAY on a newly accepted socket: "
-          << s.ToString() << THROTTLE_MSG;
-      continue;
+    if (remote.is_ip()) {
+      s = new_sock.SetNoDelay(true);
+      if (!s.ok()) {
+        KLOG_EVERY_N_SECS(WARNING, 1) << "Acceptor with remote = " << remote.ToString()
+                                      << " failed to set TCP_NODELAY on a newly accepted socket: "
+                                      << s.ToString() << THROTTLE_MSG;
+        continue;
+      }
     }
     rpc_connections_accepted_->Increment();
     messenger_->RegisterInboundSocket(&new_sock, remote);

src/kudu/rpc/connection.cc
@@ -907,7 +907,7 @@ Status Connection::DumpPB(const DumpConnectionsRequestPB& req,
     LOG(FATAL);
   }
 #ifdef __linux__
-  if (negotiation_complete_) {
+  if (negotiation_complete_ && remote_.is_ip()) {
     // TODO(todd): it's a little strange to not set socket level stats during
     // negotiation, but we don't have access to the socket here until negotiation
     // is complete.

src/kudu/rpc/connection_id.cc
@@ -52,7 +52,7 @@ void ConnectionId::set_network_plane(string network_plane) {
 
 string ConnectionId::ToString() const {
   string remote;
-  if (hostname_ != remote_.host()) {
+  if (remote_.is_ip() && hostname_ != remote_.host()) {
     remote = strings::Substitute("$0 ($1)", remote_.ToString(), hostname_);
   } else {
     remote = remote_.ToString();

src/kudu/rpc/messenger.cc
@@ -307,7 +307,7 @@ Status Messenger::AddAcceptorPool(const Sockaddr &accept_addr,
   }
 
   Socket sock;
-  RETURN_NOT_OK(sock.Init(0));
+  RETURN_NOT_OK(sock.Init(accept_addr.family(), 0));
   RETURN_NOT_OK(sock.SetReuseAddr(true));
   if (reuseport_) {
     RETURN_NOT_OK(sock.SetReusePort(true));

commit 3ff21bbd3bea76422d888866fbaa327f23188eae

[mac] Fix compilation on macOS
Commit fbde2371 broke the build on macOS with the following error:
 `error: use of undeclared identifier 'SO_DOMAIN’`

This patch fixes the build by disabling the use of `SO_DOMAIN`
to disable `TCP_CORK` in the case of an `AF_UNIX` socket on
macOS.

Change-Id: I815421a7531136c6a7c468b1664221cfc5ea5df1
Reviewed-on: http://gerrit.cloudera.org:8080/15771
Tested-by: Kudu Jenkins
Reviewed-by: Alexey Serbin <aserbin@cloudera.com>

src/kudu/security/tls_socket.cc
@@ -45,6 +45,10 @@ TlsSocket::TlsSocket(int fd, c_unique_ptr<SSL> ssl)
     : Socket(fd),
       ssl_(std::move(ssl)) {
   use_cork_ = true;
+
+#ifndef __APPLE__
+// `SO_DOMAIN` is not available on macOS. This code can be safely
+// skipped because SetTcpCork() is a no-op on macOS.
   if (fd >= 0) {
     int dom;
     socklen_t len = sizeof(dom);
@@ -53,6 +57,7 @@ TlsSocket::TlsSocket(int fd, c_unique_ptr<SSL> ssl)
       use_cork_ = false;
     }
   }
+#endif
 }
 
 TlsSocket::~TlsSocket() {