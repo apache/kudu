5aacd6c64ddd2669005d0b3ca57801a7a7124033
Alexey Serbin <alexey@apache.org>
2020-12-01 16:53:03 -0800

    [build-support] devtoolset-8 allows to build on CentOS 6
    RHEL/CentOS 6 is beyond its EOL date already, but it doesn't hurt
    to keep Kudu building on old build servers (even if that hardware
    is close to its own EOL as well).
    
    With this patch, I was able to rebuild all thirdparty components
    and Kudu itself in DEBUG and RELEASE configuration after installing
    devtoolset-8 on old CentOS 6 build server.
    
    Change-Id: I15f58edd090243ceb07a46f92d44e9998747ec5e
    Reviewed-on: http://gerrit.cloudera.org:8080/16806
    Tested-by: Kudu Jenkins
    Reviewed-by: Grant Henke <granthenke@apache.org>

--- build-support/enable_devtoolset.sh
+++ build-support/enable_devtoolset.sh
@@ -17,14 +17,17 @@
 # specific language governing permissions and limitations
 # under the License.
 #
-# Enables the Red Hat devtoolset on RHEL 6 based systems and executes the
-# arguments. On non-RHEL 6 systems, the arguments are executed without changes
+# Enables the Red Hat devtoolset and executes the arguments on RHEL-based
+# systems where the default C++ compiler does not support all features
+# necessary to compile Kudu from source (C++17). On other (newer) RHEL-based
+# systems and non-RHEL systems, the arguments are executed without changes
 # to the environment.
+#
 # USAGE: ./enable_devtoolset.sh <command> <args>...
 
 set -e
 if [[ "$OSTYPE" =~ ^linux ]] && \
-   [[ "$(lsb_release -irs)" =~ (CentOS|RedHatEnterpriseServer)[[:space:]]+7\.[[:digit:]]+ ]]; then
+   [[ "$(lsb_release -irs)" =~ (CentOS|RedHatEnterpriseServer|OracleServer)[[:space:]]+(6|7)\.[[:digit:]]+ ]]; then
   # Invoke the inner script, which may do some additional customization within
   # the devtoolset.
   ROOT=$(cd $(dirname "$BASH_SOURCE") ; pwd)
