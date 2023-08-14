75c38d227c687f69935a638946c6408dff876c79
Zoltan Martonka <zmartonka@gmail.com>
2023-08-14 16:10:13 +0200

    [thirdparty] fix TSAN build on Ubuntu 22.04
    There are 2 things missing from llvm 11:
    
    1. SIGSTKSZ is no longer a constant in the newer glibc, so caching it in a
      static variable can result in a crash.
    
      This causes the libunwind build to fail at configure:
       "configure: error: cannot run C compiled programs."
      The problem is that the conftest.c compile crashes.
    
      Although the problem occurs at libunwind build first, it is an llvm
      issue.
    
      More info:
      https://gcc.gnu.org/bugzilla/show_bug.cgi?id=100114
    
    + "/usr/bin/krb5-config --libs gssapi" brings the config "-flto=auto",
      which is ignored by newer clangs (starting from 14.0), but clang 11
      still gives an error.
    
      The problem occurs while building curl:
      "configure: error: one or more libs available at link-time
      are not available run-time"
      But the error message is misleading: clang just dies with
      unknown option.
    
      See:
      https://reviews.llvm.org/D99501
    
    Change-Id: Ib9493a9f7212c26a9cfc1e6a6015340cc5fbfdc5
    Reviewed-on: http://gerrit.cloudera.org:8080/20336
    Reviewed-by: Alexey Serbin <alexey@apache.org>
    Tested-by: Alexey Serbin <alexey@apache.org>

--- thirdparty/download-thirdparty.sh
+++ thirdparty/download-thirdparty.sh
@@ -331,14 +331,16 @@ fetch_and_patch \
  $PYTHON_SOURCE \
  $PYTHON_PATCHLEVEL
 
-LLVM_PATCHLEVEL=3
+LLVM_PATCHLEVEL=5
 fetch_and_patch \
  llvm-${LLVM_VERSION}-iwyu-${IWYU_VERSION}.src.tar.gz \
  $LLVM_SOURCE \
  $LLVM_PATCHLEVEL \
  "patch -p1 < $TP_DIR/patches/llvm-add-iwyu.patch" \
  "patch -d projects -p1 < $TP_DIR/patches/llvm-remove-cyclades-inclusion-in-sanitizer.patch" \
- "patch -p2 < $TP_DIR/patches/llvm-fix-missing-include.patch"
+ "patch -p2 < $TP_DIR/patches/llvm-fix-missing-include.patch" \
+ "patch -d projects -p1 < $TP_DIR/patches/llvm-Sanitizer-built-against-glibc-2_34-doesnt-work.patch" \
+ "patch -d tools -p1 < $TP_DIR/patches/llvm-ignore-flto-values.diff"
 
 LZ4_PATCHLEVEL=0
 fetch_and_patch \
