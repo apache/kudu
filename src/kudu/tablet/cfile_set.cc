0de168f7e0abcf0c29facefcc9c0c9e12b284140
Abhishek Chennaka <achennaka@cloudera.com>
2024-04-15 15:52:48 -0700

    KUDU-3518: Fix the unexpected scan predicate
    With the changes introduced in a previous patch[1], there have been
    reports of errors during scan operations. This patch fixes these
    scan errors.
    
    This patch removes NON-NULL predicates on primary columns implicitly 
    added by ScanSpec::LiftPrimaryKeyBounds(), calling  
    ScanSpec::OptimizeScan() while optimizing InList scan predicates at the 
    DRS level.  That's similar to what's done be optimizing the InList 
    predicates based on tablet PK bounds at a higher level.
    
    The issue has been investigated and fixed in collaboration with Alexey 
    Serbin.
    
    There will be follow-up patches to include tests of this failure.
    
    [1] https://gerrit.cloudera.org/#/c/18434/
    
    Change-Id: I220df7ec1b4f95726c913a19125f4647267d12b1
    Reviewed-on: http://gerrit.cloudera.org:8080/21244
    Reviewed-by: Alexey Serbin <alexey@apache.org>
    Tested-by: Alexey Serbin <alexey@apache.org>
    Reviewed-by: Yifan Zhang <chinazhangyifan@163.com>
    (cherry picked from commit 946acb711d722b1e6fe27af2c7de92960d724980)
    Reviewed-on: http://gerrit.cloudera.org:8080/21314

--- src/kudu/tablet/cfile_set.cc
+++ src/kudu/tablet/cfile_set.cc
@@ -453,7 +453,7 @@ Status CFileSet::Iterator::OptimizePKPredicates(ScanSpec* spec) {
   }
 
   if (modify_lower_bound_key || modify_upper_bound_key) {
-    spec->UnifyPrimaryKeyBoundsAndColumnPredicates(tablet_schema, &arena_, true);
+    spec->OptimizeScan(tablet_schema, &arena_, true);
   }
   return Status::OK();
 }
