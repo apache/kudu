34089941debf91a0d1de538a41da482fcb8dcf4a
Alexey Serbin <alexey@apache.org>
2025-03-03 23:53:28 -0800

    KUDU-3646 fix Base64Decode()
    Prior to this patch, Base64Decode() in src/kudu/util/url-coding.cc
    had a bug that lead to truncating the output data by discarding
    trailing null/zero bytes.
    
    Originally imported from the Impala's codebase [1], the bug has been
    lurking for 11+ years in the Kudu's codebase.  Meanwhile, Impala fixed
    the issue a long time ago [2].
    
    This patch addresses the issue and adds corresponding test coverage.
    In addition, this patch adds extra validation for the input of
    Base64Decode() and provides test coverage for that as well.
    Also, reference binding to null pointer in url-coding-test.cc has been
    addressed as well.
    
    [1] https://github.com/apache/kudu/commit/ce4d760ca
    [2] https://issues.apache.org/jira/browse/IMPALA-2878
    
    Change-Id: I7f5d41eb4e41897d520f00b12c6e487ecc5a00fc
    Reviewed-on: http://gerrit.cloudera.org:8080/22573
    Reviewed-by: Ashwani Raina <araina@cloudera.com>
    Tested-by: Alexey Serbin <alexey@apache.org>
    Reviewed-by: Abhishek Chennaka <achennaka@cloudera.com>

--- src/kudu/util/url-coding-test.cc
+++ src/kudu/util/url-coding-test.cc
@@ -23,6 +23,7 @@
 #include <string>
 #include <vector>
 
+#include <glog/logging.h>
 #include <gtest/gtest.h>
 
 using std::ostringstream;
@@ -65,7 +66,9 @@ void TestBase64(const string& input, const string& expected_encoded) {
 
   // Convert string to vector and try that also
   vector<uint8_t> input_vector(input.size());
-  memcpy(&input_vector[0], input.c_str(), input.size());
+  if (!input_vector.empty()) {
+    memcpy(&input_vector[0], input.c_str(), input.size());
+  }
   string intermediate2;
   Base64Encode(input_vector, &intermediate2);
   EXPECT_EQ(intermediate, intermediate2);
@@ -94,12 +97,73 @@ TEST(UrlCodingTest, PathSeparators) {
 }
 
 TEST(Base64Test, Basic) {
+  TestBase64("", "");
   TestBase64("a", "YQ==");
+  TestBase64(string("a\0", 2), "YQA=");
+  TestBase64(string("\0a", 2), "AGE=");
+  TestBase64(string("a\0\0", 3), "YQAA");
+  TestBase64(string("\0a\0", 3), "AGEA");
+  TestBase64(string("\0 a \0", 5), "ACBhIAA=");
+  TestBase64(string(" \0a\0 ", 5), "IABhACA=");
   TestBase64("ab", "YWI=");
+  TestBase64(string("ab\0", 3), "YWIA");
+  TestBase64("a a", "YSBh");
   TestBase64("abc", "YWJj");
+  TestBase64("a a ", "YSBhIA==");
+  TestBase64(string("a a \0", 5), "YSBhIAA=");
+  TestBase64(string("a a \0\0", 6), "YSBhIAAA");
+  TestBase64(string("\0a\0 \0a\0 \0\0", 10), "AGEAIABhACAAAA==");
   TestBase64("abcd", "YWJjZA==");
+  TestBase64(string("abcd\0", 5), "YWJjZAA=");
   TestBase64("abcde", "YWJjZGU=");
+  TestBase64(string("abcde\0", 6), "YWJjZGUA");
   TestBase64("abcdef", "YWJjZGVm");
+  TestBase64(string("a\0b", 3), "YQBi");
+  TestBase64(string("a\0b\0", 4), "YQBiAA==");
+  TestBase64(string("a\0b\0\0", 5), "YQBiAAA=");
+  TestBase64(string("a\0b\0\0\0", 6), "YQBiAAAA");
+}
+
+TEST(Base64DecodeTest, InvalidInput) {
+  // These should fail to decode.
+  string out;
+  ASSERT_FALSE(Base64Decode("A", &out));
+  ASSERT_FALSE(Base64Decode("=", &out));
+  ASSERT_FALSE(Base64Decode("A=", &out));
+  ASSERT_FALSE(Base64Decode("==", &out));
+  ASSERT_FALSE(Base64Decode("A==", &out));
+  ASSERT_FALSE(Base64Decode("=A=", &out));
+  ASSERT_FALSE(Base64Decode("===", &out));
+  ASSERT_FALSE(Base64Decode("A===", &out));
+  ASSERT_FALSE(Base64Decode("====", &out));
+  ASSERT_FALSE(Base64Decode("A====", &out));
+  ASSERT_FALSE(Base64Decode("==A==", &out));
+  ASSERT_FALSE(Base64Decode("YQ=", &out));
+  ASSERT_FALSE(Base64Decode("=W=", &out));
+  ASSERT_FALSE(Base64Decode("YQW==", &out));
+  ASSERT_FALSE(Base64Decode("=YQW=", &out));
+  ASSERT_FALSE(Base64Decode("=Y=W=", &out));
+  ASSERT_FALSE(Base64Decode("ADCD=", &out));
+  ASSERT_FALSE(Base64Decode("ADCD==", &out));
+  ASSERT_FALSE(Base64Decode("ADCD===", &out));
+}
+
+TEST(Base64DecodeTest, InvalidInputMisplacedPaddingSymbols) {
+#if !DCHECK_IS_ON()
+  GTEST_SKIP() << "this test is only for DCHECK_IS_ON()";
+#endif
+  string out;
+  ASSERT_DEATH(({ Base64Decode("=a",        &out); }), "non-trailing '='");
+  ASSERT_DEATH(({ Base64Decode("==a",       &out); }), "non-trailing '='");
+  ASSERT_DEATH(({ Base64Decode("===a",      &out); }), "non-trailing '='");
+  ASSERT_DEATH(({ Base64Decode("=A==",      &out); }), "non-trailing '='");
+  ASSERT_DEATH(({ Base64Decode("==A=",      &out); }), "non-trailing '='");
+  ASSERT_DEATH(({ Base64Decode("YQA=YQA=",  &out); }), "non-trailing '='");
+  ASSERT_DEATH(({ Base64Decode("YQA=ABCD",  &out); }), "non-trailing '='");
+  ASSERT_DEATH(({ Base64Decode("YQA==ABCD", &out); }), "non-trailing '='");
+  ASSERT_DEATH(({ Base64Decode("=QA=ABCD",  &out); }), "non-trailing '='");
+  ASSERT_DEATH(({ Base64Decode("YQ==YQ==",  &out); }), "non-trailing '='");
+  ASSERT_DEATH(({ Base64Decode("YQ==YQA=",  &out); }), "non-trailing '='");
 }
 
 TEST(HtmlEscapingTest, Basic) {
