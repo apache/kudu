f8e57b5131bae879e537595893269dcd2c52c7d9
Alexey Serbin <alexey@apache.org>
2026-01-23 11:15:44 -0800

    [test_util] report random seed only with verbose logging
    Usually, it's not important to know the exact seed used to initialize
    a random engine, and logging about that every time just pollutes test
    output.  This patch changes LOG(INFO) to VLOG(1) for SeedRandom().
    
    Change-Id: I148fdbdda5ddc7750ca7ea9fdaedd0e93875394f
    Reviewed-on: http://gerrit.cloudera.org:8080/23899
    Tested-by: Alexey Serbin <alexey@apache.org>
    Reviewed-by: Zoltan Martonka <zmartonka@cloudera.com>
    Reviewed-by: Abhishek Chennaka <achennaka@cloudera.com>

--- src/kudu/util/test_util.cc
+++ src/kudu/util/test_util.cc
@@ -273,7 +273,7 @@ int SeedRandom() {
   } else {
     seed = FLAGS_test_random_seed;
   }
-  LOG(INFO) << "Using random seed: " << seed;
+  VLOG(1) << "Using random seed: " << seed;
   srand(seed);
   return seed;
 }
